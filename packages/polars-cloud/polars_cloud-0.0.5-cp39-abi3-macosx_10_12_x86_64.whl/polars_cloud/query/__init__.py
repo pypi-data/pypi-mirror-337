02b468079d78853866b98d167ffb749ae171b430
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-28 06:04:01 +0000

    Add package polars-cloud/0.0.5/polars_cloud-0.0.5-cp39-abi3-macosx_10_12_x86_64.whl

---
+++ packages/polars-cloud/polars_cloud-0.0.5-cp39-abi3-macosx_10_12_x86_64.whl/polars_cloud/query/__init__.py
@@ -0,0 +1,27 @@
+from polars_cloud.query.dst import ParquetDst
+from polars_cloud.query.ext import LazyFrameExt
+from polars_cloud.query.query import (
+    BatchQuery,
+    InteractiveQuery,
+    spawn,
+    spawn_blocking,
+    spawn_many,
+    spawn_many_blocking,
+)
+from polars_cloud.query.query_info import QueryInfo
+from polars_cloud.query.query_result import QueryResult
+from polars_cloud.query.query_status import QueryStatus
+
+__all__ = [
+    "BatchQuery",
+    "InteractiveQuery",
+    "LazyFrameExt",
+    "ParquetDst",
+    "QueryInfo",
+    "QueryResult",
+    "QueryStatus",
+    "spawn",
+    "spawn_blocking",
+    "spawn_many",
+    "spawn_many_blocking",
+]

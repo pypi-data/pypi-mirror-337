b675d9ce55ffa55cdedc4016b48406ce0fc72e46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-28 03:16:12 +0000

    Add package subsurface-terra/2025.1.0rc7/subsurface_terra-2025.1.0rc7.tar.gz

---
+++ packages/subsurface-terra/subsurface_terra-2025.1.0rc7.tar.gz/subsurface_terra-2025.1.0rc7/subsurface/modules/reader/__init__.py
@@ -0,0 +1,9 @@
+# import warnings
+
+from .profiles import *
+
+from .topography.topo_core import read_structured_topography, read_unstructured_topography
+
+from .mesh.omf_mesh_reader import omf_stream_to_unstructs
+from .mesh.dxf_reader import dxf_stream_to_unstruct_input, dxf_file_to_unstruct_input
+from .mesh.mx_reader import mx_to_unstruc_from_binary

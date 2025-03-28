96dd494c885cc775529aced14ca82971d67c1310
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-28 04:03:56 +0000

    Add package xorq/0.2.0/xorq-0.2.0-cp38-abi3-win_amd64.whl

---
+++ packages/xorq/xorq-0.2.0-cp38-abi3-win_amd64.whl/xorq/backends/let/datafusion/tests/test_provider.py
@@ -0,0 +1,45 @@
+from pathlib import Path
+
+import pyarrow as pa
+import pytest
+
+import xorq as xo
+from xorq.backends.let.datafusion.provider import IbisTableProvider
+
+
+@pytest.fixture
+def tmp_model_dir(tmpdir):
+    # Create a temporary directory for the model
+    model_dir = tmpdir.mkdir("models")
+    return model_dir
+
+
+@pytest.fixture(scope="session")
+def data_dir():
+    root = Path(__file__).absolute().parents[6]
+    data_dir = root / "ci" / "ibis-testing-data"
+    return data_dir
+
+
+@pytest.fixture(scope="session")
+def con(data_dir):
+    conn = xo.connect()
+    parquet_dir = data_dir / "parquet"
+    conn.register(parquet_dir / "functional_alltypes.parquet", "functional_alltypes")
+
+    return conn
+
+
+def test_table_provider_scan(con):
+    table_provider = IbisTableProvider(con.table("functional_alltypes"))
+    batches = table_provider.scan()
+
+    assert batches is not None
+    assert isinstance(batches, pa.RecordBatchReader)
+
+
+def test_table_provider_schema(con):
+    table_provider = IbisTableProvider(con.table("functional_alltypes"))
+    schema = table_provider.schema()
+    assert schema is not None
+    assert isinstance(schema, pa.Schema)

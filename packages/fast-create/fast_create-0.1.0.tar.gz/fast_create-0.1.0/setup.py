9c76a6c6a4bd45f0849b99bb82e270fd991bc415
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-28 05:04:32 +0000

    Add package fast-create/0.1.0/fast_create-0.1.0.tar.gz

---
+++ packages/fast-create/fast_create-0.1.0.tar.gz/fast_create-0.1.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name="fast-create",
+    version="0.1",
+    py_modules=["fast_create"],
+    install_requires=['fastapi[all]' , 'uvicorn', 'python-dotenv', 'psycopg2', 'sqlmodel', 'fastapi_mail', 'pydantic', "python-jose[cryptography]",'oauth2', 'passlib', 'sqlalchemy'],
+    entry_points={
+        "console_scripts": [
+            "fast-create=fast_create:main",
+        ],
+    },
+)

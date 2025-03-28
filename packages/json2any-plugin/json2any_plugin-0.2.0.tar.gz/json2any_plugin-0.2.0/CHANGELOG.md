e5898527d0447be5806e47eb9ca38cab86cd9122
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-28 17:05:00 +0000

    Add package json2any-plugin/0.2.0/json2any_plugin-0.2.0.tar.gz

---
+++ packages/json2any-plugin/json2any_plugin-0.2.0.tar.gz/json2any_plugin-0.2.0/CHANGELOG.md
@@ -0,0 +1,27 @@
+Change History Log
+==================
+Note: below this line changes are maintained by `bump2version` tool.
+
+* Add your changes to section below
+* The tool will automatically add section for next release
+
+Pending changes
+---------------
+
+*
+
+Release v0.2.0
+---------------
+
+* added AbstractSchemaProvider
+
+Release v0.1.1
+---------------
+
+* Breaking changes:
+    * AbstractDataProvider.name() is now property
+* Initial Implementation
+    * AbstractArgParser
+    * AbstractDataProvider
+    * AbstractHelperProvider
+    * AbstractTemplateProvider
---
+++ packages/json2any-plugin/json2any_plugin-0.2.0.tar.gz/json2any_plugin-0.2.0/src/json2any_plugin/AbstractDataProvider.py
@@ -0,0 +1,63 @@
+from abc import abstractmethod
+from argparse import ArgumentParser, Namespace
+from typing import Dict, Any
+
+from json2any_plugin.AbstractProvider import AbstractProvider
+
+
+class AbstractDataProvider(AbstractProvider):
+    """
+    Abstract base class for creating data loader
+    """
+
+    def __init__(self, data_key='data'):
+        super().__init__()
+        self.__data_key = data_key
+        self.__json_path_query = None
+
+    @property
+    def data_key(self) -> str:
+        """
+        variable name under which the data will be available in template
+        """
+        return self.__data_key
+
+    @abstractmethod
+    def update_arg_parser(self, parser: ArgumentParser) -> None:
+        """
+
+        :param parser:
+        :return:
+        """
+
+        parser.add_argument(f'--{self.arg_prefix}-key',
+                            help='name of key under which the data will be available in template')
+        parser.add_argument(f'--{self.arg_prefix}-query',
+                            help='JSONPath Query applied to loaded data')
+
+    @abstractmethod
+    def process_args(self, args: Namespace) -> bool:
+        if hasattr(args, f'--{self.arg_prefix}-key'):
+            self.__data_key = getattr(args, f'--{self.arg_prefix}-key')
+
+        # this return is here to silent the warning only. Implement your own logic.
+        return False
+
+    @abstractmethod
+    def load_data(self) -> Dict[str, Any]:
+        """
+        loads the data for template.
+        Do not use the "FORBIDDEN_KEYS"
+        :return Dict[str, Any]: the data for template. This dictionary will be "mounted" at "root" in template,
+         along with the "DATA_KEY_XXX" data. Do not use the "FORBIDDEN_KEYS" as keys in returned data.
+         i.e. if you return {"some_key": "Key Data"} you will access it in template {{ some_key }}
+        """
+        raise NotImplementedError()
+
+    def query_data(self, data: Any) -> Any:
+        if self.__json_path_query is None:
+            return data
+
+        matcher = jpng.parse(self.__json_path_query)
+        values = [m.value for m in matcher.find(data)]
+        return values

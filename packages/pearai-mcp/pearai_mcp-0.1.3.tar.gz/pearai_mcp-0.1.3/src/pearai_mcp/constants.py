10368cb0108173ed7cb9267a9e9b97926476e40e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-28 14:04:18 +0000

    Add package pearai-mcp/0.1.3/pearai_mcp-0.1.3.tar.gz

---
+++ packages/pearai-mcp/pearai_mcp-0.1.3.tar.gz/pearai_mcp-0.1.3/src/pearai_mcp/constants.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+# Server configuration
+# SERVER_URL = "https://stingray-app-gb2an.ondigitalocean.app/pearai-server-api2"
+
+# Use this for dev
+SERVER_URL = "http://localhost:8000"

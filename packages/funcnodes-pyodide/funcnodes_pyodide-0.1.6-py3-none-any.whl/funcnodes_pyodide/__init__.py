888d2528b9690170e847a03d1387dc4d4cd4ddda
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-28 06:03:56 +0000

    Add package funcnodes-pyodide/0.1.6/funcnodes_pyodide-0.1.6-py3-none-any.whl

---
+++ packages/funcnodes-pyodide/funcnodes_pyodide-0.1.6-py3-none-any.whl/funcnodes_pyodide/__init__.py
@@ -0,0 +1,16 @@
+from .worker import PyodideWorker
+import asyncio
+
+import sys
+
+if sys.platform == "emscripten":
+    from .patch import patch
+
+    patch()
+
+
+async def new_worker(*args, **kwargs):
+    worker = PyodideWorker(*args, **kwargs)
+    loop = asyncio.get_event_loop()
+    loop.create_task(worker.run_forever_async())
+    return worker

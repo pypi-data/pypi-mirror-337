90f7a8ef1bb201d63e3c60ba5c5ab925fb435268
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-28 17:05:17 +0000

    Add package flask-inputfilter/0.2.0a3/flask_inputfilter-0.2.0a3-cp310-cp310-win32.whl

---
+++ packages/flask-inputfilter/flask_inputfilter-0.2.0a3-cp310-cp310-win32.whl/flask_inputfilter/Filter/ToDataclassFilter.py
@@ -0,0 +1,18 @@
+from typing import Any, Type, Union
+
+from flask_inputfilter.Filter import BaseFilter
+
+
+class ToDataclassFilter(BaseFilter):
+    """
+    Filter that converts a dictionary to a dataclass.
+    """
+
+    def __init__(self, dataclass_type: Type[dict]) -> None:
+        self.dataclass_type = dataclass_type
+
+    def apply(self, value: Any) -> Union[Any]:
+        if not isinstance(value, dict):
+            return value
+
+        return self.dataclass_type(**value)

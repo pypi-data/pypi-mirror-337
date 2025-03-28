98cab155490ccee17a5863b375cb7c1bb9e773fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-28 17:05:04 +0000

    Add package kubemq/3.5.1/kubemq-3.5.1.tar.gz

---
+++ packages/kubemq/kubemq-3.5.1.tar.gz/kubemq-3.5.1/setup.py
@@ -0,0 +1,29 @@
+import pathlib
+from setuptools import setup, find_packages  # Corrected import statement
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+setup(
+    name="kubemq",
+    version="3.5.1",
+    description="KubeMQ SDK for Python",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/kubemq-io/kubemq-Python",
+    author="KubeMQ",
+    author_email="info@kubemq.io",
+    license="MIT",
+    packages=find_packages(),  # Corrected function call
+    install_requires=[
+        "grpcio==1.71.0",
+        "protobuf>=4.21.0",
+        "setuptools>=40.8.0",
+        "PyJWT>=2.6.0",
+        "pydantic",
+    ],
+    zip_safe=False,
+)

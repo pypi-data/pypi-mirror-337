1862c4cd7a6f74001dbea2cc48aa2d10c10f7da2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-28 04:03:40 +0000

    Add package ada-py/0.4.2/ada_py-0.4.2.tar.gz

---
+++ packages/ada-py/ada_py-0.4.2.tar.gz/ada_py-0.4.2/src/ada/cadit/ifc/write/write_shapes.py
@@ -0,0 +1,243 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+import ada.cadit.ifc.write.geom.curves as write_cu
+import ada.geom.curves as geo_cu
+import ada.geom.solids as geo_so
+import ada.geom.surfaces as geo_su
+from ada import (
+    Boolean,
+    PrimBox,
+    PrimCone,
+    PrimCyl,
+    PrimExtrude,
+    PrimRevolve,
+    PrimSphere,
+    PrimSweep,
+    Shape,
+)
+from ada.base.units import Units
+from ada.cadit.ifc.utils import (
+    add_colour,
+    create_ifc_placement,
+    create_ifcextrudedareasolid,
+    create_ifcindexpolyline,
+    create_ifcrevolveareasolid,
+    create_local_placement,
+    tesselate_shape,
+)
+from ada.cadit.ifc.write.geom.placement import ifc_placement_from_axis3d
+from ada.core.constants import O, X, Z
+from ada.core.utils import to_real
+from ada.geom.solids import Box, Cone, Cylinder
+
+from ..write.geom.surfaces import (
+    advanced_face,
+    arbitrary_profile_def,
+    create_closed_shell,
+    curve_bounded_plane,
+)
+
+if TYPE_CHECKING:
+    from ada.cadit.ifc.store import IfcStore
+
+
+def write_ifc_shape(ifc_store: IfcStore, shape: Shape):
+    if shape.parent is None:
+        raise ValueError("Parent cannot be None for IFC export")
+
+    a = shape.parent.get_assembly()
+    f = a.ifc_store.f
+
+    owner_history = a.ifc_store.owner_history
+    parent = f.by_guid(shape.parent.guid)
+    schema = f.wrapped_data.schema
+
+    shape_placement = create_local_placement(f, relative_to=parent.ObjectPlacement)
+
+    if issubclass(type(shape), Shape):
+        ifc_shape = generate_parametric_solid(shape, f)
+    else:
+        tol = Units.get_general_point_tol(a.units)
+        serialized_geom = tesselate_shape(shape.solid_occ(), schema, tol)
+        ifc_shape = f.add(serialized_geom)
+
+    # Add colour
+    if shape.color is not None:
+        color_name = next(ifc_store.writer.color_name_gen)
+        add_colour(f, ifc_shape.Representations[0].Items[0], color_name, shape.color)
+
+    from ada.base.ifc_types import ShapeTypes
+
+    ifc_elem = f.create_entity(
+        str(shape.ifc_class.value) if isinstance(shape.ifc_class, ShapeTypes) else shape.ifc_class,
+        GlobalId=shape.guid,
+        OwnerHistory=owner_history,
+        Name=shape.name,
+        ObjectType=None,
+        ObjectPlacement=shape_placement,
+        Representation=ifc_shape,
+    )
+
+    return ifc_elem
+
+
+def generate_parametric_solid(shape: Shape | PrimSphere, f):
+    a = shape.parent.get_assembly()
+    body_context = a.ifc_store.get_context("Body")
+
+    if isinstance(shape, Boolean):
+        raise ValueError(f'Penetration type "{shape}" is not yet supported')
+
+    param_geom_map = {
+        PrimSphere: generate_ifc_prim_sphere_geom,
+        PrimBox: generate_ifc_box_geom,
+        PrimCyl: generate_ifc_cylinder_geom,
+        PrimCone: generate_ifc_cone_geom,
+        PrimExtrude: generate_ifc_prim_extrude_geom,
+        PrimRevolve: generate_ifc_prim_revolve_geom,
+        PrimSweep: generate_ifc_prim_sweep_geom,
+        geo_su.AdvancedFace: advanced_face,
+        geo_su.CurveBoundedPlane: curve_bounded_plane,
+        geo_su.ClosedShell: create_closed_shell,
+    }
+
+    if type(shape) is Shape:
+        param_geo = shape.geom.geometry
+    else:
+        param_geo = shape
+
+    ifc_geom_converter = param_geom_map.get(type(param_geo), None)
+    if ifc_geom_converter is None:
+        raise NotImplementedError(f'Shape type "{type(shape)}" is not yet supported for export to IFC')
+
+    solid_geom = ifc_geom_converter(param_geo, f)
+
+    repr_type_map = {
+        PrimSphere: "SweptSolid",
+        PrimBox: "SweptSolid",
+        PrimCyl: "SweptSolid",
+        PrimCone: "SweptSolid",
+        PrimExtrude: "SweptSolid",
+        PrimRevolve: "SweptSolid",
+        PrimSweep: "AdvancedSweptSolid",
+        geo_su.AdvancedFace: "AdvancedSurface",
+        geo_su.CurveBoundedPlane: "AdvancedSurface",
+        geo_su.ClosedShell: "AdvancedSurface",
+    }
+    repr_type_str = repr_type_map.get(type(param_geo), None)
+    shape_representation = f.create_entity(
+        "IfcShapeRepresentation",
+        ContextOfItems=body_context,
+        RepresentationIdentifier="Body",
+        RepresentationType=repr_type_str,
+        Items=[solid_geom],
+    )
+    ifc_shape = f.create_entity(
+        "IfcProductDefinitionShape", Name=None, Description=None, Representations=[shape_representation]
+    )
+
+    return ifc_shape
+
+
+def generate_ifc_cone_geom(shape: PrimCone, f):
+    c_geom: Cone = shape.solid_geom().geometry
+    axis3d = ifc_placement_from_axis3d(c_geom.position, f)
+    return f.createIfcRightCircularCone(Position=axis3d, Height=c_geom.height, BottomRadius=c_geom.bottom_radius)
+
+
+def generate_ifc_prim_sphere_geom(shape: PrimSphere, f):
+    """Create IfcSphere from primitive PrimSphere"""
+    opening_axis_placement = create_ifc_placement(f, to_real(shape.cog), Z, X)
+    return f.createIfcSphere(opening_axis_placement, float(shape.radius))
+
+
+def generate_ifc_box_geom(shape: PrimBox, f):
+    """Create IfcBlock from primitive PrimBox"""
+    geom: Box = shape.solid_geom().geometry
+    axis3d = ifc_placement_from_axis3d(geom.position, f)
+    return f.createIfcBlock(Position=axis3d, XLength=geom.x_length, YLength=geom.y_length, ZLength=geom.z_length)
+
+
+def generate_ifc_cylinder_geom(shape: PrimCyl, f):
+    """Create IfcExtrudedAreaSolid from primitive PrimCyl"""
+    cyl_geom: Cylinder = shape.solid_geom().geometry
+    axis3d = ifc_placement_from_axis3d(cyl_geom.position, f)
+    return f.createIfcRightCircularCylinder(Position=axis3d, Height=cyl_geom.height, Radius=cyl_geom.radius)
+
+
+def generate_ifc_prim_extrude_geom(shape: PrimExtrude, f):
+    """Create IfcExtrudedAreaSolid from primitive PrimExtrude"""
+    # https://standards.buildingsmart.org/IFC/RELEASE/IFC4_1/FINAL/HTML/link/annex-e.htm
+    # polyline = self.create_ifcpolyline(self.file, [p[:3] for p in points])
+    normal = shape.poly.normal
+    h = shape.extrude_depth
+    points = [tuple(x.astype(float).tolist()) for x in shape.poly.seg_global_points]
+    seg_index = shape.poly.seg_index
+    polyline = create_ifcindexpolyline(f, points, seg_index)
+    profile = f.createIfcArbitraryClosedProfileDef("AREA", None, polyline)
+    opening_axis_placement = create_ifc_placement(f, O, Z, X)
+    return create_ifcextrudedareasolid(f, profile, opening_axis_placement, [float(n) for n in normal], h)
+
+
+def generate_ifc_prim_revolve_geom(shape: PrimRevolve, f):
+    """Create IfcRevolveAreaSolid from primitive PrimRevolve"""
+    # https://standards.buildingsmart.org/IFC/RELEASE/IFC4_1/FINAL/HTML/link/annex-e.htm
+    # 8.8.3.28 IfcRevolvedAreaSolid
+
+    revolve_axis = [float(n) for n in shape.revolve_axis]
+    revolve_origin = [float(x) for x in shape.revolve_origin]
+    revolve_angle = shape.revolve_angle
+    points = [tuple(x.astype(float).tolist()) for x in shape.poly.seg_global_points]
+    seg_index = shape.poly.seg_index
+    polyline = create_ifcindexpolyline(f, points, seg_index)
+    profile = f.createIfcArbitraryClosedProfileDef("AREA", None, polyline)
+    opening_axis_placement = create_ifc_placement(f, O, Z, X)
+    return create_ifcrevolveareasolid(
+        f,
+        profile,
+        opening_axis_placement,
+        revolve_origin,
+        revolve_axis,
+        revolve_angle,
+    )
+
+
+def generate_ifc_prim_sweep_geom(shape: PrimSweep, f):
+    geom = shape.solid_geom()
+    if isinstance(geom.geometry.swept_area, geo_su.ArbitraryProfileDef):
+        profile = arbitrary_profile_def(geom.geometry.swept_area, f)
+    else:
+        raise NotImplementedError(f"Not implemented {type(geom.geometry.swept_area).__name__}")
+
+    if isinstance(geom.geometry, geo_so.FixedReferenceSweptAreaSolid):
+        if isinstance(geom.geometry.directrix, geo_cu.IndexedPolyCurve):
+            sweep_curve = write_cu.indexed_poly_curve(geom.geometry.directrix, f)
+        elif isinstance(geom.geometry.directrix, geo_cu.Edge):
+            # line = geom.geometry.directrix.to_line()
+            # sweep_curve = write_cu.create_line(line, f)
+            curve = geo_cu.IndexedPolyCurve(segments=[geom.geometry.directrix])
+            sweep_curve = write_cu.indexed_poly_curve(curve, f)
+        else:
+            raise NotImplementedError(f"Unsupported curve type {type(geom.geometry.directrix)}")
+    else:
+        raise NotImplementedError(f"Unsupported curve type {type(geom.geometry.sweep_curve)}")
+
+    fixed_ref = f.create_entity("IfcDirection", to_real(shape.sweep_curve.start_vector.tolist()))
+    ifc_axis3d = ifc_placement_from_axis3d(geom.geometry.position, f)
+
+    if shape.derived_reference:
+        sweep_type = "IfcDirectrixDerivedReferenceSweptAreaSolid"
+    else:
+        sweep_type = "IfcFixedReferenceSweptAreaSolid"
+
+    solid = f.create_entity(
+        sweep_type,
+        SweptArea=profile,
+        Position=ifc_axis3d,
+        Directrix=sweep_curve,
+        FixedReference=fixed_ref,
+    )
+
+    return solid

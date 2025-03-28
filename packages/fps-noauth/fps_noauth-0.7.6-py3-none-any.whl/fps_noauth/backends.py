8d6dde4957d2c7feb7e72ff5f0bef431f3cfb626
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-28 17:04:51 +0000

    Add package fps-noauth/0.7.6/fps_noauth-0.7.6-py3-none-any.whl

---
+++ packages/fps-noauth/fps_noauth-0.7.6-py3-none-any.whl/fps_noauth/backends.py
@@ -0,0 +1,33 @@
+from typing import Any, Dict, List, Optional, Tuple
+
+from fastapi import WebSocket
+
+from jupyverse_api.auth import Auth, User
+
+USER = User()
+
+
+class _NoAuth(Auth):
+    def current_user(self, *args, **kwargs):
+        async def _():
+            return USER
+
+        return _
+
+    def websocket_auth(self, permissions: Optional[Dict[str, List[str]]] = None):
+        async def _(
+            websocket: WebSocket,
+        ) -> Optional[Tuple[WebSocket, Optional[Dict[str, List[str]]]]]:
+            return websocket, permissions
+
+        return _
+
+    async def update_user(self):
+        async def _(data: Dict[str, Any]) -> User:
+            global USER
+            user = dict(USER)
+            user.update(data)
+            USER = User(**user)
+            return USER
+
+        return _

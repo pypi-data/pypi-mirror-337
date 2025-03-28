c47318919e0a2d4eb0ecea0bf4c1ebeecd25472a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-28 16:04:20 +0000

    Add package turu-postgres/0.13.2/turu_postgres-0.13.2.tar.gz

---
+++ packages/turu-postgres/turu_postgres-0.13.2.tar.gz/turu_postgres-0.13.2/src/turu/postgres/__init__.py
@@ -0,0 +1,17 @@
+import importlib.metadata
+
+from .async_connection import AsyncConnection as AsyncConnection
+from .async_cursor import AsyncCursor as AsyncCursor
+from .connection import Connection as Connection
+from .cursor import Cursor as Cursor
+from .mock_async_connection import MockAsyncConnection as MockAsyncConnection
+from .mock_async_cursor import MockAsyncCursor as MockAsyncCursor
+from .mock_connection import MockConnection as MockConnection
+from .mock_cursor import MockCursor as MockCursor
+
+__version__ = importlib.metadata.version("turu-postgres")
+
+connect = Connection.connect
+connect_from_env = Connection.connect_from_env
+connect_async = AsyncConnection.connect
+connect_async_from_env = AsyncConnection.connect_from_env

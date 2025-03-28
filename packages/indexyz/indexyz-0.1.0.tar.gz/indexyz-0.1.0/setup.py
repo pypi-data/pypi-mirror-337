9d98f34dedff0c7cda2b7d5010a1b80d1702541e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-28 04:04:21 +0000

    Add package indexyz/0.1.0/indexyz-0.1.0.tar.gz

---
+++ packages/indexyz/indexyz-0.1.0.tar.gz/indexyz-0.1.0/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="indexyz",
+    version="0.1.0",
+    author="Palomino Ramos Yony Leonardo",
+    author_email="yony.palomino.r@uni.pe",
+    description="Biblioteca para interactuar con Google Sheets.",
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    install_requires=[
+        "google-auth==2.38.0",
+        "google-api-python-client==2.162.0",
+        "google-auth-httplib2==0.2.0",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.11.8",
+    license="MIT"
+)

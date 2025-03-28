592ebf585c3b3b93c9f3bd9f0bf8965a61e379c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-28 04:03:39 +0000

    Add package montecarlodata/0.126.0/montecarlodata-0.126.0.tar.gz

---
+++ packages/montecarlodata/montecarlodata-0.126.0.tar.gz/montecarlodata-0.126.0/montecarlodata/data_exports/fields.py
@@ -0,0 +1,16 @@
+# Query responses
+EXPECTED_GET_DATA_EXPORT_URL = "getDataExportUrl"
+
+# Data Export Names
+ALL_MONITORS_DATA_EXPORT_NAME = "ALL_MONITORS"
+ALL_ASSETS_DATA_EXPORT_NAME = "ALL_ASSETS"
+ALL_ALERTS_DATA_EXPORT_NAME = "ALL_ALERTS"
+ALL_EVENTS_DATA_EXPORT_NAME = "ALL_EVENTS"
+
+# Supported Schemes
+FILE_SCHEME = "file"
+S3_SCHEME = "s3"
+SCHEME_DELIM = "://"
+
+# Data Export headers
+LIST_DATA_EXPORTS_HEADERS = ["Data Export (Name)", "Description"]

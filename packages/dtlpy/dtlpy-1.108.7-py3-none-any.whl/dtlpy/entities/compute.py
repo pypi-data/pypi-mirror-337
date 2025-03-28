c4319cb952a925b5ad692a1013f46858083a7276
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-28 05:04:40 +0000

    Add package dtlpy/1.108.7/dtlpy-1.108.7-py3-none-any.whl

---
+++ packages/dtlpy/dtlpy-1.108.7-py3-none-any.whl/dtlpy/entities/compute.py
@@ -0,0 +1,487 @@
+from enum import Enum
+from typing import List, Optional, Dict
+from ..services.api_client import ApiClient
+from .. import repositories
+
+
+class ClusterProvider(str, Enum):
+    GCP = 'gcp'
+    AWS = 'aws'
+    AZURE = 'azure'
+    HPC = 'hpc'
+    LOCAL = 'local'
+    RANCHER_K3S = 'rancher-k3s'
+
+
+class ComputeType(str, Enum):
+    KUBERNETES = "kubernetes"
+
+
+class ComputeStatus(str, Enum):
+    READY = "ready"
+    INITIALIZING = "initializing"
+    PAUSE = "pause"
+    FAILED = "failed"
+
+
+class ComputeConsumptionMethod(str, Enum):
+    MQ = "MQ",
+    API = "API"
+
+
+class ComputeSettings:
+    def __init__(self, default_namespace: str, consumption_method: ComputeConsumptionMethod):
+        self.consumption_method = consumption_method
+        self.default_namespace = default_namespace
+
+    @classmethod
+    def from_json(cls, _json):
+        return cls(
+            default_namespace=_json.get('defaultNamespace'),
+            consumption_method=_json.get('consumptionMethod')
+        )
+
+    def to_json(self):
+        return {
+            'defaultNamespace': self.default_namespace,
+            'consumptionMethod': self.consumption_method
+        }
+
+
+class Toleration:
+    def __init__(self, effect: str, key: str, operator: str, value: str):
+        self.effect = effect
+        self.key = key
+        self.operator = operator
+        self.value = value
+
+    @classmethod
+    def from_json(cls, _json):
+        return cls(
+            effect=_json.get('effect'),
+            key=_json.get('key'),
+            operator=_json.get('operator'),
+            value=_json.get('value')
+        )
+
+    def to_json(self):
+        return {
+            'effect': self.effect,
+            'key': self.key,
+            'operator': self.operator,
+            'value': self.value
+        }
+
+
+class DeploymentResource:
+    def __init__(self, gpu: int, cpu: int, memory: str):
+        self.gpu = gpu
+        self.cpu = cpu
+        self.memory = memory
+
+    @classmethod
+    def from_json(cls, _json):
+        return cls(
+            gpu=_json.get('gpu', None),
+            cpu=_json.get('cpu', None),
+            memory=_json.get('memory', None)
+        )
+
+    def to_json(self):
+        _json = {}
+        if self.gpu is not None:
+            _json['gpu'] = self.gpu
+        if self.cpu is not None:
+            _json['cpu'] = self.cpu
+        if self.memory is not None:
+            _json['memory'] = self.memory
+
+
+class DeploymentResources:
+    def __init__(self, request: DeploymentResource, limit: DeploymentResource):
+        self.request = request
+        self.limit = limit
+
+    @classmethod
+    def from_json(cls, _json):
+        return cls(
+            request=DeploymentResource.from_json(_json.get('request', dict())),
+            limit=DeploymentResource.from_json(_json.get('limit', dict()))
+        )
+
+    def to_json(self):
+        return {
+            'request': self.request.to_json(),
+            'limit': self.limit.to_json()
+        }
+
+
+class NodePool:
+    def __init__(
+            self,
+            name: str,
+            is_dl_type_default: bool,
+            dl_types: Optional[List[str]] = None,
+            tolerations: Optional[List[Toleration]] = None,
+            description: str = "",
+            node_selector: str = "",
+            preemptible: bool = False,
+            deployment_resources: DeploymentResources = None
+    ):
+        self.name = name
+        self.is_dl_type_default = is_dl_type_default
+        self.dl_types = dl_types
+        self.tolerations = tolerations if tolerations is not None else []
+        self.description = description
+        self.node_selector = node_selector
+        self.preemptible = preemptible
+        self.deployment_resources = deployment_resources
+
+    @classmethod
+    def from_json(cls, _json):
+        node_pool = cls(
+            name=_json.get('name'),
+            is_dl_type_default=_json.get('isDlTypeDefault'),
+            dl_types=_json.get('dlTypes'),
+            description=_json.get('description'),
+            node_selector=_json.get('nodeSelector'),
+            preemptible=_json.get('preemptible'),
+            deployment_resources=DeploymentResources.from_json(_json.get('deploymentResources', dict())),
+            tolerations=[Toleration.from_json(t) for t in _json.get('tolerations', list())]
+        )
+
+        return node_pool
+
+    def to_json(self):
+        _json = {
+            'name': self.name,
+            'isDlTypeDefault': self.is_dl_type_default,
+            'description': self.description,
+            'nodeSelector': self.node_selector,
+            'preemptible': self.preemptible,
+            'deploymentResources': self.deployment_resources.to_json(),
+            'tolerations': [t.to_json() for t in self.tolerations]
+        }
+
+        if self.dl_types is not None:
+            _json['dlTypes'] = self.dl_types
+
+        return _json
+
+
+class AuthenticationIntegration:
+    def __init__(self, id: str, type: str):
+        self.id = id
+        self.type = type
+
+    @classmethod
+    def from_json(cls, _json):
+        return cls(
+            id=_json.get('id'),
+            type=_json.get('type')
+        )
+
+    def to_json(self):
+        return {
+            'id': self.id,
+            'type': self.type
+        }
+
+
+class Authentication:
+    def __init__(self, integration: AuthenticationIntegration):
+        self.integration = integration
+
+    @classmethod
+    def from_json(cls, _json):
+        return cls(
+            integration=AuthenticationIntegration.from_json(_json.get('integration', dict()))
+        )
+
+    def to_json(self):
+        return {
+            'integration': self.integration.to_json()
+        }
+
+
+class ComputeCluster:
+    def __init__(
+            self,
+            name: str,
+            endpoint: str,
+            kubernetes_version: str,
+            provider: ClusterProvider,
+            node_pools: Optional[List[NodePool]] = None,
+            metadata: Optional[Dict] = None,
+            authentication: Optional[Authentication] = None,
+    ):
+        self.name = name
+        self.endpoint = endpoint
+        self.kubernetes_version = kubernetes_version
+        self.provider = provider
+        self.node_pools = node_pools if node_pools is not None else []
+        self.metadata = metadata if metadata is not None else {}
+        self.authentication = authentication if authentication is not None else Authentication(
+            AuthenticationIntegration("", ""))
+
+    @classmethod
+    def from_json(cls, _json):
+        return cls(
+            name=_json.get('name'),
+            endpoint=_json.get('endpoint'),
+            kubernetes_version=_json.get('kubernetesVersion'),
+            provider=ClusterProvider(_json.get('provider')),
+            node_pools=[NodePool.from_json(np) for np in _json.get('nodePools', list())],
+            metadata=_json.get('metadata'),
+            authentication=Authentication.from_json(_json.get('authentication', dict()))
+        )
+
+    def to_json(self):
+        return {
+            'name': self.name,
+            'endpoint': self.endpoint,
+            'kubernetesVersion': self.kubernetes_version,
+            'provider': self.provider.value,
+            'nodePools': [np.to_json() for np in self.node_pools],
+            'metadata': self.metadata,
+            'authentication': self.authentication.to_json()
+        }
+
+    @classmethod
+    def from_setup_json(cls, devops_output, integration):
+        node_pools = [NodePool.from_json(n) for n in devops_output['config']['nodePools']]
+        return cls(
+            devops_output['config']['name'],
+            devops_output['config']['endpoint'],
+            devops_output['config']['kubernetesVersion'],
+            ClusterProvider(devops_output['config']['provider']),
+            node_pools,
+            {},
+            Authentication(AuthenticationIntegration(integration.id, integration.type))
+        )
+
+
+class ComputeContext:
+    def __init__(self, labels: List[str], org: str, project: Optional[str] = None):
+        self.labels = labels
+        self.org = org
+        self.project = project
+
+    @classmethod
+    def from_json(cls, _json):
+        return cls(
+            labels=_json.get('labels', list()),
+            org=_json.get('org'),
+            project=_json.get('project')
+        )
+
+    def to_json(self):
+        return {
+            'labels': self.labels,
+            'org': self.org,
+            'project': self.project
+        }
+
+
+class Compute:
+    def __init__(
+            self,
+            id: str,
+            name: str,
+            context: ComputeContext,
+            client_api: ApiClient,
+            shared_contexts: Optional[List[ComputeContext]] = None,
+            global_: Optional[bool] = None,
+            status: ComputeStatus = ComputeStatus.INITIALIZING,
+            type: ComputeType = ComputeType.KUBERNETES,
+            features: Optional[Dict] = None,
+            metadata: Optional[Dict] = None,
+            settings: Optional[ComputeSettings] = None
+    ):
+        self.id = id
+        self.name = name
+        self.context = context
+        self.shared_contexts = shared_contexts if shared_contexts is not None else []
+        self.global_ = global_
+        self.status = status
+        self.type = type
+        self.features = features if features is not None else dict()
+        self.metadata = metadata if metadata is not None else dict()
+        self._client_api = client_api
+        self._computes = None
+        self._serviceDrivers = None
+        self.settings = settings
+
+    @property
+    def computes(self):
+        if self._computes is None:
+            self._computes = repositories.Computes(client_api=self._client_api)
+        return self._computes
+
+    @property
+    def service_drivers(self):
+        if self._serviceDrivers is None:
+            self._serviceDrivers = repositories.ServiceDrivers(client_api=self._client_api)
+        return self._serviceDrivers
+
+    def delete(self):
+        return self.computes.delete(compute_id=self.id)
+
+    def update(self):
+        return self.computes.update(compute=self)
+
+    @classmethod
+    def from_json(cls, _json, client_api: ApiClient):
+        return cls(
+            id=_json.get('id'),
+            name=_json.get('name'),
+            context=ComputeContext.from_json(_json.get('context', dict())),
+            shared_contexts=[ComputeContext.from_json(sc) for sc in _json.get('sharedContexts', list())],
+            global_=_json.get('global'),
+            status=ComputeStatus(_json.get('status')),
+            type=ComputeType(_json.get('type')),
+            features=_json.get('features'),
+            client_api=client_api,
+            metadata=_json.get('metadata'),
+            settings=ComputeSettings.from_json(_json.get('settings', dict())) if _json.get('settings') else None
+        )
+
+    def to_json(self):
+        return {
+            'id': self.id,
+            'context': self.context.to_json(),
+            'sharedContexts': [sc.to_json() for sc in self.shared_contexts],
+            'global': self.global_,
+            'status': self.status.value,
+            'type': self.type.value,
+            'features': self.features,
+            'metadata': self.metadata,
+            'settings': self.settings.to_json() if isinstance(self.settings, ComputeSettings) else self.settings
+        }
+
+
+class KubernetesCompute(Compute):
+    def __init__(
+            self,
+            id: str,
+            name: str,
+            context: ComputeContext,
+            cluster: ComputeCluster,
+            shared_contexts: Optional[List[ComputeContext]] = None,
+            global_: Optional[bool] = None,
+            status: ComputeStatus = ComputeStatus.INITIALIZING,
+            type: ComputeType = ComputeType.KUBERNETES,
+            features: Optional[Dict] = None,
+            metadata: Optional[Dict] = None,
+            client_api: ApiClient = None,
+            settings: Optional[ComputeSettings] = None
+    ):
+        super().__init__(id=id, context=context, shared_contexts=shared_contexts, global_=global_, status=status,
+                         type=type, features=features, metadata=metadata, client_api=client_api, settings=settings, name=name)
+        self.cluster = cluster
+
+    @classmethod
+    def from_json(cls, _json, client_api: ApiClient):
+        return cls(
+            id=_json.get('id'),
+            context=ComputeContext.from_json(_json.get('context', dict())),
+            cluster=ComputeCluster.from_json(_json.get('cluster', dict())),
+            shared_contexts=[ComputeContext.from_json(sc) for sc in _json.get('sharedContexts', list())],
+            global_=_json.get('global'),
+            status=ComputeStatus(_json.get('status')),
+            type=ComputeType(_json.get('type')),
+            features=_json.get('features'),
+            metadata=_json.get('metadata'),
+            client_api=client_api,
+            settings=ComputeSettings.from_json(_json.get('settings', dict())) if _json.get('settings') else None
+        )
+
+    def to_json(self):
+        return {
+            'id': self.id,
+            'context': self.context.to_json(),
+            'cluster': self.cluster.to_json(),
+            'sharedContexts': [sc.to_json() for sc in self.shared_contexts],
+            'global': self.global_,
+            'status': self.status.value,
+            'type': self.type.value,
+            'features': self.features,
+            'metadata': self.metadata,
+            'settings': self.settings.to_json() if isinstance(self.settings, ComputeSettings) else self.settings
+        }
+
+
+class ServiceDriver:
+    def __init__(
+            self,
+            name: str,
+            context: ComputeContext,
+            compute_id: str,
+            client_api: ApiClient,
+            type: ComputeType = None,
+            created_at: str = None,
+            updated_at: str = None,
+            namespace: str = None,
+            metadata: Dict = None,
+            url: str = None,
+            archived: bool = None,
+            id: str = None,
+            is_cache_available: bool = None
+    ):
+        self.name = name
+        self.context = context
+        self.compute_id = compute_id
+        self.client_api = client_api
+        self.type = type or ComputeType.KUBERNETES
+        self.created_at = created_at
+        self.updated_at = updated_at
+        self.namespace = namespace
+        self.metadata = metadata
+        self.url = url
+        self.archived = archived
+        self.id = id
+        self.is_cache_available = is_cache_available
+
+    @classmethod
+    def from_json(cls, _json, client_api: ApiClient):
+        return cls(
+            name=_json.get('name'),
+            context=ComputeContext.from_json(_json.get('context', dict())),
+            compute_id=_json.get('computeId'),
+            client_api=client_api,
+            type=_json.get('type', None),
+            created_at=_json.get('createdAt', None),
+            updated_at=_json.get('updatedAt', None),
+            namespace=_json.get('namespace', None),
+            metadata=_json.get('metadata', None),
+            url=_json.get('url', None),
+            archived=_json.get('archived', None),
+            id=_json.get('id', None),
+            is_cache_available=_json.get('isCacheAvailable', None)
+        )
+
+    def to_json(self):
+        _json = {
+            'name': self.name,
+            'context': self.context.to_json(),
+            'computeId': self.compute_id,
+            'type': self.type,
+        }
+        if self.created_at is not None:
+            _json['createdAt'] = self.namespace
+        if self.updated_at is not None:
+            _json['updatedAt'] = self.updated_at
+        if self.namespace is not None:
+            _json['namespace'] = self.namespace
+        if self.metadata is not None:
+            _json['metadata'] = self.metadata
+        if self.url is not None:
+            _json['url'] = self.url
+        if self.archived is not None:
+            _json['archived'] = self.archived
+        if self.id is not None:
+            _json['id'] = self.id
+        if self.is_cache_available is not None:
+            _json['isCacheAvailable'] = self.is_cache_available
+
+        return _json
---
+++ packages/dtlpy/dtlpy-1.108.7-py3-none-any.whl/dtlpy/repositories/computes.py
@@ -0,0 +1,350 @@
+import base64
+import datetime
+import json
+
+from ..services.api_client import ApiClient
+from .. import exceptions, entities, repositories
+from typing import List, Optional, Dict
+from ..entities import ComputeCluster, ComputeContext, ComputeType, Project
+from ..entities.integration import IntegrationType
+
+
+class Computes:
+
+    def __init__(self, client_api: ApiClient):
+        self._client_api = client_api
+        self._base_url = '/compute'
+        self._commands = None
+        self._projects = None
+        self._organizations = None
+
+    @property
+    def commands(self) -> repositories.Commands:
+        if self._commands is None:
+            self._commands = repositories.Commands(client_api=self._client_api)
+        return self._commands
+
+    @property
+    def projects(self):
+        if self._projects is None:
+            self._projects = repositories.Projects(client_api=self._client_api)
+        return self._projects
+
+    @property
+    def organizations(self):
+        if self._organizations is None:
+            self._organizations = repositories.Organizations(client_api=self._client_api)
+        return self._organizations
+
+    def create(
+            self,
+            name: str,
+            context: entities.ComputeContext,
+            shared_contexts: Optional[List[entities.ComputeContext]],
+            cluster: entities.ComputeCluster,
+            type: entities.ComputeType = entities.ComputeType.KUBERNETES,
+            is_global: Optional[bool] = False,
+            features: Optional[Dict] = None,
+            wait=True,
+            status: entities.ComputeStatus = None,
+            settings: entities.ComputeSettings = None
+    ):
+        """
+        Create a new compute
+
+        :param name: Compute name
+        :param context: Compute context
+        :param shared_contexts: Shared contexts
+        :param cluster: Compute cluster
+        :param type: Compute type
+        :param is_global: Is global
+        :param features: Features
+        :param wait: Wait for compute creation
+        :param status: Compute status
+        :param settings: Compute settings
+        :return: Compute
+        """
+
+        shared_contexts_json = []
+        for shared_context in shared_contexts:
+            src_json = shared_context.to_json() if isinstance(shared_context, entities.ComputeContext) else shared_context
+            shared_contexts_json.append(src_json)
+        payload = {
+            'name': name,
+            'context': context.to_json(),
+            'type': type.value,
+            'global': is_global,
+            'features': features,
+            'sharedContexts': shared_contexts_json,
+            'cluster': cluster.to_json(),
+            'status': status,
+            "settings": settings.to_json() if isinstance(settings, entities.ComputeSettings) else settings
+        }
+
+        # request
+        success, response = self._client_api.gen_request(
+            req_type='post',
+            path=self._base_url,
+            json_req=payload
+        )
+
+        if not success:
+            raise exceptions.PlatformException(response)
+
+        compute = entities.Compute.from_json(
+            _json=response.json(),
+            client_api=self._client_api
+        )
+
+        if wait:
+            command_id = compute.metadata.get('system', {}).get('commands', {}).get('create', None)
+            if command_id is not None:
+                command = self.commands.get(command_id=command_id, url='api/v1/commands/faas/{}'.format(command_id))
+                command.wait()
+                compute = self.get(compute_id=compute.id)
+
+        return compute
+
+    def get(self, compute_id: str):
+        """
+        Get a compute
+
+        :param compute_id: Compute ID
+        :return: Compute
+        """
+
+        # request
+        success, response = self._client_api.gen_request(
+            req_type='get',
+            path=self._base_url + '/{}'.format(compute_id)
+        )
+
+        if not success:
+            raise exceptions.PlatformException(response)
+
+        compute = entities.Compute.from_json(
+            _json=response.json(),
+            client_api=self._client_api
+        )
+
+        return compute
+
+    def update(self, compute: entities.Compute):
+        """
+        Update a compute
+
+        :param compute: Compute
+        :return: Compute
+        """
+
+        # request
+        success, response = self._client_api.gen_request(
+            req_type='patch',
+            path=self._base_url + '/{}'.format(compute.id),
+            json_req=compute.to_json()
+        )
+
+        if not success:
+            raise exceptions.PlatformException(response)
+
+        compute = entities.Compute.from_json(
+            _json=response.json(),
+            client_api=self._client_api
+        )
+
+        return compute
+
+    def delete(self, compute_id: str):
+        """
+        Delete a compute
+
+        :param compute_id: compute ID
+        """
+
+        # request
+        success, response = self._client_api.gen_request(
+            req_type='delete',
+            path=self._base_url + '/{}'.format(compute_id)
+        )
+
+        if not success:
+            raise exceptions.PlatformException(response)
+
+        return True
+
+    @staticmethod
+    def read_file(file_path):
+        try:
+            with open(file_path, 'r') as file:
+                content = file.read()
+            return content
+        except FileNotFoundError:
+            print(f"The file at {file_path} was not found.")
+        except IOError:
+            print(f"An error occurred while reading the file at {file_path}.")
+
+    def decode_and_parse_input(self, file_path):
+        """Decode a base64 encoded string from file a and parse it as JSON."""
+        decoded_bytes = base64.b64decode(self.read_file(file_path))
+        return json.loads(decoded_bytes)
+
+    @staticmethod
+    def create_integration(org, name, auth_data):
+        """Create a new key-value integration within the specified project."""
+        return org.integrations.create(
+            integrations_type=IntegrationType.KEY_VALUE,
+            name=name,
+            options={
+                'key': name,
+                'value': json.dumps(auth_data)
+            }
+        )
+
+    def setup_compute_cluster(self, config, integration, org_id, project=None):
+        """Set up a compute cluster using the provided configuration and integration."""
+        cluster = ComputeCluster.from_setup_json(config, integration)
+        project_id = None
+        if project is not None:
+            project_id = project.id
+        compute = self.create(
+            config['config']['name'],
+            ComputeContext([], org_id, project_id),
+            [],
+            cluster,
+            ComputeType.KUBERNETES,
+            status=config['config'].get('status', None),
+            settings=config['config'].get('settings', None))
+
+        return compute
+
+    def create_from_config_file(self, config_file_path, org_id, project_name: Optional[str] = None):
+        config = self.decode_and_parse_input(config_file_path)
+        project = None
+        if project_name is not None:
+            project = self.projects.get(project_name=project_name)
+        org = self.organizations.get(organization_id=org_id)
+        integration_name = ('cluster_integration_test_' + datetime.datetime.now().isoformat().split('.')[0]
+                            .replace(':', '_'))
+        integration = self.create_integration(org, integration_name, config['authentication'])
+        compute = self.setup_compute_cluster(config, integration, org_id, project)
+        return compute
+
+
+class ServiceDrivers:
+
+    def __init__(self, client_api: ApiClient):
+        self._client_api = client_api
+        self._base_url = '/serviceDrivers'
+
+    def create(
+            self,
+            name: str,
+            compute_id: str,
+            context: entities.ComputeContext
+    ):
+        """
+        Create a new service driver
+
+        :param name: Service driver name
+        :param compute_id: Compute ID
+        :param context: Compute context
+        :return: Service driver
+
+        """
+
+        payload = {
+            'name': name,
+            'computeId': compute_id,
+            'context': context.to_json()
+        }
+
+        # request
+        success, response = self._client_api.gen_request(
+            req_type='post',
+            path=self._base_url,
+            json_req=payload
+        )
+
+        if not success:
+            raise exceptions.PlatformException(response)
+
+        service_driver = entities.ServiceDriver.from_json(
+            _json=response.json(),
+            client_api=self._client_api
+        )
+
+        return service_driver
+
+    def get(self, service_driver_id: str):
+        """
+        Get a service driver
+
+        :param service_driver_id: Service driver ID
+        :return: Service driver
+        """
+
+        # request
+        success, response = self._client_api.gen_request(
+            req_type='get',
+            path=self._base_url + '/{}'.format(service_driver_id)
+        )
+
+        if not success:
+            raise exceptions.PlatformException(response)
+
+        service_driver = entities.ServiceDriver.from_json(
+            _json=response.json(),
+            client_api=self._client_api
+        )
+
+        return service_driver
+
+    def delete(self, service_driver_id: str):
+        """
+        Delete a service driver
+
+        :param service_driver_id: Service driver ID
+        """
+
+        # request
+        success, response = self._client_api.gen_request(
+            req_type='delete',
+            path=self._base_url + '/{}'.format(service_driver_id)
+        )
+
+        if not success:
+            raise exceptions.PlatformException(response)
+
+        return True
+
+    def set_default(self, service_driver_id: str, org_id: str, update_existing_services=False):
+        """
+        Set a service driver as default
+
+        :param service_driver_id: Compute name
+        :param org_id: Organization ID
+        :param update_existing_services: Update existing services
+
+        :return: Service driver
+        """
+
+        # request
+        success, response = self._client_api.gen_request(
+            req_type='post',
+            path=self._base_url + '/default',
+            json_req={
+                'organizationId': org_id,
+                'updateExistingServices': update_existing_services,
+                'driverName': service_driver_id
+            }
+        )
+
+        if not success:
+            raise exceptions.PlatformException(response)
+
+        service_driver = entities.ServiceDriver.from_json(
+            _json=response.json(),
+            client_api=self._client_api
+        )
+
+        return service_driver

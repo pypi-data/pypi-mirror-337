54a07ed33425eab3e782eb5eb977a41350b4417a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-28 17:04:56 +0000

    Add package pepbench/0.1.1/pepbench-0.1.1.tar.gz

---
+++ packages/pepbench/pepbench-0.1.1.tar.gz/pepbench-0.1.1/src/pepbench/plotting/__init__.py
@@ -0,0 +1,24 @@
+"""Module for plotting signals and results of PEP benchmarks."""
+
+from pepbench.plotting import algorithms, results
+from pepbench.plotting._base_plotting import (
+    plot_blandaltman,
+    plot_paired,
+    plot_signals,
+    plot_signals_from_challenge_results,
+    plot_signals_with_algorithm_results,
+    plot_signals_with_reference_labels,
+    plot_signals_with_reference_pep,
+)
+
+__all__ = [
+    "plot_signals",
+    "plot_paired",
+    "plot_blandaltman",
+    "plot_signals_with_reference_labels",
+    "plot_signals_from_challenge_results",
+    "plot_signals_with_reference_pep",
+    "plot_signals_with_algorithm_results",
+    "algorithms",
+    "results",
+]

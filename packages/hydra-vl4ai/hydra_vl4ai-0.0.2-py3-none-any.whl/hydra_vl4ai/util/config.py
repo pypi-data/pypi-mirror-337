de7fb3d0022b851e5f75385108649ee7012d1229
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-28 17:05:13 +0000

    Add package hydra-vl4ai/0.0.2/hydra_vl4ai-0.0.2-py3-none-any.whl

---
+++ packages/hydra-vl4ai/hydra_vl4ai-0.0.2-py3-none-any.whl/hydra_vl4ai/util/config.py
@@ -0,0 +1,42 @@
+from typing import Any
+from tensorneko_util.util import Singleton
+import tensorneko_util as N
+
+
+@Singleton
+class Config:
+
+    def __init__(self):
+        self.model_config_path: str | None = None
+        self._base_config_path: str | None = None
+        self._dqn_config_path: str | None = None
+        self.base_config: dict[str, Any] = dict()
+        self.dqn_config: dict[str, Any] = dict()
+
+    @property
+    def base_config_path(self):
+        return self._base_config_path
+
+    @base_config_path.setter
+    def base_config_path(self, value):
+        if value is not None:
+            self._base_config_path = value
+            self.base_config = N.read(value)
+
+    @property
+    def dqn_config_path(self):
+        return self._dqn_config_path
+
+    @dqn_config_path.setter
+    def dqn_config_path(self, value):
+        if value is not None:
+            self._dqn_config_path = value
+            self.dqn_config = N.read(value)
+
+    @property
+    def debug(self):
+        return Config.base_config["debug"]
+    
+    @debug.setter
+    def debug(self, value):
+        Config.base_config["debug"] = value

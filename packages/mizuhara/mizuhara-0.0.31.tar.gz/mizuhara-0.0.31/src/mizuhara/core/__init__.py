1e54b3ed6d9695be65d34b4d9696f1a292e4e12e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-28 03:15:56 +0000

    Add package mizuhara/0.0.31/mizuhara-0.0.31.tar.gz

---
+++ packages/mizuhara/mizuhara-0.0.31.tar.gz/mizuhara-0.0.31/src/mizuhara/core/__init__.py
@@ -0,0 +1,24 @@
+
+
+class UserInfo:
+    """
+    UserInfo:
+
+    this class is charge of saving telegram user's information in mizuhara.core.routes.CLIENT_INFO
+    """
+
+    def __init__(self, **kwargs):
+        self.route: str = ""
+        self.info: dict = {}
+        self.data: dict = {}
+        self.index: int = 0
+        self.page: int = 0
+        self.is_signin: bool = False
+        self.language: str = "en"
+
+    def get(self, key: str, default=None):
+        return self.__dict__.get(key, default)
+
+    def update(self, **kwargs):
+        for k, v in kwargs.items():
+            setattr(self, k, v)

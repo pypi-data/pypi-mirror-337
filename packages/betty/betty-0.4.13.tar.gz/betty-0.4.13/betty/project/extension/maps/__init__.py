81da63450f61f281292f767fce1ca678256cd2de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-28 14:04:14 +0000

    Add package betty/0.4.13/betty-0.4.13.tar.gz

---
+++ packages/betty/betty-0.4.13.tar.gz/betty-0.4.13/betty/project/extension/maps/__init__.py
@@ -0,0 +1,50 @@
+"""Integrate Betty with `Leaflet.js <https://leafletjs.com/>`_."""
+
+from __future__ import annotations
+
+from pathlib import Path
+from typing import TYPE_CHECKING, final
+
+from typing_extensions import override
+
+from betty.locale.localizable import _
+from betty.plugin import ShorthandPluginBase
+from betty.project.extension.webpack import Webpack
+from betty.project.extension.webpack.build import EntryPointProvider
+
+if TYPE_CHECKING:
+    from betty.project.extension import Extension
+    from betty.plugin import PluginIdentifier
+    from collections.abc import Sequence
+
+
+@final
+class Maps(ShorthandPluginBase, EntryPointProvider):
+    """
+    Provide interactive maps for use on web pages.
+    """
+
+    _plugin_id = "maps"
+    _plugin_label = _("Maps")
+    _plugin_description = _(
+        'Display lists of places as interactive maps using <a href="https://leafletjs.com/">Leaflet</a>.'
+    )
+
+    @override
+    @classmethod
+    def depends_on(cls) -> set[PluginIdentifier[Extension]]:
+        return {Webpack}
+
+    @override
+    @classmethod
+    def assets_directory_path(cls) -> Path | None:
+        return Path(__file__).parent / "assets"
+
+    @override
+    @classmethod
+    def webpack_entry_point_directory_path(cls) -> Path:
+        return Path(__file__).parent / "webpack"
+
+    @override
+    def webpack_entry_point_cache_keys(self) -> Sequence[str]:
+        return ()
---
+++ packages/betty/betty-0.4.13.tar.gz/betty-0.4.13/betty/project/extension/trees/webpack/main.ts
@@ -0,0 +1,8 @@
+'use strict'
+
+import {initializeAncestryTrees} from './trees.js'
+import {BETTY} from "@betty.py/betty/main.ts"
+
+await BETTY.addInitializer(
+    initializeAncestryTrees // eslint-disable-line @typescript-eslint/no-unsafe-argument
+)

0755de5b821ddc66899efaf1f37f12788811288a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-28 17:04:52 +0000

    Add package jupyverse/0.7.6/jupyverse-0.7.6.tar.gz

---
+++ packages/jupyverse/jupyverse-0.7.6.tar.gz/jupyverse-0.7.6/plugins/auth/fps_auth/models.py
@@ -0,0 +1,23 @@
+import uuid
+from typing import Dict, List
+
+from fastapi_users import schemas
+
+from jupyverse_api.auth import User
+
+
+class JupyterUser(User):
+    anonymous: bool = True
+    permissions: Dict[str, List[str]]
+
+
+class UserRead(schemas.BaseUser[uuid.UUID], JupyterUser):
+    pass
+
+
+class UserCreate(schemas.BaseUserCreate, JupyterUser):
+    pass
+
+
+class UserUpdate(schemas.BaseUserUpdate, JupyterUser):
+    pass

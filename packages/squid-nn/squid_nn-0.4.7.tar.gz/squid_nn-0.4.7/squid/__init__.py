4c02be92abeb620ede850ef604d14d72f3c4c40b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-28 06:04:36 +0000

    Add package squid-nn/0.4.7/squid_nn-0.4.7.tar.gz

---
+++ packages/squid-nn/squid_nn-0.4.7.tar.gz/squid_nn-0.4.7/squid/__init__.py
@@ -0,0 +1,9 @@
+from . import impress 
+from . import mave 
+from . import mutagenizer 
+from . import predictor 
+from . import surrogate_zoo 
+from . import utils 
+
+# Version info
+__version__ = "0.4.7"

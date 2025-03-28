e82324c6c27eed94ee5e414f0de227b707250a93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-28 04:04:24 +0000

    Add package pamji-bot/1.0.0/pamji_bot-1.0.0.tar.gz

---
+++ packages/pamji-bot/pamji_bot-1.0.0.tar.gz/pamji_bot-1.0.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='pamji_bot',
+    version='1.0.0',
+    packages=['pamji_bot'],
+    install_requires=[
+    "discord",
+    "requests",
+    "ai_api_utils",
+    "dotenv",
+    ]
+)

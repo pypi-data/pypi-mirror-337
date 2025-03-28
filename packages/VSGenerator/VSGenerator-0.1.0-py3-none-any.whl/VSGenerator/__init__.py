4312ba02451db02825ed411d9eeee7d3d7f6a2a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-28 16:04:47 +0000

    Add package VSGenerator/0.1.0/VSGenerator-0.1.0-py3-none-any.whl

---
+++ packages/VSGenerator/VSGenerator-0.1.0-py3-none-any.whl/VSGenerator/__init__.py
@@ -0,0 +1,9 @@
+import datetime
+now = datetime.datetime.now()
+formatted_date_time = now.strftime('%Y-%m-%d %H:%M:%S')
+
+print('DVSNet, Bin Cao, Hong Kong University of Science and Technology (Guangzhou).')
+print('URL : https://github.com/Bin-Cao/Bgolearn')
+print('URL : https://github.com/Bgolearn/VSGenerator')
+print('Executed on :',formatted_date_time, ' | Have a great day.')  
+print('\n')

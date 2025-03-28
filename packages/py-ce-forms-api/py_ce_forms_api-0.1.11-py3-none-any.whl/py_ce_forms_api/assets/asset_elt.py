1d69f654f847baf43e13c342e9923ecf3002d9a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-28 04:03:51 +0000

    Add package py-ce-forms-api/0.1.11/py_ce_forms_api-0.1.11-py3-none-any.whl

---
+++ packages/py-ce-forms-api/py_ce_forms_api-0.1.11-py3-none-any.whl/py_ce_forms_api/assets/asset_elt.py
@@ -0,0 +1,29 @@
+class AssetElt():
+    """
+    Utility class to manage forms asset element
+    """
+    def __init__(self, data: (bytes|None), block_value) -> None:
+        self.data = data
+        self.block_value = block_value
+    
+    def id(self) -> str:
+        return self.block_value["id"]
+    
+    def mimetype(self) -> str:
+        return self.block_value["mimetype"]
+    
+    def name(self) -> str:
+        return self.block_value["name"]
+    
+    def original_name(self) -> str:
+        return self.block_value["originalname"]
+    
+    def ref(self) -> str:
+        return self.block_value["ref"]
+    
+    def get_bytes(self) -> bytes:
+        return self.data
+    
+    def get_value(self):
+        return self.block_value
+    

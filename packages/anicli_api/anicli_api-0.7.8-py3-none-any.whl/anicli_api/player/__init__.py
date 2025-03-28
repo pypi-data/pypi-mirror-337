cc624eac7428b31fef87f559c87a6adce270ac7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-28 07:03:49 +0000

    Add package anicli_api/0.7.8/anicli_api-0.7.8-py3-none-any.whl

---
+++ packages/anicli_api/anicli_api-0.7.8-py3-none-any.whl/anicli_api/player/__init__.py
@@ -0,0 +1,8 @@
+from anicli_api.player.aniboom import Aniboom
+from anicli_api.player.csst import CsstOnline
+from anicli_api.player.kodik import Kodik
+from anicli_api.player.sibnet import SibNet
+from anicli_api.player.sovetromantica import SovietRomanticaPlayer
+from anicli_api.player.sovetromantica_embed import SovietRomanticaEmbed
+
+ALL_DECODERS = (Kodik, Aniboom, SibNet, CsstOnline, SovietRomanticaPlayer, SovietRomanticaEmbed)

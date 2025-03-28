443f0ca3046e93655ce2ae0155d518de1b388a4a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-28 16:04:56 +0000

    Add package PyThermoDB/1.7.22/pythermodb-1.7.22.tar.gz

---
+++ packages/PyThermoDB/pythermodb-1.7.22.tar.gz/pythermodb-1.7.22/tests/test3-1.py
@@ -0,0 +1,30 @@
+# import packages/modules
+import os
+import pyThermoDB as ptdb
+from rich import print
+
+# dir
+# print(dir(pt))
+# get versions
+# print(pt.get_version())
+print(ptdb.__version__)
+
+# ====================================
+# BUILD THERMODB
+# ====================================
+# build a thermodb
+thermo_db = ptdb.build_thermodb()
+print(type(thermo_db))
+
+# add TableData
+# add TableEquation
+
+# property name
+property_name = 'CO2-enthalpy-of-formation'
+
+# add dict
+thermo_db.add_data(
+    property_name, {'dHf_IG': 152, 'units': 'kJ/mol', 'message': 'from NIST'})
+
+# save
+thermo_db.save(f'{property_name}', 'tests')

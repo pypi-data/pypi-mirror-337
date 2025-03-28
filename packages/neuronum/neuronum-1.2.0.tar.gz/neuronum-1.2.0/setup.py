6c8f4f19ccd56d274db75d7eb2f97c261c5fa1ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-28 03:16:09 +0000

    Add package neuronum/1.2.0/neuronum-1.2.0.tar.gz

---
+++ packages/neuronum/neuronum-1.2.0.tar.gz/neuronum-1.2.0/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='neuronum',
+    version='1.2.0',
+    author='Neuronum Cybernetics',
+    author_email='welcome@neuronum.net',
+    description='Interact with the Neuronum Network to build, connect & automate economic data streams',
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/neuronumcybernetics/neuronum",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        'requests', 
+        'websocket-client', 
+    ],
+    entry_points={
+    "console_scripts": [
+        "neuronum = neuronum.cli:main",
+    ],
+    },
+    python_requires='>=3.6', 
+)

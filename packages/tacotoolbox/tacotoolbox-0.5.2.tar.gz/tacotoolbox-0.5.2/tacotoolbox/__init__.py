d65b80b834f5e2668d46a47295e09c49681ee2e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-28 17:04:55 +0000

    Add package tacotoolbox/0.5.2/tacotoolbox-0.5.2.tar.gz

---
+++ packages/tacotoolbox/tacotoolbox-0.5.2.tar.gz/tacotoolbox-0.5.2/tacotoolbox/__init__.py
@@ -0,0 +1,14 @@
+import pytortilla as tortilla
+
+from tacotoolbox import datamodel, edit
+from tacotoolbox.create import create, taco2tortilla, tortilla2taco
+
+__all__ = [
+    "datamodel",
+    "create",
+    "tortilla2taco",
+    "taco2tortilla",
+    "tortilla",
+    "edit"
+]
+__version__ = "0.5.0-beta1"

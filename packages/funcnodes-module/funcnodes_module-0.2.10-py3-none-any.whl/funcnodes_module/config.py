3aecf562f69ba5ef95a6e451563903d802a8c580
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-28 04:03:53 +0000

    Add package funcnodes-module/0.2.10/funcnodes_module-0.2.10-py3-none-any.whl

---
+++ packages/funcnodes-module/funcnodes_module-0.2.10-py3-none-any.whl/funcnodes_module/config.py
@@ -0,0 +1,38 @@
+import os
+
+template_path = os.path.join(os.path.dirname(__file__), "template_folder")
+files_to_overwrite = [
+    os.path.join(".github", "workflows", "py_test.yml"),
+    os.path.join(".github", "workflows", "version_publish_main.yml"),
+    os.path.join(".github", "actions", "install_package", "action.yml"),
+    os.path.join("tests", "all_nodes_test_base.py"),
+]
+
+files_to_copy_if_missing = [
+    os.path.join("tests", "test_all_nodes.py"),
+    os.path.join(".pre-commit-config.yaml"),
+    os.path.join(".flake8"),
+    os.path.join("MANIFEST.in"),
+    os.path.join("pytest.ini"),
+]
+
+files_to_overwrite_on_force = [
+    os.path.join(".pre-commit-config.yaml"),
+    os.path.join(".flake8"),
+]
+
+
+package_requirements = [
+    "funcnodes",
+]
+
+dev_requirements = [
+    "pre-commit",
+    "pytest",
+    "funcnodes-module",
+]
+
+gitpaths = [
+    ".github",
+    ".git",
+]

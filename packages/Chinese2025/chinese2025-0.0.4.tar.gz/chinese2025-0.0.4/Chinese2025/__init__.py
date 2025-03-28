58fcd5059f034ca5408833b47b3ff0a654d2281b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-28 04:03:41 +0000

    Add package Chinese2025/0.0.4/chinese2025-0.0.4.tar.gz

---
+++ packages/Chinese2025/chinese2025-0.0.4.tar.gz/chinese2025-0.0.4/Chinese2025/__init__.py
@@ -0,0 +1,6 @@
+from 繁體廣韻搜索 import 繁體廣韻搜索
+from 繁體方言搜索 import 繁體方言搜索
+from 王力魏晉南北朝擬音搜索 import 王力魏晉南北朝擬音搜索
+from FanFangYan import FanFangYang
+from FanGuangYun import FanGuangYun
+__all__ = ['繁體廣韻搜索', '繁體方言搜索','王力魏晉南北朝擬音搜索','FanFangYang','FanGuangYun']

c38d07083219ddad7491bffbbe37732cf246d1e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-28 16:05:11 +0000

    Add package kubed/2.0.0/kubed-2.0.0-py3-none-any.whl

---
+++ packages/kubed/kubed-2.0.0-py3-none-any.whl/kubed/__init__.py
@@ -0,0 +1,11 @@
+"""
+Kubed - CLI productivity tool with autocompletion for DevOps tools.
+
+This package provides autocompletion and helpful aliases for:
+- Docker
+- Terraform
+- Helm
+- Kubernetes (kubectl)
+"""
+
+__version__ = "0.1.0" 

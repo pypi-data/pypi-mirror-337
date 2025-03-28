5809b8f5d0de58cf7f40dce31ee37aa86e5be562
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-28 03:15:57 +0000

    Add package json-to-clickhouse/0.1.6/json_to_clickhouse-0.1.6-py3-none-any.whl

---
+++ packages/json-to-clickhouse/json_to_clickhouse-0.1.6-py3-none-any.whl/json_to_clickhouse/__init__.py
@@ -0,0 +1,3 @@
+from .json_to_clickhouse import escape_sql_string, ClickHouseJSONHandler, flatten_dict, infer_table_structure
+
+__all__ = ["escape_sql_string", "ClickHouseJSONHandler", "flatten_dict","infer_table_structure"]

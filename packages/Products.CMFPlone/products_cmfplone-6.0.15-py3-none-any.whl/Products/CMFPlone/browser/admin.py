7a5b03aea3fbd7047f1483a7bd92a8122b8c7f9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-28 07:03:56 +0000

    Add package Products.CMFPlone/6.0.15/products_cmfplone-6.0.15-py3-none-any.whl

---
+++ packages/Products.CMFPlone/products_cmfplone-6.0.15-py3-none-any.whl/Products/CMFPlone/browser/admin.py
@@ -0,0 +1,364 @@
+from AccessControl import getSecurityManager
+from AccessControl.Permissions import view as View
+from collections import OrderedDict
+from importlib.metadata import distribution
+from importlib.metadata import PackageNotFoundError
+from OFS.interfaces import IApplication
+from plone.base.interfaces import INonInstallable
+from plone.base.interfaces import IPloneSiteRoot
+from plone.base.utils import get_installer
+from plone.i18n.locales.interfaces import IContentLanguageAvailability
+from plone.keyring.interfaces import IKeyManager
+from plone.protect.authenticator import check as checkCSRF
+from plone.protect.interfaces import IDisableCSRFProtection
+from Products.CMFCore.permissions import ManagePortal
+from Products.CMFPlone.factory import _DEFAULT_PROFILE
+from Products.CMFPlone.factory import addPloneSite
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+from Products.GenericSetup import BASE
+from Products.GenericSetup import EXTENSION
+from Products.GenericSetup import profile_registry
+from Products.GenericSetup.upgrade import normalize_version
+from urllib import parse
+from ZODB.broken import Broken
+from zope.component import adapter
+from zope.component import getAllUtilitiesRegisteredFor
+from zope.component import getUtility
+from zope.component import queryMultiAdapter
+from zope.component import queryUtility
+from zope.i18n.interfaces import IUserPreferredLanguages
+from zope.i18n.locales import LoadLocaleError
+from zope.i18n.locales import locales
+from zope.interface import alsoProvides
+from zope.interface import Interface
+from zope.publisher.browser import BrowserView
+from zope.publisher.interfaces import IRequest
+from zope.schema.interfaces import IVocabularyFactory
+from ZPublisher.BaseRequest import DefaultPublishTraverse
+
+import logging
+
+
+try:
+    distribution("plone.volto")
+    HAS_VOLTO = True
+except PackageNotFoundError:
+    HAS_VOLTO = False
+
+LOGGER = logging.getLogger("Products.CMFPlone")
+
+
+@adapter(IApplication, IRequest)
+class AppTraverser(DefaultPublishTraverse):
+    def publishTraverse(self, request, name):
+        if name == "index_html":
+            view = queryMultiAdapter(
+                (self.context, request), Interface, "plone-overview"
+            )
+            if view is not None:
+                return view
+        return DefaultPublishTraverse.publishTraverse(self, request, name)
+
+
+class Overview(BrowserView):
+    has_volto = HAS_VOLTO
+
+    def sites(self, root=None):
+        if root is None:
+            root = self.context
+
+        result = []
+        secman = getSecurityManager()
+        candidates = (obj for obj in root.values() if not isinstance(obj, Broken))
+        for obj in candidates:
+            if obj.meta_type == "Folder":
+                result = result + self.sites(obj)
+            elif IPloneSiteRoot.providedBy(obj):
+                if secman.checkPermission(View, obj):
+                    result.append(obj)
+            elif obj.getId() in getattr(root, "_mount_points", {}):
+                result.extend(self.sites(root=obj))
+        return result
+
+    def outdated(self, obj):
+        # Try to pick the portal_migration as an attribute
+        # (Plone 5 unmigrated site root) or as an item
+        mig = getattr(obj, "portal_migration", None) or obj.get(
+            "portal_migration", None
+        )
+        if mig is not None:
+            return mig.needUpgrading()
+        return False
+
+    def can_manage(self):
+        secman = getSecurityManager()
+        return secman.checkPermission(ManagePortal, self.context)
+
+    def upgrade_url(self, site, can_manage=None):
+        if can_manage is None:
+            can_manage = self.can_manage()
+        if can_manage:
+            return site.absolute_url() + "/@@plone-upgrade"
+        else:
+            return self.context.absolute_url() + "/@@plone-root-login"
+
+
+class RootLoginRedirect(BrowserView):
+    """@@plone-root-login
+
+    This view of the Zope root forces authentication via the root
+    acl_users and then redirects elsewhere.
+    """
+
+    def __call__(self, came_from=None):
+        if came_from is not None:
+            # see if this is a relative url or an absolute
+            if len(parse.urlparse(came_from)[1]) == 0:
+                # No host specified, so url is relative.  Get an absolute url.
+                # Note: '\\domain.org' is not recognised as host,
+                # which is good.
+                came_from = parse.urljoin(
+                    self.context.absolute_url() + "/",
+                    came_from,
+                )
+            elif not came_from.startswith(self.context.absolute_url()):
+                # Note: we cannot use portal_url.isURLInPortal here, because we
+                # are not in a Plone portal, but in the Zope root.
+                came_from = None
+        if came_from is None:
+            came_from = self.context.absolute_url()
+        self.request.response.redirect(came_from)
+
+
+class RootLogout(BrowserView):
+    """@@plone-root-logout"""
+
+    logout = ViewPageTemplateFile("templates/plone-admin-logged-out.pt")
+
+    def __call__(self):
+        response = self.request.response
+        realm = response.realm
+        response.setStatus(401)
+        response.setHeader("WWW-Authenticate", 'basic realm="%s"' % realm, 1)
+        response.setBody(self.logout())
+        return
+
+
+class FrontPage(BrowserView):
+    index = ViewPageTemplateFile("templates/plone-frontpage.pt")
+
+
+class AddPloneSite(BrowserView):
+    # Profiles that are installed by default,
+    # but can be removed later.
+    default_extension_profiles = (
+        "plone.app.caching:default",
+        "plonetheme.barceloneta:default",
+    )
+    # Let's have a separate list for Volto.
+    volto_default_extension_profiles = (
+        "plone.app.caching:default",
+        "plonetheme.barceloneta:default",
+        "plone.volto:default",
+    )
+
+    def profiles(self):
+        base_profiles = []
+        extension_profiles = []
+        if HAS_VOLTO and not self.request.get("classic"):
+            selected_extension_profiles = self.volto_default_extension_profiles
+        else:
+            selected_extension_profiles = self.default_extension_profiles
+
+        # profiles available for install/uninstall, but hidden at the time
+        # the Plone site is created
+        not_installable = [
+            "Products.CMFPlacefulWorkflow:CMFPlacefulWorkflow",
+        ]
+        utils = getAllUtilitiesRegisteredFor(INonInstallable)
+        for util in utils:
+            not_installable.extend(
+                util.getNonInstallableProfiles()
+                if hasattr(util, "getNonInstallableProfiles")
+                else []
+            )
+
+        for info in profile_registry.listProfileInfo():
+            if info.get("type") == EXTENSION and info.get("for") in (
+                IPloneSiteRoot,
+                None,
+            ):
+                profile_id = info.get("id")
+                if profile_id not in not_installable:
+                    if profile_id in selected_extension_profiles:
+                        info["selected"] = "selected"
+                    extension_profiles.append(info)
+
+        def _key(v):
+            # Make sure implicitly selected items come first
+            selected = v.get("selected") and "automatic" or "manual"
+            return "{}-{}".format(selected, v.get("title", ""))
+
+        extension_profiles.sort(key=_key)
+
+        for info in profile_registry.listProfileInfo():
+            if info.get("type") == BASE and info.get("for") in (IPloneSiteRoot, None):
+                base_profiles.append(info)
+
+        return dict(
+            base=tuple(base_profiles),
+            default=_DEFAULT_PROFILE,
+            extensions=tuple(extension_profiles),
+        )
+
+    def browser_language(self):
+        language = "en"
+        pl = IUserPreferredLanguages(self.request)
+        if pl is not None:
+            languages = pl.getPreferredLanguages()
+            for httplang in languages:
+                parts = (httplang.split("-") + [None, None])[:3]
+                if parts[0] == parts[1]:
+                    # Avoid creating a country code for simple languages codes
+                    parts = [parts[0], None, None]
+                try:
+                    locale = locales.getLocale(*parts)
+                    language = locale.getLocaleID().replace("_", "-").lower()
+                    break
+                except LoadLocaleError:
+                    # Just try the next combination
+                    pass
+        return language
+
+    def grouped_languages(self, default="en"):
+        util = queryUtility(IContentLanguageAvailability)
+        available = util.getLanguages(combined=True)
+        languages = dict(util.getLanguageListing())
+
+        # Group country specific versions by language
+        grouped = OrderedDict()
+        for langcode, data in available.items():
+            lang = langcode.split("-")[0]
+            language = languages.get(lang, lang)  # Label
+
+            struct = grouped.get(lang, {"label": language, "languages": []})
+
+            langs = struct["languages"]
+            langs.append(
+                {
+                    "langcode": langcode,
+                    "label": data.get("native", data.get("name")),
+                }
+            )
+
+            grouped[lang] = struct
+
+        # Sort list by language, next by country
+        data = sorted(grouped.values(), key=lambda k: k["label"])
+        for item in data:
+            item["languages"] = sorted(
+                item["languages"], key=lambda k: k["label"].lower()
+            )
+        return data
+
+    def timezones(self):
+        tz_vocab = getUtility(
+            IVocabularyFactory, "plone.app.vocabularies.CommonTimezones"
+        )(self.context)
+
+        grouped = OrderedDict()
+        tz_values = [it.value for it in tz_vocab]
+        for value in tz_values:
+            split = value.split("/")
+            group = split.pop(0)
+            label = "/".join(split)
+
+            entries = grouped.get(group, [])
+            entries.append({"label": label or group, "value": value})
+            grouped[group] = entries
+
+        return grouped
+
+    def __call__(self):
+        context = self.context
+        form = self.request.form
+        submitted = form.get("form.submitted", False)
+        if submitted:
+            site_id = form.get("site_id", "Plone")
+
+            # CSRF protect. DO NOT use auto CSRF protection for adding a site
+            alsoProvides(self.request, IDisableCSRFProtection)
+
+            # check if keyring is installed on root, disable CSRF protection
+            # if it is because it is not installed until a plone site
+            # is created
+            if queryUtility(IKeyManager) is None:
+                LOGGER.info("CSRF protection disabled on initial site " "creation")
+            else:
+                # we have a keymanager, check csrf protection manually now
+                checkCSRF(self.request)
+
+            site = addPloneSite(
+                context,
+                site_id,
+                title=form.get("title", ""),
+                profile_id=form.get("profile_id", _DEFAULT_PROFILE),
+                extension_ids=form.get("extension_ids", ()),
+                setup_content=form.get("setup_content", False),
+                default_language=form.get("default_language", "en"),
+                portal_timezone=form.get("portal_timezone", "UTC"),
+            )
+            self.request.response.redirect(site.absolute_url())
+            return ""
+
+        return self.index()
+
+
+class Upgrade(BrowserView):
+    def upgrades(self):
+        pm = getattr(self.context, "portal_migration")
+        return pm.listUpgrades()
+
+    def versions(self):
+        pm = getattr(self.context, "portal_migration")
+        result = {}
+        result["instance"] = pm.getInstanceVersion()
+        result["fs"] = pm.getFileSystemVersion()
+        result["equal"] = result["instance"] == result["fs"]
+        instance_version = normalize_version(result["instance"])
+        fs_version = normalize_version(result["fs"])
+        result["instance_gt"] = instance_version > fs_version
+        result["instance_lt"] = instance_version < fs_version
+        result["corelist"] = pm.coreVersions()
+        return result
+
+    def __call__(self):
+        form = self.request.form
+        submitted = form.get("form.submitted", False)
+        if submitted:
+            # CSRF protect. DO NOT use auto CSRF protection for upgrading sites
+            alsoProvides(self.request, IDisableCSRFProtection)
+
+            pm = getattr(self.context, "portal_migration")
+            report = pm.upgrade(
+                REQUEST=self.request,
+                dry_run=form.get("dry_run", False),
+            )
+            return self.index(
+                report=report,
+            )
+
+        return self.index()
+
+    def can_migrate_to_volto(self):
+        if not HAS_VOLTO:
+            return False
+        pm = getattr(self.context, "portal_migration")
+        if pm.getInstanceVersion() < "6005":
+            return False
+        try:
+            from plone.volto.browser import migrate_to_volto  # noqa: F401
+        except ImportError:
+            return False
+        installer = get_installer(self.context, self.request)
+        return not installer.is_product_installed("plone.volto")
---
+++ packages/Products.CMFPlone/products_cmfplone-6.0.15-py3-none-any.whl/Products/CMFPlone/controlpanel/browser/overview.py
@@ -0,0 +1,179 @@
+from AccessControl import getSecurityManager
+from Acquisition import aq_inner
+from App.config import getConfiguration
+from datetime import date
+from importlib.metadata import version as dist_version
+from plone.app.registry.browser import controlpanel
+from plone.base.interfaces.controlpanel import IMailSchema
+from plone.memoize.instance import memoize
+from plone.registry.interfaces import IRegistry
+from Products.CMFCore.permissions import ManagePortal
+from Products.CMFCore.utils import getToolByName
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+from zope.component import getUtility
+from ZPublisher.HTTPRequest import WSGIRequest
+
+import sys
+import warnings
+
+
+try:
+    import plone.app.event
+
+    plone.app.event  # pyflakes
+    HAS_PAE = True
+except ImportError:
+    HAS_PAE = False
+
+
+# When is a Python 3 minor version out of support?
+# See https://devguide.python.org/versions/#versions
+_PYTHON_MINOR_OUT_OF_SUPPORT = {
+    9: date(2025, 10, 31),
+    10: date(2026, 10, 31),
+    11: date(2027, 10, 31),
+    12: date(2028, 10, 31),
+    13: date(2029, 10, 31),
+}
+# This date is specific to the current Plone major version.
+# See https://plone.org/download/release-schedule
+_PLONE_OUT_OF_SECURITY_SUPPORT = date(2027, 12, 31)
+
+
+class OverviewControlPanel(controlpanel.RegistryEditForm):
+    template = ViewPageTemplateFile("overview.pt")
+
+    base_category = "controlpanel"
+    ignored_categories = "controlpanel_user"
+
+    def __call__(self):
+        self.request.set("disable_border", 1)
+        return self.template()
+
+    @memoize
+    def cptool(self):
+        return getToolByName(aq_inner(self.context), "portal_controlpanel")
+
+    @memoize
+    def migration(self):
+        return getToolByName(aq_inner(self.context), "portal_migration")
+
+    @memoize
+    def core_versions(self):
+        return self.migration().coreVersions()
+
+    def pil(self):
+        return "PIL" in self.core_versions()
+
+    def server_info(self):
+        wsgi = isinstance(self.request, WSGIRequest)
+        server_name = "unknown"
+        server_version = ""
+
+        server_name = self.request.get("SERVER_SOFTWARE")
+        if server_name:
+            if "ZServer" in server_name:
+                server_name = "ZServer"
+            elif "/" in server_name:
+                server_name = server_name.split("/")[0]
+            try:
+                server_version = dist_version(server_name)
+            except Exception:
+                warnings.warn(
+                    "Cannot find or parse version for %r"
+                    % self.request.get("SERVER_SOFTWARE"),
+                )
+        return {
+            "wsgi": wsgi,
+            "server_name": server_name,
+            "version": server_version,
+        }
+
+    def version_overview(self):
+        core_versions = self.core_versions()
+        versions = [
+            "Plone {} ({})".format(
+                core_versions["Plone"], core_versions["Plone Instance"]
+            )
+        ]
+
+        for v in ("CMF", "Zope", "Python"):
+            versions.append(v + " " + core_versions.get(v))
+        pil = core_versions.get("PIL", None)
+        if pil is not None:
+            versions.append("PIL " + pil)
+        return versions
+
+    def is_dev_mode(self):
+        return getConfiguration().debug_mode
+
+    def upgrade_warning(self):
+        mt = getToolByName(aq_inner(self.context), "portal_migration")
+        if mt.needUpgrading():
+            # if the user can't run the upgrade, no sense in displaying the
+            # message
+            sm = getSecurityManager()
+            if sm.checkPermission(ManagePortal, self.context):
+                return True
+        return False
+
+    def mailhost_warning(self):
+        registry = getUtility(IRegistry)
+        mail_settings = registry.forInterface(IMailSchema, prefix="plone", check=False)
+        mailhost = mail_settings.smtp_host
+        email = mail_settings.email_from_address
+        if mailhost and email:
+            return False
+        return True
+
+    def timezone_warning(self):
+        """Returns true, if the portal_timezone is not set in the registry."""
+        if not HAS_PAE:
+            # No point of having a portal timezone configured without
+            # plone.app.event installed.
+            # TODO: Above applies to situation at time of writing. If other
+            # datetimes outside plone.app.event use proper timezones too, the
+            # HAS_PAE should be removed.
+            return False
+        # check if 'plone.portal_timezone' is in registry
+        registry = getUtility(IRegistry)
+        reg_key = "plone.portal_timezone"
+        if reg_key not in registry:
+            # else use 'plone.app.event.portal_timezone'
+            # < Plone 5
+            reg_key = "plone.app.event.portal_timezone"
+        if reg_key not in registry:
+            return True
+        portal_timezone = registry[reg_key]
+        if portal_timezone:
+            return False
+        return True  # No portal_timezone found.
+
+    def python_warning(self):
+        minor_version = sys.version_info.minor
+        deadline = _PYTHON_MINOR_OUT_OF_SUPPORT.get(minor_version)
+        if not deadline:
+            # This Python version is not supported at all
+            return True
+        # Warn when today is after the deadline for this minor version.
+        return date.today() > deadline
+
+    def plone_maintenance_warning(self):
+        return True
+
+    def plone_security_warning(self):
+        return date.today() > _PLONE_OUT_OF_SECURITY_SUPPORT
+
+    def version_warning(self):
+        # Is there *any* version warning?
+        return (
+            self.python_warning()
+            or self.plone_maintenance_warning()
+            or self.plone_security_warning()
+        )
+
+    def categories(self):
+        return self.cptool().getGroups()
+
+    def sublists(self, category):
+        return self.cptool().enumConfiglets(group=category)

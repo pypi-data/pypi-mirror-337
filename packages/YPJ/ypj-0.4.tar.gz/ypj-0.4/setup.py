affbfaf822801a427f15564108ce152c02e0ca11
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-28 03:16:04 +0000

    Add package YPJ/0.4/ypj-0.4.tar.gz

---
+++ packages/YPJ/ypj-0.4.tar.gz/ypj-0.4/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+setup(
+    name='YPJ', 
+    version='0.4',
+    packages=find_packages(),
+    author='JokerPy',
+    author_email='tvvvyvv@gmail.com',
+    description='Simple',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',  
+        'Operating System :: OS Independent',
+        ]
+     ) 

2979e7d6b39bc937cad4adb2806ecbd18c3155f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-28 17:04:50 +0000

    Add package pynitrokey/0.8.1/pynitrokey-0.8.1.tar.gz

---
+++ packages/pynitrokey/pynitrokey-0.8.1.tar.gz/pynitrokey-0.8.1/pynitrokey/cli/exceptions.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright 2022 Nitrokey Developers
+#
+# Licensed under the Apache License, Version 2.0, <LICENSE-APACHE or
+# http://apache.org/licenses/LICENSE-2.0> or the MIT license <LICENSE-MIT or
+# http://opensource.org/licenses/MIT>, at your option. This file may not be
+# copied, modified, or distributed except according to those terms.
+
+from typing import Any
+
+from pynitrokey.helpers import local_critical
+
+
+class CliException(Exception):
+    def __init__(
+        self,
+        *messages: Any,
+        support_hint: bool = True,
+        ret_code: int = 1,
+        **kwargs: Any,
+    ) -> None:
+        super().__init__("\n".join([str(message) for message in messages]))
+
+        self.messages = messages
+        self.support_hint = support_hint
+        self.ret_code = ret_code
+        self.kwargs = kwargs
+
+    def show(self) -> None:
+        local_critical(
+            *self.messages,
+            support_hint=self.support_hint,
+            ret_code=self.ret_code,
+            **self.kwargs,
+        )

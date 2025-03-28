aebfdc199621657fa97d69de9081e00960e3415d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-28 16:04:31 +0000

    Add package ebpkg/0.0.1/ebpkg-0.0.1.tar.gz

---
+++ packages/ebpkg/ebpkg-0.0.1.tar.gz/ebpkg-0.0.1/readme.md
@@ -0,0 +1,6 @@
+<!--
+- SPDX-License-Identifier: GPL-2.0-only
+- Copyright (C) 2025 Jayesh Badwaik <j.badwaik@fz-juelich.de>
+-->
+
+# ebpkg - EasyBuild Package Manager
---
+++ packages/ebpkg/ebpkg-0.0.1.tar.gz/ebpkg-0.0.1/src/ebpkg/cli/__init__.py
@@ -0,0 +1,8 @@
+# ------------------------------------------------------------------------------
+# SPDX-License-Identifier: GPL-2.0-only
+# Copyright (C) 2025 Jayesh Badwaik <j.badwaik@fz-juelich.de>
+# ------------------------------------------------------------------------------
+
+
+def main():
+    print("Hello World!")

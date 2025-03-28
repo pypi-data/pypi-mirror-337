51dc82880270c50aa42b586f7953c3d28ebefe93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-28 07:04:11 +0000

    Add package mdai/0.15.1/mdai-0.15.1.tar.gz

---
+++ packages/mdai/mdai-0.15.1.tar.gz/mdai-0.15.1/mdai/__init__.py
@@ -0,0 +1,21 @@
+"""MD.ai Python client library."""
+
+try:
+    from importlib import metadata
+except ImportError:  # for Python<3.8
+    import importlib_metadata as metadata
+__version__ = metadata.version("mdai")
+
+from . import preprocess
+from .client import Client
+from .utils import common_utils
+from .utils import transforms
+from .utils import dicom_utils
+from .inference import delete_env, run_inference, infer
+
+try:
+    CAN_VISUALIZE = True
+    from . import visualize
+except ImportError:
+    # matplotlib backend missing or cannot be loaded
+    CAN_VISUALIZE = False

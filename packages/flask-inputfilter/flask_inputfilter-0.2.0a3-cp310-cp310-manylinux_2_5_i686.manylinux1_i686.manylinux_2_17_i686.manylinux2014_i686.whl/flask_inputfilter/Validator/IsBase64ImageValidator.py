9e5a71083d435fa6934001e375543d626bb0e432
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-28 17:05:16 +0000

    Add package flask-inputfilter/0.2.0a3/flask_inputfilter-0.2.0a3-cp310-cp310-manylinux_2_5_i686.manylinux1_i686.manylinux_2_17_i686.manylinux2014_i686.whl

---
+++ packages/flask-inputfilter/flask_inputfilter-0.2.0a3-cp310-cp310-manylinux_2_5_i686.manylinux1_i686.manylinux_2_17_i686.manylinux2014_i686.whl/flask_inputfilter/Validator/IsBase64ImageValidator.py
@@ -0,0 +1,32 @@
+import base64
+import io
+from typing import Any, Optional
+
+from PIL import Image
+
+from flask_inputfilter.Exception import ValidationError
+from flask_inputfilter.Validator import BaseValidator
+
+
+class IsBase64ImageValidator(BaseValidator):
+    """
+    Validator that checks if a Base64 string is a valid image.
+    """
+
+    def __init__(
+        self,
+        error_message: Optional[str] = None,
+    ) -> None:
+        self.error_message = error_message
+
+    def validate(self, value: Any) -> None:
+        try:
+            decodedData = base64.b64decode(value)
+            image = Image.open(io.BytesIO(decodedData))
+            image.verify()
+
+        except Exception:
+            raise ValidationError(
+                self.error_message
+                or "The image is invalid or does not have an allowed size."
+            )

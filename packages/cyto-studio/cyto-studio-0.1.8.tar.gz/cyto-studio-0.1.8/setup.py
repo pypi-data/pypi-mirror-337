4902355a949776b9edc72a765c2b1a800e6986ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-28 04:04:08 +0000

    Add package cyto-studio/0.1.8/cyto-studio-0.1.8.tar.gz

---
+++ packages/cyto-studio/cyto-studio-0.1.8.tar.gz/cyto-studio-0.1.8/setup.py
@@ -0,0 +1,49 @@
+"""Setup script for cyto-studio"""
+
+import os.path
+from setuptools import setup, find_packages
+
+# The directory containing this file
+HERE = os.path.abspath(os.path.dirname(__file__))
+
+# The text of the README file
+with open(os.path.join(HERE, "README.md")) as fid:
+    README = fid.read()
+
+# this grabs the requirements from requirements.txt
+#REQUIREMENTS = [i.strip() for i in open("requirements.txt").readlines()]
+
+# This call to setup() does all the work
+setup(
+    name="cyto-studio",
+    version="0.1.8",
+    description="napari viewer which can read multiplex images as zarr files",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/TristanWhitmarsh/cyto-studio",
+    author="Tristan Whitmarsh",
+    author_email="tw401@cam.ac.uk",
+    license="GNU",
+    classifiers=[
+        "License :: OSI Approved :: GNU Affero General Public License v3",
+        "Programming Language :: Python :: 3",
+    ],
+    packages=find_packages(),
+    include_package_data=True,
+    package_data={
+        "cyto_studio": ["custom.qss", "logo.ico"],
+    },
+    install_requires=[
+        'opencv-python-headless>=4.5.1.48',
+        'napari[pyside2]==0.4.17',
+        'PySide2==5.15.2.1',
+        'numpy==1.24.3',
+        'xarray==2023.4.2',
+        'zarr==2.14.2',
+        'SimpleITK==2.2.1',
+        'napari-animation==0.0.7',
+        'tifffile==2023.4.12',
+        'pyarrow==19.0.1'
+    ],
+    entry_points={"console_scripts": ["cyto-studio=cyto_studio.__main__:main"]},
+)

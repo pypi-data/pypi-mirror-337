fe1a2b62afbc4fceb8b09f1db032a754f302c284
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-28 05:03:57 +0000

    Add package compress-utils/0.4.4/compress_utils-0.4.4-cp36-cp36m-win_amd64.whl

---
+++ packages/compress-utils/compress_utils-0.4.4-cp36-cp36m-win_amd64.whl/compress_utils/__init__.py
@@ -0,0 +1,10 @@
+# __init__.py
+
+from .compress_utils_py import (
+    compress, 
+    decompress,
+    compressor,
+    Algorithm
+)
+
+__all__ = ['compress', 'decompress', 'compressor', 'Algorithm']

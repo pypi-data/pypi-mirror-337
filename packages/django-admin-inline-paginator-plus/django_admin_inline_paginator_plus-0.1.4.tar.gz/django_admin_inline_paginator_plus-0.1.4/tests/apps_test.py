07b897b05f6a4cd4756c04d306c81f4aec219d72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-28 05:03:55 +0000

    Add package django-admin-inline-paginator-plus/0.1.4/django_admin_inline_paginator_plus-0.1.4.tar.gz

---
+++ packages/django-admin-inline-paginator-plus/django_admin_inline_paginator_plus-0.1.4.tar.gz/django_admin_inline_paginator_plus-0.1.4/tests/apps_test.py
@@ -0,0 +1,20 @@
+import unittest
+
+from django.apps import AppConfig
+from django.utils.translation import gettext_lazy as _
+
+from django_admin_inline_paginator_plus.apps import DjangoAdminInlinePaginatorPlusConfig
+
+
+class TestDjangoAppConfig(unittest.TestCase):
+    def test_valid_subclass_appconfig(self):
+        self.assertEqual(issubclass(DjangoAdminInlinePaginatorPlusConfig, AppConfig), True)
+
+    def test_valid_name(self):
+        name = DjangoAdminInlinePaginatorPlusConfig.name
+        self.assertEqual(isinstance(name, str), True)
+        self.assertEqual(name, 'django_admin_inline_paginator_plus')
+
+    def test_valid_verbose_name(self):
+        verbose_name = DjangoAdminInlinePaginatorPlusConfig.verbose_name
+        self.assertEqual(verbose_name, _('Django Admin Inline Paginator Plus'))

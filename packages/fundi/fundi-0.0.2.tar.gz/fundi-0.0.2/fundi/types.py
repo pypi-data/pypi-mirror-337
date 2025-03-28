e5d203f1fef192b80af8dc4cc241bb2193fd3c55
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-28 16:04:57 +0000

    Add package fundi/0.0.2/fundi-0.0.2.tar.gz

---
+++ packages/fundi/fundi-0.0.2.tar.gz/fundi-0.0.2/fundi/types.py
@@ -0,0 +1,29 @@
+import typing
+from dataclasses import dataclass
+
+__all__ = ["R", "Parameter", "CallableInfo", "ParameterResult"]
+
+R = typing.TypeVar("R")
+
+
+@dataclass
+class Parameter:
+    name: str
+    annotation: type
+    from_: "CallableInfo | None"
+
+
+@dataclass
+class CallableInfo(typing.Generic[R]):
+    call: typing.Callable[..., typing.Any]
+    async_: bool
+    generator: bool
+    parameters: list[Parameter]
+
+
+@dataclass
+class ParameterResult:
+    parameter_name: str
+    value: typing.Any | None
+    dependency: CallableInfo | None
+    resolved: bool

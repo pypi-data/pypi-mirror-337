f344ca3f14349f13260fe3170ecc49864736facc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-28 06:03:59 +0000

    Add package jkr/0.9/jkr-0.9.tar.gz

---
+++ packages/jkr/jkr-0.9.tar.gz/jkr-0.9/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+setup(
+    name='jkr', 
+    version='0.9',
+    packages=find_packages(),
+    author='Muhammad',
+    author_email='virtualsteve786@gmail.com',
+    description='Simple',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',  
+        'Operating System :: OS Independent',
+        ]
+     )

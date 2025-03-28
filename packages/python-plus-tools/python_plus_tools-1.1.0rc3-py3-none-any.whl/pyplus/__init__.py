e887194bf8d33de49803b037218f5bcfad3cf305
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-28 03:15:59 +0000

    Add package python-plus-tools/1.1.0rc3/python_plus_tools-1.1.0rc3-py3-none-any.whl

---
+++ packages/python-plus-tools/python_plus_tools-1.1.0rc3-py3-none-any.whl/pyplus/__init__.py
@@ -0,0 +1,17 @@
+'''
+The python Plus - Pyplus
+================
+the python's plus library.\n
+'''
+
+from .tools.update import *
+from .tools.update import get_update_from_toml
+from . import science ,tools
+from sys import prefix
+
+__all__=["science" ,"tools" ,
+         "ALL" ,"NEW" ,"WILL" ,
+         "get_update" ,"get_version_update_time" ,"get_news_update_time" ,"get_new" ,"get_all" ,"get_will" ,"upload"
+        ]
+
+get_update_from_toml(prefix + "\\Lib\\site-packages\\pyplus\\update.toml",code_name = "main")

bac133158e630ac7ca48318586444d3131ff4711
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-28 14:04:33 +0000

    Add package PerSent/1.0.21/persent-1.0.21.tar.gz

---
+++ packages/PerSent/persent-1.0.21.tar.gz/persent-1.0.21/PerSent/__init__.py
@@ -0,0 +1,7 @@
+from .CommentAnalyzer import CommentAnalyzer
+from .WeightedSentimentAnalyzer import WeightedSentimentAnalyzer
+
+
+
+__version__ = "1.0.21"
+__all__ = ['CommentAnalyzer','WeightedSentimentAnalyzer']

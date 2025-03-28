2e6da82ee0e3ff08c0f6287261303c6230a36902
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-28 16:04:18 +0000

    Add package discord-framework/1.14.0/discord_framework-1.14.0-py3-none-any.whl

---
+++ packages/discord-framework/discord_framework-1.14.0-py3-none-any.whl/discord_framework/__init__.py
@@ -0,0 +1,4 @@
+from discord_framework.api import Apiserver
+from discord_framework.discord_framework import DiscordBot
+
+__all__ = ["DiscordBot", "Apiserver"]
---
+++ packages/discord-framework/discord_framework-1.14.0-py3-none-any.whl/discord_framework/api.py
@@ -0,0 +1,35 @@
+import logging
+from datetime import datetime
+
+from aiohttp import web
+from discord.ext import commands, tasks
+
+
+class Apiserver(commands.Cog):
+    def __init__(self, host: str, port: int) -> None:
+        self.app: web.Application = web.Application()
+        self.routes: web.RouteTableDef = web.RouteTableDef()
+
+        self.webserver_host: str = host
+        "The address the api responds to. e.g. 0.0.0.0"
+        self.webserver_port: int = port
+        "The port the api is listening on. e.g. 80"
+
+        self.logger = logging.getLogger("discord")
+
+        @self.routes.get("/heartbeat")
+        async def heartbeat(request) -> web.Response:
+            return web.Response(text=f"pong {datetime.now()}")
+
+    @tasks.loop()
+    async def run(self) -> None:
+        """Starts the api event loop. Only run during or after on_ready."""
+        self.app.add_routes(self.routes)
+        runner = web.AppRunner(self.app)
+        await runner.setup()
+
+        site = web.TCPSite(runner, host=self.webserver_host, port=self.webserver_port)
+        await site.start()
+        self.logger.info(
+            f"Discord API started on {self.webserver_host}:{self.webserver_port}"
+        )

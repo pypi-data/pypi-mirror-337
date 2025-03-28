da0da28eeacc844b0cc9bdf94e28e330e5c35906
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-28 06:04:01 +0000

    Add package polars-cloud/0.0.5/polars_cloud-0.0.5-cp39-abi3-win_amd64.whl

---
+++ packages/polars-cloud/polars_cloud-0.0.5-cp39-abi3-win_amd64.whl/polars_cloud/__init__.py
@@ -0,0 +1,55 @@
+"""Polars Cloud client.
+
+Enables users to interact with workspaces, clusters, and queries on Polars Cloud.
+"""
+
+from polars_cloud import exceptions
+from polars_cloud._version import __version__
+from polars_cloud.context import (
+    ComputeContext,
+    ComputeContextStatus,
+    set_compute_context,
+)
+from polars_cloud.polars_cloud import LogLevelSchema, login
+from polars_cloud.query import (
+    BatchQuery,
+    InteractiveQuery,
+    LazyFrameExt,
+    ParquetDst,
+    QueryInfo,
+    QueryResult,
+    QueryStatus,
+    spawn,
+    spawn_blocking,
+    spawn_many,
+    spawn_many_blocking,
+)
+from polars_cloud.workspace import (
+    Workspace,
+    WorkspaceDefaultComputeSpecs,
+    WorkspaceStatus,
+)
+
+__all__ = [
+    "BatchQuery",
+    "ComputeContext",
+    "ComputeContextStatus",
+    "InteractiveQuery",
+    "LazyFrameExt",
+    "LogLevelSchema",
+    "ParquetDst",
+    "QueryInfo",
+    "QueryResult",
+    "QueryStatus",
+    "Workspace",
+    "WorkspaceDefaultComputeSpecs",
+    "WorkspaceStatus",
+    "__version__",
+    "exceptions",
+    "login",
+    "set_compute_context",
+    "spawn",
+    "spawn_blocking",
+    "spawn_many",
+    "spawn_many_blocking",
+]

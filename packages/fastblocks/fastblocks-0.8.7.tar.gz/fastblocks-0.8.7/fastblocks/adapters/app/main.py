21e0f6b701edb904c451f3465e0fe27e7e79ace4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-28 03:16:06 +0000

    Add package fastblocks/0.8.7/fastblocks-0.8.7.tar.gz

---
+++ packages/fastblocks/fastblocks-0.8.7.tar.gz/fastblocks-0.8.7/fastblocks/adapters/app/main.py
@@ -0,0 +1,72 @@
+import typing as t
+from contextlib import asynccontextmanager
+from time import perf_counter
+
+from acb.adapters import get_adapter, import_adapter
+from acb.config import Config
+from acb.depends import depends
+from fastblocks.applications import FastBlocks
+
+from ._base import AppBase, AppBaseSettings
+
+main_start = perf_counter()
+
+Auth, Monitoring, Storage, Sql = import_adapter()  # type: ignore
+
+
+class AppSettings(AppBaseSettings):
+    url: str = "http://localhost:8000"
+
+    @depends.inject
+    def __init__(self, config: Config = depends(), **data: t.Any) -> None:
+        super().__init__(**data)
+        self.url = self.url if not config.deployed else f"https://{self.domain}"
+
+
+class App(FastBlocks, AppBase):
+    def __init__(self) -> None:
+        super().__init__(lifespan=self.lifespan)
+
+    async def init(self) -> None:
+        self.templates = depends.get(import_adapter("templates")).app
+        self.routes.extend(depends.get(import_adapter("routes")).routes)
+
+    @asynccontextmanager
+    @depends.inject
+    async def lifespan(
+        self,
+        app: FastBlocks,
+        auth: Auth = depends(),
+    ) -> t.AsyncGenerator[None, None]:
+        if get_adapter("admin").enabled:
+            admin = depends.get(import_adapter("admin"))
+            admin.__init__(
+                app,
+                engine=depends.get(import_adapter("sql")).engine,
+                title=self.config.admin.title,
+                debug=self.config.debug.admin,
+                base_url=self.config.admin.url,
+                logo_url=self.config.admin.logo_url,
+                authentication_backend=auth,
+            )
+            self.router.routes.insert(0, self.router.routes.pop())
+
+        async def post_startup() -> None:
+            if not self.config.deployed:
+                from aioconsole import aprint
+                from pyfiglet import Figlet
+
+                fig = Figlet(font="slant", width=90, justify="center")
+                await aprint(f"\n\n{fig.renderText(self.config.app.name.upper())}\n")
+            if not self.config.debug.production and self.config.deployed:
+                self.logger.info("Entering production mode...")
+
+        await post_startup()
+        main_start_time = perf_counter() - main_start
+        self.logger.warning(f"App started in {main_start_time} s")
+        yield
+        self.logger.critical("Application shut down")
+        await self.logger.complete()
+
+
+depends.set(App)
---
+++ packages/fastblocks/fastblocks-0.8.7.tar.gz/fastblocks-0.8.7/fastblocks/exceptions.py
@@ -0,0 +1,20 @@
+from acb.depends import depends
+from asgi_htmx import HtmxRequest
+from starlette.exceptions import HTTPException
+from starlette.responses import PlainTextResponse
+
+
+async def handle_exception(request: HtmxRequest, exc: HTTPException):
+    responses = {404: "Content not found", 500: "Server error"}
+    templates = depends.get()
+    status_code = getattr(exc, "status_code", 500)
+    if request.scope["htmx"]:
+        return PlainTextResponse(
+            content=responses[status_code], status_code=status_code
+        )
+    return await templates.app.render_template(
+        request,
+        "index.html",
+        status_code=status_code,
+        context=dict(page=str(status_code)),
+    )

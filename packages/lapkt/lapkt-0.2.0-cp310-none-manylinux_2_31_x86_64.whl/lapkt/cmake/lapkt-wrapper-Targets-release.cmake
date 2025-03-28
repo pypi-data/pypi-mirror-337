6d4056b8d4ab8b94b01c1907f20440610e2b4d10
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-28 05:05:03 +0000

    Add package lapkt/0.2.0/lapkt-0.2.0-cp310-none-manylinux_2_31_x86_64.whl

---
+++ packages/lapkt/lapkt-0.2.0-cp310-none-manylinux_2_31_x86_64.whl/lapkt/cmake/lapkt-wrapper-Targets-release.cmake
@@ -0,0 +1,19 @@
+#----------------------------------------------------------------
+# Generated CMake target import file for configuration "Release".
+#----------------------------------------------------------------
+
+# Commands may need to know the format version.
+set(CMAKE_IMPORT_FILE_VERSION 1)
+
+# Import target "wrapper" for configuration "Release"
+set_property(TARGET wrapper APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(wrapper PROPERTIES
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lapkt/core/lib/wrapper.so"
+  IMPORTED_SONAME_RELEASE "wrapper.so"
+  )
+
+list(APPEND _cmake_import_check_targets wrapper )
+list(APPEND _cmake_import_check_files_for_wrapper "${_IMPORT_PREFIX}/lapkt/core/lib/wrapper.so" )
+
+# Commands beyond this point should not need to know the version.
+set(CMAKE_IMPORT_FILE_VERSION)

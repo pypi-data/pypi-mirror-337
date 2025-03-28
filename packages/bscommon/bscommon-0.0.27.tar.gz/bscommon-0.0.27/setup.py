ff77b2b28b6fb99c9fa08fdec2018bbe038cee2f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-28 16:05:11 +0000

    Add package bscommon/0.0.27/bscommon-0.0.27.tar.gz

---
+++ packages/bscommon/bscommon-0.0.27.tar.gz/bscommon-0.0.27/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+with open("version.txt") as f:
+    version = f.read()
+    vs=version.split(".")
+    vs[-1]=str(int(vs[-1])+1)
+    version=".".join(vs)
+with open("version.txt","w") as f:
+    f.write(version)
+
+setup(
+    name="bscommon",
+    version=version,
+    packages=find_packages(where="src"),
+    package_dir={'': 'src'},
+    install_requires=requirements,
+    author="bs",
+    description="冰鼠常用操作库"
+)

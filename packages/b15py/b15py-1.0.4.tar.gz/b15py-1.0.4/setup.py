aeec30688820c7d8ed5219781051d5855bc9c330
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-28 07:04:08 +0000

    Add package b15py/1.0.4/b15py-1.0.4.tar.gz

---
+++ packages/b15py/b15py-1.0.4.tar.gz/b15py-1.0.4/setup.py
@@ -0,0 +1,14 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="b15py",
+    packages=find_packages(include=["b15py"]),
+    version="1.0.4",
+    description="Ein Module um das B15F-Board mit Python zu programmieren.",
+    author="mmeiler-dev",
+    install_requires=[
+        "pyserial>=3.5",
+        "typeguard>=4.4.1",
+        "matplotlib>=3.10.1"
+    ]
+)

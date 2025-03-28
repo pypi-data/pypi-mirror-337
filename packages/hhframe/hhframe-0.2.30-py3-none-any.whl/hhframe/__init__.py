881a17466f8adca4e3655f6735f6ed990825f4d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-28 16:04:51 +0000

    Add package hhframe/0.2.30/hhframe-0.2.30-py3-none-any.whl

---
+++ packages/hhframe/hhframe-0.2.30-py3-none-any.whl/hhframe/__init__.py
@@ -0,0 +1,12 @@
+
+# -*- codeing = utf-8 -*-
+# @Time：2021/6/23 1:24
+# @Author：立树
+# @File：__init__.py.py
+# @IDE：PyCharm
+
+# from .hhQiniu import hhQiniu
+# from .hhMysql import hhMysql
+from .hhDouban import hhDouban
+
+# print("hello hhframe")

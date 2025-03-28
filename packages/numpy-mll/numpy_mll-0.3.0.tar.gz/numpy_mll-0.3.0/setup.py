571366343cb06c2f95dd54e0e780ae549c17c91b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-28 06:04:16 +0000

    Add package numpy-mll/0.3.0/numpy_mll-0.3.0.tar.gz

---
+++ packages/numpy-mll/numpy_mll-0.3.0.tar.gz/numpy_mll-0.3.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="numpy_mll",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[],
+    include_package_data=True,
+)

2d39e8b270477b2ab42e10cb2ef908eb7b23cfc9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-28 06:03:52 +0000

    Add package domible/0.1.16/domible-0.1.16.tar.gz

---
+++ packages/domible/domible-0.1.16.tar.gz/domible-0.1.16/src/dicli/__init__.py
@@ -0,0 +1,16 @@
+""" domible/src/dicli/__init__.py 
+"""
+
+# read version from installed package
+# using same version as domible as it's the main package 
+from importlib.metadata import version
+__version__ = version("domible")
+
+
+from dicli import main 
+
+def run():
+    main.run()
+
+
+## end of file

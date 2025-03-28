dc463a3773039e4ead9cacddc293e8be51a864b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-28 16:05:13 +0000

    Add package duckduckgo-ai-chat/0.0.8/duckduckgo_ai_chat-0.0.8.tar.gz

---
+++ packages/duckduckgo-ai-chat/duckduckgo_ai_chat-0.0.8.tar.gz/duckduckgo_ai_chat-0.0.8/duckduckgo_ai_chat/cli.py
@@ -0,0 +1,41 @@
+import argparse
+from duckduckgo_ai_chat.app import mainrun, MODELS
+
+
+def create_parser():
+    parser = argparse.ArgumentParser(
+        description="CLI to use DuckDuckGo AI Chat service from terminal",
+        formatter_class=argparse.RawTextHelpFormatter,
+    )
+    parser.add_argument(
+        "-m",
+        "--model",
+        choices=MODELS.keys(),
+        help="Select a model by key:\n "
+        + "\n ".join(f"{k}: {v}" for k, v in MODELS.items()),
+        default=None,
+    )
+
+    parser.add_argument(
+        "-y",
+        "--yes",
+        action="store_true",
+        help="If provided, means user accept terms of service.",
+    )
+
+    parser.add_argument(
+        "-q", "--query", help="First query to submit to the model", default=None
+    )
+
+    return parser
+
+
+def cli():
+    "CLI to use DuckDuckGo AI Chat service from terminal"
+    parser = create_parser()
+    args = parser.parse_args()
+    mycommand(args)
+
+
+def mycommand(args):
+    _ = mainrun(args)

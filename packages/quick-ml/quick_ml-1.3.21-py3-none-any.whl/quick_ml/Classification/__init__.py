06fcc57d936b2e83a04e7fdb0f0ecb7e9174bbfc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-28 16:04:32 +0000

    Add package quick-ml/1.3.21/quick_ml-1.3.21-py3-none-any.whl

---
+++ packages/quick-ml/quick_ml-1.3.21-py3-none-any.whl/quick_ml/Classification/__init__.py
@@ -0,0 +1,11 @@
+from .tfrecords_maker import *
+from .begin_tpu import *
+from .visualize_and_check_data import *
+from .load_models_quick import *
+from .training_predictions import *
+from .predictions import *
+from .augments import *
+from .k_fold_training import *
+
+#import quick_ml.data_preprocessing.remove_imbalance
+__version__ = '1.3.21'

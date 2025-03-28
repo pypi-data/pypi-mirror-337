28e8956b41b772b0656c26b912ea158eb2e9a618
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-28 14:04:15 +0000

    Add package deepspeed/0.16.5/deepspeed-0.16.5-cp310-cp310-win_amd64.whl

---
+++ packages/deepspeed/deepspeed-0.16.5-cp310-cp310-win_amd64.whl/deepspeed/git_version_info_installed.py
@@ -0,0 +1,6 @@
+version='0.16.5'
+git_hash='unknown'
+git_branch='unknown'
+installed_ops={'async_io': False, 'fused_adam': 1, 'cpu_adam': 1, 'cpu_adagrad': 1, 'cpu_lion': 1, 'evoformer_attn': False, 'fp_quantizer': False, 'fused_lamb': 1, 'fused_lion': 1, 'gds': False, 'transformer_inference': 1, 'inference_core_ops': 1, 'cutlass_ops': False, 'quantizer': 1, 'ragged_device_ops': False, 'ragged_ops': 1, 'random_ltd': 1, 'sparse_attn': False, 'spatial_inference': 1, 'transformer': 1, 'stochastic_transformer': 1}
+accelerator_name='cuda'
+torch_info={'version': '2.6', 'bf16_support': True, 'cuda_version': '12.4', 'nccl_version': '0.0', 'hip_version': '0.0'}

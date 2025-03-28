a06546645b818bd620f1157706ff4a9f78fd4d24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-28 14:04:15 +0000

    Add package py-posh/1.2.2/py_posh-1.2.2-py3-none-any.whl

---
+++ packages/py-posh/py_posh-1.2.2-py3-none-any.whl/posh/__init__.py
@@ -0,0 +1,9 @@
+from .posh import sh
+from .posh import Files
+PIPE = Files.PIPE
+VAR = Files.VAR
+NULL = Files.NULL
+DEFAULT = Files.DEFAULT
+STDIN = Files.STDIN
+STDOUT = Files.STDOUT
+STDERR = Files.STDERR

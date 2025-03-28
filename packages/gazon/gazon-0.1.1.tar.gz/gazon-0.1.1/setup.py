78c6d771829c50f50fb0120d8bbe5943f3ee4d4e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-28 04:04:25 +0000

    Add package gazon/0.1.1/gazon-0.1.1.tar.gz

---
+++ packages/gazon/gazon-0.1.1.tar.gz/gazon-0.1.1/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="gazon",
+    version="0.1.1",
+    author="RagnarekUA",
+    author_email="mrleyyt@gmail.com",
+    description="Парсер информации о товарах с Ozon",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/unionium/gazon",
+    packages=find_packages(),
+    install_requires=[
+        'selenium>=4.0',
+        'webdriver-manager>=3.0',
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+)

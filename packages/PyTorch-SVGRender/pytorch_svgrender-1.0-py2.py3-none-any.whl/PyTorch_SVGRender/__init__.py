e6562c2c503a9a34ddb29153154d746258154395
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-28 04:03:56 +0000

    Add package PyTorch-SVGRender/1.0/pytorch_svgrender-1.0-py2.py3-none-any.whl

---
+++ packages/PyTorch-SVGRender/pytorch_svgrender-1.0-py2.py3-none-any.whl/PyTorch_SVGRender/__init__.py
@@ -0,0 +1,8 @@
+"""PyTorch-SVGRender: A package for rendering SVGs using PyTorch."""
+
+# -*- coding: utf-8 -*-
+# Author: ximing
+# Copyright (c) 2023, XiMing Xing.
+# License: MPL-2.0 License
+
+__version__ = "1.0"

4fad1415695cb3a665fd4456a09adfa7a0a767c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-28 17:05:14 +0000

    Add package WildParse/0.1.2/WildParse-0.1.2.tar.gz

---
+++ packages/WildParse/WildParse-0.1.2.tar.gz/WildParse-0.1.2/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="WildParse",
+    version="0.1.2",
+    author="ThunderFoxYT, FBEmpire",
+    author_email="bulbapotato2.0@gmail.com",
+    description="это копия одной либы(специально не буду говорить какой), но без надоедливого высера в консоли при запуске(из разряда: ВАШИ ДАННЫЕ ПРИНАДЛЕЖАТ НЕ ВАМ А MICROSOFT, У У У У У Я ТУПАЯ МАКАКА, У У У Я НЕНАВИЖУ ВИНДУ)",
+    long_description=open("README.md", "r", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    url="",
+    packages=find_packages(),
+    install_requires=[
+        "requests>=2.25.1",
+        "beautifulsoup4>=4.9.3",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    include_package_data=True,
+    package_data={
+        '': ['*.txt', '*.md', '*.json'],
+        'tests': ['*.*'],
+    },
+)
---
+++ packages/WildParse/WildParse-0.1.2.tar.gz/WildParse-0.1.2/tests/testlib.py
@@ -0,0 +1,4 @@
+import WildParse as wb
+
+data = wb.getinfo("https://www.wildberries.ru/catalog/12345678/detail.aspx")
+print(data)

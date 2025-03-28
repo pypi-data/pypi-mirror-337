040254701d3005975d53e2c4938a25fe6d2d927f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-28 06:04:11 +0000

    Add package mycommand-tool/0.1.1/mycommand_tool-0.1.1-py3-none-any.whl

---
+++ packages/mycommand-tool/mycommand_tool-0.1.1-py3-none-any.whl/my_package/__init__.py
@@ -0,0 +1,10 @@
+"""
+My Package - A brief description of what your package does.
+"""
+
+__version__ = "0.1.0"
+
+# Import your main package components here
+# For example:
+# from .core import main_function
+# from .utils import helper_function 
---
+++ packages/mycommand-tool/mycommand_tool-0.1.1-py3-none-any.whl/mycommand/__init__.py
@@ -0,0 +1,7 @@
+"""
+Mycommand - A command-line tool for project management
+"""
+
+__version__ = "0.1.1"
+
+from .cli import cli 

a426fb8d3fbb737e284bc3fe1a259ca8f8608780
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-28 16:04:52 +0000

    Add package django-glue/0.8.2/django_glue-0.8.2-py3-none-any.whl

---
+++ packages/django-glue/django_glue-0.8.2-py3-none-any.whl/django_glue/context_processors.py
@@ -0,0 +1,15 @@
+import json
+
+from django.core.serializers.json import DjangoJSONEncoder
+
+from django_glue import constants
+from django_glue.conf import settings
+from django_glue.session import Session
+
+
+def django_glue(request):
+    return {
+        constants.VERSION_CONTEXT_NAME: constants.__VERSION__,
+        constants.KEEP_LIVE_INTERVAL_TIME_MILLISECONDS_CONTEXT_NAME: (settings.DJANGO_GLUE_KEEP_LIVE_EXPIRE_TIME_SECONDS / 2.2) * 1000,
+        constants.SESSION_DATA_CONTEXT_NAME: json.dumps(Session(request).session, cls=DjangoJSONEncoder),
+    }

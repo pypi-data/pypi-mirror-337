7a8ff70f83c0c5b214ae67364ba5113b5bf1b711
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-28 14:04:19 +0000

    Add package arpakitlib/1.8.62/arpakitlib-1.8.62.tar.gz

---
+++ packages/arpakitlib/arpakitlib-1.8.62.tar.gz/arpakitlib-1.8.62/arpakitlib/_arpakit_project_template_v_5/project/api/router/client/get_current_user_token.py
@@ -0,0 +1,42 @@
+import fastapi.requests
+from fastapi import APIRouter
+
+from project.api.authorize import APIAuthorizeData, api_authorize, require_user_token_dbm_api_authorize_middleware, \
+    require_api_key_dbm_api_authorize_middleware
+from project.api.schema.common import BaseRouteSO
+from project.api.schema.out.client.user import UserClient1SO
+from project.api.schema.out.client.user_token import UserTokenClient1SO
+from project.api.schema.out.common.error import ErrorCommonSO
+from project.sqlalchemy_db_.sqlalchemy_model import UserDBM
+
+
+class GetCurrentUserTokenClientRouteSO(BaseRouteSO, UserTokenClient1SO):
+    user: UserClient1SO
+
+
+api_router = APIRouter()
+
+
+@api_router.get(
+    "",
+    name="Get current user token",
+    status_code=fastapi.status.HTTP_200_OK,
+    response_model=GetCurrentUserTokenClientRouteSO | ErrorCommonSO,
+)
+async def _(
+        *,
+        request: fastapi.requests.Request,
+        response: fastapi.responses.Response,
+        api_auth_data: APIAuthorizeData = fastapi.Depends(api_authorize(middlewares=[
+            require_api_key_dbm_api_authorize_middleware(
+                require_active=True
+            ),
+            require_user_token_dbm_api_authorize_middleware(
+                require_active_user_token=True,
+                require_user_roles=[UserDBM.Roles.client]
+            )
+        ]))
+):
+    return GetCurrentUserTokenClientRouteSO.from_dbm(
+        simple_dbm=api_auth_data.user_token_dbm
+    )

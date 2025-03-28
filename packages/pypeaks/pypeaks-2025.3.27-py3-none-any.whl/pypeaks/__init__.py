181067461469a17e26c3cbc361b64fa28a2bc0ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-28 03:15:54 +0000

    Add package pypeaks/2025.3.27/pypeaks-2025.3.27-py3-none-any.whl

---
+++ packages/pypeaks/pypeaks-2025.3.27-py3-none-any.whl/pypeaks/__init__.py
@@ -0,0 +1,10 @@
+from .data import Data
+from .data import Data as Histogram
+from .intervals import Intervals
+from datetime import datetime
+
+__version__ = datetime.now().strftime("%Y.%m.%d")
+
+# del data
+# del intervals
+# del slope

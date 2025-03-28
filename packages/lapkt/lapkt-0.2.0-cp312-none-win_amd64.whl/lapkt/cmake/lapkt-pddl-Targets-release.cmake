9e3f63e67e10dba2a5eb6a31710e20ff55aaf2db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-28 05:05:05 +0000

    Add package lapkt/0.2.0/lapkt-0.2.0-cp312-none-win_amd64.whl

---
+++ packages/lapkt/lapkt-0.2.0-cp312-none-win_amd64.whl/lapkt/cmake/lapkt-pddl-Targets-release.cmake
@@ -0,0 +1,20 @@
+#----------------------------------------------------------------
+# Generated CMake target import file for configuration "Release".
+#----------------------------------------------------------------
+
+# Commands may need to know the format version.
+set(CMAKE_IMPORT_FILE_VERSION 1)
+
+# Import target "pddl" for configuration "Release"
+set_property(TARGET pddl APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(pddl PROPERTIES
+  IMPORTED_IMPLIB_RELEASE "${_IMPORT_PREFIX}/lapkt/core/lib/libpddl.dll.a"
+  IMPORTED_LINK_DEPENDENT_LIBRARIES_RELEASE "Python::Python;core;wrapper"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lapkt/core/lib/pddl.cp312-win_amd64.pyd"
+  )
+
+list(APPEND _cmake_import_check_targets pddl )
+list(APPEND _cmake_import_check_files_for_pddl "${_IMPORT_PREFIX}/lapkt/core/lib/libpddl.dll.a" "${_IMPORT_PREFIX}/lapkt/core/lib/pddl.cp312-win_amd64.pyd" )
+
+# Commands beyond this point should not need to know the version.
+set(CMAKE_IMPORT_FILE_VERSION)

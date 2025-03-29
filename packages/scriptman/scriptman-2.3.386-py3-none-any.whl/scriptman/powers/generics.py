66fb6fd90f8ec031da7e14359da531b539d2745c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-29 05:04:59 +0000

    Add package scriptman/2.3.386/scriptman-2.3.386-py3-none-any.whl

---
+++ packages/scriptman/scriptman-2.3.386-py3-none-any.whl/scriptman/powers/generics.py
@@ -0,0 +1,14 @@
+from typing import Any, Callable, Coroutine, TypeVar, Union
+
+# ⚙ Type variables for generic types
+T = TypeVar("T")  # For generic argument types
+R = TypeVar("R")  # For generic return types (if both T and R are used)
+
+# For synchronous functions
+SyncFunc = Callable[..., T]
+
+# For asynchronous functions
+AsyncFunc = Callable[..., Coroutine[Any, Any, T]]
+
+# For both sync and async functions
+Func = Union[SyncFunc[T], AsyncFunc[T]]

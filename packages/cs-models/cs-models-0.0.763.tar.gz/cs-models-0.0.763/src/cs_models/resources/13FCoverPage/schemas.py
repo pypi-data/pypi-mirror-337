a3e5884de7ec50708d95ca617faf6f457c65c563
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-29 14:05:43 +0000

    Add package cs-models/0.0.763/cs-models-0.0.763.tar.gz

---
+++ packages/cs-models/cs-models-0.0.763.tar.gz/cs-models-0.0.763/src/cs_models/resources/13FCoverPage/schemas.py
@@ -0,0 +1,25 @@
+from marshmallow import Schema, fields
+
+
+class CoverpageSchema(Schema):
+    ACCESSION_NUMBER = fields.String(required=True)
+    REPORTCALENDARORQUARTER = fields.Date(required=True)
+    ISAMENDMENT = fields.String()
+    AMENDMENTNO = fields.Integer()
+    AMENDMENTTYPE = fields.String()
+    CONFDENIEDEXPIRED = fields.String()
+    DATEDENIEDEXPIRED = fields.Date()
+    DATEREPORTED = fields.Date()
+    REASONFORNONCONFIDENTIALITY = fields.String()
+    FILINGMANAGER_NAME = fields.String(required=True)
+    FILINGMANAGER_STREET1 = fields.String()
+    FILINGMANAGER_STREET2 = fields.String()
+    FILINGMANAGER_CITY = fields.String()
+    FILINGMANAGER_STATEORCOUNTRY = fields.String()
+    FILINGMANAGER_ZIPCODE = fields.String()
+    REPORTTYPE = fields.String(required=True)
+    FORM13FFILENUMBER = fields.String()
+    CRDNUMBER = fields.String()
+    SECFILENUMBER = fields.String()
+    PROVIDEINFOFORINSTRUCTION5 = fields.String(required=True)
+    ADDITIONALINFORMATION = fields.String()

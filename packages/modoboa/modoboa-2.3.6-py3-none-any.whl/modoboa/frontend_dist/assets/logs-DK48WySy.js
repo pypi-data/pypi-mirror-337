99278fc163525b7b60290f586473d5947bfd6ab8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-29 03:15:48 +0000

    Add package modoboa/2.3.6/modoboa-2.3.6-py3-none-any.whl

---
+++ packages/modoboa/modoboa-2.3.6-py3-none-any.whl/modoboa/frontend_dist/assets/logs-DK48WySy.js
@@ -0,0 +1,1 @@
+import{ac as e}from"./index-Bbkf9DIc.js";const t="logs",a={getAuditTrail(s){return e.get(`/${t}/audit-trail/`,{params:s})},getMessages(s){return e.get(`/${t}/messages/`,{params:s})}};export{a as l};

d78d6a3583894f9a11ec187fd19d12917f8c0fdc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-29 06:05:18 +0000

    Add package minecraft-parser-table-von23334/0.0.2/minecraft_parser_table_von23334-0.0.2-py3-none-any.whl

---
+++ packages/minecraft-parser-table-von23334/minecraft_parser_table_von23334-0.0.2-py3-none-any.whl/recipeminecraft.py
@@ -0,0 +1,35 @@
+from http.client import responses
+
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+class RecipeScraper:
+    def __init__(self) -> None:
+        self.soup = None
+        self.data_frame = None
+    def scrape_url(self, url:str) -> None:
+        response = requests.get(url)
+        response.encoding = 'utf-8'
+        self.soup = BeautifulSoup(response.text, "html.parser")
+    def get_table_data(self) -> pd.DataFrame:
+        tables = self.soup.find_all("table")[1:]
+        headers = self.soup.find_all("h2")
+        data_tables = []
+        for i,table in enumerate(tables):
+            table_rows = table.find_all("tr")[1:]
+            data_tables.append({'table_name': headers[i].text,
+                                'table_data': []})
+            for row in table_rows:
+                cells = row.find_all('td')
+                data_tables[i]["table_data"].append({
+                    'name': cells[0].text,
+                    'ingredients': cells[1].text,
+                    'images': cells[2].find('img')['src'],
+                    'description': cells[3].text
+                })
+        self.data_frame = pd.DataFrame(data_tables)
+        return self.data_frame
+
+rs = RecipeScraper()
+rs.scrape_url('https://www.minecraftcrafting.info/')
+print(rs.get_table_data())

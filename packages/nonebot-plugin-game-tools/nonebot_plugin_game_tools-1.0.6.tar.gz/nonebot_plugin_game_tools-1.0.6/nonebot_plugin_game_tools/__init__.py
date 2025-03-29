d36eab2a262c191cb338a0d1ef6b7e7703bd24f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-29 15:05:19 +0000

    Add package nonebot-plugin-game-tools/1.0.6/nonebot_plugin_game_tools-1.0.6.tar.gz

---
+++ packages/nonebot-plugin-game-tools/nonebot_plugin_game_tools-1.0.6.tar.gz/nonebot_plugin_game_tools-1.0.6/nonebot_plugin_game_tools/__init__.py
@@ -0,0 +1,23 @@
+from nonebot import get_plugin_config, on_command
+from nonebot.plugin import PluginMetadata
+
+from .config import Config
+
+__plugin_meta__ = PluginMetadata(
+    name="nonebot_plugin_game_tools",
+    description="游戏工具插件.",
+    usage="菜单",
+    type="application",
+    homepage="https://github.com/snowrabbit-top/nonebot_plugin_game_tools",
+    config=Config,
+    supported_adapters={"~onebot.v11"},
+)
+
+config = get_plugin_config(Config)
+
+ping = on_command("ping")
+
+
+@ping.handle()
+async def _():
+    await ping.send("pong")

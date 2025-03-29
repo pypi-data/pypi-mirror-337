e3570e7b434a02370ce87d1913679b5f3f009133
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-29 14:05:01 +0000

    Add package kubed/2.1.2/kubed-2.1.2.tar.gz

---
+++ packages/kubed/kubed-2.1.2.tar.gz/kubed-2.1.2/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="kubed",
+    version="2.1.2",
+    packages=find_packages(),
+    include_package_data=True,
+    entry_points={
+        "console_scripts": [
+            "kubed-setup=kubed.cli:setup_command",
+            "kubed-completions-path=kubed.cli:completions_path_command",
+            "kubed-aliases-path=kubed.cli:aliases_path_command",
+        ],
+    },
+    author="Dale Yarborough",
+    author_email="daleyarborough@gmail.com",
+    description="A CLI tool for managing Kubernetes, Docker, Terraform, and Helm workflows",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/daleyarborough/kubed",
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    python_requires=">=3.6",
+)

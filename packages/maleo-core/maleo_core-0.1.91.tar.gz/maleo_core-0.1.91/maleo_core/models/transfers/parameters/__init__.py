62b818fb6d5ac1939278b990aa147d8861f0e49e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-29 15:05:07 +0000

    Add package maleo-core/0.1.91/maleo_core-0.1.91.tar.gz

---
+++ packages/maleo-core/maleo_core-0.1.91.tar.gz/maleo_core-0.1.91/maleo_core/models/transfers/parameters/__init__.py
@@ -0,0 +1,9 @@
+from __future__ import annotations
+from .general import GeneralParameters
+from .service import ServiceParameters
+from .client import ClientParameters
+
+class BaseParameters:
+    General = GeneralParameters
+    Service = ServiceParameters
+    Client = ClientParameters

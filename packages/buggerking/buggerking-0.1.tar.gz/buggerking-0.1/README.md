6a882c49c5cd6a9c352a155e7747e7428cb7a5cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-29 15:05:48 +0000

    Add package buggerking/0.1/buggerking-0.1.tar.gz

---
+++ packages/buggerking/buggerking-0.1.tar.gz/buggerking-0.1/README.md
@@ -0,0 +1,11 @@
+# BuggerKing
+
+👑 The royal debugger for serveless architecture.
+
+## Usage
+
+```python
+from buggerking import debug
+
+print(debug())
+```
---
+++ packages/buggerking/buggerking-0.1.tar.gz/buggerking-0.1/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name='buggerking',
+    version='0.1',
+    description='The royal debugger for your Python kingdom',
+    author='DogyunHyunseoKyeongyeon',
+    author_email='hyunseo0412@naver.com',
+    url='https://github.com/hyunseopythonl/buggerking',
+    license='MIT',
+    packages=['buggerking'],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+    ],
+    python_requires='>=3.6',
+    zip_safe=False
+)

2c847af46da6979cf0844ac95c0360797cffb195
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-29 04:04:42 +0000

    Add package livesession-ai/0.0.0.post0/livesession_ai-0.0.0.post0.tar.gz

---
+++ packages/livesession-ai/livesession_ai-0.0.0.post0.tar.gz/livesession_ai-0.0.0.post0/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='livesession-ai',
+    version='0.0.0-0',
+    packages=find_packages(),
+    description='',
+    author='LiveSession',
+    author_email='hello@livesession.io',
+#     url='https://github.com/yourusername/my_empty_package',
+    classifiers=[
+    ],
+    python_requires='>=3.6',
+)

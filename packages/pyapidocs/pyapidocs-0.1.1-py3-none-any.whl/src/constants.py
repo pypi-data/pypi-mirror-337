1721a77358d62d2e1a75d23725c8834fd6c68b1a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-29 04:04:49 +0000

    Add package pyapidocs/0.1.1/pyapidocs-0.1.1-py3-none-any.whl

---
+++ packages/pyapidocs/pyapidocs-0.1.1-py3-none-any.whl/src/constants.py
@@ -0,0 +1,10 @@
+documentation_message = {
+    "role": "system",
+    "content": "Write API documentation in markdown format based upon the code files provided.",
+}
+
+readme_message = {
+    "role": "system",
+    "content": """Write a readme file to document this project, include a table of contents, overview, features,
+    an installation and run guide with basic examples of how to use it""",
+}

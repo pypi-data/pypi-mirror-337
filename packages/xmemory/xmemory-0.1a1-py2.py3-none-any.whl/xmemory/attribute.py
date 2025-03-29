2491755487c9d7070e0d8dc9ed746652b74af64b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-29 04:04:43 +0000

    Add package xmemory/0.1a1/xmemory-0.1a1-py2.py3-none-any.whl

---
+++ packages/xmemory/xmemory-0.1a1-py2.py3-none-any.whl/xmemory/attribute.py
@@ -0,0 +1,15 @@
+# coding:utf-8
+
+from urllib.parse import urljoin
+
+__project__ = "xmemory"
+__version__ = "0.1.alpha.1"
+__description__ = "Memory usage analysis"
+__urlhome__ = "https://github.com/bondbox/xmemory/"
+__urlcode__ = __urlhome__
+__urldocs__ = __urlhome__
+__urlbugs__ = urljoin(__urlhome__, "issues")
+
+# author
+__author__ = "Mingzhe Zou"
+__author_email__ = "zoumingzhe@outlook.com"

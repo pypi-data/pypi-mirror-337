5ca110e8c52f44738cc9de5bef3ee165a84bf857
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-29 05:04:58 +0000

    Add package malevich-app/0.3.15/malevich_app-0.3.15.tar.gz

---
+++ packages/malevich-app/malevich_app-0.3.15.tar.gz/malevich_app-0.3.15/malevich_app/export/start.py
@@ -0,0 +1,16 @@
+import os
+import uvicorn
+import asyncio
+
+import malevich_app.export.secondary.const as C
+from malevich_app.export.processes.main import logs_streaming_restart
+
+
+if __name__ == "__main__":
+    C.IS_LOCAL = False
+    if C.IS_EXTERNAL:
+        os.makedirs(C.MOUNT_PATH, exist_ok=True)
+        os.makedirs(C.MOUNT_PATH_OBJ, exist_ok=True)
+    if C.LOGS_STREAMING:
+        asyncio.run(logs_streaming_restart(wait=False))
+    uvicorn.run("malevich_app.export.api.api:app", host="0.0.0.0", port=int(os.environ["PORT"]), loop="asyncio", reload=False, workers=1)

8abc14cfda00293365aa24bb2b374b3183f176f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-29 15:05:03 +0000

    Add package maleo-core/0.1.90/maleo_core-0.1.90.tar.gz

---
+++ packages/maleo-core/maleo_core-0.1.90.tar.gz/maleo_core-0.1.90/maleo_core/utils/exceptions.py
@@ -0,0 +1,15 @@
+from fastapi import Request, status
+from fastapi.exceptions import RequestValidationError
+from fastapi.responses import JSONResponse
+from starlette.exceptions import HTTPException as StarletteHTTPException
+from maleo_core.models import BaseSchemas
+
+async def validation_exception_handler(request:Request, exc:RequestValidationError):
+    return JSONResponse(status_code=status.HTTP_422_UNPROCESSABLE_ENTITY, content=BaseSchemas.Response.ValidationError(other=exc.errors()).model_dump())
+
+async def http_exception_handler(request:Request, exc:StarletteHTTPException):
+    if exc.status_code == 404:
+        return JSONResponse(status_code=status.HTTP_404_NOT_FOUND, content=BaseSchemas.Response.NotFoundError().model_dump())
+
+    #* Handle other HTTP exceptions normally
+    return None

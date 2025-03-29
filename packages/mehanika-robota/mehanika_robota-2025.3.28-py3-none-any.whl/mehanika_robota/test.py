546dc74ce407320ea76f77038194cfe908a971d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-29 06:05:09 +0000

    Add package mehanika-robota/2025.3.28/mehanika_robota-2025.3.28-py3-none-any.whl

---
+++ packages/mehanika-robota/mehanika_robota-2025.3.28-py3-none-any.whl/mehanika_robota/test.py
@@ -0,0 +1,7 @@
+import numpy as np
+import mehanika_robota.mehanika.kinematika as kin
+import mehanika_robota.mehanika.kretanje_krutog_tela as kkt
+
+rng = np.random.default_rng()
+
+print(rng.random(3))

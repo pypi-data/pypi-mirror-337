d488c28572f73793d2e52026c9703b59d7c82842
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-29 05:05:22 +0000

    Add package arve/0.2.7/arve-0.2.7.tar.gz

---
+++ packages/arve/arve-0.2.7.tar.gz/arve-0.2.7/setup.py
@@ -0,0 +1,34 @@
+import arve
+import os
+from   setuptools import find_packages, setup
+
+with open("README.md") as file:
+    long_description = file.read()
+
+with open(os.path.join(os.path.dirname(__file__), "requirements.txt")) as file:
+    install_requires = file.read().splitlines()
+
+setup(
+    name="arve",
+    version=arve.__version__,
+    author="Khaled Al Moulla",
+    author_email="khaled.almoulla@gmail.com",
+    description="Analyzing Radial Velocity Elements",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/almoulla/arve",
+    license="MIT License",
+    packages=find_packages(),
+    python_requires=">=3.10",
+    install_requires=install_requires,
+    classifiers=["Development Status :: 1 - Planning"    ,
+                 "Intended Audience :: Science/Research" ,
+                 "License :: OSI Approved :: MIT License",
+                 "Programming Language :: Python :: 3"   ,
+                 ],
+    include_package_data=True,
+    package_data={"arve": ["aux_data/masks/*.csv.zip",
+                           "aux_data/spectra/*.csv.zip",
+                           "aux_data/tellurics/*.csv.zip",
+                           "aux_data/wavelengths/*.csv.zip"]}
+)

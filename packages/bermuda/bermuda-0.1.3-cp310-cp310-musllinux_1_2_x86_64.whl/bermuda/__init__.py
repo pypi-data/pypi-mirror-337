9f049ea0da88aa70096efa36d5083665a9d540fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-29 04:04:43 +0000

    Add package bermuda/0.1.3/bermuda-0.1.3-cp310-cp310-musllinux_1_2_x86_64.whl

---
+++ packages/bermuda/bermuda-0.1.3-cp310-cp310-musllinux_1_2_x86_64.whl/bermuda/__init__.py
@@ -0,0 +1,13 @@
+from bermuda._bermuda import (
+    triangulate_path_edge,
+    triangulate_polygons_face,
+    triangulate_polygons_face_3d,
+    triangulate_polygons_with_edge,
+)
+
+__all__ = (
+    'triangulate_path_edge',
+    'triangulate_polygons_face',
+    'triangulate_polygons_face_3d',
+    'triangulate_polygons_with_edge',
+)

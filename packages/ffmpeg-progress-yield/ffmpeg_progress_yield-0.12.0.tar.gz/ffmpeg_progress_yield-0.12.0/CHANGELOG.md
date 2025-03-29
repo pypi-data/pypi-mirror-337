eea9f9da70b9dc62c6cf28066aba0a47c300ff83
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-29 06:05:53 +0000

    Add package ffmpeg-progress-yield/0.12.0/ffmpeg_progress_yield-0.12.0.tar.gz

---
+++ packages/ffmpeg-progress-yield/ffmpeg_progress_yield-0.12.0.tar.gz/ffmpeg_progress_yield-0.12.0/CHANGELOG.md
@@ -0,0 +1,288 @@
+# Changelog
+
+
+## v0.12.0 (2025-03-28)
+
+* Docs: add @scufre as a contributor.
+
+* Update readme.
+
+* Add support to send ffmpeg log (not progress) to a different file.
+
+* Disallow blank issues.
+
+
+## v0.11.3 (2024-12-11)
+
+* Fix: Clear and print progress when tqdm is not available.
+
+  This relates two printing sequence:
+
+  (1) Print one decimal place after printing two decimal places
+
+  0.74/100 -> 0.8/100 (1 less character)
+
+  (2) Print 100/100 after printing decimal progress
+
+  99.74/100 -> 100/100 (2 less characters)
+
+  99.8/100 -> 100/100 (1 less character)
+
+  So clear the line first, and print progress, showing for a moment, and then repeat, finally a newline. :)
+
+
+## v0.11.2 (2024-12-10)
+
+* Chore: One-liner progress when tqdm is not available.
+
+
+## v0.11.1 (2024-11-22)
+
+* Make setup name PEP 625 compliant.
+
+
+## v0.11.0 (2024-11-22)
+
+* Remove python 3.8, add 3.12, 3.13.
+
+
+## v0.10.0 (2024-11-18)
+
+* Fix: ensure percentage is always in float.
+
+
+## v0.9.1 (2024-09-16)
+
+* Fix: the logic of the option --progress-only.
+
+
+## v0.9.0 (2024-09-16)
+
+* Docs: add @LaunchLee as a contributor.
+
+* Chore: Add an option to print progress only in cli usage.
+
+  This option makes the output cleaner when doing batch conversions.
+
+* Code cleanup, refactoring.
+
+  To avoid having to juggle around variables
+
+
+## v0.8.1 (2024-09-09)
+
+* Fix type-checks.
+
+
+## v0.8.0 (2024-09-09)
+
+* Add async support.
+
+
+## v0.7.9 (2024-08-14)
+
+* Docs: add @ammgws as a contributor.
+
+* Fix typing error.
+
+* Chore: remove unused dependency.
+
+* Fix badge link.
+
+
+## v0.7.8 (2023-06-01)
+
+* Image handling.
+
+  Check if image2 inputs use looping or not, and set duration to infinity if needed
+
+
+## v0.7.6 (2023-05-30)
+
+* Always use duration_override if present.
+
+
+## v0.7.5 (2023-05-30)
+
+* Fix duration for multiple inputs, fixes #13.
+
+* Fix 'Test' string in tqdm.
+
+
+## v0.7.4 (2023-05-06)
+
+* Fix: round percentage numbers.
+
+
+## v0.7.3 (2023-05-05)
+
+* Update readme.
+
+* Add progress as percent, fixes #12.
+
+
+## v0.7.2 (2023-03-04)
+
+* Do not print input information when probing, addresses #10.
+
+
+## v0.7.1 (2023-02-24)
+
+* Fix types in CI.
+
+* Remove unneeded import.
+
+* Typo.
+
+* Docs: add @kskadart as a contributor.
+
+* Fix formatting.
+
+* Fix types.
+
+* Feat(ffprobe): FEAT-0001 try to get duration by ffprobe in case if loglevel=error.
+
+* Fix CI file.
+
+
+## v0.7.0 (2023-01-24)
+
+* Add duration override to API.
+
+* Remove manifest.in.
+
+* Add mypy settings.
+
+
+## v0.6.1 (2022-12-18)
+
+* Add py.typed.
+
+* Move API docs to existing section.
+
+
+## v0.6.0 (2022-12-17)
+
+* Link to API docs.
+
+* Add API docs.
+
+* Add export.
+
+* Bump requirements to python 3.8 or higher.
+
+* Document methods.
+
+* Remove unused import.
+
+* Docs: add @WyattBlue as a contributor.
+
+* Docs: add @slhck as a contributor.
+
+* Unhide to_ms.
+
+* Add type hints + simplify.
+
+* Add python CI badge.
+
+* Fix quit tests.
+
+* Add all-contributors.
+
+* Add pytest to dev requirements.
+
+* Add github workflows.
+
+* Formatting.
+
+* Fix a few type and formatting errors.
+
+
+## v0.5.0 (2022-12-12)
+
+* Add stderr callback method.
+
+* Update README.
+
+* Add graceful quit method.
+
+* Add a GIF in the readme.
+
+
+## v0.4.0 (2022-12-11)
+
+* Add a quit method, fixes #4.
+
+
+## v0.3.0 (2022-08-02)
+
+* Update python requirements.
+
+
+## v0.2.0 (2021-11-21)
+
+* Add a usage option.
+
+
+## v0.1.2 (2021-08-14)
+
+* Remove universal_newlines for Windows compat.
+
+
+## v0.1.1 (2021-07-01)
+
+* Remove stats_period option for backwards compatibility, fixes #2.
+
+
+## v0.1.0 (2021-06-30)
+
+* Format code with black.
+
+* Yield 0 in progress and improve logic.
+
+* Set universal_newlines to true and add kwargs support.
+
+* Increase stats period.
+
+* Document method.
+
+* Add typing.
+
+* Also check for 0 in output.
+
+* Update gitignore.
+
+* Drop python 3.5 support.
+
+* Update badge link.
+
+
+## v0.0.4 (2021-03-10)
+
+* Add python_requires to setup.py.
+
+
+## v0.0.3 (2021-03-06)
+
+* Remove release script.
+
+
+## v0.0.2 (2021-03-06)
+
+* Fix release script.
+
+* Remove support for older versions.
+
+* Format setup.py.
+
+* Remove requirement for command to start with ffmpeg.
+
+* Add link to similar project.
+
+* Add changelog.
+
+* Rename project.
+
+* Initial commit.
+
+
---
+++ packages/ffmpeg-progress-yield/ffmpeg_progress_yield-0.12.0.tar.gz/ffmpeg_progress_yield-0.12.0/test/test.py
@@ -0,0 +1,205 @@
+#!/usr/bin/env pytest
+import os
+import subprocess
+import sys
+
+import pytest
+
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), "../"))
+
+from ffmpeg_progress_yield import FfmpegProgress  # noqa: E402
+
+_TEST_ASSET = os.path.join(os.path.dirname(__file__), "test.mp4")
+
+
+class TestLibrary:
+    cmd = [
+        "ffmpeg",
+        "-i",
+        _TEST_ASSET,
+        "-c:v",
+        "libx264",
+        "-vf",
+        "scale=1920x1080",
+        "-preset",
+        "fast",
+        "-f",
+        "null",
+        "/dev/null",
+    ]
+
+    unknown_dur_cmd = [
+        "ffmpeg",
+        "-re",
+        "-f",
+        "lavfi",
+        "-i",
+        "testsrc=d=10",
+        "-t",
+        "5",
+        "-f",
+        "null",
+        "/dev/null",
+    ]
+
+    def test_library(self):
+        ff = FfmpegProgress(TestLibrary.cmd)
+        elapsed = 0
+        for progress in ff.run_command_with_progress():
+            print(f"{progress}/100")
+            assert progress >= elapsed
+            elapsed = progress
+        # assert that we get 100% progress
+        assert elapsed == 100
+
+    def test_unknown_dur(self):
+        ff = FfmpegProgress(TestLibrary.unknown_dur_cmd)
+        progresses = set([0.0])
+        for progress in ff.run_command_with_progress():
+            progresses.add(progress)
+        # assert that we get only 0 and 100% progress since we have no implicit duration
+        assert list(progresses) == [0, 100]
+
+    def test_manual_dur(self):
+        ff = FfmpegProgress(TestLibrary.unknown_dur_cmd)
+        progresses = set([0.0])
+        for progress in ff.run_command_with_progress(duration_override=5):
+            progresses.add(progress)
+        # assert that we get more than just 0 and 100
+        assert len(progresses) > 2
+
+    def test_getting_stderr(self):
+        ff = FfmpegProgress(TestLibrary.cmd)
+        for progress in ff.run_command_with_progress():
+            print(f"{progress}/100")
+            if progress > 0 and ff.stderr is not None:
+                assert len(ff.stderr) > 0
+                break
+
+    def test_quit(self):
+        ff = FfmpegProgress(TestLibrary.cmd)
+        proc = None
+        for progress in ff.run_command_with_progress():
+            print(f"{progress}/100")
+            if progress > 0:
+                proc = ff.process
+                ff.quit()
+                break
+        # expect that no ffmpeg process is running after this test after sleeping for 1 second
+        assert proc is not None
+        proc.wait()
+        assert proc.returncode == -9
+
+    def test_quit_gracefully(self):
+        ff = FfmpegProgress(TestLibrary.cmd)
+        proc = None
+        for progress in ff.run_command_with_progress():
+            print(f"{progress}/100")
+            if progress > 0 and ff.process is not None:
+                proc = ff.process
+                ff.quit_gracefully()
+                break
+        # expect that no ffmpeg process is running after this test after sleeping for 1 second
+        assert proc is not None
+        assert proc.returncode == 0
+
+    def test_stderr_callback(self):
+        def stderr_callback(line):
+            print(line)
+
+        ff = FfmpegProgress(TestLibrary.cmd)
+        ff.set_stderr_callback(stderr_callback)
+        for progress in ff.run_command_with_progress():
+            print(f"{progress}/100")
+            if progress > 0:
+                break
+
+    def test_progress_with_loglevel_error(self):
+        cmd = TestLibrary.cmd + ["-loglevel", "error"]
+        ff = FfmpegProgress(cmd)
+        for progress in ff.run_command_with_progress():
+            print(f"{progress}/100")
+            if progress > 0:
+                assert 0 < progress < 100
+                break
+
+    def test_stderr_without_progress(self):
+        ff = FfmpegProgress(TestLibrary.cmd, exclude_progress=True)
+        for progress in ff.run_command_with_progress():
+            print(f"{progress}/100")
+            if progress > 0 and ff.stderr is not None:
+                assert len(ff.stderr) > 0
+                break
+        assert "out_time=" not in ff.stderr
+
+    def test_stderr_with_progress(self):
+        ff = FfmpegProgress(TestLibrary.cmd, exclude_progress=False)
+        for progress in ff.run_command_with_progress():
+            print(f"{progress}/100")
+            if progress > 0 and ff.stderr is not None:
+                assert len(ff.stderr) > 0
+                break
+        assert "out_time=" in ff.stderr
+
+class TestProgress:
+    def test_progress(self):
+        cmd = [
+            "python3",
+            "-m",
+            "ffmpeg_progress_yield",
+            "ffmpeg",
+            "-i",
+            _TEST_ASSET,
+            "-c:v",
+            "libx264",
+            "-preset",
+            "fast",
+            "-f",
+            "null",
+            "/dev/null",
+        ]
+        ret = subprocess.run(cmd, capture_output=True, universal_newlines=True)
+        assert "0/100" in ret.stderr
+        assert "100.0/100" in ret.stderr or "100/100" in ret.stderr
+
+
+class TestAsyncLibrary:
+    cmd = TestLibrary.cmd
+
+    @pytest.mark.asyncio
+    async def test_async_library(self):
+        ff = FfmpegProgress(TestAsyncLibrary.cmd)
+        elapsed = 0
+        async for progress in ff.async_run_command_with_progress():
+            print(f"{progress}/100")
+            assert progress >= elapsed
+            elapsed = progress
+        # assert that we get 100% progress
+        assert elapsed == 100
+
+    @pytest.mark.asyncio
+    async def test_async_quit(self):
+        ff = FfmpegProgress(TestAsyncLibrary.cmd)
+        proc = None
+        async for progress in ff.async_run_command_with_progress():
+            print(f"{progress}/100")
+            if progress > 0:
+                proc = ff.process
+                await ff.async_quit()
+                break
+        assert proc is not None
+        await proc.wait()
+        assert proc.returncode != 0
+
+    @pytest.mark.asyncio
+    async def test_async_quit_gracefully(self):
+        ff = FfmpegProgress(TestAsyncLibrary.cmd)
+        proc = None
+        async for progress in ff.async_run_command_with_progress():
+            print(f"{progress}/100")
+            if progress > 0 and ff.process is not None:
+                proc = ff.process
+                await ff.async_quit_gracefully()
+                break
+        assert proc is not None
+        assert proc.returncode == 0

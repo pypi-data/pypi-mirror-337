9ad017bfd510fa355a436aa230901776ddb3a13c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-29 15:05:08 +0000

    Add package maleo-core/0.1.92/maleo_core-0.1.92-py3-none-any.whl

---
+++ packages/maleo-core/maleo_core-0.1.92-py3-none-any.whl/maleo_core/models/transfers/results/service.py
@@ -0,0 +1,7 @@
+from __future__ import annotations
+from maleo_core.models.transfers.results.general import GeneralResults
+
+class ServiceResults:
+    Fail = GeneralResults.Fail
+    SingleData = GeneralResults.SingleData
+    MultipleData = GeneralResults.MultipleData

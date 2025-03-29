c83e2e503874f3b2b0b4dab6d5e0536a1dd5a2a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-29 03:14:49 +0000

    Add package imio.events.core/1.2.31/imio.events.core-1.2.31-py3-none-any.whl

---
+++ packages/imio.events.core/imio.events.core-1.2.31-py3-none-any.whl/imio/events/core/browser/json_recurrence.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+from imio.events.core.utils import remove_zero_interval_from_recrule
+from plone.formwidget.recurrence.browser.json_recurrence import RecurrenceView
+
+import logging
+
+logger = logging.getLogger("imio.events.core")
+
+
+class LoggingRecurrenceView(RecurrenceView):
+    @property
+    def json_string(self):
+        logger.info(f"Event recurrence request: {self.request.form}")
+        if "rrule" in self.request.form:
+            rrule = self.request.form["rrule"]
+            # INTERVAL=0 is not allowed in RFC 5545
+            # See https://github.com/plone/plone.formwidget.recurrence/issues/39
+            self.request.form["rrule"] = remove_zero_interval_from_recrule(rrule)
+        return super(LoggingRecurrenceView, self).json_string

64e75b72583eb81f2fc02a11e998f73b9fb86631
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-29 03:14:31 +0000

    Add package datamorphers/1.0.3/datamorphers-1.0.3.tar.gz

---
+++ packages/datamorphers/datamorphers-1.0.3.tar.gz/datamorphers-1.0.3/datamorphers/base.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import logging
+from abc import ABC, abstractmethod
+from narwhals.typing import FrameT
+
+
+class DataMorpher(ABC):
+    def __init__(self):
+        pass
+
+    @abstractmethod
+    def _datamorph(self, df: FrameT) -> FrameT:
+        """Applies a transformation on the DataFrame."""
+        pass

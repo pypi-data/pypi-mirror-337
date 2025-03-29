7a1fedae3391f873f26bf70080fcc7fbc41c4bc6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-29 04:05:11 +0000

    Add package arquee/0.8/arquee-0.8.tar.gz

---
+++ packages/arquee/arquee-0.8.tar.gz/arquee-0.8/arquee/config.py
@@ -0,0 +1,6 @@
+from os.path import join
+
+base_folder = "arquee_utils"
+logs_folder = join(base_folder, "logs")
+datalake = join(base_folder, "data", "datalake")
+warehouse = join(base_folder, "data", "warehouse")

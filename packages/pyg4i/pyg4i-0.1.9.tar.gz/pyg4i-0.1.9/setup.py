bca2888cb1ef5d8c9790d300ab569147db4ac14b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-29 15:05:24 +0000

    Add package pyg4i/0.1.9/pyg4i-0.1.9.tar.gz

---
+++ packages/pyg4i/pyg4i-0.1.9.tar.gz/pyg4i-0.1.9/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pyg4i',
+    version='0.1.9',
+    author='pyg4i',
+    author_email='kigzobadi@gmail.com',
+    description='Более удобное использование g4f',
+    long_description=open('README.md', encoding='utf-8').read(),
+    long_description_content_type='text/markdown',
+    packages=find_packages(),
+    install_requires=[
+        'g4f',
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)

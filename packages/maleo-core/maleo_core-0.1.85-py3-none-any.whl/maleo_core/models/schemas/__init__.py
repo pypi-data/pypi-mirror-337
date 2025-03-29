4c263523e8d022047d298b2e173a7f8338d5355b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-29 04:05:19 +0000

    Add package maleo-core/0.1.85/maleo_core-0.1.85-py3-none-any.whl

---
+++ packages/maleo-core/maleo_core-0.1.85-py3-none-any.whl/maleo_core/models/schemas/__init__.py
@@ -0,0 +1,7 @@
+from __future__ import annotations
+from .request import BaseRequests
+from .response import BaseResponses
+
+class BaseSchemas:
+    Request = BaseRequests
+    Response = BaseResponses
---
+++ packages/maleo-core/maleo_core-0.1.85-py3-none-any.whl/maleo_core/models/transfers/results/__init__.py
@@ -0,0 +1,11 @@
+from __future__ import annotations
+from .general import GeneralResults
+from .controllers import ControllerResults
+from .service import ServiceResults
+from .query import QueryResults
+
+class BaseResults:
+    General = GeneralResults
+    Controller = ControllerResults
+    Service = ServiceResults
+    Query = QueryResults

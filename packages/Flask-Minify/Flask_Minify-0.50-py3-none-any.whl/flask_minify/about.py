94f8b3d516b32b067d7bacbd3b1a405dc3cd9288
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-29 15:05:04 +0000

    Add package Flask-Minify/0.50/Flask_Minify-0.50-py3-none-any.whl

---
+++ packages/Flask-Minify/Flask_Minify-0.50-py3-none-any.whl/flask_minify/about.py
@@ -0,0 +1,6 @@
+__version__ = "0.50"
+__doc__ = "Flask extension to minify html, css, js and less."
+__license__ = "MIT"
+__author__ = "Mohamed Feddad"
+__email__ = "mrf345@gmail.com"
+__supported_versions__ = ["3.8", "3.9", "3.10", "3.11", "3.12", "3.13"]

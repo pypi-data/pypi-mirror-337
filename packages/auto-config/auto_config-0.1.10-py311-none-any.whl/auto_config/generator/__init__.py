c5042eb27a02e7eed1723c11b499d30bb396223b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-29 04:05:09 +0000

    Add package auto-config/0.1.10/auto_config-0.1.10-py311-none-any.whl

---
+++ packages/auto-config/auto_config-0.1.10-py311-none-any.whl/auto_config/generator/__init__.py
@@ -0,0 +1,5 @@
+from .ansible_hosts import AnsibleHostsGenerator
+from .base import Block, GeneratorBase
+from .caddy_file import CaddyFileGenerator
+from .dns_config import DNSConfigGenerator
+from .ssh_hosts import SSHHostsGenerator

ea25790216de3157651c48683c0be055da05b1d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-29 03:15:57 +0000

    Add package J4useragent/0.1.0/j4useragent-0.1.0.tar.gz

---
+++ packages/J4useragent/j4useragent-0.1.0.tar.gz/j4useragent-0.1.0/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="J4useragent",
+    version="0.1.0",
+    description="Generator User-Agent untuk berbagai perangkat",
+    author="Saepul Fajar",
+    author_email="s.fajar.id@gmail.com",
+    url="https://github.com/Fajarrrrky/J4useragent",  
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
+    classifiers=[
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.9',
+)

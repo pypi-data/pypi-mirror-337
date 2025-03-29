860234f5172b2926929a4f9a4b32e5cfcc928c2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-29 03:15:52 +0000

    Add package bapt/1.2.0/bapt-1.2.0-py2.py3-none-any.whl

---
+++ packages/bapt/bapt-1.2.0-py2.py3-none-any.whl/bapt/__init__.py
@@ -0,0 +1,230 @@
+# coding: utf-8
+# Copyright (c) Alex Ganose
+# Distributed under the terms of the MIT License.
+
+from .plotting import (pretty_plot, gbar, cbar, vb_cmap, cb_cmap, dashed_arrow, _linewidth, fadebar)
+
+from matplotlib.ticker import MaxNLocator, MultipleLocator
+from matplotlib.colors import LinearSegmentedColormap
+
+
+def get_plot(data, height=5, width=None, emin=None, colours=None,
+             bar_width=3, show_axis=False, label_size=15, plt=None, gap=0.5,
+             font=None, show_ea=False, name_colour='w', fade_cb=False, gradients=True, photocat=False):
+
+    width = (bar_width/2. + gap/2.) * len(data) if not width else width
+    emin = emin if emin else -max([d['ip'] for d in data]) - 2
+
+    plt = pretty_plot(width=width, height=height, plt=plt, fonts=[font])
+    ax = plt.gca()
+
+    pad = 2. / emin
+    for i, compound in enumerate(data):
+        x = i * (bar_width + gap)
+        ip = -compound['ip']
+        ea = -compound['ea']
+
+        fade = 'fade' in compound and compound['fade']
+        edge_c_cb = '#808080' if fade or fade_cb else 'k'
+        edge_c_vb = '#808080' if fade else 'k'
+        edge_z = 4 if fade else 5
+
+        if gradients:
+            vg = vb_cmap if 'vb_gradient' not in compound else \
+                compound['vb_gradient']
+            cg = cb_cmap if 'cb_gradient' not in compound else \
+                compound['cb_gradient']
+
+            gbar(ax, x, ip, bottom=emin, bar_width=bar_width, show_edge=True,
+                gradient=vg, edge_colour=edge_c_vb, edge_zorder=edge_z)
+            gbar(ax, x, ea, bar_width=bar_width, show_edge=True,
+                gradient=cg, edge_colour=edge_c_cb, edge_zorder=edge_z)
+
+        else: 
+            vc = '#219ebc' if 'vb_colour' not in compound else \
+                compound['vb_colour']
+            cc ='#fb8500' if 'cb_colour' not in compound else \
+                compound['cb_colour']
+
+            cbar(ax, x, ip, vc, bottom=emin, bar_width=bar_width, show_edge=True, 
+                edge_colour=edge_c_vb, edge_zorder=edge_z)
+            cbar(ax, x, ea, cc, bar_width=bar_width, show_edge=True, 
+                edge_colour=edge_c_vb, edge_zorder=edge_z)
+            
+        if show_ea:
+            dashed_arrow(ax, x + bar_width/6., ea - pad/3, 0,
+                         -ea + 2 * pad/3, colour='k', line_width=_linewidth)
+            dashed_arrow(ax, x + bar_width/6., ip - pad/3, 0,
+                         ea-ip + 2 * pad/3, colour='k', end_head=False,
+                         line_width=_linewidth)
+            ax.text(x + bar_width/4., ip - pad/2,
+                    '{:.1f} eV'.format(compound['ip']), ha='left',
+                    va='bottom', size=label_size, color='k', zorder=2)
+            ax.text(x + bar_width/4., pad * 2,
+                    '{:.1f} eV'.format(compound['ea']), ha='left', va='top',
+                    size=label_size, color='k', zorder=2)
+        else:
+            dashed_arrow(ax, x + bar_width/6., ip - pad/3, 0,
+                         -ip + 2 * pad/3, colour='k', line_width=_linewidth)
+            ax.text(x + bar_width/4., pad * 2,
+                    '{:.1f} eV'.format(compound['ip']), ha='left', va='top',
+                    size=label_size, color='k', zorder=2)
+
+        ax.text(x + bar_width/2., ip + pad, compound['name'], zorder=2,
+                ha='center', va='top', size=label_size, color=name_colour)
+
+        if fade:
+            fadebar(ax, x, emin, bar_width=bar_width, bottom=0)
+        elif fade_cb:
+            fadebar(ax, x, ea, bar_width=bar_width, bottom=0, zorder=1)
+    
+    ax.set_ylim((emin, 0))
+    ax.set_xlim((0, (len(data) * bar_width) + ((len(data) - 1) * gap)))
+
+    if photocat: 
+        end = (len(data) * bar_width) + ((len(data) - 1) * gap)
+        ax.hlines([-4.44, -5.67], 0, end, colors=['#808080', '#808080'], zorder=0, linewidths=1, linestyles='dotted', label=['H3O$^+$/H2', 'H$_2$O/O$_2$'])
+        plt.text(end+0.1, -4.44, '[H$^+$/H$_2$]', va='center', size=label_size)
+        plt.text(end+0.1, -5.67, '[H$_2$O/O$_2$]', va='center', size=label_size)
+
+    ax.set_xticks([])
+
+    if show_axis:
+        ax.set_ylabel("Energy (eV)", size=label_size)
+        ax.yaxis.set_major_locator(MaxNLocator(5))
+        for spine in ax.spines.values():
+            spine.set_zorder(5)
+    else:
+        for spine in ax.spines.values():
+            spine.set_visible(False)
+        ax.yaxis.set_visible(False)
+
+    ax.set_title('Vacuum Level', size=label_size)
+    ax.set_xlabel('Valence Band', size=label_size)
+    return plt
+
+
+def read_config(filename):
+    import yaml
+
+    with open(filename, 'r') as f:
+        config = yaml.safe_load(f)
+
+    settings = config['settings'] if 'settings' in config else {}
+    gradient_data = config['gradients'] if 'gradients' in config else []
+    gradients = {}
+    for d in gradient_data:
+        g = LinearSegmentedColormap.from_list(d['id'], [d['start'], d['end']],
+                                              N=200)
+        gradients[d['id']] = g
+
+    band_edge_data = config['compounds']
+    for compound in band_edge_data:
+        if 'gradient' in compound:
+            ids = list(map(int, compound['gradient'].split(',')))
+            compound.pop('gradient', None)
+            if len(ids) == 1:
+                compound['vb_gradient'] = gradients[ids[0]]
+                compound['cb_gradient'] = gradients[ids[0]]
+            else:
+                compound['vb_gradient'] = gradients[ids[0]]
+                compound['cb_gradient'] = gradients[ids[1]]
+    return band_edge_data, settings
+
+
+def get_plot_novac(data, height=5, width=None, emin=None, emax=None,
+                   colours=None, bar_width=3, show_axis=False, hide_cbo=False,
+                   hide_vbo=False, label_size=15, plt=None, gap=0.5, font=None,
+                   show_ea=False, name_colour='w', fade_cb=False, gradients=True):
+
+    width = (bar_width/2. + gap/2.) * len(data) if not width else width
+    emin = emin if emin else min([d['vbo'] for d in data]) - 2
+    emax = emax if emax else max([d['vbo'] + d['band_gap'] for d in data]) + 2
+
+    plt = pretty_plot(width=width, height=height, plt=plt, fonts=[font])
+    ax = plt.gca()
+
+    pad = - (emax - emin) / 20
+    for i, compound in enumerate(data):
+        x = i * (bar_width + gap)
+        ip = compound['vbo']
+        ea = compound['vbo'] + compound['band_gap']
+
+        fade = 'fade' in compound and compound['fade']
+        edge_c_cb = '#808080' if fade or fade_cb else 'k'
+        edge_c_vb = '#808080' if fade else 'k'
+        edge_z = 4 if fade else 5
+        
+        if gradients: 
+            vc = vb_cmap if 'vb_gradient' not in compound else \
+                compound['vb_gradient']
+            cc = cb_cmap if 'cb_gradient' not in compound else \
+                compound['cb_gradient']
+
+            gbar(ax, x, ip, bottom=emin, bar_width=bar_width, show_edge=True,
+                gradient=vc, edge_colour=edge_c_vb, edge_zorder=edge_z)
+            gbar(ax, x, ea, bottom=emax, bar_width=bar_width, show_edge=True,
+                gradient=cc, edge_colour=edge_c_cb, edge_zorder=edge_z)
+        else: 
+            vc = '#219ebc' if 'vb_colour' not in compound else \
+                compound['vb_colour']
+            cc ='#fb8500' if 'cb_colour' not in compound else \
+                compound['cb_colour']
+
+            cbar(ax, x, ip, vc, bottom=emin, bar_width=bar_width, show_edge=True,       
+                edge_colour=edge_c_vb, edge_zorder=edge_z)
+            cbar(ax, x, ea, cc, bottom=emax, bar_width=bar_width, show_edge=True, 
+                edge_colour=edge_c_vb, edge_zorder=edge_z)
+
+        dashed_arrow(ax, x + bar_width/6., ip - pad/3, 0,
+                     ea-ip + 2 * pad/3, colour='k', line_width=_linewidth)
+        ax.text(x + bar_width/4., ip + compound['band_gap']/2,
+                '{:.2f} eV'.format(compound['band_gap']), ha='left',
+                va='center', size=label_size, color='k', zorder=2)
+
+        t = ax.text(x + bar_width/2., ip + pad / 2, compound['name'], zorder=2,
+                    ha='center', va='top', size=label_size, color=name_colour)
+
+        # use renderer to get position of compound label text
+        renderer = plt.gcf().canvas.get_renderer()
+        bb = t.get_window_extent(renderer=renderer)
+        inv = ax.transData.inverted()
+        y1 = inv.transform([bb.y0, bb.y1 + 2 * bb.height])[1]
+        if i > 0:
+            if not hide_vbo:
+                vbo = compound['vbo'] - data[i-1]['vbo']
+                ax.text(x + bar_width/2., y1,
+                        "{:+.2f} eV".format(vbo), zorder=2, ha='center',
+                        va='top', size=label_size, color=name_colour)
+
+            if not hide_cbo:
+                cbo = compound['cbo'] - data[i-1]['cbo']
+                ax.text(x + bar_width/2., ea - pad / 4,
+                        "{:+.2f} eV".format(cbo), zorder=2, ha='center',
+                        va='bottom', size=label_size, color=name_colour)
+
+        if fade:
+            fadebar(ax, x, emin, bar_width=bar_width, bottom=emax)
+        elif fade_cb:
+            fadebar(ax, x, ea, bar_width=bar_width, bottom=emax, zorder=1)
+
+    ax.set_ylim((emin, emax))
+    ax.set_xlim((0, (len(data) * bar_width) + ((len(data) - 1) * gap)))
+    ax.set_xticks([])
+
+    if show_axis:
+        ax.set_ylabel("Energy (eV)", size=label_size)
+        ax.yaxis.set_major_locator(MultipleLocator(1))
+        for spine in ax.spines.values():
+            spine.set_zorder(5)
+        ax.tick_params(which='major', width=_linewidth)
+        ax.tick_params(which='minor', right='on')
+        ax.yaxis.set_minor_locator(MultipleLocator(0.5))
+    else:
+        for spine in ax.spines.values():
+            spine.set_visible(False)
+        ax.yaxis.set_visible(False)
+
+    ax.set_title('Conduction Band', size=label_size)
+    ax.set_xlabel('Valence Band', size=label_size)
+    return plt
---
+++ packages/bapt/bapt-1.2.0-py2.py3-none-any.whl/bapt/cli.py
@@ -0,0 +1,140 @@
+# coding: utf-8
+# Copyright (c) Alex Ganose
+# Distributed under the terms of the MIT License.
+
+import sys
+import argparse
+
+from . import read_config, get_plot, get_plot_novac
+
+__author__ = "Alex Ganose"
+__version__ = "0.1"
+__maintainer__ = "Alex Ganose"
+__email__ = "alexganose@googlemail.com"
+__date__ = "Oct 19, 2017"
+
+
+def main():
+    parser = argparse.ArgumentParser(description="""
+    Plotter for electronic band alignment diagrams.""",
+                                     epilog="""
+    Author: {}
+    Version: {}
+    Last updated: {}""".format(__author__, __version__, __date__))
+    parser.add_argument('-f', '--filename', default=None,
+                        help='Path to file containing alignment information.')
+    parser.add_argument('-n', '--name',
+                        help='List of compound names (comma seperated). ' +
+                        'Must be used in conguction with --ip and --ea. ' +
+                        'Use $_x$ and $^y$ for subscript and superscript.')
+    parser.add_argument('-i', '--ip',
+                        help='List of ionisation potentials (comma separated).')
+    parser.add_argument('-e', '--ea',
+                        help='List of electron affinities (comma separated).')
+    parser.add_argument('-c', '--cbo', default=None,
+                        help='List of conduction band offsets (comma separated). ' +
+                        '(Relative to first compound)(-> No vacuum alignment)')
+    parser.add_argument('-v', '--vbo', default=None,
+                        help='List of valence band offsets (comma separated). ' +
+                        '(Relative to first compound)(-> No vacuum alignment)')
+    parser.add_argument('-b', '--band-gap', dest='band_gap',
+                        help='List of band gaps (comma separated).')
+    parser.add_argument('-o', '--output', default='alignment.pdf',
+                        help='Output file name (defaults to alignment.pdf).')
+    parser.add_argument('--show-ea', action='store_true', dest='show_ea',
+                        help='Display the electron affinity value.')
+    parser.add_argument('--hide-cbo', action='store_true', dest='hide_cbo',
+                        help='Hide the conduction band offsets.')
+    parser.add_argument('--hide-vbo', action='store_true', dest='hide_vbo',
+                        help='Hide the valence band offsets.')
+    parser.add_argument('--show-axis', action='store_true', dest='show_axis',
+                        help='Display the energy yaxis bar and label.')
+    parser.add_argument('--height', type=float, default=5,
+                        help='Set figure height in inches.')
+    parser.add_argument('--width', type=float, default=None,
+                        help='Set figure width in inches.')
+    parser.add_argument('--emin', type=float, default=None,
+                        help='Set energy minium on y axis.')
+    parser.add_argument('--gap', type=float, default=0,
+                        help='Set gap between bars.')
+    parser.add_argument('--bar-width', type=float, dest='bar_width', default=3,
+                        help='Set the width per bar for each compound.')
+    parser.add_argument('--font', default=None, help='Font to use.')
+    parser.add_argument('--font-size', type=float, dest='label_size',
+                        default=15, help='Set font size all labels.')
+    parser.add_argument('--name-colour', dest='name_colour', default='w',
+                        help='Set the colour for the compound name.')
+    parser.add_argument('--fade-cb', action='store_true', dest='fade_cb',
+                        help='Apply a fade to the conduction band segments.')
+    parser.add_argument('--dpi', default=400,
+                        help='Dots-per-inch for file output.')
+    parser.add_argument('--no-gradient', action='store_false', dest='gradients',
+                        help='Plot the boxes as solid colours.')
+    parser.add_argument('--photocat', action='store_true',
+                         help='Plot water redox potentials')       
+    args = parser.parse_args()
+
+    emsg = None
+    if not args.filename and not (args.name or args.ip or args.ea or args.band_gap or args.cbo or args.vbo):
+        emsg = "ERROR: no arguments specified."
+    elif not args.filename and not (args.ip or args.ea) and \
+            not (args.name and args.band_gap and (args.cbo or args.vbo)):
+        emsg = "ERROR: --name, --band-gap and --cbo or --vbo flags must specified concurrently."
+    elif not args.filename and not (args.cbo or args.vbo or args.band_gap) and \
+            not (args.name and args.ip and args.ea):
+        emsg = "ERROR: --name, --ip and --ea flags must specified concurrently."
+    elif not args.filename and (args.cbo and args.vbo):
+        emsg = "ERROR: cbo and vbo specified simultaneously."
+    elif args.filename and (args.name or args.ip or args.ea or args.band_gap or args.cbo or args.vbo):
+        emsg = "ERROR: filename and name/ip/ea/cbo/vbo specified simultaneously."
+
+    if emsg:
+        print(emsg)
+        sys.exit()
+
+    output_file = args.output
+
+    if args.filename:
+        data, settings = read_config(args.filename)
+        for item in data:
+            if 'cbo' in item:
+                item['vbo'] = data[0]['band_gap'] - item['band_gap'] + item['cbo']
+            if 'vbo' in item:
+                item['cbo'] = -data[0]['band_gap'] + item['band_gap'] + item['vbo']
+
+    else:
+        for k, v in {'cbo': args.cbo, 'vbo': args.vbo}.items():
+            if v:
+                data = [{'name': name, 'band_gap': band_gap, k: c_or_v_bo} for name, band_gap, c_or_v_bo in
+                        zip(args.name.split(','), map(float, args.band_gap.split(',')),
+                            [0] + list(map(float, v.split(','))))]
+                for item in data:
+                    if k == 'cbo':
+                        item['vbo'] = data[0]['band_gap'] - item['band_gap'] + item['cbo']
+                    if k == 'vbo':
+                        item['cbo'] = -data[0]['band_gap'] + item['band_gap'] + item['vbo']
+        if args.ip:
+            data = [{'name': name, 'ip': ip, 'ea': ea} for name, ip, ea in
+                    zip(args.name.split(','), map(float, args.ip.split(',')),
+                        map(float, args.ea.split(',')))]
+
+        settings = {}
+
+    properties = vars(args)
+    remove_keys = ('filename', 'ip', 'ea', 'band_gap', 'cbo',
+                   'vbo', 'name', 'output', 'dpi')
+    for key in remove_keys:
+        properties.pop(key, None)
+    properties.update(settings)
+
+    if 'vbo' in data[0]:  # no vacuum alignment
+        [properties.pop(key, None) for key in ['photocat_hlines', 'photocat']]
+        plt = get_plot_novac(data, **properties)
+    else:
+        [properties.pop(key, None) for key in ['hide_cbo', 'hide_vbo']]
+        plt = get_plot(data, **properties)
+    plt.savefig(output_file, dpi=400, bbox_inches='tight')
+
+
+if __name__ == "__main__":
+    main()

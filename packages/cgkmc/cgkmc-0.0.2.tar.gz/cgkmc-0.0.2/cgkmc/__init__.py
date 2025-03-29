ca6586863447b79108cd852d8c145ce6ea0a448d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-29 15:05:33 +0000

    Add package cgkmc/0.0.2/cgkmc-0.0.2.tar.gz

---
+++ packages/cgkmc/cgkmc-0.0.2.tar.gz/cgkmc-0.0.2/cgkmc/__init__.py
@@ -0,0 +1,23 @@
+"""
+.. include:: ../README.md
+
+# Examples
+
+## 📦 Simple cubic growth
+
+Below is an example of the growth of a small simple cubic lattice, printing a LAMMPS-style dump string
+
+```py
+.. include:: ../examples/simple_cubic.py
+```
+
+"""
+
+__version__ = "0.0.2"
+__authors__ = ["Jacob Jeffries"]
+__author_emails__ = ["jwjeffr@clemson.edu"]
+__url__ = "https://github.com/jwjeffr/cgkmc"
+
+from . import containers as containers
+from . import simulations as simulations
+from . import utils as utils

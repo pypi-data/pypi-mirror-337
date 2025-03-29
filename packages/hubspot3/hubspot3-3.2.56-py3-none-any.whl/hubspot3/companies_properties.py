f83a86339f54a9bcabaab0c7ba21dd56fa6d6c07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-29 05:05:30 +0000

    Add package hubspot3/3.2.56/hubspot3-3.2.56-py3-none-any.whl

---
+++ packages/hubspot3/hubspot3-3.2.56-py3-none-any.whl/hubspot3/companies_properties.py
@@ -0,0 +1,34 @@
+"""
+hubspot companies properties api
+"""
+
+from typing import List, Optional, Union
+from hubspot3.base import BaseClient
+from hubspot3.utils import get_log
+
+
+COMPANIES_PROPERTIES_API_VERSION = "1"
+
+
+class CompaniesPropertiesClient(BaseClient):
+    """
+    The hubspot3 Companies Properties client uses the _make_request method to call the
+    API for data.  It returns a python object translated from the json returned.
+    """
+
+    def __init__(self, *args, **kwargs):
+        """initialize an companies properties client"""
+        super(CompaniesPropertiesClient, self).__init__(*args, **kwargs)
+        self.log = get_log("hubspot3.companies_properties")
+
+    def _get_path(self, subpath: str) -> str:
+        return f"properties/v{COMPANIES_PROPERTIES_API_VERSION}/companies/{subpath}"
+
+    def get_all_companies_properties(
+        self, extra_properties: Union[str, List, None] = None, **options
+    ) -> Optional[List]:
+        """
+        Retrieve all of the company properties, including their definition, for a given account.
+        :see: https://developers.hubspot.com/docs/methods/companies/get_company_properties
+        """
+        return self._call("properties", **options)

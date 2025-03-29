9adeb897c30cc3627cdf24470666ca837a9e7a30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-29 05:05:07 +0000

    Add package sl-detection/0.1.4/sl_detection-0.1.4-py3-none-any.whl

---
+++ packages/sl-detection/sl_detection-0.1.4-py3-none-any.whl/sl_detection/__init__.py
@@ -0,0 +1,8 @@
+# asl/__init__.py
+from asl.data.preprocessor import ASLPreprocessor
+from asl.detection.hand_detector import HandDetector
+from asl.models.coords_model import CoordsModel
+from asl.pipeline import ASLPipeline
+from asl.visualization.visualizer import ASLVisualizer
+
+__version__ = "0.1.4"

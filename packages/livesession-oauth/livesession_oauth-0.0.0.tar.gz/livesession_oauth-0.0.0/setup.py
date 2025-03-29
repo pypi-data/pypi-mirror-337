983c1f383be985e721737a879009fc4fb2fd6f70
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-29 04:04:41 +0000

    Add package livesession-oauth/0.0.0/livesession_oauth-0.0.0.tar.gz

---
+++ packages/livesession-oauth/livesession_oauth-0.0.0.tar.gz/livesession_oauth-0.0.0/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='livesession-oauth',
+    version='0.0.0',
+    packages=find_packages(),
+    description='LiveSession OAuth2 library for Python',
+    author='LiveSession',
+    author_email='hello@livesession.io',
+#     url='https://github.com/yourusername/my_empty_package',
+    classifiers=[
+    ],
+    python_requires='>=3.6',
+)

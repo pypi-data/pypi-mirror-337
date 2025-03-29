f05d0fc9aaa86ccc25fa25a623e0adabbf898674
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-29 14:06:23 +0000

    Add package mestra-jobs/0.1.1/mestra_jobs-0.1.1.tar.gz

---
+++ packages/mestra-jobs/mestra_jobs-0.1.1.tar.gz/mestra_jobs-0.1.1/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="mestra-jobs",
+    version="0.1.1",
+    packages=find_packages(),
+    author='Mestra',
+    keywords='mestra_job',
+    author_email='mestraac@gmail.com',
+    description='Lib responsavel por gerenciar task e jobs para executar determinadas tarefas',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://bitbucket.org/mestra1/jobs_lib',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+)

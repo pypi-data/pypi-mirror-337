0ef89dc79fbb5e442d74c75c6e7d0f72c6e5a340
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-29 15:05:07 +0000

    Add package cgkmc/0.0.1/cgkmc-0.0.1-py3-none-any.whl

---
+++ packages/cgkmc/cgkmc-0.0.1-py3-none-any.whl/cgkmc/__init__.py
@@ -0,0 +1,21 @@
+"""
+.. include:: ../README.md
+
+# Examples
+
+## 📦 Simple cubic growth
+
+```py
+.. include:: ../examples/simple_cubic.py
+```
+
+"""
+
+__version__ = "0.0.1"
+__authors__ = ["Jacob Jeffries"]
+__author_emails__ = ["jwjeffr@clemson.edu"]
+__url__ = "https://github.com/jwjeffr/cgkmc"
+
+from . import containers as containers
+from . import simulations as simulations
+from . import utils as utils

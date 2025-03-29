dd91319e5f594c191a7cdc970a47ab1a8722110f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-29 14:06:48 +0000

    Add package hafez/0.2.8/hafez-0.2.8.tar.gz

---
+++ packages/hafez/hafez-0.2.8.tar.gz/hafez-0.2.8/hafez/utils/db.py
@@ -0,0 +1,46 @@
+import pandas as pd
+import pathlib
+import os
+import json
+
+URI_DB_FOLDER = str(pathlib.Path(os.path.abspath(os.path.dirname(__file__))).parents[0].resolve())
+URI_JSON_DB = URI_DB_FOLDER + "/data/hafez.json"
+
+
+def filter_columns(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    This function filters the columns of the DataFrame.
+    :param df: DataFrame
+    :return: DataFrame with selected columns
+    """
+    columns = ["id", "poem", "interpretation", "alt_interpretation", "mp3"]
+    df = df[columns]
+    return df
+
+def get_data(id: int = None):
+
+    _, df = get_connection()
+
+    if id is not None:
+        df = df[df["id"] == id]
+
+    return df
+
+
+def search_data(query: str = None):
+
+    _, df = get_connection()
+    df['poem_string'] = [','.join(map(str, l)) for l in df['poem']]
+
+    if query is not None and len(query) > 0:
+        lst_query = query.split(" ")
+        df = df[df["poem_string"].str.contains('|'.join(lst_query))]
+
+    return df
+
+
+def get_connection():
+    with open(URI_JSON_DB, "r", encoding="utf8") as json_db:
+        json_data = json.load(json_db)
+    df = pd.DataFrame(json_data)
+    return json_data, df

b370ee8c659760972eb0aff0030c941cb83b783e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-29 14:05:32 +0000

    Add package onbbu/0.2.0/onbbu-0.2.0-py3-none-any.whl

---
+++ packages/onbbu/onbbu-0.2.0-py3-none-any.whl/onbbu/manager.pyi
@@ -0,0 +1,23 @@
+from argparse import ArgumentParser, Namespace
+from onbbu.database import database as database
+from onbbu.logger import LogLevel as LogLevel, logger as logger
+
+class BaseCommand:
+    name: str
+    help: str
+    def add_arguments(self, parser: ArgumentParser) -> None: ...
+    async def handler(self, args: Namespace) -> None: ...
+
+class MigrateCommand(BaseCommand):
+    name: str
+    help: str
+    async def handler(self, args: Namespace) -> None: ...
+
+class CreateModuleCommand(BaseCommand):
+    name: str
+    help: str
+    def add_arguments(self, parser: ArgumentParser) -> None: ...
+    async def handler(self, args: Namespace) -> None: ...
+
+async def menu_cli(description: str, commands: list[BaseCommand]) -> None: ...
+async def cli() -> None: ...

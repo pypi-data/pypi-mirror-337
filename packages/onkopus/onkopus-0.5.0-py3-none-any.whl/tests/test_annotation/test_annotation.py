a206c6fe88df1977a2275f8ca3ab9bd82ac0ac3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-29 04:04:45 +0000

    Add package onkopus/0.5.0/onkopus-0.5.0-py3-none-any.whl

---
+++ packages/onkopus/onkopus-0.5.0-py3-none-any.whl/tests/test_annotation/test_annotation.py
@@ -0,0 +1,16 @@
+import unittest, os
+import onkopus
+
+
+class TestCLIAnnotation(unittest.TestCase):
+#
+
+    def test_cli_annotation(self):
+        __location__ = os.path.realpath(
+            os.path.join(os.getcwd(), os.path.dirname(__file__)))
+        infile = __location__ + "/../test_files/somaticMutations.vcf"
+        outfile = infile + ".anno.csv"
+        bframe = onkopus.read_file(infile)
+        bframe.data = onkopus.annotate_variant_data(bframe.data)
+        onkopus.write_file(outfile,bframe)
+

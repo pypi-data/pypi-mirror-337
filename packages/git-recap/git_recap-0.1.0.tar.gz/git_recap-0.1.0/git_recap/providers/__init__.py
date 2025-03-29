8842a6afc9916475c9de4898a1a7950085a37001
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-29 14:05:17 +0000

    Add package git-recap/0.1.0/git_recap-0.1.0.tar.gz

---
+++ packages/git-recap/git_recap-0.1.0.tar.gz/git_recap-0.1.0/git_recap/providers/__init__.py
@@ -0,0 +1,9 @@
+from git_recap.providers.azure_fetcher import AzureFetcher
+from git_recap.providers.github_fetcher import GitHubFetcher
+from git_recap.providers.gitlab_fetcher import GitLabFetcher
+
+__all__ = [
+    "AzureFetcher",
+    "GitHubFetcher",
+    "GitLabFetcher"
+]

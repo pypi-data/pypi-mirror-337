46259fcb3b3e02912da326e732869f2b261fbb2d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-29 15:05:03 +0000

    Add package numerapi/2.20.1/numerapi-2.20.1.tar.gz

---
+++ packages/numerapi/numerapi-2.20.1.tar.gz/numerapi-2.20.1/numerapi/__init__.py
@@ -0,0 +1,15 @@
+""" Numerai Python API"""
+
+import pkg_resources
+
+try:
+    __version__ = pkg_resources.get_distribution(__name__).version
+except pkg_resources.DistributionNotFound:
+    __version__ = 'unknown'
+
+
+# pylint: disable=wrong-import-position
+from numerapi.numerapi import NumerAPI
+from numerapi.signalsapi import SignalsAPI
+from numerapi.cryptoapi import CryptoAPI
+# pylint: enable=wrong-import-position

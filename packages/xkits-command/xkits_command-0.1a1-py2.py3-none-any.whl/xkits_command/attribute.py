cc7e25cfe59a1e2a8b0b036a5d005c5e262dd84b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-29 15:05:26 +0000

    Add package xkits-command/0.1a1/xkits_command-0.1a1-py2.py3-none-any.whl

---
+++ packages/xkits-command/xkits_command-0.1a1-py2.py3-none-any.whl/xkits_command/attribute.py
@@ -0,0 +1,12 @@
+# coding:utf-8
+
+from urllib.parse import urljoin
+
+__project__ = "xkits-command"
+__version__ = "0.1.alpha.1"
+__description__ = "Command line module"
+__urlhome__ = "https://github.com/bondbox/xcommand/"
+
+# author
+__author__ = "Mingzhe Zou"
+__author_email__ = "zoumingzhe@outlook.com"

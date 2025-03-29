4ba42d9c5f8995be432ccd376074f69fe423e768
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-29 15:05:20 +0000

    Add package abadpour/6.103.1/abadpour-6.103.1.tar.gz

---
+++ packages/abadpour/abadpour-6.103.1.tar.gz/abadpour-6.103.1/abadpour/__init__.py
@@ -0,0 +1,13 @@
+NAME = "abadpour"
+
+ICON = "📜"
+
+DESCRIPTION = f"{ICON} Arash Abadpour's CV."
+
+VERSION = "6.103.1"
+
+ALIAS = NAME
+
+MARQUEE = (
+    "https://github.com/kamangir/assets/blob/main/abadpour/2020-11-21.jpg?raw=true"
+)
---
+++ packages/abadpour/abadpour-6.103.1.tar.gz/abadpour-6.103.1/abadpour/build.py
@@ -0,0 +1,9 @@
+from blue_objects import file
+from abadpour import VERSION
+
+
+def build() -> bool:
+    return file.save_text(
+        "_revision.tex",
+        ["\\vspace{0.5cm}revision\\space" + VERSION + "\\space-\\space\\today"],
+    )

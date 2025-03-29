4e631057c916fc9601df6f27d13e986489f0a9fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-29 14:06:36 +0000

    Add package metaflow-stubs/2.15.7/metaflow_stubs-2.15.7.tar.gz

---
+++ packages/metaflow-stubs/metaflow_stubs-2.15.7.tar.gz/metaflow_stubs-2.15.7/metaflow-stubs/plugins/azure/azure_exceptions.pyi
@@ -0,0 +1,24 @@
+######################################################################################################
+#                                 Auto-generated Metaflow stub file                                  #
+# MF version: 2.15.7                                                                                 #
+# Generated on 2025-03-29T00:30:30.608298                                                            #
+######################################################################################################
+
+from __future__ import annotations
+
+import metaflow
+import typing
+if typing.TYPE_CHECKING:
+    import metaflow.exception
+
+from ...exception import MetaflowException as MetaflowException
+
+class MetaflowAzureAuthenticationError(metaflow.exception.MetaflowException, metaclass=type):
+    ...
+
+class MetaflowAzureResourceError(metaflow.exception.MetaflowException, metaclass=type):
+    ...
+
+class MetaflowAzurePackageError(metaflow.exception.MetaflowException, metaclass=type):
+    ...
+
---
+++ packages/metaflow-stubs/metaflow_stubs-2.15.7.tar.gz/metaflow_stubs-2.15.7/metaflow-stubs/plugins/secrets/inline_secrets_provider.pyi
@@ -0,0 +1,25 @@
+######################################################################################################
+#                                 Auto-generated Metaflow stub file                                  #
+# MF version: 2.15.7                                                                                 #
+# Generated on 2025-03-29T00:30:30.598617                                                            #
+######################################################################################################
+
+from __future__ import annotations
+
+import metaflow
+import abc
+import typing
+if typing.TYPE_CHECKING:
+    import metaflow.plugins.secrets
+    import abc
+
+from . import SecretsProvider as SecretsProvider
+
+class InlineSecretsProvider(metaflow.plugins.secrets.SecretsProvider, metaclass=abc.ABCMeta):
+    def get_secret_as_dict(self, secret_id, options = {}, role = None):
+        """
+        Intended to be used for testing purposes only.
+        """
+        ...
+    ...
+

e252a887b34941622c511e59306d74d1f6dbcf27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-29 06:05:39 +0000

    Add package mixedbread/0.1.0a34/mixedbread-0.1.0a34.tar.gz

---
+++ packages/mixedbread/mixedbread-0.1.0a34.tar.gz/mixedbread-0.1.0a34/src/mixedbread/types/extractions/enhanced_json_schema.py
@@ -0,0 +1,11 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+
+from ..._models import BaseModel
+
+__all__ = ["EnhancedJsonSchema"]
+
+
+class EnhancedJsonSchema(BaseModel):
+    json_schema: object
+    """The enhanced JSON schema"""

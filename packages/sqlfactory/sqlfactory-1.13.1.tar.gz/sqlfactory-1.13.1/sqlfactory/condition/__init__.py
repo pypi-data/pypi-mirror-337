7c892f9e658f77314763634d2eaf5d57937256a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-29 15:05:34 +0000

    Add package sqlfactory/1.13.1/sqlfactory-1.13.1.tar.gz

---
+++ packages/sqlfactory/sqlfactory-1.13.1.tar.gz/sqlfactory-1.13.1/sqlfactory/condition/__init__.py
@@ -0,0 +1,45 @@
+"""Conditions for WHERE, ON, HAVING clauses in SQL statements."""
+
+from sqlfactory.condition.base import And, Condition, ConditionBase, Or
+from sqlfactory.condition.between import Between, NotBetween
+from sqlfactory.condition.in_condition import In, NotIn
+from sqlfactory.condition.like import Like, NotLike
+from sqlfactory.condition.simple import (
+    Eq,
+    Equals,
+    Ge,
+    GreaterThan,
+    GreaterThanOrEquals,
+    Gt,
+    Le,
+    LessThan,
+    LessThanOrEquals,
+    Lt,
+    Ne,
+    NotEquals,
+)
+
+__all__ = [
+    "And",
+    "Between",
+    "Condition",
+    "ConditionBase",
+    "Eq",
+    "Equals",
+    "Ge",
+    "GreaterThan",
+    "GreaterThanOrEquals",
+    "Gt",
+    "In",
+    "Le",
+    "LessThan",
+    "LessThanOrEquals",
+    "Like",
+    "Lt",
+    "Ne",
+    "NotBetween",
+    "NotEquals",
+    "NotIn",
+    "NotLike",
+    "Or",
+]

277c4914cb66edbf9dfff8b1733d4810260f2de0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-29 03:14:42 +0000

    Add package dinamis-sdk/0.5.2/dinamis_sdk-0.5.2-py3-none-any.whl

---
+++ packages/dinamis-sdk/dinamis_sdk-0.5.2-py3-none-any.whl/dinamis_sdk/__init__.py
@@ -0,0 +1,22 @@
+"""Dinamis SDK module."""
+
+# flake8: noqa
+
+from importlib.metadata import version, PackageNotFoundError
+from dinamis_sdk.signing import (
+    sign,
+    sign_inplace,
+    sign_urls,
+    sign_item,
+    sign_asset,
+    sign_item_collection,
+    sign_url_put,
+)  # noqa
+from .oauth2 import OAuth2Session  # noqa
+from .upload import push
+from .http import get_headers, get_userinfo, get_username
+
+try:
+    __version__ = version("dinamis_sdk")
+except PackageNotFoundError:
+    pass

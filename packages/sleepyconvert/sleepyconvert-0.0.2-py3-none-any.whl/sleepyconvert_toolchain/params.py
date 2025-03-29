826eba15e0895403bd77c327e4d905302e6c3253
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-29 14:06:12 +0000

    Add package sleepyconvert/0.0.2/sleepyconvert-0.0.2-py3-none-any.whl

---
+++ packages/sleepyconvert/sleepyconvert-0.0.2-py3-none-any.whl/sleepyconvert_toolchain/params.py
@@ -0,0 +1,11 @@
+supported_data_formats = [
+  'csv',
+  'json',
+  'parquet',
+  'pkl',
+  'xlsx',
+]
+
+supported_img_formats = [] # TODO: add supported image formats
+
+supported_doc_formats = [] # TODO: add supported document formats

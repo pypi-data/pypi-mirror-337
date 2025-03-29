59e46f200b48db3cb7c3278ab5f6c0f90b8b6068
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-29 04:04:51 +0000

    Add package goose-py/0.11.24/goose_py-0.11.24.tar.gz

---
+++ packages/goose-py/goose_py-0.11.24.tar.gz/goose_py-0.11.24/goose/__init__.py
@@ -0,0 +1,7 @@
+from goose._internal.agent import Agent
+from goose._internal.flow import FlowArguments, flow
+from goose._internal.result import Result, TextResult
+from goose._internal.task import task
+from goose._internal.types.telemetry import AgentResponse
+
+__all__ = ["Agent", "flow", "FlowArguments", "Result", "TextResult", "task", "AgentResponse"]

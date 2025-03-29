fc00b51246bd789db164145d08c7bf7877b069e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-29 14:05:22 +0000

    Add package agentcore/0.1.4/agentcore-0.1.4-py3-none-any.whl

---
+++ packages/agentcore/agentcore-0.1.4-py3-none-any.whl/agentcore/models.py
@@ -0,0 +1,18 @@
+from typing import Dict, Optional, List, Any
+
+from pydantic import BaseModel
+
+
+class V1UserProfile(BaseModel):
+    email: str
+    display_name: Optional[str] = None
+    handle: Optional[str] = None
+    picture: Optional[str] = None
+    organization: Optional[str] = None
+    role: Optional[str] = None
+    actor: Optional[str] = None
+    organizations: Optional[Dict[str, Dict[str, str]]] = None
+    created: Optional[int] = None
+    updated: Optional[int] = None
+    token: Optional[str] = None
+    entitlements: Optional[List[Dict[str, Any]]] = None

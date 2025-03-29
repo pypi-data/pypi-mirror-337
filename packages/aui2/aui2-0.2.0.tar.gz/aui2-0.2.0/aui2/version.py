7724d5b972dcd7c3b9c5eb199f0827ee36e4022e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-29 15:05:20 +0000

    Add package aui2/0.2.0/aui2-0.2.0.tar.gz

---
+++ packages/aui2/aui2-0.2.0.tar.gz/aui2-0.2.0/aui2/version.py
@@ -0,0 +1,9 @@
+PROJECT_NAME     = 'aui2'
+
+VER_MAJOR        = 0
+VER_MINOR        = 2
+VER_RELEASE      = 0
+
+VER_FLAGS        = ""
+
+__version__ = f'{VER_MAJOR}.{VER_MINOR}.{VER_RELEASE}{VER_FLAGS}'

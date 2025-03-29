f9e4847f156b16c0a167b6e275e1ca9594f5cb7c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-29 04:05:11 +0000

    Add package islamic-times/1.7.2/islamic_times-1.7.2.tar.gz

---
+++ packages/islamic-times/islamic_times-1.7.2.tar.gz/islamic_times-1.7.2/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='islamic_times',
+    version='1.7.2',
+    description='Various calculations for Islamic purposes',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Hassan Tahan',
+    author_email='contact@hassantahan.com',
+    url='https://github.com/hassantahan/islamic_times',
+    packages=find_packages(),
+    install_requires=[
+        'numpy', 'timezonefinder', 'pytz', 'matplotlib', 'shapely', 'geopandas'
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+    ],
+    python_requires='>=3.7',
+)

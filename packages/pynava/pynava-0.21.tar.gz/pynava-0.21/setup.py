56967359b4de6c7e1cb7b5edcaf62af15e440608
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-29 15:05:49 +0000

    Add package pynava/0.21/pynava-0.21.tar.gz

---
+++ packages/pynava/pynava-0.21.tar.gz/pynava-0.21/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="pynava",  # Numele bibliotecii tale
+    version="0.21",  # Versiunea bibliotecii tale
+    author="Brian",  # Numele autorului
+    author_email="codingc245@gmail.com",  # Adresa ta de email
+    description="This is a python library that aims to simplify code for beginners and experts alike.",  # O descriere scurtă a bibliotecii
+    long_description=open('README.md').read(),  # Descriere mai detaliată, preluată din fișierul README.md
+    long_description_content_type="text/markdown",  # Tipul de conținut al descrierii (Markdown în acest caz)
+    packages=find_packages(),  # Găsește toate pachetele Python din directorul curent
+    license="MIT",  # Specificarea licenței în sintaxa SPDX
+    python_requires='>=3.6',
+    install_requires=[
+        "matplotlib",
+        "numpy",
+        "nltk",
+        "scikit-learn",
+    ],
+)

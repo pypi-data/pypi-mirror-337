cbb891252ecfc62d21f49f768a012acc8cdcb3c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-29 06:04:44 +0000

    Add package netdots/0.1.5/netdots-0.1.5.tar.gz

---
+++ packages/netdots/netdots-0.1.5.tar.gz/netdots-0.1.5/netdots/__init__.py
@@ -0,0 +1,16 @@
+# netdots/__init__.py
+
+# Global API key that the user must set.
+api_key = None
+
+# (Optional) Expose the internal client if needed.
+from .api_client import Netdots
+
+# Dynamically expose integrations from the server
+try:
+    from server.netdots.oros.integrations import *
+    #except ImportError:
+except Exception as e:
+    print(f"WARNING: Integrations not found. Ensure the server integrations are properly set up. {e}")
+
+__all__ = ["api_key", "Netdots", "integrations"]

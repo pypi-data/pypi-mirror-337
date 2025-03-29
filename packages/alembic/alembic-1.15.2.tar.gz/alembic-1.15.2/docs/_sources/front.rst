2edda0987fc9486a1f160fd8e1e2d6ece16954fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-29 04:04:41 +0000

    Add package alembic/1.15.2/alembic-1.15.2.tar.gz

---
+++ packages/alembic/alembic-1.15.2.tar.gz/alembic-1.15.2/docs/_sources/front.rst
@@ -0,0 +1,132 @@
+============
+Front Matter
+============
+
+Information about the Alembic project.
+
+Project Homepage
+================
+
+Alembic is hosted on GitHub at https://github.com/sqlalchemy/alembic under the SQLAlchemy organization.
+
+Releases and project status are available on Pypi at https://pypi.python.org/pypi/alembic.
+
+The most recent published version of this documentation should be at https://alembic.sqlalchemy.org.
+
+
+.. _installation:
+
+Installation
+============
+
+While Alembic can be installed system wide, it's more common that it's
+installed local to a `virtual environment
+<https://docs.python.org/3/tutorial/venv.html>`_ , as it also uses libraries
+such as SQLAlchemy and database drivers that are more appropriate for
+local installations.
+
+The documentation below is **only one kind of approach to installing Alembic
+for a project**; there are many such approaches. The documentation below is
+provided only for those users who otherwise have no specific project setup
+chosen.
+
+To build a virtual environment for a specific project, first we assume that
+`Python virtualenv <https://pypi.org/project/virtualenv/>`_ is installed
+systemwide.  Then::
+
+    $ cd /path/to/your/project
+    $ virtualenv .venv
+
+There is now a Python interpreter that you can access in
+``/path/to/your/project/.venv/bin/python``, as well as the `pip
+<http://pypi.python.org/pypi/pip>`_ installer tool in
+``/path/to/your/project/.venv/bin/pip``.
+
+We now install Alembic as follows::
+
+    $ /path/to/your/project/.venv/bin/pip install alembic
+
+The install will add the ``alembic`` command to the virtual environment.  All
+operations with Alembic in terms of this specific virtual environment will then
+proceed through the usage of this command, as in::
+
+    $ /path/to/your/project/.venv/bin/alembic init alembic
+
+The next step is **optional**.   If our project itself has a ``setup.py``
+file, we can also install it in the local virtual environment in
+`editable mode <https://pip.pypa.io/en/stable/reference/pip_install/#editable-installs>`_::
+
+    $ /path/to/your/project/.venv/bin/pip install -e .
+
+If we don't "install" the project locally, that's fine as well; the default
+``alembic.ini`` file includes a directive ``prepend_sys_path = .`` so that the
+local path is also in ``sys.path``. This allows us to run the ``alembic``
+command line tool from this directory without our project being "installed" in
+that environment.
+
+As a final step, the `virtualenv activate <https://virtualenv.pypa.io/en/latest/userguide/#activate-script>`_
+tool can be used so that the ``alembic`` command is available without any
+path information, within the context of the current shell::
+
+    $ source /path/to/your/project/.venv/bin/activate
+
+Dependencies
+------------
+
+Alembic's install process will ensure that SQLAlchemy_
+is installed, in addition to other dependencies.  Alembic will work with
+SQLAlchemy as of version **1.4.0**.
+
+.. versionchanged:: 1.15.0 Support for SQLAlchemy older than 1.4.0 was dropped.
+
+Alembic supports Python versions **3.9 and above**
+
+.. versionchanged::  1.15  Alembic now supports Python 3.9 and newer.
+
+.. _versioning_scheme:
+
+Versioning Scheme
+-----------------
+
+Alembic's versioning scheme is based on that of
+`SQLAlchemy's versioning scheme <https://www.sqlalchemy.org/download.html#versions>`_.
+In particular, it should be noted that while Alembic uses a three-number
+versioning scheme, it **does not use SemVer**. In SQLAlchemy and Alembic's
+scheme, **the middle digit is considered to be a "Significant Minor Release",
+which may include removal of previously deprecated APIs with some risk of
+non-backwards compatibility in a very small number of cases**.
+
+This means that version "1.8.0", "1.9.0", "1.10.0", "1.11.0", etc. are
+**Significant Minor Releases**, which will include new API features and may
+remove or modify existing ones.
+
+Therefore, when `pinning <https://pip.pypa.io/en/stable/topics/repeatable-installs/>`_
+Alembic releases, pin to the "major" and "minor" digits to avoid API changes.
+
+A true "Major" release such as a change to "2.0" would include complete
+redesigns/re-architectures of foundational features; currently no such series
+of changes are planned, although changes such as replacing the entire
+"autogenerate" scheme with a new approach would qualify for that level of
+change.
+
+
+
+Community
+=========
+
+Alembic is developed by `Mike Bayer <http://techspot.zzzeek.org>`_, and is
+part of the SQLAlchemy_ project.
+
+User issues, discussion of potential bugs and features are most easily
+discussed using `GitHub Discussions <https://github.com/sqlalchemy/alembic/discussions/>`_.
+
+.. _bugs:
+
+Bugs
+====
+
+Bugs and feature enhancements to Alembic should be reported on the `GitHub
+issue tracker
+<https://github.com/sqlalchemy/alembic/issues/>`_.
+
+.. _SQLAlchemy: https://www.sqlalchemy.org
---
+++ packages/alembic/alembic-1.15.2.tar.gz/alembic-1.15.2/tests/test_config.py
@@ -0,0 +1,258 @@
+import os
+import tempfile
+
+from alembic import config
+from alembic import testing
+from alembic import util
+from alembic.migration import MigrationContext
+from alembic.operations import Operations
+from alembic.script import ScriptDirectory
+from alembic.testing import assert_raises_message
+from alembic.testing import eq_
+from alembic.testing import mock
+from alembic.testing.assertions import expect_raises_message
+from alembic.testing.env import _no_sql_testing_config
+from alembic.testing.env import _write_config_file
+from alembic.testing.env import clear_staging_env
+from alembic.testing.env import staging_env
+from alembic.testing.fixtures import capture_db
+from alembic.testing.fixtures import TestBase
+
+
+class FileConfigTest(TestBase):
+    def test_config_args(self):
+        cfg = _write_config_file(
+            """
+[alembic]
+migrations = %(base_path)s/db/migrations
+"""
+        )
+        test_cfg = config.Config(
+            cfg.config_file_name, config_args=dict(base_path="/home/alembic")
+        )
+        eq_(
+            test_cfg.get_section_option("alembic", "migrations"),
+            "/home/alembic/db/migrations",
+        )
+
+    def tearDown(self):
+        clear_staging_env()
+
+
+class ConfigTest(TestBase):
+    def test_config_no_file_main_option(self):
+        cfg = config.Config()
+        cfg.set_main_option("url", "postgresql://foo/bar")
+
+        eq_(cfg.get_main_option("url"), "postgresql://foo/bar")
+
+    def test_config_no_file_section_option(self):
+        cfg = config.Config()
+        cfg.set_section_option("foo", "url", "postgresql://foo/bar")
+
+        eq_(cfg.get_section_option("foo", "url"), "postgresql://foo/bar")
+
+        cfg.set_section_option("foo", "echo", "True")
+        eq_(cfg.get_section_option("foo", "echo"), "True")
+
+    def test_config_set_main_option_percent(self):
+        cfg = config.Config()
+        cfg.set_main_option("foob", "a %% percent")
+
+        eq_(cfg.get_main_option("foob"), "a % percent")
+
+    def test_config_set_section_option_percent(self):
+        cfg = config.Config()
+        cfg.set_section_option("some_section", "foob", "a %% percent")
+
+        eq_(cfg.get_section_option("some_section", "foob"), "a % percent")
+
+    def test_config_set_section_option_interpolation(self):
+        cfg = config.Config()
+        cfg.set_section_option("some_section", "foob", "foob_value")
+
+        cfg.set_section_option("some_section", "bar", "bar with %(foob)s")
+
+        eq_(
+            cfg.get_section_option("some_section", "bar"),
+            "bar with foob_value",
+        )
+
+    def test_standalone_op(self):
+        eng, buf = capture_db()
+
+        env = MigrationContext.configure(eng)
+        op = Operations(env)
+
+        op.alter_column("t", "c", nullable=True)
+        eq_(buf, ["ALTER TABLE t ALTER COLUMN c DROP NOT NULL"])
+
+    def test_no_script_error(self):
+        cfg = config.Config()
+        assert_raises_message(
+            util.CommandError,
+            "No 'script_location' key found in configuration.",
+            ScriptDirectory.from_config,
+            cfg,
+        )
+
+    def test_attributes_attr(self):
+        m1 = mock.Mock()
+        cfg = config.Config()
+        cfg.attributes["connection"] = m1
+        eq_(cfg.attributes["connection"], m1)
+
+    def test_attributes_constructor(self):
+        m1 = mock.Mock()
+        m2 = mock.Mock()
+        cfg = config.Config(attributes={"m1": m1})
+        cfg.attributes["connection"] = m2
+        eq_(cfg.attributes, {"m1": m1, "connection": m2})
+
+    @testing.combinations(
+        (
+            "legacy raw string 1",
+            None,
+            "/foo",
+            ["/foo"],
+        ),
+        (
+            "legacy raw string 2",
+            None,
+            "/foo /bar",
+            ["/foo", "/bar"],
+        ),
+        (
+            "legacy raw string 3",
+            "space",
+            "/foo",
+            ["/foo"],
+        ),
+        (
+            "legacy raw string 4",
+            "space",
+            "/foo /bar",
+            ["/foo", "/bar"],
+        ),
+        (
+            "multiline string 1",
+            "newline",
+            " /foo  \n/bar  ",
+            ["/foo", "/bar"],
+        ),
+        (
+            "Linux pathsep 1",
+            ":",
+            "/Project A",
+            ["/Project A"],
+        ),
+        (
+            "Linux pathsep 2",
+            ":",
+            "/Project A:/Project B",
+            ["/Project A", "/Project B"],
+        ),
+        (
+            "Windows pathsep 1",
+            ";",
+            r"C:\Project A",
+            [r"C:\Project A"],
+        ),
+        (
+            "Windows pathsep 2",
+            ";",
+            r"C:\Project A;C:\Project B",
+            [r"C:\Project A", r"C:\Project B"],
+        ),
+        (
+            "os pathsep",
+            "os",
+            r"path_number_one%(sep)spath_number_two%(sep)s"
+            % {"sep": os.pathsep},
+            [r"path_number_one", r"path_number_two"],
+        ),
+        (
+            "invalid pathsep 2",
+            "|",
+            "/foo|/bar",
+            ValueError(
+                "'|' is not a valid value for version_path_separator; "
+                "expected 'space', 'newline', 'os', ':', ';'"
+            ),
+        ),
+        id_="iaaa",
+        argnames="separator, string_value, expected_result",
+    )
+    def test_version_locations(self, separator, string_value, expected_result):
+        cfg = config.Config()
+        if separator is not None:
+            cfg.set_main_option(
+                "version_path_separator",
+                separator,
+            )
+        cfg.set_main_option("script_location", tempfile.gettempdir())
+        cfg.set_main_option("version_locations", string_value)
+
+        if isinstance(expected_result, ValueError):
+            message = str(expected_result)
+            with expect_raises_message(ValueError, message, text_exact=True):
+                ScriptDirectory.from_config(cfg)
+        else:
+            s = ScriptDirectory.from_config(cfg)
+            eq_(s.version_locations, expected_result)
+
+
+class StdoutOutputEncodingTest(TestBase):
+    def test_plain(self):
+        stdout = mock.Mock(encoding="latin-1")
+        cfg = config.Config(stdout=stdout)
+        cfg.print_stdout("test %s %s", "x", "y")
+        eq_(
+            stdout.mock_calls,
+            [mock.call.write("test x y"), mock.call.write("\n")],
+        )
+
+    def test_utf8_unicode(self):
+        stdout = mock.Mock(encoding="latin-1")
+        cfg = config.Config(stdout=stdout)
+        cfg.print_stdout("méil %s %s", "x", "y")
+        eq_(
+            stdout.mock_calls,
+            [mock.call.write("méil x y"), mock.call.write("\n")],
+        )
+
+    def test_ascii_unicode(self):
+        stdout = mock.Mock(encoding=None)
+        cfg = config.Config(stdout=stdout)
+        cfg.print_stdout("méil %s %s", "x", "y")
+        eq_(
+            stdout.mock_calls,
+            [mock.call.write("m?il x y"), mock.call.write("\n")],
+        )
+
+    def test_only_formats_output_with_args(self):
+        stdout = mock.Mock(encoding=None)
+        cfg = config.Config(stdout=stdout)
+        cfg.print_stdout("test 3%")
+        eq_(
+            stdout.mock_calls,
+            [mock.call.write("test 3%"), mock.call.write("\n")],
+        )
+
+
+class TemplateOutputEncodingTest(TestBase):
+    def setUp(self):
+        staging_env()
+        self.cfg = _no_sql_testing_config()
+
+    def tearDown(self):
+        clear_staging_env()
+
+    def test_default(self):
+        script = ScriptDirectory.from_config(self.cfg)
+        eq_(script.output_encoding, "utf-8")
+
+    def test_setting(self):
+        self.cfg.set_main_option("output_encoding", "latin-1")
+        script = ScriptDirectory.from_config(self.cfg)
+        eq_(script.output_encoding, "latin-1")

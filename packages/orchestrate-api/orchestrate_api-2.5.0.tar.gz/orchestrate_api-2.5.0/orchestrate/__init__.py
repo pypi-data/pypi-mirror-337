7f422f87e05f77c7dd707f65bd9a8be01c4fb73a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-29 05:05:31 +0000

    Add package orchestrate-api/2.5.0/orchestrate_api-2.5.0.tar.gz

---
+++ packages/orchestrate-api/orchestrate_api-2.5.0.tar.gz/orchestrate_api-2.5.0/orchestrate/__init__.py
@@ -0,0 +1,18 @@
+from orchestrate._internal.version import __version__
+from orchestrate._internal.api import OrchestrateApi
+from orchestrate import terminology
+from orchestrate import insight
+from orchestrate import convert
+from orchestrate import exceptions
+from orchestrate import identity
+
+__version__ = __version__
+
+__all__ = [
+    "OrchestrateApi",
+    "terminology",
+    "insight",
+    "convert",
+    "exceptions",
+    "identity",
+]

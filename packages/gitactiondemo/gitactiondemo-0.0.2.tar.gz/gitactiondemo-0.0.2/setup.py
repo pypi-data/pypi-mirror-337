16ccfc6c05995ef9ecfa5409a3c610226cc933f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-29 07:04:35 +0000

    Add package gitactiondemo/0.0.2/gitactiondemo-0.0.2.tar.gz

---
+++ packages/gitactiondemo/gitactiondemo-0.0.2.tar.gz/gitactiondemo-0.0.2/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='gitactiondemo',
+    version='0.0.2',
+    description='git action demo',
+    author='Lian Park',
+    author_email='g1000white@gmail.com',
+    url='',
+    install_requires=['selenium',],
+    packages=find_packages(exclude=[]),
+    keywords=['python tutorial', 'pypi'],
+    python_requires='>=3.6',
+    package_data={},
+    zip_safe=False,
+    classifiers=[
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+)

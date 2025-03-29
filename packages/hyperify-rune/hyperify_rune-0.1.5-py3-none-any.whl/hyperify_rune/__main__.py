d2b04085b381bb1eff1e993d60f7eed4d5cc39d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-29 14:06:17 +0000

    Add package hyperify-rune/0.1.5/hyperify_rune-0.1.5-py3-none-any.whl

---
+++ packages/hyperify-rune/hyperify_rune-0.1.5-py3-none-any.whl/hyperify_rune/__main__.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+# Rune Preprocessor Entry Point
+# Copyright 2024-2025 HyperifyIO <info@hyperify.io>
+
+import sys
+import os
+import argparse
+from . import process_files
+
+def main():
+    parser = argparse.ArgumentParser(description="Merge all YAML files in a directory into a single array and print it as JSON or YAML.")
+    parser.add_argument("directory", type=str, help="Directory containing the YAML files to merge.")
+    parser.add_argument("output_type", type=str, choices=['json', 'yml'], help="Output format: 'json' or 'yml'.")
+    args = parser.parse_args()
+    
+    process_files(args.directory, args.output_type, os.path.join(args.directory, "translations"))
+
+if __name__ == "__main__":
+    main()

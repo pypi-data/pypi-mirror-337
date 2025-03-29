fc01968c9138820e37e181bd84c248058f262ff0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-29 15:05:33 +0000

    Add package resctk/0.1.4/resctk-0.1.4-py3-none-any.whl

---
+++ packages/resctk/resctk-0.1.4-py3-none-any.whl/resctk/error_messages.py
@@ -0,0 +1,13 @@
+no_sections = {"name":"Name not found",
+             "phone":"Phone Number not found",
+             "email":"Email not found",
+             "experience":"Experience Section not found",
+             "skills":"Skills Section not found",
+             "education":"Education Section not found",
+             "projects":"Projects Section not found"
+            }
+
+no_data = {"no_data":"No valid data found !!",
+           "education_data":"No Education data found!",
+            "company_data":"No Company names found !!",
+            "university_data":"No University names found!"}
---
+++ packages/resctk/resctk-0.1.4-py3-none-any.whl/resctk/info.py
@@ -0,0 +1,4 @@
+""" 
+- Score Resume: the score is calculated out of 5. Any resume with score equal-to, or above 2.4 is considered a good match for the Job.
+- Semantic Similarity: the score is in the range 0 to 1; 0 = completely opposite meaning, 0.5 = no similarity, 1 = exactly same.
+"""

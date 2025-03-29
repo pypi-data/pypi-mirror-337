5232f3e0348cb0f06f994e95cde62780c2544c29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-29 04:04:45 +0000

    Add package auto-coder/0.1.326/auto_coder-0.1.326.tar.gz

---
+++ packages/auto-coder/auto_coder-0.1.326.tar.gz/auto_coder-0.1.326/src/autocoder/common/stream_out_type.py
@@ -0,0 +1,17 @@
+from enum import Enum
+
+class AutoCommandStreamOutType(Enum):
+    COMMAND_SUGGESTION = "command_suggestion"
+
+class IndexFilterStreamOutType(Enum):
+    FILE_NUMBER_LIST = "file_number_list"
+
+
+class CodeGenerateStreamOutType(Enum):
+    CODE_GENERATE = "code_generate"
+
+class CodeRankStreamOutType(Enum):
+    CODE_RANK = "code_rank"
+
+class LintStreamOutType(Enum):
+    LINT = "lint"

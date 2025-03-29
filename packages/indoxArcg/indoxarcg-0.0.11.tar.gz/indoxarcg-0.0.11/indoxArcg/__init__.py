1482a84562cec06321d7f3b299dc8b5c40335804
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-29 14:05:16 +0000

    Add package indoxArcg/0.0.11/indoxarcg-0.0.11.tar.gz

---
+++ packages/indoxArcg/indoxarcg-0.0.11.tar.gz/indoxarcg-0.0.11/indoxArcg/__init__.py
@@ -0,0 +1,12 @@
+# indoxRag/__init__.py
+# from .indoxRag import IndoxRetrievalAugmentation
+
+# from .cag import CAG
+
+# import importlib.metadata
+
+# __version__ = importlib.metadata.version("indoxRag")
+__version__ = "0.0.11"
+__author__ = "osllm.ai"
+__license__ = "AGPL-3.0"
+__copyright__ = "Copyright (c) 2024 osllm.ai"

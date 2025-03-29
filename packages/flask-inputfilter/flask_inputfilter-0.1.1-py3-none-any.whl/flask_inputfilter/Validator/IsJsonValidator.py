f31cc42926b10cf4c8f1c759b541034b5d743f0c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-29 15:05:32 +0000

    Add package flask-inputfilter/0.1.1/flask_inputfilter-0.1.1-py3-none-any.whl

---
+++ packages/flask-inputfilter/flask_inputfilter-0.1.1-py3-none-any.whl/flask_inputfilter/Validator/IsJsonValidator.py
@@ -0,0 +1,24 @@
+import json
+from typing import Any, Optional
+
+from flask_inputfilter.Exception import ValidationError
+from flask_inputfilter.Validator import BaseValidator
+
+
+class IsJsonValidator(BaseValidator):
+    """
+    Validator that checks if a value is a valid JSON string.
+    """
+
+    def __init__(self, error_message: Optional[str] = None) -> None:
+        self.error_message = error_message
+
+    def validate(self, value: Any) -> None:
+        try:
+            json.loads(value)
+
+        except (TypeError, ValueError):
+            raise ValidationError(
+                self.error_message
+                or f"Value '{value}' is not a valid JSON string."
+            )
---
+++ packages/flask-inputfilter/flask_inputfilter-0.1.1-py3-none-any.whl/flask_inputfilter/Validator/RegexValidator.py
@@ -0,0 +1,28 @@
+import re
+from typing import Optional
+
+from flask_inputfilter.Exception import ValidationError
+from flask_inputfilter.Validator import BaseValidator
+
+
+class RegexValidator(BaseValidator):
+    """
+    Validator that checks if a value matches a given regular
+    expression pattern.
+    """
+
+    def __init__(
+        self,
+        pattern: str,
+        error_message: Optional[str] = None,
+    ) -> None:
+        self.pattern = pattern
+        self.error_message = error_message
+
+    def validate(self, value: str) -> None:
+        if not re.match(self.pattern, value):
+            raise ValidationError(
+                self.error_message
+                or f"Value '{value}' does not match the required "
+                f"pattern '{self.pattern}'."
+            )

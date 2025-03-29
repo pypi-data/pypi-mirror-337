27331f5296a993de763ba17bd42fe6acef09fc1a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-29 05:04:51 +0000

    Add package whops-image-utils/0.1.0/whops_image_utils-0.1.0-py3-none-any.whl

---
+++ packages/whops-image-utils/whops_image_utils-0.1.0-py3-none-any.whl/image_utils/__init__.py
@@ -0,0 +1,4 @@
+from .image_utils import resize_image  # Import your key functions
+
+__version__ = "0.1.0"  # Initial version
+__all__ = ['resize_image']  # List of what to import when using 'from image_utils import *'

fd90d4efc888d7c8a6bc6b24696687c9b71199e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-29 03:15:52 +0000

    Add package ok-script/0.0.504/ok_script-0.0.504-cp312-cp312-win_amd64.whl

---
+++ packages/ok-script/ok_script-0.0.504-cp312-cp312-win_amd64.whl/ok/gui/widget/BaseWindow.py
@@ -0,0 +1,114 @@
+# coding:utf-8
+import sys
+
+from PySide6.QtCore import Qt, QSize, QRect
+from PySide6.QtGui import QPainter, QColor
+from PySide6.QtWidgets import QApplication
+from qfluentwidgets import qconfig, isDarkTheme
+from qfluentwidgets.common.animation import BackgroundAnimationWidget
+from qfluentwidgets.components.widgets.frameless_window import FramelessWindow
+from qframelesswindow import TitleBarBase
+
+from ok import og
+from ok.gui.Communicate import communicate
+from ok.gui.util.app import show_info_bar
+from ok.gui.widget.BaseLoading import BaseLoading
+
+
+class BaseWindow(BackgroundAnimationWidget, FramelessWindow, BaseLoading):
+    """ Fluent window base class """
+
+    def __init__(self, parent=None):
+        self._isMicaEnabled = False
+        self._lightBackgroundColor = QColor(240, 244, 249)
+        self._darkBackgroundColor = QColor(32, 32, 32)
+
+        super().__init__(parent=parent)
+        og.set_dpi_scaling(self)
+        # FluentStyleSheet.FLUENT_WINDOW.apply(self)
+
+        # enable mica effect on win11
+        self.setMicaEffectEnabled(True)
+
+        # show system title bar buttons on macOS
+        if sys.platform == "darwin":
+            self.setSystemTitleBarButtonVisible(True)
+
+        communicate.notification.connect(self.show_notification)
+        qconfig.themeChangedFinished.connect(self._onThemeChangedFinished)
+
+    def _updateStackedBackground(self):
+        isTransparent = self.stackedWidget.currentWidget().property("isStackedTransparent")
+        if bool(self.stackedWidget.property("isTransparent")) == isTransparent:
+            return
+
+        self.stackedWidget.setProperty("isTransparent", isTransparent)
+        self.stackedWidget.setStyle(QApplication.style())
+
+    def setCustomBackgroundColor(self, light, dark):
+        """ set custom background color
+
+        Parameters
+        ----------
+        light, dark: QColor | Qt.GlobalColor | str
+            background color in light/dark theme mode
+        """
+        self._lightBackgroundColor = QColor(light)
+        self._darkBackgroundColor = QColor(dark)
+        self._updateBackgroundColor()
+
+    def _normalBackgroundColor(self):
+        if not self.isMicaEffectEnabled():
+            return self._darkBackgroundColor if isDarkTheme() else self._lightBackgroundColor
+
+        return QColor(0, 0, 0, 0)
+
+    def _onThemeChangedFinished(self):
+        if self.isMicaEffectEnabled():
+            self.windowEffect.setMicaEffect(self.winId(), isDarkTheme())
+
+    def paintEvent(self, e):
+        super().paintEvent(e)
+        painter = QPainter(self)
+        painter.setPen(Qt.NoPen)
+        painter.setBrush(self.backgroundColor)
+        painter.drawRect(self.rect())
+
+    def setMicaEffectEnabled(self, isEnabled: bool):
+        """ set whether the mica effect is enabled, only available on Win11 """
+        if sys.platform != 'win32' or sys.getwindowsversion().build < 22000:
+            return
+
+        self._isMicaEnabled = isEnabled
+
+        if isEnabled:
+            self.windowEffect.setMicaEffect(self.winId(), isDarkTheme())
+        else:
+            self.windowEffect.removeBackgroundEffect(self.winId())
+
+        self.setBackgroundColor(self._normalBackgroundColor())
+
+    def show_notification(self, message, title=None, error=False, tray=False):
+        show_info_bar(self.window(), message, title, error)
+
+    def isMicaEffectEnabled(self):
+        return self._isMicaEnabled
+
+    def systemTitleBarRect(self, size: QSize) -> QRect:
+        """ Returns the system title bar rect, only works for macOS
+
+        Parameters
+        ----------
+        size: QSize
+            original system title bar rect
+        """
+        return QRect(size.width() - 75, 0 if self.isFullScreen() else 9, 75, size.height())
+
+    def setTitleBar(self, titleBar):
+        super().setTitleBar(titleBar)
+
+        # hide title bar buttons on macOS
+        if sys.platform == "darwin" and self.isSystemButtonVisible() and isinstance(titleBar, TitleBarBase):
+            titleBar.minBtn.hide()
+            titleBar.maxBtn.hide()
+            titleBar.closeBtn.hide()

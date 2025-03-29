9e89b59e54b27019d79ade5ce2c3d51248198230
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-29 07:04:36 +0000

    Add package linguistics_robin/0.5.9/linguistics_robin-0.5.9.tar.gz

---
+++ packages/linguistics_robin/linguistics_robin-0.5.9.tar.gz/linguistics_robin-0.5.9/linguistics_robin/__init__.py
@@ -0,0 +1,13 @@
+"""A Python 3 linguistics collection library."""
+from .phonetics import (Soundex,
+                        Metaphone,
+                        MatchingRatingApproach,
+                        FuzzySoundex,
+                        Lein,
+                        RefinedSoundex,
+                        NYSIIS,
+                        DoubleMetaphone,
+                        Caverphone,
+                        Caverphone2)
+
+__version__ = '0.5.9'

307414196e0ef77d2810ee2f332015aa88549244
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-29 14:05:50 +0000

    Add package vllm-ascend/0.7.3rc2/vllm_ascend-0.7.3rc2.tar.gz

---
+++ packages/vllm-ascend/vllm_ascend-0.7.3rc2.tar.gz/vllm_ascend-0.7.3rc2/vllm_ascend/models/__init__.py
@@ -0,0 +1,15 @@
+from vllm import ModelRegistry
+
+
+def register_model():
+    ModelRegistry.register_model(
+        "Qwen2VLForConditionalGeneration",
+        "vllm_ascend.models.qwen2_vl:CustomQwen2VLForConditionalGeneration")
+
+    ModelRegistry.register_model(
+        "DeepseekV2ForCausalLM",
+        "vllm_ascend.models.deepseek_v2:CustomDeepseekV2ForCausalLM")
+
+    ModelRegistry.register_model(
+        "DeepseekV3ForCausalLM",
+        "vllm_ascend.models.deepseek_v2:CustomDeepseekV3ForCausalLM")

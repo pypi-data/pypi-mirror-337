6f8972d1a8250c0f1455f3bf6cc84baf4f528d9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-29 15:05:06 +0000

    Add package nonebot-plugin-jmcomic/0.1.9/nonebot_plugin_jmcomic-0.1.9.tar.gz

---
+++ packages/nonebot-plugin-jmcomic/nonebot_plugin_jmcomic-0.1.9.tar.gz/nonebot_plugin_jmcomic-0.1.9/nonebot_plugin_jmcomic/__init__.py
@@ -0,0 +1,54 @@
+from importlib.metadata import version
+
+from nonebot.adapters.onebot.v11 import (
+    Bot,
+    GroupMessageEvent,
+    Message,
+)
+from nonebot.params import CommandArg
+from nonebot.plugin import PluginMetadata
+from nonebot.plugin.on import on_command
+
+from .config import Config, jm_config
+from .utils import *  # noqa
+
+try:
+    __version__ = version("nonebot_plugin_jmcomic")
+except Exception:
+    __version__ = "0.0.0"
+
+__plugin_meta__ = PluginMetadata(
+    name="nonebot-plugin-jmcomic",
+    description="下载禁漫并发送",
+    usage="jm + id",
+    homepage="https://github.com/zhulinyv/nonebot_plugin_jmcomic",
+    type="application",
+    supported_adapters={"~onebot.v11"},
+    config=Config,
+    extra={
+        "author": "zhulinyv",
+        "version": __version__,
+    },
+)
+
+jm = on_command("jm", aliases={"JM"}, priority=30, block=True)
+
+
+@jm.handle()
+async def _(bot: Bot, event: GroupMessageEvent, msg: Message = CommandArg()):
+    id = msg.extract_plain_text().strip()
+
+    await jm.send("正在下载中...", at_sender=True)
+    await async_download_album(id)  # noqa
+
+    file = (
+        "file:///" + str(jm_data_dir / f"{id}.pdf")  # noqa
+        if jm_config.jm_client
+        else str(jm_data_dir / f"{id}.pdf")  # noqa
+    )
+
+    await bot.upload_group_file(
+        group_id=event.group_id,
+        file=file,
+        name=f"{id}.pdf",
+    )
---
+++ packages/nonebot-plugin-jmcomic/nonebot_plugin_jmcomic-0.1.9.tar.gz/nonebot_plugin_jmcomic-0.1.9/nonebot_plugin_jmcomic/utils.py
@@ -0,0 +1,43 @@
+import asyncio
+import os
+from pathlib import Path
+
+import jmcomic
+from nonebot import require
+
+require("nonebot_plugin_localstore")
+
+import nonebot_plugin_localstore as store  # noqa
+
+jm_data_dir: Path = store.get_plugin_data_dir()
+jm_config_file: Path = store.get_plugin_config_file("option.yml")
+
+jm_data_dir_str = str(jm_data_dir)
+
+
+async def async_download_album(id):
+    option = jmcomic.create_option_by_file(str(jm_config_file))
+    await asyncio.to_thread(jmcomic.download_album, id, option)
+
+
+if not os.path.exists(jm_config_file):
+    with open(jm_config_file, "w", encoding="utf-8") as file:
+        file.write(
+            f"""# 更多配置请参考: https://github.com/hect0x7/JMComic-Crawler-Python/blob/master/assets/docs/sources/option_file_syntax.md
+dir_rule:
+  base_dir: {jm_data_dir_str}
+
+plugins:
+  after_photo:
+    - plugin: img2pdf
+      kwargs:
+        pdf_dir: {jm_data_dir_str}
+        filename_rule: Pid
+  
+  after_album:
+    - plugin: img2pdf
+      kwargs:
+        pdf_dir: {jm_data_dir_str}
+        filename_rule: Aname
+"""
+        )

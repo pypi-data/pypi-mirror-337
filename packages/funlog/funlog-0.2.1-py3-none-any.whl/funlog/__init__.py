1ae7cb3a98ca1cbfc240ea3a033978161426c17b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-29 06:06:12 +0000

    Add package funlog/0.2.1/funlog-0.2.1-py3-none-any.whl

---
+++ packages/funlog/funlog-0.2.1-py3-none-any.whl/funlog/__init__.py
@@ -0,0 +1,23 @@
+from .funlog import (
+    abbreviate_arg,
+    balance_quotes,
+    format_args,
+    format_duration,
+    format_func_call,
+    log_calls,
+    log_if_modifies,
+    log_tallies,
+    tally_calls,
+)
+
+__all__ = [
+    "log_calls",
+    "log_if_modifies",
+    "tally_calls",
+    "log_tallies",
+    "format_func_call",
+    "format_args",
+    "format_duration",
+    "abbreviate_arg",
+    "balance_quotes",
+]

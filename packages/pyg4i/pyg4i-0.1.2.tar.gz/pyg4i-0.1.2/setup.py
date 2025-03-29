b096c1e0192f2b94737a80c2e57ecf84928986ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-29 06:06:07 +0000

    Add package pyg4i/0.1.2/pyg4i-0.1.2.tar.gz

---
+++ packages/pyg4i/pyg4i-0.1.2.tar.gz/pyg4i-0.1.2/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pyg4i',
+    version='0.1.2',
+    author='pyg4i',
+    author_email='kigzobadi@gmail.com',
+    description='Более удобное использование pyg4i',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    packages=find_packages(),
+    install_requires=[
+        'pyg4i',
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)

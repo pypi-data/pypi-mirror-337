6bad8427e2a55ce91b7145aa1b1adca513d973dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-29 04:04:47 +0000

    Add package robotframework-robocop/6.0.1/robotframework_robocop-6.0.1.tar.gz

---
+++ packages/robotframework-robocop/robotframework_robocop-6.0.1.tar.gz/robotframework_robocop-6.0.1/src/robocop/files.py
@@ -0,0 +1,46 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING, Any
+
+import click
+
+if TYPE_CHECKING:
+    from pathlib import Path
+
+try:
+    import tomllib as toml
+except ImportError:  # Python < 3.11
+    import tomli as toml
+
+
+def load_toml_file(config_path: Path) -> dict[str, Any]:
+    try:
+        with config_path.open("rb") as tf:
+            return toml.load(tf)
+    except (toml.TOMLDecodeError, OSError) as e:
+        raise click.FileError(filename=str(config_path), hint=f"Error reading configuration file: {e}") from None
+
+
+def read_toml_config(config_path: Path) -> dict[str, Any] | None:
+    """
+    Load and return toml configuration file.
+
+    For pyproject.toml files we need to retrieve configuration from [tool.robocop] section. This section is not
+    required for the robocop.toml file.
+    """
+    config = load_toml_file(config_path)
+    if config_path.name == "robocop.toml":
+        if tool_config := config.get("tool", {}).get("robocop", {}):
+            config = tool_config
+    else:
+        config = config.get("tool", {}).get("robocop", {})
+    if not config:
+        return None
+    return {k.replace("-", "_"): v for k, v in config.items()}
+
+
+def get_path_relative_to_path(path: Path, root_parent: Path) -> Path:
+    try:
+        return path.relative_to(root_parent)
+    except ValueError:
+        return path
---
+++ packages/robotframework-robocop/robotframework_robocop-6.0.1.tar.gz/robotframework_robocop-6.0.1/tests/test_cli.py
@@ -0,0 +1,82 @@
+"""Test CLI commands / options common for linter and formatter."""
+
+from typer.testing import CliRunner
+
+from robocop import __version__
+from robocop.run import app
+from tests import working_directory
+
+
+def test_version():
+    runner = CliRunner()
+    result = runner.invoke(app, ["--version"])
+    assert result.stdout == f"robocop, version {__version__}\n"
+
+
+def test_print_docs_rule():
+    runner = CliRunner()
+    result = runner.invoke(app, ["docs", "line-too-long"])
+    assert "Rule: line-too-long (LEN08)" in result.stdout
+
+
+def test_print_docs_report():
+    runner = CliRunner()
+    result = runner.invoke(app, ["docs", "file_stats"])
+    assert "Report that displays overall statistics about number of processed files." in result.stdout
+
+
+def test_print_docs_formatter():
+    runner = CliRunner()
+    result = runner.invoke(app, ["docs", "NormalizeSeparators"])
+    assert "All separators (pipes included) are converted to fixed length of 4 spaces " in result.stdout
+
+
+def test_print_docs_invalid():
+    runner = CliRunner()
+    result = runner.invoke(app, ["docs", "idontexist"])
+    assert result.exit_code == 2
+    assert result.stdout == "There is no rule, formatter or a report with a 'idontexist' name.\n"
+
+
+def test_invalid_threshold():
+    runner = CliRunner()
+    result = runner.invoke(app, ["check", "--threshold", "unknown"])
+    assert result.exit_code == 2
+    assert result.stdout == "ConfigurationError: Invalid severity value 'unknown'. Choose one from: I, W, E.\n"
+
+
+class TestListFormatters:
+    def test_list_formatters_default(self):
+        runner = CliRunner()
+        result = runner.invoke(app, ["list", "formatters"])
+        assert result.exit_code == 0
+        assert "NormalizeNewLines" in result.stdout
+        assert "Translate" in result.stdout
+
+    def test_list_enabled(self):
+        runner = CliRunner()
+        result = runner.invoke(app, ["list", "formatters", "--filter", "ENABLED"])
+        assert result.exit_code == 0
+        assert "NormalizeNewLines" in result.stdout
+        assert "ReplaceReturns" in result.stdout
+        assert "Translate" not in result.stdout
+
+    def test_list_disabled(self):
+        runner = CliRunner()
+        result = runner.invoke(app, ["list", "formatters", "--filter", "DISABLED"])
+        assert result.exit_code == 0
+        assert "NormalizeNewLines" not in result.stdout
+        assert "Translate" in result.stdout
+
+    def test_target_version(self):
+        runner = CliRunner()
+        result = runner.invoke(app, ["list", "formatters", "--filter", "ENABLED", "--target-version", "4"])
+        assert result.exit_code == 0
+        assert "NormalizeNewLines" in result.stdout
+        assert "ReplaceReturns" not in result.stdout
+
+    def test_return_status(self, tmp_path):
+        runner = CliRunner()
+        with working_directory(tmp_path):
+            result = runner.invoke(app, ["check", "--reports", "return_status"])
+        assert result.exit_code == 0

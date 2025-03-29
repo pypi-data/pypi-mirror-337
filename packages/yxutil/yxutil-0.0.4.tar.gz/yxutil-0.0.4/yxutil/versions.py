ed1d5545feaba46293bcf28cabb4832bca998e33
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-29 06:04:48 +0000

    Add package yxutil/0.0.4/yxutil-0.0.4.tar.gz

---
+++ packages/yxutil/yxutil-0.0.4.tar.gz/yxutil-0.0.4/yxutil/versions.py
@@ -0,0 +1,30 @@
+def get_versions():
+    return versions[0]["number"]
+
+
+versions = [
+    {
+        "number": "0.0.4",
+        "features": [
+            "1. add json related functions",
+        ],
+    },
+    {
+        "number": "0.0.3",
+        "features": [
+            "1. add job",
+        ],
+    },
+    {
+        "number": "0.0.2",
+        "features": [
+            "1. add dict related functions",
+        ],
+    },
+    {
+        "number": "0.0.1",
+        "features": [
+            "1. init",
+        ],
+    },
+]

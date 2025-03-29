3bedcd4a74bc100d96106911472d825d43edf9ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-29 03:14:16 +0000

    Add package impresso-pipelines/0.4.3.42/impresso_pipelines-0.4.3.42-py3-none-any.whl

---
+++ packages/impresso-pipelines/impresso_pipelines-0.4.3.42-py3-none-any.whl/impresso_pipelines/mallet/config.py
@@ -0,0 +1,10 @@
+SUPPORTED_LANGUAGES = {
+    "de": "de_core_news_md",
+    "fr": "fr_core_news_md",
+    "lb": None,  # No official SpaCy model for Luxembourgish
+}
+
+MODEL_URLS = {
+    "de_core_news_md": "https://github.com/explosion/spacy-models/releases/download/de_core_news_md-3.6.0/de_core_news_md-3.6.0.tar.gz",
+    "fr_core_news_md": "https://github.com/explosion/spacy-models/releases/download/fr_core_news_md-3.6.0/fr_core_news_md-3.6.0.tar.gz",
+}

991ba99704f831b8ccc4b14445ebfcc2b17a8408
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-29 15:05:07 +0000

    Add package montu/0.9.10/montu-0.9.10-py3-none-any.whl

---
+++ packages/montu/montu-0.9.10-py3-none-any.whl/montu/__init__.py
@@ -0,0 +1,47 @@
+###############################################################
+# Import montu modules
+###############################################################
+from montu.version import *
+from montu.util import *
+from montu.time import *
+from montu.stars import *
+from montu.observer import *
+from montu.sebau import *
+
+###############################################################
+# Aliases
+###############################################################
+D2H = Util.dec2hex
+VPRINT = Util.vprint
+PRINTDF = Util.print_df
+TABLEDF = Util.table_df
+
+###############################################################
+# External modules
+###############################################################
+import warnings
+import numpy as np
+
+###############################################################
+# Constants
+###############################################################
+# Numerical Constants
+RAD = 180/np.pi
+DEG = 1/RAD
+MARCSEC = 1e-3/3600 # milliarcsec in degrees 
+
+
+###############################################################
+# Initialization
+###############################################################
+# Avoid warnings
+warnings.filterwarnings("ignore")
+
+# Load basic SPICE kernels
+Util.load_kernels()
+
+# Load planetary data
+ALLPLANETS = Util.load_planets()
+
+# Showing version 
+print(f"Running MontuPython version {version}")

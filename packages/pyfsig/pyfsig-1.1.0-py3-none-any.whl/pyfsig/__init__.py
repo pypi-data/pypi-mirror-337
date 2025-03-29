bd5a9cb40a332192cbf79df6fd444ad083ded5c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-29 14:06:48 +0000

    Add package pyfsig/1.1.0/pyfsig-1.1.0-py3-none-any.whl

---
+++ packages/pyfsig/pyfsig-1.1.0-py3-none-any.whl/pyfsig/__init__.py
@@ -0,0 +1,5 @@
+from pyfsig.constants import SIGNATURES  # noqa: F401
+from pyfsig.interface import (
+    find_matches_for_file_path,  # noqa: F401
+    find_matches_for_file_header,  # noqa: F401
+)

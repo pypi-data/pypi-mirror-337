9e95f26ea9ad3169543d01d1c3cd5082cfb7e2fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-30 15:04:55 +0000

    Add package charachorder.py/0.6.0/charachorder_py-0.6.0.tar.gz

---
+++ packages/charachorder.py/charachorder_py-0.6.0.tar.gz/charachorder_py-0.6.0/readme.md
@@ -0,0 +1,52 @@
+# charachorder.py
+
+A wrapper for [CharaChorder](https://www.charachorder.com)'s [Serial API](https://docs.charachorder.com/SerialAPI.html) written in Python
+
+> **Warning:**
+> This project is in beta; breaking changes will occur without notice. Please wait for v1.0.0
+
+## Features
+
+- Supports all major versions of Python3.
+- Exhaustive list of commands as in the Serial API.
+- Events (coming soon).
+
+## Installation
+
+**Python 3.9 or higher is required.**
+
+```sh
+# Linux/macOS
+python3 -m pip install -U charachorder.py
+
+# Windows
+py -3 -m pip install -U charachorder.py
+```
+
+To install the development version, run the following:
+```sh
+git clone https://github.com/CharaChorder/charachorder.py
+cd charachorder.py
+python3 -m pip install -U .
+```
+
+## Usage
+
+```py
+from charachorder import CharaChorder
+
+for device in CharaChorder.list_devices():
+    # Method 1
+    with device:
+        print(device.get_id())
+
+    # Method 2
+    device.open()
+    print(device.get_id())
+    device.close()
+```
+
+## Links
+
+- [Documentation](https://charachorder.github.io/charachorder.py)
+- [CharaChorder's Official Discord Server](https://discord.gg/QZJeZGtznG)
---
+++ packages/charachorder.py/charachorder_py-0.6.0.tar.gz/charachorder_py-0.6.0/setup.py
@@ -0,0 +1,46 @@
+import re
+from pathlib import Path
+
+from setuptools import setup
+
+with open("charachorder/__init__.py") as f:
+    match = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]', f.read(), re.MULTILINE)
+    if not match:
+        raise RuntimeError("version is not set")
+    version = match.group(1)
+
+PROJECT_URL = "https://github.com/CharaChorder/charachorder.py"
+
+setup(
+    name="charachorder.py",
+    version=version,
+    license="MIT",
+    description="A wrapper for CharaChorder's Serial API written in Python",
+    long_description=Path("readme.md").read_text(),
+    long_description_content_type="text/markdown",
+    url=PROJECT_URL,
+    author="GetPsyched",
+    author_email="dev@getpsyched.dev",
+    project_urls={
+        "Documentation": "https://charachorder.github.io/charachorder.py",
+        "Issue tracker": f"{PROJECT_URL}/issues",
+    },
+    packages=["charachorder"],
+    include_package_data=True,
+    python_requires=">=3.9.0",
+    install_requires=["inquirer", "pyserial"],
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Intended Audience :: Developers",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Utilities",
+        "Typing :: Typed",
+    ],
+)

43795942b5ed7eace0fd9acf6c3ab6176327dff3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-30 04:05:00 +0000

    Add package nhc/0.4.11/nhc-0.4.11-py3-none-any.whl

---
+++ packages/nhc/nhc-0.4.11-py3-none-any.whl/nhc/const.py
@@ -0,0 +1,25 @@
+MODES = ['low', 'medium', 'high', 'boost']
+PRESET_MODES = {'boost': 3, 'high': 2, 'medium': 1, 'low': 0}
+
+COVER_OPEN = 255
+COVER_CLOSE = 254
+COVER_STOP = 253
+
+DEFAULT_PORT = 8000
+
+THERMOSTAT_MODES = {
+    0: "day",
+    1: "night",
+    2: "eco",
+    3: "off",
+    4: "cool",
+    5: "prog1",
+    6: "prog2",
+    7: "prog3",
+}
+
+ENERGY_TYPES = {
+    0: "import",
+    1: "sub_usage", 
+    2: "export",
+}

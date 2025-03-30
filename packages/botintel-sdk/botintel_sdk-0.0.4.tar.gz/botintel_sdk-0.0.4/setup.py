8a88f93c34d4f2d0952f9e85083876108bc2112b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-30 04:04:55 +0000

    Add package botintel-sdk/0.0.4/botintel_sdk-0.0.4.tar.gz

---
+++ packages/botintel-sdk/botintel_sdk-0.0.4.tar.gz/botintel_sdk-0.0.4/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+# Fix for UnicodeDecodeError when reading the README file
+def read_file(filename):
+    with open(filename, encoding="utf-8") as f:
+        return f.read()
+
+setup(
+    name="botintel-sdk",
+    version="0.0.4",
+    packages=find_packages(exclude=["tests", "examples"]),  # Excluding unnecessary directories
+    install_requires=[
+        'requests>=2.25.1,<3.0.0',  # Added upper version limit for requests
+    ],
+    author="Abdullah Huseynli",
+    author_email="botintelai1@gmail.com",
+    description="Official Python SDK for BotIntel API",
+    long_description=read_file("README.md"),  # Reading README.md for the long description
+    long_description_content_type="text/markdown",  # Ensure Markdown content is parsed
+    url="https://github.com/DeveleplorAbdullahH/botintel",  # Double-check repo URL
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",  # Specify minimum version of Python
+)

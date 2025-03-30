0c336014867cf63eb38e8ef9775b3e31ac3ed367
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-30 03:20:18 +0000

    Add package jmcomic-api/0.5.1.0/jmcomic_api-0.5.1.0.tar.gz

---
+++ packages/jmcomic-api/jmcomic_api-0.5.1.0.tar.gz/jmcomic_api-0.5.1.0/jmcomic_api/models/core/route.py
@@ -0,0 +1,11 @@
+from typing import List
+from abc import ABC, abstractmethod
+
+
+class Route(ABC):
+    path: str
+    method: List
+
+    @abstractmethod
+    async def route_def(self):
+        pass

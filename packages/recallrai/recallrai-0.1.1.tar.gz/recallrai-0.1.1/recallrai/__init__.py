9c841e3cc1f7aff794175095c92d0672979c4539
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-30 04:05:03 +0000

    Add package recallrai/0.1.1/recallrai-0.1.1.tar.gz

---
+++ packages/recallrai/recallrai-0.1.1.tar.gz/recallrai-0.1.1/recallrai/__init__.py
@@ -0,0 +1,20 @@
+# Path: recallrai/__init__.py
+# Description: Package initialization file with SDK version and main class exports
+
+"""
+RecallrAI Python SDK
+
+This package provides a Python interface to interact with the RecallrAI API.
+"""
+
+from .client import RecallrAI
+from .user import User
+from .session import Session
+
+__version__ = "0.1.1"
+
+__all__ = [
+    "RecallrAI",
+    "User",
+    "Session",
+]

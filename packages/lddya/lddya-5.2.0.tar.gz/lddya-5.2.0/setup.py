17661368f5daa19ae377f7ab6e0309ca967d27c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-30 03:20:23 +0000

    Add package lddya/5.2.0/lddya-5.2.0.tar.gz

---
+++ packages/lddya/lddya-5.2.0.tar.gz/lddya-5.2.0/setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+
+#############################################
+# File Name: setup.py
+# Author: lidongdong
+# Mail: 927052521@qq.com
+# Created Time: 2022.10.21  19.50
+############################################
+
+from setuptools import setup, find_packages
+
+setup(
+    name = "lddya",
+    version = "5.2.0",
+    keywords = {"pip", "license","licensetool", "tool", "gm"},
+    description = "统一了算法的公共输出参数，并通过抽象类定义了参数。",
+    long_description = "具体功能，请自行挖掘。",
+    license = "MIT Licence",
+
+    url = "https://github.com/not_define/please_wait",
+    author = "lidongdong",
+    author_email = "927052521@qq.com",
+
+    packages = find_packages(),
+    include_package_data = True,
+    platforms = "any",
+    install_requires = ['numpy','matplotlib','pygame','pandas']
+)

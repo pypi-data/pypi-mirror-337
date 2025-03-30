211a21ebc5322f07c6fb5fd1cf1f3298872ff6cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-30 15:04:18 +0000

    Add package jbang/0.5.6/jbang-0.5.6.tar.gz

---
+++ packages/jbang/jbang-0.5.6.tar.gz/jbang-0.5.6/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+# read the contents of your README file
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setup(name='jbang',
+      version='0.5.6',
+      description='Python for JBang - Java Script in your Python',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
+      url='http://jbang.dev',
+      author='JBang Developers',
+      author_email='team@jbang.dev',
+      license='MIT',
+      packages=['jbang'],
+      zip_safe=False,
+      entry_points={
+          'console_scripts': [
+              'jbang-python=jbang:main',
+          ],
+      },
+      extras_require={
+          'test': [
+              'pytest',
+          ],
+      })

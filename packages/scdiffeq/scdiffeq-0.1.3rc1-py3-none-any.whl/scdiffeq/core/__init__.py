db5347d9836a0c62eef4fcc0d200b131c7206b86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-30 05:05:02 +0000

    Add package scdiffeq/0.1.3rc1/scdiffeq-0.1.3rc1-py3-none-any.whl

---
+++ packages/scdiffeq/scdiffeq-0.1.3rc1-py3-none-any.whl/scdiffeq/core/__init__.py
@@ -0,0 +1,14 @@
+
+__author__ = "Michael E. Vinyard"
+__email__ = "mvinyard.ai@gmai.com"
+
+
+# -- import models accessed as sdq.models.<MODEL>: ---------------------------------------
+from ._scdiffeq import scDiffEq
+
+from . import lightning_models
+from . import utils
+from . import configs
+from . import callbacks
+
+__all__ = ["scDiffEq", "lightning_models", "utils", "configs", "callbacks"]

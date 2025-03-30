7c23bca46399bda223f37a579f1e0f35c602fe66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-30 04:04:55 +0000

    Add package blue-south/4.8.1/blue_south-4.8.1.tar.gz

---
+++ packages/blue-south/blue_south-4.8.1.tar.gz/blue_south-4.8.1/blue_south/__init__.py
@@ -0,0 +1,17 @@
+NAME = "blue_south"
+
+ICON = "🌀"
+
+DESCRIPTION = f"{ICON} Global South AI: Free from Big Tech."
+
+VERSION = "4.8.1"
+
+REPO_NAME = "blue-south"
+
+MARQUEE = "https://github.com/kamangir/assets/raw/main/blue-south/marquee.png?raw=true"
+
+ALIAS = "@south"
+
+
+def fullname() -> str:
+    return f"{NAME}-{VERSION}"

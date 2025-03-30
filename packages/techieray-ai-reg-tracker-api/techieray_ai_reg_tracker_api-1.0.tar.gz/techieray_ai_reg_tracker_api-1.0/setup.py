a95aed824730ce79e5409b2a3dc26ce95dda6209
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-30 03:20:19 +0000

    Add package techieray-ai-reg-tracker-api/1.0/techieray_ai_reg_tracker_api-1.0.tar.gz

---
+++ packages/techieray-ai-reg-tracker-api/techieray_ai_reg_tracker_api-1.0.tar.gz/techieray_ai_reg_tracker_api-1.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+	name = "techieray_ai_reg_tracker_api",
+	version = "1.0",
+	packages = find_packages(),
+	install_requires = [
+		'requests',
+	]
+
+)

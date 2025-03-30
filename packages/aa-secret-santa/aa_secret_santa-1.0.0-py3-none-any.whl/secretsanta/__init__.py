1ec37622c1f8c830f807e34312074fbf9802076d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-30 03:20:24 +0000

    Add package aa-secret-santa/1.0.0/aa_secret_santa-1.0.0-py3-none-any.whl

---
+++ packages/aa-secret-santa/aa_secret_santa-1.0.0-py3-none-any.whl/secretsanta/__init__.py
@@ -0,0 +1,5 @@
+"""
+Alliance Auth Secret Santa
+"""
+__version__ = "1.0.0"
+__title__ = "Secret Santa"
---
+++ packages/aa-secret-santa/aa_secret_santa-1.0.0-py3-none-any.whl/secretsanta/admin.py
@@ -0,0 +1,36 @@
+from django.contrib import admin
+
+from .models import (
+    ActiveSecretSantaFilter, Application, SantaPair, SecretSantaConfiguration,
+    Webhook, Year,
+)
+
+
+@admin.register(Application)
+class ApplicationAdmin(admin.ModelAdmin):
+    list_display = ['year', 'user']
+
+
+@admin.register(SantaPair)
+class SantaPairAdmin(admin.ModelAdmin):
+    list_display = ['year', 'santa', 'santee', 'delivered']
+
+
+@admin.register(Webhook)
+class WebhookAdmin(admin.ModelAdmin):
+    list_display = ['name', 'url']
+
+
+@admin.register(SecretSantaConfiguration)
+class SecretSantaConfigurationAdmin(admin.ModelAdmin):
+    list_display = ['id']
+
+
+@admin.register(Year)
+class YearAdmin(admin.ModelAdmin):
+    list_display = ['year', 'open']
+
+
+@admin.register(ActiveSecretSantaFilter)
+class ActiveSecretSantaFilterAdmin(admin.ModelAdmin):
+    list_display = ['year', 'reversed_logic']

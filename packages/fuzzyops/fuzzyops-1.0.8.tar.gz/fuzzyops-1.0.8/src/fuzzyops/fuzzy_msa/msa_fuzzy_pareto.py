da3fcbbe00b9109d858509233be57483164c35ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-30 03:20:45 +0000

    Add package fuzzyops/1.0.8/fuzzyops-1.0.8.tar.gz

---
+++ packages/fuzzyops/fuzzyops-1.0.8.tar.gz/fuzzyops-1.0.8/src/fuzzyops/fuzzy_msa/msa_fuzzy_pareto.py
@@ -0,0 +1,48 @@
+from typing import List
+from fuzzyops.fuzzy_numbers import FuzzyNumber
+
+
+def _is_dominated(check_solution: List[FuzzyNumber], main_solution: List[FuzzyNumber]) -> bool:
+    """
+    Проверяет, доминирует ли основное решение по всем критериям над проверяемым решением.
+
+    Args:
+        check_solution (List[FuzzyNumber]): Решение, которое проверяется на доминирование.
+        main_solution (List[FuzzyNumber]): Основное решение, по которому проверяется доминирование.
+
+    Returns:
+        bool: True, если основное решение доминирует над проверяемым, False в противном случае.
+    """
+
+    better_in_one_criterion = False
+    for a, b in zip(check_solution, main_solution):
+        if float(b) < float(a):
+            return False
+        if float(b) > float(a):
+            better_in_one_criterion = True
+    return better_in_one_criterion
+
+
+def fuzzy_pareto_solver(solutions: List[List[FuzzyNumber]]) -> List[List[FuzzyNumber]]:
+    """
+    Находит решения, не доминируемые другими решениями в многокритериальной задаче.
+
+    Args:
+        solutions (List[List[FuzzyNumber]]): Список решений, каждое из которых представлено списком нечетких чисел,
+        которые располагаются на границе Парето.
+
+    Returns:
+        List[List[FuzzyNumber]]: Список решений, которые не доминируются другими решениями
+        (существует хотя бы одно решение, по которому другие решения лучше по всем критериям).
+    """
+
+    pareto_solutions = []
+    for solution in solutions:
+        dominated = False
+        for other_solution in solutions:
+            if _is_dominated(solution, other_solution):
+                dominated = True
+                break
+        if not dominated:
+            pareto_solutions.append(solution)
+    return pareto_solutions

d58b8f031fa02340b72294aaeb1534b948c265d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-30 05:05:12 +0000

    Add package turkish-syllable/0.1.3/turkish_syllable-0.1.3.tar.gz

---
+++ packages/turkish-syllable/turkish_syllable-0.1.3.tar.gz/turkish_syllable-0.1.3/tests/test.py
@@ -0,0 +1,4 @@
+from turkish_syllable import syllabify
+
+print(syllabify("halkların astların testlerin bantlamak da gerekir kelimesi budur, ardından neler gelir bilemiyorum!", with_punctuation=False))
+print(syllabify("aktivist", with_punctuation=True))

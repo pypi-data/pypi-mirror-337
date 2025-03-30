af88442e50a2d63fb104c9b3edf29a6a26f7dc62
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-30 03:20:56 +0000

    Add package api-python-bet-project/0.1.21/api_python_bet_project-0.1.21.tar.gz

---
+++ packages/api-python-bet-project/api_python_bet_project-0.1.21.tar.gz/api_python_bet_project-0.1.21/app/routers/hello_world.py
@@ -0,0 +1,19 @@
+<<<<<<< HEAD
+from fastapi import APIRouter
+
+router = APIRouter(prefix="/api/v1/hello-word", tags=["hello-world"])
+
+
+@router.get("")
+async def get_hello_word() -> str:
+    return "Hello World!"
+=======
+from fastapi import APIRouter
+
+router = APIRouter(prefix="/api/v1/hello-word", tags=["hello-world"])
+
+
+@router.get("")
+async def get_hello_word() -> str:
+    return "Hello World!"
+>>>>>>> master

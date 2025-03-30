4b25bc1e038878200191ec830476bece937222ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-30 15:04:55 +0000

    Add package isaacus/0.2.0/isaacus-0.2.0-py3-none-any.whl

---
+++ packages/isaacus/isaacus-0.2.0-py3-none-any.whl/isaacus/resources/__init__.py
@@ -0,0 +1,19 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from .classifications import (
+    ClassificationsResource,
+    AsyncClassificationsResource,
+    ClassificationsResourceWithRawResponse,
+    AsyncClassificationsResourceWithRawResponse,
+    ClassificationsResourceWithStreamingResponse,
+    AsyncClassificationsResourceWithStreamingResponse,
+)
+
+__all__ = [
+    "ClassificationsResource",
+    "AsyncClassificationsResource",
+    "ClassificationsResourceWithRawResponse",
+    "AsyncClassificationsResourceWithRawResponse",
+    "ClassificationsResourceWithStreamingResponse",
+    "AsyncClassificationsResourceWithStreamingResponse",
+]

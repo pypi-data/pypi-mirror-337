d60245433136fbb27f8ef5d9aa8c379294383b7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-30 15:04:54 +0000

    Add package tprofiler/1.0.3/tprofiler-1.0.3.tar.gz

---
+++ packages/tprofiler/tprofiler-1.0.3.tar.gz/tprofiler-1.0.3/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="tprofiler",
+    version="1.0.3",
+    author="Subhransu S. Bhattacharjee",
+    author_email="Subhransu.Bhattacharjee@anu.edu.au",
+    description="A combined time and memory profiler using psutil",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/1ssb/tprofile",
+    packages=find_packages(),
+    install_requires=[
+        "psutil", "line_profiler"
+    ],
+    entry_points={
+        "console_scripts": [
+            "tprofiler = tprofiler.core:main",
+        ],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+)

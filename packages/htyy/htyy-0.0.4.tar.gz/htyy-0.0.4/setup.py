a049aa07963e7ece41c22d642af4e66573bee99b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-30 04:04:56 +0000

    Add package htyy/0.0.4/htyy-0.0.4.tar.gz

---
+++ packages/htyy/htyy-0.0.4.tar.gz/htyy-0.0.4/setup.py
@@ -0,0 +1,32 @@
+from setuptools import find_packages, setup
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name='htyy',
+    version='0.0.4',
+    description='htyy',
+    long_description=long_description,
+    author='huang yi yi',
+    author_email='363766687@qq.com',
+    packages=find_packages(),
+    package_data={
+        "htyy":["*.pyd", "*.mp3"]
+    },
+    include_package_data=True,
+    python_requires='>=3.6',
+    install_requires=[
+        "watchdog",
+        "paramiko",
+        "pycryptodome",
+        "pywin32",
+        "rich",
+        "plyer",
+        "mpmath",
+        "miniaudio",
+        "argostranslate",
+        "googletrans",
+        "pyautogui",
+    ],
+)

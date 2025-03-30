1dfa4a262ac2204575b45d9d1e7ba058576a863c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-30 14:05:34 +0000

    Add package open-trivia-database-wrapper/0.3.2/open_trivia_database_wrapper-0.3.2-py3-none-any.whl

---
+++ packages/open-trivia-database-wrapper/open_trivia_database_wrapper-0.3.2-py3-none-any.whl/opentdb/models.py
@@ -0,0 +1,25 @@
+from typing import List, Optional
+from pydantic import BaseModel
+from opentdb.enums import QuestionCategories, QuestionDifficulties, QuestionTypes, QuestionEncodings
+
+
+class Question(BaseModel):
+    type: str
+    difficulty: str
+    category: str
+    question: str
+    correct_answer: str
+    incorrect_answers: List[str]
+
+
+class Results(BaseModel):
+    response_code: int
+    results: List[Question]
+
+
+class QuestionParameters(BaseModel):
+    amount: int
+    category: Optional[QuestionCategories] = None
+    difficulty: Optional[QuestionDifficulties] = None
+    type: Optional[QuestionTypes] = None
+    encode: Optional[QuestionEncodings] = None

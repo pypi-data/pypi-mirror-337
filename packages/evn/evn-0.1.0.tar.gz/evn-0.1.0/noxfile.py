2f2ba2cd94be23a4fa1d888d606146c7a468cf58
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-30 04:05:16 +0000

    Add package evn/0.1.0/evn-0.1.0.tar.gz

---
+++ packages/evn/evn-0.1.0.tar.gz/evn-0.1.0/noxfile.py
@@ -0,0 +1,52 @@
+import json
+import glob
+import nox
+import os
+from packaging.tags import sys_tags
+
+nox.options.sessions = ['test_matrix']
+# nox.options.sessions = ['test_matrix', 'build']
+sesh = dict(python=["3.9", "3.10", "3.11", "3.12", "3.13"], venv_backend='uv')
+
+@nox.session(**sesh)
+def test_matrix(session):
+    nprocs = min(8, os.cpu_count() or 1)
+    if session.posargs and (session.python) != session.posargs[0]:
+        session.skip(f"Skipping {session.python} because it's not in posargs {session.posargs}")
+    session.install(*'ruff pytest pytest-xdist pytest-repeat'.split())
+    whl = select_wheel(session)
+    print(f"Installing {whl}")
+    session.install(whl)
+    session.run(*f'pytest -n{nprocs} --count 1 --doctest-modules --pyargs evn'.split())
+
+def get_supported_tags_session(session):
+    result = session.run(
+        "python",
+        "-c",
+        ("from packaging.tags import sys_tags; import json;"
+         "print(json.dumps([str(tag) for tag in sys_tags()]))"),
+        silent=True,
+    )
+    result = json.loads(result)
+    return result
+
+def get_supported_tags(session=None):
+    if session: return get_supported_tags_session(session)
+    return {(tag.interpreter, tag.abi, tag.platform) for tag in sys_tags()}
+
+def parse_wheel_tags(filename):
+    parts = filename.split('-')
+    if len(parts) < 5: return None
+    tag = '-'.join([parts[2], parts[3], parts[4].split('.')[0]])
+    return tag
+
+def select_wheel(session):
+    supported = get_supported_tags(session)
+    wheels = glob.glob("wheelhouse/*.whl")
+    picks = []
+    for wheel in wheels:
+        tags = parse_wheel_tags(wheel)
+        if tags and tags in supported:
+            picks.append(wheel)
+    assert picks
+    return picks[0]

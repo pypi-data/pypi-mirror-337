a8125af5d697cc391584d500cd13bf0aa1d80977
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-30 04:05:21 +0000

    Add package geo-activity-playground/0.38.2/geo_activity_playground-0.38.2-py3-none-any.whl

---
+++ packages/geo-activity-playground/geo_activity_playground-0.38.2-py3-none-any.whl/geo_activity_playground/__main__.py
@@ -0,0 +1,130 @@
+import argparse
+import logging
+import os
+import pathlib
+
+import coloredlogs
+
+from .importers.strava_checkout import convert_strava_checkout
+from geo_activity_playground.core.activities import ActivityRepository
+from geo_activity_playground.core.config import ConfigAccessor
+from geo_activity_playground.core.config import import_old_config
+from geo_activity_playground.core.config import import_old_strava_config
+from geo_activity_playground.explorer.tile_visits import TileVisitAccessor
+from geo_activity_playground.explorer.video import explorer_video_main
+from geo_activity_playground.heatmap_video import main_heatmap_video
+from geo_activity_playground.webui.app import web_ui_main
+from geo_activity_playground.webui.upload_blueprint import scan_for_activities
+
+logger = logging.getLogger(__name__)
+
+
+def main() -> None:
+    parser = argparse.ArgumentParser(
+        description="Utilities to work with recorded activities."
+    )
+    parser.set_defaults(func=lambda options: parser.print_help())
+    parser.add_argument("--basedir", type=pathlib.Path, default=pathlib.Path.cwd())
+    parser.add_argument(
+        "--loglevel",
+        choices=["debug", "info", "warning", "error", "critical"],
+        default="info",
+    )
+
+    subparsers = parser.add_subparsers(
+        description="The tools are organized in subcommands.", metavar="Command"
+    )
+
+    # subparser = subparsers.add_parser(
+    #     "explorer",
+    #     help="Generate GeoJSON/GPX files with explored and missing explorer tiles.",
+    # )
+    # subparser.set_defaults(
+    #     func=lambda options: main_explorer(
+    #         make_time_series_source(options.basedir)
+    #     )
+    # )
+
+    subparser = subparsers.add_parser(
+        "explorer-video", help="Generate video with explorer timeline."
+    )
+    subparser.set_defaults(func=lambda options: explorer_video_main())
+
+    subparser = subparsers.add_parser(
+        "convert-strava-checkout",
+        help="Converts a Strava checkout to the structure used by this program.",
+    )
+    subparser.set_defaults(
+        func=lambda options: convert_strava_checkout(
+            options.checkout_path, options.playground_path
+        )
+    )
+    subparser.add_argument("checkout_path", type=pathlib.Path)
+    subparser.add_argument("playground_path", type=pathlib.Path)
+
+    subparser = subparsers.add_parser("serve", help="Launch webserver")
+    subparser.set_defaults(
+        func=lambda options: web_ui_main(
+            *make_activity_repository(options.basedir, options.skip_reload),
+            host=options.host,
+            port=options.port,
+        )
+    )
+    subparser.add_argument(
+        "--host", default="127.0.0.1", help="IP address to listen on"
+    )
+    subparser.add_argument(
+        "--port", default=5000, type=int, help="the port to run listen on"
+    )
+    subparser.add_argument("--skip-reload", action=argparse.BooleanOptionalAction)
+
+    subparser = subparsers.add_parser("cache", help="Cache stuff")
+    subparser.set_defaults(func=lambda options: main_cache(options.basedir))
+
+    subparser = subparsers.add_parser(
+        "heatmap-video", help="Create a video with the evolution of the heatmap"
+    )
+    subparser.add_argument("latitude", type=float)
+    subparser.add_argument("longitude", type=float)
+    subparser.add_argument("zoom", type=int)
+    subparser.add_argument("--decay", type=float, default=0.05)
+    subparser.add_argument("--video-width", type=int, default=1920)
+    subparser.add_argument("--video-height", type=int, default=1080)
+    subparser.set_defaults(func=main_heatmap_video)
+
+    options = parser.parse_args()
+    coloredlogs.install(
+        fmt="%(asctime)s %(name)s %(levelname)s %(message)s",
+        level=options.loglevel.upper(),
+    )
+
+    logging.getLogger("stravalib.protocol.ApiV3").setLevel(logging.WARNING)
+
+    options.func(options)
+
+
+def make_activity_repository(
+    basedir: pathlib.Path, skip_reload: bool
+) -> tuple[ActivityRepository, TileVisitAccessor, ConfigAccessor]:
+    os.chdir(basedir)
+
+    repository = ActivityRepository()
+    tile_visit_accessor = TileVisitAccessor()
+    config_accessor = ConfigAccessor()
+    import_old_config(config_accessor)
+    import_old_strava_config(config_accessor)
+
+    if not skip_reload:
+        scan_for_activities(repository, tile_visit_accessor, config_accessor())
+
+    return repository, tile_visit_accessor, config_accessor
+
+
+def main_cache(basedir: pathlib.Path) -> None:
+    (repository, tile_visit_accessor, config_accessor) = make_activity_repository(
+        basedir, False
+    )
+
+
+if __name__ == "__main__":
+    main()
---
+++ packages/geo-activity-playground/geo_activity_playground-0.38.2-py3-none-any.whl/geo_activity_playground/webui/app.py
@@ -0,0 +1,179 @@
+import datetime
+import importlib
+import json
+import pathlib
+import secrets
+import shutil
+import urllib.parse
+
+from flask import Flask
+from flask import render_template
+from flask import request
+
+from ..core.activities import ActivityRepository
+from ..core.config import Config
+from ..core.config import ConfigAccessor
+from ..explorer.tile_visits import TileVisitAccessor
+from .activity.blueprint import make_activity_blueprint
+from .activity.controller import ActivityController
+from .auth_blueprint import make_auth_blueprint
+from .authenticator import Authenticator
+from .calendar.blueprint import make_calendar_blueprint
+from .calendar.controller import CalendarController
+from .eddington_blueprint import make_eddington_blueprint
+from .entry_controller import EntryController
+from .equipment_blueprint import make_equipment_blueprint
+from .explorer.blueprint import make_explorer_blueprint
+from .explorer.controller import ExplorerController
+from .heatmap.blueprint import make_heatmap_blueprint
+from .search_blueprint import make_search_blueprint
+from .settings.blueprint import make_settings_blueprint
+from .square_planner_blueprint import make_square_planner_blueprint
+from .summary_blueprint import make_summary_blueprint
+from .tile_blueprint import make_tile_blueprint
+from .upload_blueprint import make_upload_blueprint
+from geo_activity_playground.webui.search_util import SearchQueryHistory
+
+
+def route_start(app: Flask, repository: ActivityRepository, config: Config) -> None:
+    entry_controller = EntryController(repository, config)
+
+    @app.route("/")
+    def index():
+        return render_template("home.html.j2", **entry_controller.render())
+
+
+def get_secret_key():
+    secret_file = pathlib.Path("Cache/flask-secret.json")
+    if secret_file.exists():
+        with open(secret_file) as f:
+            secret = json.load(f)
+    else:
+        secret = secrets.token_hex()
+        with open(secret_file, "w") as f:
+            json.dump(secret, f)
+    return secret
+
+
+def web_ui_main(
+    repository: ActivityRepository,
+    tile_visit_accessor: TileVisitAccessor,
+    config_accessor: ConfigAccessor,
+    host: str,
+    port: int,
+) -> None:
+    repository.reload()
+
+    app = Flask(__name__)
+    app.config["UPLOAD_FOLDER"] = "Activities"
+    app.secret_key = get_secret_key()
+
+    @app.template_filter()
+    def dt(value: datetime.datetime):
+        return value.strftime("%Y-%m-%d %H:%M")
+
+    @app.template_filter()
+    def td(v: datetime.timedelta):
+        seconds = v.total_seconds()
+        h = int(seconds // 3600)
+        m = int(seconds // 60 % 60)
+        s = int(seconds // 1 % 60)
+        return f"{h}:{m:02d}:{s:02d}"
+
+    authenticator = Authenticator(config_accessor())
+    search_query_history = SearchQueryHistory(config_accessor, authenticator)
+
+    config = config_accessor()
+    activity_controller = ActivityController(repository, tile_visit_accessor, config)
+    calendar_controller = CalendarController(repository)
+    explorer_controller = ExplorerController(
+        repository, tile_visit_accessor, config_accessor
+    )
+
+    route_start(app, repository, config_accessor())
+
+    app.register_blueprint(make_auth_blueprint(authenticator), url_prefix="/auth")
+    app.register_blueprint(
+        make_activity_blueprint(activity_controller, repository, authenticator),
+        url_prefix="/activity",
+    )
+    app.register_blueprint(
+        make_calendar_blueprint(calendar_controller), url_prefix="/calendar"
+    )
+    app.register_blueprint(
+        make_eddington_blueprint(repository, search_query_history),
+        url_prefix="/eddington",
+    )
+    app.register_blueprint(
+        make_equipment_blueprint(repository, config), url_prefix="/equipment"
+    )
+    app.register_blueprint(
+        make_explorer_blueprint(explorer_controller, authenticator),
+        url_prefix="/explorer",
+    )
+    app.register_blueprint(
+        make_heatmap_blueprint(
+            repository, tile_visit_accessor, config_accessor(), search_query_history
+        ),
+        url_prefix="/heatmap",
+    )
+    app.register_blueprint(
+        make_settings_blueprint(config_accessor, authenticator),
+        url_prefix="/settings",
+    )
+    app.register_blueprint(
+        make_square_planner_blueprint(tile_visit_accessor),
+        url_prefix="/square-planner",
+    )
+    app.register_blueprint(
+        make_search_blueprint(
+            repository, search_query_history, authenticator, config_accessor
+        ),
+        url_prefix="/search",
+    )
+    app.register_blueprint(
+        make_summary_blueprint(repository, config, search_query_history),
+        url_prefix="/summary",
+    )
+
+    app.register_blueprint(make_tile_blueprint(config), url_prefix="/tile")
+    app.register_blueprint(
+        make_upload_blueprint(
+            repository, tile_visit_accessor, config_accessor(), authenticator
+        ),
+        url_prefix="/upload",
+    )
+
+    base_dir = pathlib.Path("Open Street Map Tiles")
+    dir_for_source = base_dir / urllib.parse.quote_plus(config_accessor().map_tile_url)
+    if base_dir.exists() and not dir_for_source.exists():
+        subdirs = base_dir.glob("*")
+        dir_for_source.mkdir()
+        for subdir in subdirs:
+            shutil.move(subdir, dir_for_source)
+
+    @app.context_processor
+    def inject_global_variables() -> dict:
+        variables = {
+            "version": _try_get_version(),
+            "num_activities": len(repository),
+            "map_tile_attribution": config_accessor().map_tile_attribution,
+            "search_query_favorites": search_query_history.prepare_favorites(),
+            "search_query_last": search_query_history.prepare_last(),
+            "request_url": urllib.parse.quote_plus(request.url),
+        }
+        if len(repository):
+            variables["equipments_avail"] = sorted(
+                repository.meta["equipment"].unique()
+            )
+            variables["kinds_avail"] = sorted(repository.meta["kind"].unique())
+        return variables
+
+    app.run(host=host, port=port)
+
+
+def _try_get_version():
+    try:
+        return importlib.metadata.version("geo-activity-playground")
+    except importlib.metadata.PackageNotFoundError:
+        pass

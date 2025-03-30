7e2892528f567347fe3f0720c96168f3451304b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-30 04:04:54 +0000

    Add package botslab/0.1.0/botslab-0.1.0.tar.gz

---
+++ packages/botslab/botslab-0.1.0.tar.gz/botslab-0.1.0/setup.py
@@ -0,0 +1,31 @@
+"""Setup installation script for the package."""
+
+import os
+
+from setuptools import find_packages, setup
+
+REQUIRED_PACKAGES = open("requirements.txt").readlines()
+
+README = ""
+if os.path.exists("README.md"):
+    README = open("README.md").read()
+
+setup(
+    name="botslab",
+    version="0.1.0",
+    description="Botslab General Library",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    author="rilckbotslab",
+    author_email="rilck.lima@botslab.com.br",
+    url="https://github.com/rilckbotslab/session_manager",
+    install_requires=REQUIRED_PACKAGES,
+    packages=find_packages(exclude=["tests", "tests.*"]),
+    python_requires=">=3.7",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    platforms="any",
+)

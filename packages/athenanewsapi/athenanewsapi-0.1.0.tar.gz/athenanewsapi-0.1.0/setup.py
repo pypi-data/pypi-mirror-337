a255c8bbf086f28e0fb99bd3549e81e4df6df846
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-30 14:05:14 +0000

    Add package athenanewsapi/0.1.0/athenanewsapi-0.1.0.tar.gz

---
+++ packages/athenanewsapi/athenanewsapi-0.1.0.tar.gz/athenanewsapi-0.1.0/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="athenanewsapi",               # Package name
+    version="0.1.0",                    # Initial release version
+    author="Matt F.",   
+    author_email="matt@runathena.com",
+    description="A simple wrapper for the Athena News API to simplify searching for articles.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/athenanewsapi/athenanews", 
+    packages=setuptools.find_packages(),
+    install_requires=[
+        "requests",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    #license_files=("LICENSE",)  
+)

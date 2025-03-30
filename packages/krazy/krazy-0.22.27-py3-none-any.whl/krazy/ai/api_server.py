4c40a24f99a63505922022a5e537fa06f7594a87
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-30 04:04:59 +0000

    Add package krazy/0.22.27/krazy-0.22.27-py3-none-any.whl

---
+++ packages/krazy/krazy-0.22.27-py3-none-any.whl/krazy/ai/api_server.py
@@ -0,0 +1,54 @@
+from fastapi import FastAPI
+from pydantic import BaseModel, Field, create_model
+from typing import Optional, List, Dict, Any
+from krazy.ai.agent import AIAssistant
+from general import QueryRequest, build_dynamic_schema_recursive
+from krazy.ai.tools import Config
+
+
+def create_api(config: Config) -> FastAPI:
+    app = FastAPI()
+    assistant = AIAssistant(verbose=True, config=config)
+
+    @app.post("/invoke")
+    def ask_query(payload: QueryRequest):
+        try:
+            if payload.schema:
+                dynamic_model = build_dynamic_schema_recursive("DynamicModel", dict(payload.output_schema))
+                assistant.output_schema = dynamic_model
+                assistant.agent_generator()
+                assistant.task_generator()
+                assistant.crew_generator()
+
+            result = assistant.invoke(
+                prompt=payload.query,
+                use_memory=payload.use_memory,
+                save_chat_history=payload.save_chat_history,
+                response_format=payload.response_format,
+            )
+            return result
+        except Exception as e:
+            return {"error": str(e)}
+
+    @app.get("/memory")
+    def get_memory(query: str):
+        try:
+            memory = assistant.retrieve_memory(query)
+            return {"query": query, "memory": memory}
+        except Exception as e:
+            return {"error": str(e)}
+        
+    @app.get("/sample_payload")
+    def get_sample_payload():
+        sample_payload = {
+            "query": "What is the capital of France?",
+            "use_memory": True,
+            "save_chat_history": True,
+            "response_format": "text",
+            "schema": {
+                'hint': 'use function extract_dynamic_schema from module general'
+            }
+        }
+        return sample_payload
+
+    return app

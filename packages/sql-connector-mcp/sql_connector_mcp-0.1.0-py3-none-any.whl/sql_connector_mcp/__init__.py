6560ae018e5cc8418cc0a7d3211fd776701ac5fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-30 03:20:27 +0000

    Add package sql-connector-mcp/0.1.0/sql_connector_mcp-0.1.0-py3-none-any.whl

---
+++ packages/sql-connector-mcp/sql_connector_mcp-0.1.0-py3-none-any.whl/sql_connector_mcp/__init__.py
@@ -0,0 +1,9 @@
+from .server import fetch_form, all_table_names, filter_table_names, schema_definitions, execute_query
+
+__all__ = [
+    "fetch_form",
+    "all_table_names",
+    "filter_table_names",
+    "schema_definitions",
+    "execute_query"
+]

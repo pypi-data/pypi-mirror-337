ff9ca73837bc10a437ef7ad9084d52d5103b804c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-30 15:05:02 +0000

    Add package pdefinder/0.1.1/pdefinder-0.1.1.tar.gz

---
+++ packages/pdefinder/pdefinder-0.1.1.tar.gz/pdefinder-0.1.1/pdefinder/__init__.py
@@ -0,0 +1,13 @@
+# __init__.py
+from .core import (
+    data_load,
+    subsample_data,
+    build_linear_system,
+    TrainSTRidge,
+    print_and_save_pde,
+    print_and_save_pde_func_final,
+    extract_terms_from_pde,
+    llm_get_coefficients,
+    verify_llm,
+    run_pde_finder  # the main pipeline function
+)

5f075ca51b5744e172e3fea6c97677c3ca635f4a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-30 04:04:58 +0000

    Add package meu-investimento-alura-djflucena/0.1/meu_investimento_alura_djflucena-0.1.tar.gz

---
+++ packages/meu-investimento-alura-djflucena/meu_investimento_alura_djflucena-0.1.tar.gz/meu_investimento_alura_djflucena-0.1/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+   name='meu_investimento_alura_djflucena',
+   version='0.1',
+   packages=find_packages(),
+   install_requires=[],
+   author='David J F de Lucena',
+   author_email='djflucena@sefaz.al.gov.br',
+   description='Uma biblioteca para cálculos de investimentos.',
+   url='https://github.com/djflucena/meu_investimento',
+   classifiers=[
+       'Programming Language :: Python :: 3',
+       'License :: OSI Approved :: MIT License',
+       'Operating System :: OS Independent',
+   ],
+   python_requires='>=3.6',
+)

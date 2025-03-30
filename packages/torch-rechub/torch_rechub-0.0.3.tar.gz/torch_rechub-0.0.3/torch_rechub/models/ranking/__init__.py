4a9d663047b6e7ea46587e14bacecafe614d95e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-30 14:05:41 +0000

    Add package torch-rechub/0.0.3/torch_rechub-0.0.3.tar.gz

---
+++ packages/torch-rechub/torch_rechub-0.0.3.tar.gz/torch_rechub-0.0.3/torch_rechub/models/ranking/__init__.py
@@ -0,0 +1,11 @@
+from .widedeep import WideDeep
+from .deepfm import DeepFM
+from .din import DIN
+from .dcn import DCN
+from .dcn_v2 import DCNv2
+from .edcn import EDCN
+from .deepffm import DeepFFM, FatDeepFFM
+from .fibinet import FiBiNet
+from .bst import BST
+from .afm import AFM
+from .dien import DIEN

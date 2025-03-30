9ec2f63dcfe554fd9750455bf163b44e686f764f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-30 04:05:17 +0000

    Add package tabpro/0.4.7/tabpro-0.4.7.tar.gz

---
+++ packages/tabpro/tabpro-0.4.7.tar.gz/tabpro-0.4.7/tabpro/commands/aggregate_tables.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+
+import argparse
+
+from .. core.aggregate import aggregate
+
+def run(
+    args: argparse.Namespace,
+):
+    aggregate(
+        input_files=args.input_files,
+        output_file=args.output_file,
+        verbose=args.verbose,
+    )
+
+def setup_parser(
+    parser: argparse.ArgumentParser,
+):
+    parser.add_argument(
+        'input_files',
+        metavar='input-file',
+        nargs='+',
+        help='Input files to aggregate',
+    )
+    parser.add_argument(
+        '--output-file', '--output',
+        required=False,
+        help='Path to output file',
+    )
+    parser.add_argument(
+        '--verbose', '-v',
+        action='store_true',
+        help='Verbose output',
+    )
+    parser.set_defaults(handler=run)

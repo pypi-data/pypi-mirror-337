3a7d412a924ef37a06f520ac549f773dd720a2a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-30 04:05:04 +0000

    Add package database-mysql-local/0.0.443/database_mysql_local-0.0.443-py3-none-any.whl

---
+++ packages/database-mysql-local/database_mysql_local-0.0.443-py3-none-any.whl/database_mysql_local/point.py
@@ -0,0 +1,23 @@
+from .to_sql_interface import ToSQLInterface
+
+
+class Point(ToSQLInterface):
+    def __init__(self, longitude: float, latitude: float) -> None:
+        self.longitude = longitude
+        self.latitude = latitude
+
+    # TODO: POINT(%s, %s) with params
+    def to_sql(self) -> str:
+        return f"POINT ({self.longitude}, {self.latitude})"
+
+    @staticmethod
+    def create_select_stmt(column_name: str) -> str:
+        return f"ST_X({column_name}), ST_Y({column_name})"
+
+    def __eq__(self, other_point: 'Point') -> bool:
+        return (isinstance(other_point, Point) and
+                self.longitude == other_point.longitude and
+                self.latitude == other_point.latitude)
+
+    def __repr__(self) -> str:
+        return f"Point(longitude={self.longitude}, latitude={self.latitude})"

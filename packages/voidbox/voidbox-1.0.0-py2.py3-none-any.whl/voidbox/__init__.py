5c0dc59964295e293bcd1725b442799e37312dd5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-30 03:20:25 +0000

    Add package voidbox/1.0.0/voidbox-1.0.0-py2.py3-none-any.whl

---
+++ packages/voidbox/voidbox-1.0.0-py2.py3-none-any.whl/voidbox/__init__.py
@@ -0,0 +1,6 @@
+"""Free cloud file storage, built on `Voidbox.alhasubji.store` API's
+"""
+all = ["VoidBox"]
+from .VoidBoxCloud import VoidBoxAPI
+from .VoidBoxCloud import Create
+__version__='1.0.0'

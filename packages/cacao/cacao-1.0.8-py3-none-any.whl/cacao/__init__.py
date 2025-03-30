6ae52df814508411e69bf9191d9ad1963fa96535
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-30 15:04:33 +0000

    Add package cacao/1.0.8/cacao-1.0.8-py3-none-any.whl

---
+++ packages/cacao/cacao-1.0.8-py3-none-any.whl/cacao/__init__.py
@@ -0,0 +1,18 @@
+"""
+Cacao - A high-performance, reactive web framework for Python
+"""
+
+__version__ = "1.0.8"
+
+from .core.app import App
+from .core.decorators import mix
+from .core import run, run_desktop, State, Component
+
+__all__ = [
+    "App",
+    "mix",
+    "run",
+    "run_desktop",
+    "State",
+    "Component"
+]

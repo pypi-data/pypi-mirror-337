1e424a30e9c850a575943ec087e8143da7ba8984
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-30 03:20:51 +0000

    Add package lyrics-in-terminal/1.8.0/lyrics_in_terminal-1.8.0-py3-none-any.whl

---
+++ packages/lyrics-in-terminal/lyrics_in_terminal-1.8.0-py3-none-any.whl/lyrics/lyrics_in_terminal.py
@@ -0,0 +1,87 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+from lyrics.config import Config
+from lyrics.player import Player
+from lyrics.window import Window
+import traceback
+
+import sys
+import curses
+
+
+def ErrorHandler(func):
+    def wrapper(*args, **kwargs):
+        try:
+            curses.wrapper(func)
+        except KeyboardInterrupt:
+            pass
+        except curses.error as err:
+            print('Please increase terminal window size!')
+        except Exception as err:
+            print('Unexpected exception occurred.', sys.exc_info(), err)
+            traceback.print_exc()
+
+    return wrapper
+
+
+@ErrorHandler
+def init_pager(stdscr=None):
+    defaults = Config('OPTIONS')
+
+    if len(sys.argv) >= 2:
+        player_name = sys.argv[1].strip()
+        autoswitch = False
+
+    else:
+        player_name = defaults['player'].strip()
+        autoswitch = defaults.getboolean('autoswitch')
+
+    align = defaults['alignment']
+
+    if align == 'center':
+        align = 0
+    elif align == 'right':
+        align = 2
+    else:
+        align = 1
+
+    interval = defaults['interval']
+    source = defaults['source']
+    mpd_connect = [defaults['mpd_host'],
+                   defaults['mpd_port'], defaults['mpd_pass']]
+
+    player = Player(player_name, source, autoswitch, mpd_connect, align=align)
+    win = Window(stdscr, player, timeout=interval)
+
+    win.main()
+
+
+def main():
+    if len(sys.argv) >= 2:
+        if sys.argv[1] == '-t':
+            try:
+                artist = sys.argv[2].strip()
+                title = sys.argv[3].strip()
+            except IndexError:
+                print(
+                    'Please provide track info in format "-t {artist} {title}".')
+                exit(1)
+
+            from lyrics.track import Track
+
+            track = Track(artist=artist, title=title)
+            track.get_lyrics('any')
+
+            print(track.track_name)
+            print('-' * track.width, '\n')
+            print(track.get_text())
+
+            exit(0)
+
+    init_pager()
+
+
+if __name__ == "__main__":
+    main()
---
+++ packages/lyrics-in-terminal/lyrics_in_terminal-1.8.0-py3-none-any.whl/lyrics/track.py
@@ -0,0 +1,107 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+from lyrics import util
+
+
+class Track:
+    def __init__(self,
+                 artist='',
+                 title='',
+                 align=1,
+                 width=0):
+
+        self.title = title
+        self.artist = artist
+        self.alignment = align
+        self.width = width
+        self.length = 0
+        self.lyrics = []
+        self.source = None
+        self.album = None
+        self.trackid = None
+        self.sources = ['azlyrics', 'genius']
+
+    def __str__(self):
+        ''' trackname in format "{artist} - {title}"
+        '''
+        return self.artist + ' - ' + self.title
+
+    @property
+    def track_name(self):
+        ''' returns trackname in format "{artist} - {title}"
+        '''
+        return self.artist + ' - ' + self.title
+
+    def reset_width(self):
+        ''' reset track width (when track/lyrics is changed)
+        '''
+        self.width = len(max(self.lyrics, key=len))
+
+    def track_info(self, width):
+        ''' returns properly formated (3 lined, title, artist, album) track info for 
+            title 
+        '''
+        trackinfo = util.align([self.title, self.artist, self.album], width, self.alignment)
+        
+        offset = self.alignment%2
+        padding = ' ' * offset
+        trackinfo = [padding + t + ' ' * (width - len(t) - offset) for t in trackinfo]
+
+        return trackinfo
+
+    def update(self, artist, title, album, trackid):
+        ''' update currently playing track info, (change track)
+        '''
+        self.artist = artist
+        self.title = title
+        self.album = album
+        self.trackid = trackid
+        # self.art_url = art_url
+        # self.get_lyrics()
+
+    def get_lyrics(self, source, cycle_source=False, cache=True):
+        ''' fetch lyrics off the internet
+        '''
+        if self.source is None or self.source == 'cache':
+            self.source = source or self.sources[0]
+
+        if cycle_source is True and self.source not in self.sources:
+            self.source = self.sources[0]
+
+        if cycle_source:
+            curr_source = self.sources.index(self.source)
+            next_source = (curr_source + 1) % len(self.sources)
+            source = self.sources[next_source]
+            cache = False
+        else:
+            source = 'any'
+
+        self.lyrics, self.source = util.get_lyrics(self.track_name, source, cache=cache)
+        self.width = len(max(self.lyrics, key=len))
+        self.length = len(self.lyrics)
+
+    def get_text(self, wrap=False, width=0):
+        ''' returns lyrics text seperated by '\\n'
+        '''
+        if wrap:
+            lyrics=util.wrap_text(self.lyrics, width)
+        else:
+            lyrics=self.lyrics
+
+        self.width = len(max(lyrics, key=len))
+        self.length = len(lyrics)
+
+        lyrics = util.align(lyrics, self.width, self.alignment)
+
+        return '\n'.join(line for line in lyrics)
+    
+    def edit_lyrics(self):
+        ''' open lyrics file in text editor present in CONFIG path
+        '''
+        util.edit_lyrics(self.track_name)
+
+
+    def delete_lyrics(self):
+        ''' delete lyrics file
+        '''
+        return util.delete_lyrics(self.track_name)

7cd2f5ca61db6a8351cc754a8305239bd2e89093
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-30 05:05:19 +0000

    Add package gitbase/0.5.2/gitbase-0.5.2.tar.gz

---
+++ packages/gitbase/gitbase-0.5.2.tar.gz/gitbase-0.5.2/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="gitbase",
+    version="0.5.2",
+    author="Taireru LLC",
+    author_email="tairerullc@gmail.com",
+    description="A GitHub based database system ('GitBase') which offers offline backups and optional encryption.",
+    long_description=open("README.md", "r", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/TaireruLLC/gitbase",
+    packages=find_packages(),
+    install_requires=[
+        "requests",
+        "cryptography",
+        "altcolor>=0.0.5",
+        "moviepy",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    license="MIT",
+)

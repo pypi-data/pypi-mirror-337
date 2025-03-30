cd5c6ae912a65cf4d768b03be8f1bb9222ff253b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-30 04:04:59 +0000

    Add package fsrs/5.1.3/fsrs-5.1.3-py3-none-any.whl

---
+++ packages/fsrs/fsrs-5.1.3-py3-none-any.whl/fsrs/__init__.py
@@ -0,0 +1,27 @@
+"""
+py-fsrs
+-------
+
+Py-FSRS is the official Python implementation of the FSRS scheduler algorithm, which can be used to develop spaced repetition systems.
+"""
+
+from .fsrs import (
+    Scheduler,
+    Card,
+    Rating,
+    ReviewLog,
+    State,
+    DEFAULT_PARAMETERS,
+)
+
+from .optimizer import Optimizer
+
+__all__ = [
+    "Scheduler",
+    "Card",
+    "Rating",
+    "ReviewLog",
+    "State",
+    "Optimizer",
+    "DEFAULT_PARAMETERS",
+]

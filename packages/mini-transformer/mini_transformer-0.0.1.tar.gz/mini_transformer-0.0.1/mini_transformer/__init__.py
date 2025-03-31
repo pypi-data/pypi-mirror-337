36a08b2645b9f888d666edb8f1fc0c745660d54e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-31 03:19:39 +0000

    Add package mini-transformer/0.0.1/mini_transformer-0.0.1.tar.gz

---
+++ packages/mini-transformer/mini_transformer-0.0.1.tar.gz/mini_transformer-0.0.1/mini_transformer/__init__.py
@@ -0,0 +1,17 @@
+from .layer import (
+    RMSNorm,
+    MLA,
+    GQA,
+    MLP,
+    Block,
+    Transformer
+)
+
+__all__ = [
+    'RMSNorm',
+    'MLA',
+    'GQA',
+    'MLP',
+    'Block',
+    'Transformer',
+]

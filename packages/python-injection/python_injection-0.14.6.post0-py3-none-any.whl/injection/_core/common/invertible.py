4a43e6920c26526c69a0650d3f021804848a92c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-31 06:05:11 +0000

    Add package python-injection/0.14.6.post0/python_injection-0.14.6.post0-py3-none-any.whl

---
+++ packages/python-injection/python_injection-0.14.6.post0-py3-none-any.whl/injection/_core/common/invertible.py
@@ -0,0 +1,19 @@
+from abc import abstractmethod
+from collections.abc import Callable
+from dataclasses import dataclass
+from typing import Protocol, runtime_checkable
+
+
+@runtime_checkable
+class Invertible[T](Protocol):
+    @abstractmethod
+    def __invert__(self) -> T:
+        raise NotImplementedError
+
+
+@dataclass(repr=False, eq=False, frozen=True, slots=True)
+class SimpleInvertible[T](Invertible[T]):
+    callable: Callable[..., T]
+
+    def __invert__(self) -> T:
+        return self.callable()

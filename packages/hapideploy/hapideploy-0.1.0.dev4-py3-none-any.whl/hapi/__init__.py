fa0b1014ae71c30a30bdec8c7c7fb740d4727eb1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-31 14:05:18 +0000

    Add package hapideploy/0.1.0.dev4/hapideploy-0.1.0.dev4-py3-none-any.whl

---
+++ packages/hapideploy/hapideploy-0.1.0.dev4-py3-none-any.whl/hapi/__init__.py
@@ -0,0 +1,20 @@
+"""hapideploy"""
+
+from .__version import __version__
+from .core import (
+    ArrayInputOutput,
+    CommandResult,
+    ConsoleInputOutput,
+    Container,
+    Context,
+    Deployer,
+    InputOutput,
+    Printer,
+    Program,
+    Provider,
+    Proxy,
+    Remote,
+    RemoteBag,
+    Task,
+    TaskBag,
+)

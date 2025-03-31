a2241e5ab23bfa3077d779f5398a3ee372f03db4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-31 14:05:30 +0000

    Add package agentr/0.1.8/agentr-0.1.8.tar.gz

---
+++ packages/agentr/agentr-0.1.8.tar.gz/agentr-0.1.8/src/agentr/integrations/__init__.py
@@ -0,0 +1,5 @@
+from agentr.integrations.agentr import AgentRIntegration
+from agentr.integrations.base import Integration
+from agentr.integrations.api_key import ApiKeyIntegration
+
+__all__ = ["AgentRIntegration", "Integration", "ApiKeyIntegration"]

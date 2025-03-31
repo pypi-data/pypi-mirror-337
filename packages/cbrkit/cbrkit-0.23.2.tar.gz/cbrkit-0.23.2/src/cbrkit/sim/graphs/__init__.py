d00cfbfa6bc43be0029864fe7272d6c855c592df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-31 15:05:54 +0000

    Add package cbrkit/0.23.2/cbrkit-0.23.2.tar.gz

---
+++ packages/cbrkit/cbrkit-0.23.2.tar.gz/cbrkit-0.23.2/src/cbrkit/sim/graphs/__init__.py
@@ -0,0 +1,24 @@
+from ...helpers import optional_dependencies
+from . import astar
+from .brute_force import brute_force
+from .common import ElementMatcher, GraphSim, default_element_matcher
+from .isomorphism import isomorphism
+from .precompute import precompute
+
+with optional_dependencies():
+    from .alignment import dtw
+
+with optional_dependencies():
+    from .alignment import smith_waterman
+
+__all__ = [
+    "astar",
+    "brute_force",
+    "isomorphism",
+    "precompute",
+    "dtw",
+    "smith_waterman",
+    "GraphSim",
+    "ElementMatcher",
+    "default_element_matcher",
+]

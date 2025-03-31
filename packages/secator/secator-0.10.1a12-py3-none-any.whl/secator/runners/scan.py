f30070a4de7f0b8a108f50e1dd2eed081e0bd7dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-31 15:06:00 +0000

    Add package secator/0.10.1a12/secator-0.10.1a12-py3-none-any.whl

---
+++ packages/secator/secator-0.10.1a12-py3-none-any.whl/secator/runners/scan.py
@@ -0,0 +1,56 @@
+import logging
+
+from secator.config import CONFIG
+from secator.runners._base import Runner
+from secator.runners.workflow import Workflow
+from secator.utils import merge_opts
+
+logger = logging.getLogger(__name__)
+
+
+class Scan(Runner):
+
+	default_exporters = CONFIG.scans.exporters
+
+	@classmethod
+	def delay(cls, *args, **kwargs):
+		from secator.celery import run_scan
+		return run_scan.delay(args=args, kwargs=kwargs)
+
+	def build_celery_workflow(self):
+		"""Build Celery workflow for scan execution.
+
+		Returns:
+			celery.Signature: Celery task signature.
+		"""
+		from celery import chain
+		from secator.celery import mark_runner_started, mark_runner_completed
+		from secator.template import TemplateLoader
+
+		scan_opts = self.config.options
+
+		# Build chain of workflows
+		sigs = []
+		for name, workflow_opts in self.config.workflows.items():
+			run_opts = self.run_opts.copy()
+			run_opts['no_poll'] = True
+			opts = merge_opts(scan_opts, workflow_opts, run_opts)
+			config = TemplateLoader(name=f'workflows/{name}')
+			workflow = Workflow(
+				config,
+				self.inputs,
+				results=self.results,
+				run_opts=opts,
+				hooks=self._hooks,
+				context=self.context.copy()
+			)
+			celery_workflow = workflow.build_celery_workflow()
+			for task_id, task_info in workflow.celery_ids_map.items():
+				self.add_subtask(task_id, task_info['name'], task_info['descr'])
+			sigs.append(celery_workflow)
+
+		return chain(
+			mark_runner_started.si(self).set(queue='results'),
+			*sigs,
+			mark_runner_completed.s(self).set(queue='results'),
+		)

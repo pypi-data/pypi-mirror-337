04e1d5e8f2c48e7985b60722aa1c8d58db7a495d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-31 14:05:25 +0000

    Add package date-day-estimator/0.0.0/date_day_estimator-0.0.0.tar.gz

---
+++ packages/date-day-estimator/date_day_estimator-0.0.0.tar.gz/date_day_estimator-0.0.0/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="date_day_estimator",
+    version="0.0.0",
+    description="To estimate the day of the week from date/month/year info",
+    author="Mayur Waghchoure",
+    packages= find_packages(),
+    install_requires = [
+        'numpy',
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3.10',
+        'Operating System :: OS Independent',
+    ],
+
+)

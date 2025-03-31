85f06766f0633532442c7568de1f3a4f668049bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-31 06:05:22 +0000

    Add package MMP-PaintGunParam/0.1.0/mmp_paintgunparam-0.1.0.tar.gz

---
+++ packages/MMP-PaintGunParam/mmp_paintgunparam-0.1.0.tar.gz/mmp_paintgunparam-0.1.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='MMP_PaintGunParam',
+    version='0.1.0',
+    description='Package allowing operations of a paint gun controller installed on a robot.',
+    url='https://github.com/neurobotia/MMP-PaintGunParam/MMP_PaintGunParam',
+    author='Technologies NeuroBotIA Inc.',
+    author_email='michel.lessard@neurobotia.com',
+    license='None',
+    packages=['MMP_PaintGunParam'],
+    install_requires=['open3d==0.18.0',
+                      'numpy==1.26.4',
+                      'PyYAML==6.0',
+                      'pyModbusTCP==0.2.1'],
+)

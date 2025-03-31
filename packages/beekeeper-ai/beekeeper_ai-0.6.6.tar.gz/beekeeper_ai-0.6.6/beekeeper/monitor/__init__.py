0658820a4a91ef8d563d98c8b0149da199f09499
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-31 14:05:03 +0000

    Add package beekeeper-ai/0.6.6/beekeeper_ai-0.6.6.tar.gz

---
+++ packages/beekeeper-ai/beekeeper_ai-0.6.6.tar.gz/beekeeper_ai-0.6.6/beekeeper/monitor/__init__.py
@@ -0,0 +1,11 @@
+from beekeeper.monitor.watsonx import (
+    CloudPakforDataCredentials,
+    WatsonxExternalPromptMonitoring,
+    WatsonxPromptMonitoring,
+)
+
+__all__ = [
+    "CloudPakforDataCredentials",
+    "WatsonxExternalPromptMonitoring",
+    "WatsonxPromptMonitoring"
+]

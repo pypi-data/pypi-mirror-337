f4801fe6c3a7ea275814e1af81a0c779f05d9713
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-31 03:19:41 +0000

    Add package zmp-alert-mcp-server/0.1.6/zmp_alert_mcp_server-0.1.6.tar.gz

---
+++ packages/zmp-alert-mcp-server/zmp_alert_mcp_server-0.1.6.tar.gz/zmp_alert_mcp_server-0.1.6/src/zmp_alert_mcp_server/operations.py
@@ -0,0 +1,10 @@
+operations = {
+    "get_alerts": {
+        "path": "/api/alert/v1/alerts",
+        "method": "GET"
+    },
+    "get_alert": {
+        "path": "/api/alert/v1/alerts/{alert_id}",
+        "method": "GET"
+    }
+}

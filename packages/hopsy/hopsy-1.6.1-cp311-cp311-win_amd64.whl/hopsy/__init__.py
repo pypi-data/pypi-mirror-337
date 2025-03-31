85be12b6a625586b5fdc170345976a4252fc57b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-31 06:05:19 +0000

    Add package hopsy/1.6.1/hopsy-1.6.1-cp311-cp311-win_amd64.whl

---
+++ packages/hopsy/hopsy-1.6.1-cp311-cp311-win_amd64.whl/hopsy/__init__.py
@@ -0,0 +1,11 @@
+from .core import *
+
+del globals()["MarkovChain"]
+from .callback import *
+from .core import __build__, __is_debug__, __version__
+from .definitions import *
+from .examples import *
+from .lp import *
+from .misc import *
+from .setup import *
+from .tuning import *

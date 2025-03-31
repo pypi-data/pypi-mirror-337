aa72283af5fba2e656f0d7192bb540ae9d9d783b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-31 03:19:31 +0000

    Add package pycityproto/2.2.15/pycityproto-2.2.15-py3-none-any.whl

---
+++ packages/pycityproto/pycityproto-2.2.15-py3-none-any.whl/pycityproto/city/person/v2/__init__.pyi
@@ -0,0 +1,7 @@
+from . import carbon_pb2
+from . import motion_pb2
+from . import person_pb2
+from . import person_runtime_pb2
+from . import person_service_pb2
+from . import taxi_pb2
+from . import vehicle_pb2

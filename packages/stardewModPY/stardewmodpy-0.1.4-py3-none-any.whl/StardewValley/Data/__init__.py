246cb8f6cd53c20a900e5a90c74021ce1b3a579a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-31 14:04:59 +0000

    Add package stardewModPY/0.1.4/stardewmodpy-0.1.4-py3-none-any.whl

---
+++ packages/stardewModPY/stardewmodpy-0.1.4-py3-none-any.whl/StardewValley/Data/__init__.py
@@ -0,0 +1,30 @@
+from .Achievements import AchievementsData
+from .AdditionalFarms import AdditionalFarmsData
+from .AdditionalWallpaperFlooring import AdditionalWallpaperFlooringData
+from .AnimationDescriptions import AnimationDescriptionsData
+from .AquariumFish import AquariumFishData
+from .BigCraftables import BigCraftablesData
+from .Boots import BootsData
+from .Buffs import BuffsData
+from .Buildings import BuildingsData
+from .Bundles import BundlesData
+from .ChairTiles import ChairTiles
+from .Characters import CharactersData
+from .Concessions import ConcessionsData
+from .ConcessionTastes import ConcessionTastesData
+from .CookingRecipes import CookingRecipesData
+
+from .Objects import ObjectsData
+from .Tools import ToolsData
+from .TriggerActions import TriggerActionsData
+from .Trinkets import TrinketsData
+from .Weapons import WeaponsData
+from .WildTrees import WildTreesData
+
+__all__ = [
+    "AchievementsData", "AdditionalFarmsData", "AdditionalWallpaperFlooringData",
+    "AnimationDescriptionsData", "AquariumFishData", "BigCraftablesData", "BootsData",
+    "BuffsData", "BuildingsData", "BundlesData", "ChairTiles", "CharactersData",
+    "ConcessionsData", "ConcessionTastesData", "CookingRecipesData", "ToolsData",
+    "TriggerActionsData", "TrinketsData", "WeaponsData", "WildTreesData", "ObjectsData"
+]
---
+++ packages/stardewModPY/stardewmodpy-0.1.4-py3-none-any.whl/StardewValley/Data/model.py
@@ -0,0 +1,8 @@
+from typing import Optional, List, Any
+
+class modelsData:
+    def __init__(self, key: str|int):
+        pass
+
+    def getJson(self) -> Any:
+        return {}

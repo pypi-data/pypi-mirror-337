82490a74b10a8380cfd7696efede2c8688a06b86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-31 15:06:14 +0000

    Add package ecmhongz/0.1.3/ecmhongz-0.1.3.tar.gz

---
+++ packages/ecmhongz/ecmhongz-0.1.3.tar.gz/ecmhongz-0.1.3/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="ecmhongz",  # 你的包名称
+    version="0.1.3",  # 版本号
+    author="HongzhenHuang",
+    author_email="202130500072@mail.scut.edn.cn", # 你的邮箱
+    description="ecmhong is a Python package for Energy Consumption Monitoring.",  # 简短描述
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/SusCom-Lab/ECM-Tool",  # 你的项目主页
+    install_requires=[
+        "psutil",
+        "mysql-connector-python",  # mysql.connector 需要这个包
+        "pandas",
+        "plotly",
+        "deprecated",
+        "numpy",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.7",  # 适用的 Python 版本
+)

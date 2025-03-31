b67524b3533ee1f47f7573e3bec742f1e35353ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-31 04:04:49 +0000

    Add package stardewModPY/0.1.2/stardewmodpy-0.1.2.tar.gz

---
+++ packages/stardewModPY/stardewmodpy-0.1.2.tar.gz/stardewmodpy-0.1.2/StardewValley/Data/__init__.py
@@ -0,0 +1,16 @@
+from .Achievements import AchievementsData
+from .AdditionalFarms import AdditionalFarmsData
+from .AdditionalWallpaperFlooring import AdditionalWallpaperFlooringData
+from .AnimationDescriptions import AnimationDescriptionsData
+from .AquariumFish import AquariumFishData
+from .BigCraftables import BigCraftablesData
+from .Boots import BootsData
+from .Buffs import BuffsData
+from .Buildings import BuildingsData
+from .Objects import ObjectsData
+
+__all__ = [
+    "AchievementsData", "AdditionalFarmsData", "AdditionalWallpaperFlooringData",
+    "AnimationDescriptionsData", "AquariumFishData", "BigCraftablesData",
+    "BootsData", "BuffsData", "BuildingsData", "ObjectsData"
+]

6bac10dcf3901d463a99875ac4af8845fc743dea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-31 14:04:59 +0000

    Add package wyzeapy/0.5.28/wyzeapy-0.5.28-py3-none-any.whl

---
+++ packages/wyzeapy/wyzeapy-0.5.28-py3-none-any.whl/wyzeapy/__init__.py
@@ -0,0 +1,264 @@
+#  Copyright (c) 2021. Mulliken, LLC - All Rights Reserved
+#  You may use, distribute and modify this code under the terms
+#  of the attached license. You should have received a copy of
+#  the license with this file. If not, please write to:
+#  katie@mulliken.net to receive a copy
+import logging
+from inspect import iscoroutinefunction
+from typing import List, Optional, Set, Callable
+
+from .const import PHONE_SYSTEM_TYPE, APP_VERSION, SC, APP_VER, SV, PHONE_ID, APP_NAME, OLIVE_APP_ID, APP_INFO
+from .crypto import olive_create_signature
+from .exceptions import TwoFactorAuthenticationEnabled
+from .payload_factory import olive_create_user_info_payload
+from .services.base_service import BaseService
+from .services.bulb_service import BulbService
+from .services.camera_service import CameraService
+from .services.hms_service import HMSService
+from .services.lock_service import LockService
+from .services.sensor_service import SensorService
+from .services.switch_service import SwitchService, SwitchUsageService
+from .services.thermostat_service import ThermostatService
+from .services.wall_switch_service import WallSwitchService
+from .wyze_auth_lib import WyzeAuthLib, Token
+
+_LOGGER = logging.getLogger(__name__)
+
+
+class Wyzeapy:
+    """A module to assist developers in interacting with the Wyze service"""
+    # _client: Client
+    _auth_lib: WyzeAuthLib
+
+    def __init__(self):
+        self._bulb_service = None
+        self._switch_service = None
+        self._camera_service = None
+        self._thermostat_service = None
+        self._hms_service = None
+        self._lock_service = None
+        self._sensor_service = None
+        self._wall_switch_service = None
+        self._switch_usage_service = None
+        self._email = None
+        self._password = None
+        self._key_id = None
+        self._api_key = None
+        self._service: Optional[BaseService] = None
+        self._token_callbacks: List[Callable] = []
+
+    @classmethod
+    async def create(cls):
+        """
+        Creates the Wyzeapy class in an async way. Although this is not currently utilized
+
+        :return: An instance of the Wyzeapy class
+        """
+        self = cls()
+        return self
+
+    async def login(
+        self, email, password, key_id, api_key, token: Optional[Token] = None
+    ):
+        """
+        Logs the user in and retrieves the users token
+
+        :param email: Users email
+        :param password: Users password
+        :param key_id: Key ID for third-party API access
+        :param api_key: API Key for third-party API access
+        :param token: Users existing token from a previous session
+
+        :raises:
+            TwoFactorAuthenticationEnabled: indicates that the account has 2fa enabled
+        """
+
+        self._email = email
+        self._password = password
+        self._key_id = key_id
+        self._api_key = api_key
+
+        try:
+            self._auth_lib = await WyzeAuthLib.create(
+                email, password, key_id, api_key, token, self.execute_token_callbacks
+            )
+            if token:
+                # User token supplied, refresh on startup
+                await self._auth_lib.refresh()
+            else:
+                await self._auth_lib.get_token_with_username_password(
+                    email, password, key_id, api_key
+                )
+            self._service = BaseService(self._auth_lib)
+        except TwoFactorAuthenticationEnabled as error:
+            raise error
+
+    async def login_with_2fa(self, verification_code) -> Token:
+        """
+        Logs the user in and retrieves the users token
+
+        :param verification_code: Users 2fa verification code
+
+        """
+
+        _LOGGER.debug(f"Verification Code: {verification_code}")
+
+        await self._auth_lib.get_token_with_2fa(verification_code)
+        self._service = BaseService(self._auth_lib)
+        return self._auth_lib.token
+
+    async def execute_token_callbacks(self, token: Token):
+        """
+        Sends the token to the registered callback functions.
+
+        :param token: Users token object
+
+        """
+        for callback in self._token_callbacks:
+            if iscoroutinefunction(callback):
+                await callback(token)
+            else:
+                callback(token)
+
+    def register_for_token_callback(self, callback_function):
+        """
+        Register a callback to be called whenever the user's token is modified
+
+        :param callback_function: A callback function which expects a token object
+
+        """
+        self._token_callbacks.append(callback_function)
+
+    def unregister_for_token_callback(self, callback_function):
+        """
+        Register a callback to be called whenever the user's token is modified
+
+        :param callback_function: A callback function which expects a token object
+
+        """
+        self._token_callbacks.remove(callback_function)
+
+    @property
+    async def unique_device_ids(self) -> Set[str]:
+        """
+        Returns a list of all device ids known to the server
+        :return: A set containing the unique device ids
+        """
+
+        devices = await self._service.get_object_list()
+        device_ids = set()
+        for device in devices:
+            device_ids.add(device.mac)
+
+        return device_ids
+
+    @property
+    async def notifications_are_on(self) -> bool:
+        """
+        Reports the status of the notifications
+
+        :return: True if the notifications are enabled
+        """
+
+        response_json = await self._service.get_user_profile()
+        return response_json['data']['notification']
+
+    async def enable_notifications(self):
+        """Enables notifications on the account"""
+
+        await self._service.set_push_info(True)
+
+    async def disable_notifications(self):
+        """Disables notifications on the account"""
+
+        await self._service.set_push_info(False)
+
+    @classmethod
+    async def valid_login(
+        cls, email: str, password: str, key_id: str, api_key: str
+    ) -> bool:
+        """
+        Checks to see if a username and password return a valid login
+
+        :param email: The users email
+        :param password: The users password
+        :param key_id: Key ID for third-party API access
+        :param api_key: API Key for third-party API access
+        :return: True if the account can connect
+        """
+
+        self = cls()
+        await self.login(email, password, key_id, api_key)
+
+        return not self._auth_lib.should_refresh
+
+    @property
+    async def bulb_service(self) -> BulbService:
+        """Returns an instance of the bulb service"""
+
+        if self._bulb_service is None:
+            self._bulb_service = BulbService(self._auth_lib)
+        return self._bulb_service
+
+    @property
+    async def switch_service(self) -> SwitchService:
+        """Returns an instance of the switch service"""
+
+        if self._switch_service is None:
+            self._switch_service = SwitchService(self._auth_lib)
+        return self._switch_service
+
+    @property
+    async def camera_service(self) -> CameraService:
+        """Returns an instance of the camera service"""
+
+        if self._camera_service is None:
+            self._camera_service = CameraService(self._auth_lib)
+        return self._camera_service
+
+    @property
+    async def thermostat_service(self) -> ThermostatService:
+        """Returns an instance of the thermostat service"""
+
+        if self._thermostat_service is None:
+            self._thermostat_service = ThermostatService(self._auth_lib)
+        return self._thermostat_service
+
+    @property
+    async def hms_service(self) -> HMSService:
+        """Returns an instance of the hms service"""
+
+        if self._hms_service is None:
+            self._hms_service = await HMSService.create(self._auth_lib)
+        return self._hms_service
+
+    @property
+    async def lock_service(self) -> LockService:
+        """Returns an instance of the lock service"""
+
+        if self._lock_service is None:
+            self._lock_service = LockService(self._auth_lib)
+        return self._lock_service
+
+    @property
+    async def sensor_service(self) -> SensorService:
+        """Returns an instance of the sensor service"""
+
+        if self._sensor_service is None:
+            self._sensor_service = SensorService(self._auth_lib)
+        return self._sensor_service
+
+    @property
+    async def wall_switch_service(self) -> WallSwitchService:
+        """Returns an instance of the switch service"""
+
+        if self._wall_switch_service is None:
+            self._wall_switch_service = WallSwitchService(self._auth_lib)
+        return self._wall_switch_service
+
+    @property
+    async def switch_usage_service(self) -> SwitchUsageService:
+        """Returns an instance of the switch usage service"""
+        if self._switch_usage_service is None:
+            self._switch_usage_service = SwitchUsageService(self._auth_lib)
+        return self._switch_usage_service
---
+++ packages/wyzeapy/wyzeapy-0.5.28-py3-none-any.whl/wyzeapy/wyze_auth_lib.py
@@ -0,0 +1,335 @@
+#  Copyright (c) 2021. Mulliken, LLC - All Rights Reserved
+#  You may use, distribute and modify this code under the terms
+#  of the attached license. You should have received a copy of
+#  the license with this file. If not, please write to:
+#  katie@mulliken.net to receive a copy
+import asyncio
+import logging
+import time
+from typing import Dict, Any, Optional
+
+from aiohttp import TCPConnector, ClientSession, ContentTypeError
+
+from .const import API_KEY, PHONE_ID, APP_NAME, APP_VERSION, SC, SV, PHONE_SYSTEM_TYPE, APP_VER, APP_INFO
+from .exceptions import (
+    UnknownApiError,
+    TwoFactorAuthenticationEnabled,
+    AccessTokenError,
+)
+from .utils import create_password, check_for_errors_standard
+
+_LOGGER = logging.getLogger(__name__)
+
+
+class Token:
+    # Token is apparently good for 24 hours, so refresh after 23
+    REFRESH_INTERVAL = 82800
+
+    def __init__(self, access_token, refresh_token, refresh_time: float = None):
+        self._access_token: str = access_token
+        self._refresh_token: str = refresh_token
+        self.expired = False
+        if refresh_time:
+            self._refresh_time: float = refresh_time
+        else:
+            self._refresh_time: float = time.time() + Token.REFRESH_INTERVAL
+
+    @property
+    def access_token(self):
+        return self._access_token
+
+    @access_token.setter
+    def access_token(self, access_token):
+        self._access_token = access_token
+        self._refresh_time = time.time() + Token.REFRESH_INTERVAL
+
+    @property
+    def refresh_token(self):
+        return self._refresh_token
+
+    @refresh_token.setter
+    def refresh_token(self, refresh_token):
+        self._refresh_token = refresh_token
+
+    @property
+    def refresh_time(self):
+        return self._refresh_time
+
+
+class WyzeAuthLib:
+    token: Optional[Token] = None
+    SANITIZE_FIELDS = [
+        "email",
+        "password",
+        "access_token",
+        "accessToken",
+        "refresh_token",
+        "lat",
+        "lon",
+        "address",
+    ]
+    SANITIZE_STRING = "**Sanitized**"
+
+    def __init__(
+        self,
+        username=None,
+        password=None,
+        key_id=None,
+        api_key=None,
+        token: Optional[Token] = None,
+        token_callback=None,
+    ):
+        self._username = username
+        self._password = password
+        self._key_id = key_id
+        self._api_key = api_key
+        self.token = token
+        self.session_id = ""
+        self.verification_id = ""
+        self.two_factor_type = None
+        self.refresh_lock = asyncio.Lock()
+        self.token_callback = token_callback
+
+    @classmethod
+    async def create(
+        cls,
+        username=None,
+        password=None,
+        key_id=None,
+        api_key=None,
+        token: Optional[Token] = None,
+        token_callback=None,
+    ):
+        self = cls(
+            username=username,
+            password=password,
+            key_id=key_id,
+            api_key=api_key,
+            token=token,
+            token_callback=token_callback,
+        )
+
+        if self._username is None and self._password is None and self.token is None:
+            raise AttributeError("Must provide a username, password or token")
+        elif self.token is None and self._username is not None and self._password is not None:
+            assert self._username != ""
+            assert self._password != ""
+
+        return self
+
+    async def get_token_with_username_password(
+        self, username, password, key_id, api_key
+    ) -> Token:
+        self._username = username
+        self._password = create_password(password)
+        self._key_id = key_id
+        self._api_key = api_key
+        login_payload = {"email": self._username, "password": self._password}
+
+        headers = {
+            "keyid": key_id,
+            "apikey": api_key,
+            "User-Agent": "wyzeapy",
+        }
+
+        response_json = await self.post(
+            "https://auth-prod.api.wyze.com/api/user/login",
+            headers=headers,
+            json=login_payload,
+        )
+
+        if response_json.get('errorCode') is not None:
+            _LOGGER.error(f"Unable to login with response from Wyze: {response_json}")
+            if response_json["errorCode"] == 1000:
+                raise AccessTokenError
+            raise UnknownApiError(response_json)
+
+        if response_json.get('mfa_options') is not None:
+            # Store the TOTP verification setting in the token and raise exception
+            if "TotpVerificationCode" in response_json.get("mfa_options"):
+                self.two_factor_type = "TOTP"
+                # Store the verification_id from the response, it's needed for the 2fa payload.
+                self.verification_id = response_json["mfa_details"]["totp_apps"][0]["app_id"]
+                raise TwoFactorAuthenticationEnabled
+                # 2fa using SMS, store sms as 2fa method in token, send the code then raise exception
+            if "PrimaryPhone" in response_json.get("mfa_options"):
+                self.two_factor_type = "SMS"
+                params = {
+                    'mfaPhoneType': 'Primary',
+                    'sessionId': response_json.get("sms_session_id"),
+                    'userId': response_json['user_id'],
+                }
+                response_json = await self.post('https://auth-prod.api.wyze.com/user/login/sendSmsCode',
+                                                headers=headers, data=params)
+                # Store the session_id from this response, it's needed for the 2fa payload.
+                self.session_id = response_json['session_id']
+                raise TwoFactorAuthenticationEnabled
+
+        self.token = Token(response_json['access_token'], response_json['refresh_token'])
+        await self.token_callback(self.token)
+        return self.token
+
+    async def get_token_with_2fa(self, verification_code) -> Token:
+        headers = {
+            'Phone-Id': PHONE_ID,
+            'User-Agent': APP_INFO,
+            'X-API-Key': API_KEY,
+        }
+        # TOTP Payload
+        if self.two_factor_type == "TOTP":
+            payload = {
+                "email": self._username,
+                "password": self._password,
+                "mfa_type": "TotpVerificationCode",
+                "verification_id": self.verification_id,
+                "verification_code": verification_code
+            }
+        # SMS Payload
+        else:
+            payload = {
+                "email": self._username,
+                "password": self._password,
+                "mfa_type": "PrimaryPhone",
+                "verification_id": self.session_id,
+                "verification_code": verification_code
+            }
+
+        response_json = await self.post(
+            'https://auth-prod.api.wyze.com/user/login',
+            headers=headers, json=payload)
+
+        self.token = Token(response_json['access_token'], response_json['refresh_token'])
+        await self.token_callback(self.token)
+        return self.token
+
+    @property
+    def should_refresh(self) -> bool:
+        return time.time() >= self.token.refresh_time
+
+    async def refresh_if_should(self):
+        if self.should_refresh or self.token.expired:
+            async with self.refresh_lock:
+                if self.should_refresh or self.token.expired:
+                    _LOGGER.debug("Should refresh. Refreshing...")
+                    await self.refresh()
+
+    async def refresh(self) -> None:
+        payload = {
+            "phone_id": PHONE_ID,
+            "app_name": APP_NAME,
+            "app_version": APP_VERSION,
+            "sc": SC,
+            "sv": SV,
+            "phone_system_type": PHONE_SYSTEM_TYPE,
+            "app_ver": APP_VER,
+            "ts": int(time.time()),
+            "refresh_token": self.token.refresh_token
+        }
+
+        headers = {
+            "X-API-Key": API_KEY
+        }
+
+        async with ClientSession(connector=TCPConnector(ttl_dns_cache=(30 * 60))) as _session:
+            response = await _session.post("https://api.wyzecam.com/app/user/refresh_token", headers=headers,
+                                           json=payload)
+        response_json = await response.json()
+        check_for_errors_standard(self, response_json)
+
+        self.token.access_token = response_json['data']['access_token']
+        self.token.refresh_token = response_json['data']['refresh_token']
+        await self.token_callback(self.token)
+        self.token.expired = False
+
+    def sanitize(self, data):
+        if data and type(data) is dict:
+            # value is unused, but it prevents us from having to split the tuple to check against SANITIZE_FIELDS
+            for key, value in data.items():
+                if type(value) is dict:
+                    data[key] = self.sanitize(value)
+                if key in self.SANITIZE_FIELDS:
+                    data[key] = self.SANITIZE_STRING
+        return data
+
+    async def post(self, url, json=None, headers=None, data=None) -> Dict[Any, Any]:
+        async with ClientSession(connector=TCPConnector(ttl_dns_cache=(30 * 60))) as _session:
+            response = await _session.post(url, json=json, headers=headers, data=data)
+            # Relocated these below as the sanitization seems to modify the data before it goes to the post.
+            _LOGGER.debug("Request:")
+            _LOGGER.debug(f"url: {url}")
+            _LOGGER.debug(f"json: {self.sanitize(json)}")
+            _LOGGER.debug(f"headers: {self.sanitize(headers)}")
+            _LOGGER.debug(f"data: {self.sanitize(data)}")
+            # Log the response.json() if it exists, if not log the response.
+            try:
+                response_json = await response.json()
+                _LOGGER.debug(f"Response Json: {self.sanitize(response_json)}")
+            except ContentTypeError:
+                _LOGGER.debug(f"Response: {response}")
+            return await response.json()
+    
+    async def put(self, url, json=None, headers=None, data=None) -> Dict[Any, Any]:
+        async with ClientSession(connector=TCPConnector(ttl_dns_cache=(30 * 60))) as _session:
+            response = await _session.put(url, json=json, headers=headers, data=data)
+            # Relocated these below as the sanitization seems to modify the data before it goes to the post.
+            _LOGGER.debug("Request:")
+            _LOGGER.debug(f"url: {url}")
+            _LOGGER.debug(f"json: {self.sanitize(json)}")
+            _LOGGER.debug(f"headers: {self.sanitize(headers)}")
+            _LOGGER.debug(f"data: {self.sanitize(data)}")
+            # Log the response.json() if it exists, if not log the response.
+            try:
+                response_json = await response.json()
+                _LOGGER.debug(f"Response Json: {self.sanitize(response_json)}")
+            except ContentTypeError:
+                _LOGGER.debug(f"Response: {response}")
+            return await response.json()
+
+    async def get(self, url, headers=None, params=None) -> Dict[Any, Any]:
+        async with ClientSession(connector=TCPConnector(ttl_dns_cache=(30 * 60))) as _session:
+            response = await _session.get(url, params=params, headers=headers)
+            # Relocated these below as the sanitization seems to modify the data before it goes to the post.
+            _LOGGER.debug("Request:")
+            _LOGGER.debug(f"url: {url}")
+            _LOGGER.debug(f"headers: {self.sanitize(headers)}")
+            _LOGGER.debug(f"params: {self.sanitize(params)}")
+            # Log the response.json() if it exists, if not log the response.
+            try:
+                response_json = await response.json()
+                _LOGGER.debug(f"Response Json: {self.sanitize(response_json)}")
+            except ContentTypeError:
+                _LOGGER.debug(f"Response: {response}")
+            return await response.json()
+
+    async def patch(self, url, headers=None, params=None, json=None) -> Dict[Any, Any]:
+        async with ClientSession(connector=TCPConnector(ttl_dns_cache=(30 * 60))) as _session:
+            response = await _session.patch(url, headers=headers, params=params, json=json)
+            # Relocated these below as the sanitization seems to modify the data before it goes to the post.
+            _LOGGER.debug("Request:")
+            _LOGGER.debug(f"url: {url}")
+            _LOGGER.debug(f"json: {self.sanitize(json)}")
+            _LOGGER.debug(f"headers: {self.sanitize(headers)}")
+            _LOGGER.debug(f"params: {self.sanitize(params)}")
+            # Log the response.json() if it exists, if not log the response.
+            try:
+                response_json = await response.json()
+                _LOGGER.debug(f"Response Json: {self.sanitize(response_json)}")
+            except ContentTypeError:
+                _LOGGER.debug(f"Response: {response}")
+            return await response.json()
+
+    async def delete(self, url, headers=None, json=None) -> Dict[Any, Any]:
+        async with ClientSession(connector=TCPConnector(ttl_dns_cache=(30 * 60))) as _session:
+            response = await _session.delete(url, headers=headers, json=json)
+            # Relocated these below as the sanitization seems to modify the data before it goes to the post.
+            _LOGGER.debug("Request:")
+            _LOGGER.debug(f"url: {url}")
+            _LOGGER.debug(f"json: {self.sanitize(json)}")
+            _LOGGER.debug(f"headers: {self.sanitize(headers)}")
+            # Log the response.json() if it exists, if not log the response.
+            try:
+                response_json = await response.json()
+                _LOGGER.debug(f"Response Json: {self.sanitize(response_json)}")
+            except ContentTypeError:
+                _LOGGER.debug(f"Response: {response}")
+            return await response.json()

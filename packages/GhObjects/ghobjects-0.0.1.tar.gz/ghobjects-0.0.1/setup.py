87dbf05a0a31fb33a2f7ee97bf19c985502e1689
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-31 04:06:35 +0000

    Add package GhObjects/0.0.1/ghobjects-0.0.1.tar.gz

---
+++ packages/GhObjects/ghobjects-0.0.1.tar.gz/ghobjects-0.0.1/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='gitter-lazyload',
+    version='0.1.0',
+    packages=find_packages(),
+    description='A package providing Gitter and lazyload classes',
+    author='Lars Kruse',
+    author_email='lars@lakruzz.com',
+    url='https://github.com/thetechcollective/GhObjects',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.12',
+)

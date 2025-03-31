680218ea66d0154c207cbf162e5e8b3111bb029f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-31 04:05:20 +0000

    Add package erech/0.0.6/erech-0.0.6-py3-none-any.whl

---
+++ packages/erech/erech-0.0.6-py3-none-any.whl/erech/chains.py
@@ -0,0 +1,77 @@
+class Chains:
+    @property
+    def to(self):
+        return self
+
+    # TODO: Should do something else with this one!
+    @property
+    def a(self):
+        return self
+
+    @property
+    def be(self):
+        return self
+
+    @property
+    def been(self):
+        return self
+
+    @property
+    def that(self):
+        return self
+
+    @property
+    def which(self):
+        return self
+
+    @property
+    def has(self):
+        return self
+
+    @property
+    def have(self):
+        return self
+
+    @property
+    def at(self):
+        return self
+
+    @property
+    def of(self):
+        return self
+
+    @property
+    def the(self):
+        return self
+
+    @property
+    def same(self):
+        return self
+
+    @property
+    def with_(self):
+        return self
+
+    @property
+    def but(self):
+        return self
+
+    @property
+    def does(self):
+        return self
+
+    @property
+    def still(self):
+        return self
+
+    @property
+    def also(self):
+        return self
+
+    @property
+    def matches(self):
+        return self
+
+    @property
+    def and_(self):
+        return self

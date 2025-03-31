e13d1787da42c3e3fa797d035c34693319e5544b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-31 16:05:13 +0000

    Add package pyopsin/0.2.2/pyopsin-0.2.2.tar.gz

---
+++ packages/pyopsin/pyopsin-0.2.2.tar.gz/pyopsin-0.2.2/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='pyopsin',
+    version='0.2.2',
+    author='Dingyun Huang',
+    author_email='dh582@cam.ac.uk',
+    description='A simple python wrapper for OPSIN: Open Parser for Systematic IUPAC nomenclature.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Dingyun-Huang/pyopsin",
+    packages=setuptools.find_packages(),
+    python_requires='>=3.9',
+    install_requires=[
+        'jpype1>=1.2.0',
+    ],
+    entry_points={
+        'console_scripts': [
+            'PyOPSIN = PyOPSIN.__main__:main',
+        ],
+    },
+)

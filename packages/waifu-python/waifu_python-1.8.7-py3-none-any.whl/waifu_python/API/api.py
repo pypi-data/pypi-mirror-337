7de873183400549191e1d4cc1bff45f572b9b252
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-31 16:05:14 +0000

    Add package waifu-python/1.8.7/waifu_python-1.8.7-py3-none-any.whl

---
+++ packages/waifu-python/waifu_python-1.8.7-py3-none-any.whl/waifu_python/API/api.py
@@ -0,0 +1,39 @@
+# PROXY API
+
+PROXY_API_URL = "https://api.proxyscrape.com/v4/free-proxy-list/get?request=display_proxies&proxy_format=protocolipport&format=text"
+
+# API ENDPOINTS
+
+GRAPHQL_BASE_URL = "https://graphql.anilist.co"
+
+DANBORRU_BASE_URL = "https://danbooru.donmai.us/posts.json"
+
+GELBORRU_BASE_URL = "https://gelbooru.com/index.php"
+
+KEMONO_BASE_URL = "https://kemono.su"
+
+NEKOS_BASE_URL = "https://nekos.best/api/v2"
+
+NSFWBOT_BASE_URL = "https://api.n-sfw.com"
+
+PICRE_BASE_URL = "https://pic.re/"
+
+PURRBOT_BASE_URL = "https://purrbot.site/api"
+
+RULE34_BASE_URL = "https://api.rule34.xxx/index.php"
+
+SAFEBORRU_BASE_URL = "https://safebooru.org/index.php"
+
+SANKAKU_BASE_URL = "https://sankakuapi.com/posts"
+
+WAIFUIM_BASE_URL = "https://api.waifu.im/"
+
+WAIFUPICS_BASE_URL = "https://api.waifu.pics"
+
+YANDE_RE_BASE_URL = "https://yande.re/post.json"
+
+KONACHAN_BASE_URL = "https://konachan.net/post.json"
+
+ZEROCHAN_BASE_URL = "https://www.zerochan.net"
+
+

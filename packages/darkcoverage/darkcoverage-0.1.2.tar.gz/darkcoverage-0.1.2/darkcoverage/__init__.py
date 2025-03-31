4624916c5c074caf36331e65223e91a3dd1b313d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-31 16:05:09 +0000

    Add package darkcoverage/0.1.2/darkcoverage-0.1.2.tar.gz

---
+++ packages/darkcoverage/darkcoverage-0.1.2.tar.gz/darkcoverage-0.1.2/darkcoverage/__init__.py
@@ -0,0 +1,8 @@
+"""
+DarkCoverage - Image threshold analysis tool
+
+A tool for analyzing images by applying thresholds to grid cells and
+calculating the percentage of pixels above or below the threshold.
+"""
+
+__version__ = "0.1.2"

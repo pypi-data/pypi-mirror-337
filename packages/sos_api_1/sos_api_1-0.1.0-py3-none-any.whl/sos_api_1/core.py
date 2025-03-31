b1aa8447fc331cc8b5130719759a14c6541e5c8c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-31 15:06:15 +0000

    Add package sos_api_1/0.1.0/sos_api_1-0.1.0-py3-none-any.whl

---
+++ packages/sos_api_1/sos_api_1-0.1.0-py3-none-any.whl/sos_api_1/core.py
@@ -0,0 +1,17 @@
+"""Core functionality for Package A."""
+
+def hello_from_a():
+    """Return a greeting message from package A."""
+    return "Hello from Package A!"
+
+def add(a, b):
+    """Add two numbers together.
+    
+    Args:
+        a: First number
+        b: Second number
+        
+    Returns:
+        Sum of a and b
+    """
+    return a + b 

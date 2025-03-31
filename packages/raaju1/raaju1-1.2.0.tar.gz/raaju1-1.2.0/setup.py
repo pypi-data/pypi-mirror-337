6842f055fe3a5ed18943ad48568dceb3c7802ef2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-31 04:04:58 +0000

    Add package raaju1/1.2.0/raaju1-1.2.0.tar.gz

---
+++ packages/raaju1/raaju1-1.2.0.tar.gz/raaju1-1.2.0/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="raaju1",
+    version="1.2.0",
+    packages=find_packages(),
+    install_requires=[], 
+    entry_points={
+        "console_scripts": [
+            "raaju1 = raaju1:roomate",
+        ],
+    },
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Raaj",
+    
+    
+)

f0f56283646867c14c4244014ed3bc342f828981
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-31 15:06:00 +0000

    Add package enqueue/0.1.0/enqueue-0.1.0.tar.gz

---
+++ packages/enqueue/enqueue-0.1.0.tar.gz/enqueue-0.1.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="enqueue", 
+    version="0.1.0",
+    packages=find_packages(),  
+    install_requires=[], 
+    description="A small and easy queuing system to run a function repeatably.",
+    author="Emanuel Odén Hesselroth",
+    author_email="emanuelodenhesselroth@gmail.com",
+    url="https://github.com/EmanuelNorsk/enqueue", 
+)

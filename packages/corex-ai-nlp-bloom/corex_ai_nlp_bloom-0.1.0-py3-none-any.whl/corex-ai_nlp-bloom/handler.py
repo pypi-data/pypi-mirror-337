3c80b50511fd7d0763b612c8c6186a69bc12ee2f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-31 14:05:13 +0000

    Add package corex-ai-nlp-bloom/0.1.0/corex_ai_nlp_bloom-0.1.0-py3-none-any.whl

---
+++ packages/corex-ai-nlp-bloom/corex_ai_nlp_bloom-0.1.0-py3-none-any.whl/corex-ai_nlp-bloom/handler.py
@@ -0,0 +1,9 @@
+# Handler for bloom implementing ai_nlp interface
+from corex.core.interfaces.ai_nlp import Ai_nlpInterface
+
+class BloomHandler(Ai_nlpInterface):
+    def __init__(self):
+        pass
+
+    def example_method(self):
+        print("Handling ai_nlp with bloom")

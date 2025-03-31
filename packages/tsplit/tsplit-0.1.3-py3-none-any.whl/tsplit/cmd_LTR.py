b005424be55a8c274d3ef14839adb6c14d1e71b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-31 14:05:06 +0000

    Add package tsplit/0.1.3/tsplit-0.1.3-py3-none-any.whl

---
+++ packages/tsplit/tsplit-0.1.3-py3-none-any.whl/tsplit/cmd_LTR.py
@@ -0,0 +1,38 @@
+from tsplit.logs import init_logging
+from tsplit.parseAlign import getLTRs
+from tsplit.utils import check_tools, segWrite, tSplitchecks
+
+
+def main(args=None):
+    """Do the work."""
+    # Set up logging
+    init_logging(loglevel=args.loglevel)
+
+    # Check for required programs.
+    required_tools = ["delta-filter", "nucmer", "show-coords"]
+    optional_tools = ["blastn"]
+
+    if args.method == "blastn":
+        required_tools.append("blastn")
+        optional_tools = []
+
+    check_tools(required_tools=required_tools, optional_tools=optional_tools)
+
+    # Create output paths as required
+    outpath = tSplitchecks(args)
+
+    # If LTR mode, search for terminal repeats on same strand
+    segments = getLTRs(
+        args.infile,
+        flankdist=args.maxdist,
+        minterm=args.minterm,
+        minseed=args.minseed,
+        minid=args.minid,
+        diagfactor=args.diagfactor,
+        report=args.splitmode,
+        temp=args.outdir,
+        alignTool=args.method,
+        keeptemp=args.keeptemp,
+    )
+
+    segWrite(outpath, segs=segments)
---
+++ packages/tsplit/tsplit-0.1.3-py3-none-any.whl/tsplit/cmd_TIR.py
@@ -0,0 +1,40 @@
+from tsplit.logs import init_logging
+from tsplit.parseAlign import getTIRs
+from tsplit.utils import check_tools, segWrite, tSplitchecks
+
+
+def main(args=None):
+    """Do the work."""
+    # Set up logging
+    init_logging(loglevel=args.loglevel)
+
+    # Check for required programs.
+    required_tools = ["delta-filter", "nucmer", "show-coords"]
+    optional_tools = ["blastn"]
+
+    if args.method == "blastn":
+        required_tools.append("blastn")
+        optional_tools = []
+
+    check_tools(required_tools=required_tools, optional_tools=optional_tools)
+
+    # Create output paths as required
+    outpath = tSplitchecks(args)
+
+    # Search for inverted terminal repeats
+    # Optionally construct synthetic MITE from TIRs if detected
+    segments = getTIRs(
+        args.infile,
+        flankdist=args.maxdist,
+        minterm=args.minterm,
+        minseed=args.minseed,
+        minid=args.minid,
+        diagfactor=args.diagfactor,
+        mites=args.makemites,
+        report=args.splitmode,
+        alignTool=args.method,
+        temp=args.outdir,
+        keeptemp=args.keeptemp,
+    )
+
+    segWrite(outpath, segs=segments)

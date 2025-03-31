9ae90084c6d129b13eed0181f286a1d1da36796d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-31 04:05:42 +0000

    Add package swiss-qr-bill/0.0.28/swiss_qr_bill-0.0.28-py3-none-any.whl

---
+++ packages/swiss-qr-bill/swiss_qr_bill-0.0.28-py3-none-any.whl/swiss_qr_bill/__init__.py
@@ -0,0 +1,9 @@
+from .generator.bill import Bill, BillFormat
+from .generator.address import Address
+from .generator.enums import GraphicsFormat, Language, OutputSize, SeparatorType, VerticalBorderType
+from .generator.qr_bill import QRBill
+from .generator.qr_code_text import QRCodeText
+from .validator.validator import Validator
+
+__version__ = "0.0.28"
+__all__ = [Bill, BillFormat, Address, GraphicsFormat, Language, OutputSize, QRBill, QRCodeText, Validator, SeparatorType, VerticalBorderType]

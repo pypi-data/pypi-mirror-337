16e1a6222883237bb4ecf60c22a76e7762b43ce7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-31 03:19:43 +0000

    Add package h2w-gftools/1.0.0/h2w_gftools-1.0.0.tar.gz

---
+++ packages/h2w-gftools/h2w_gftools-1.0.0.tar.gz/h2w_gftools-1.0.0/gftools/__init__.py
@@ -0,0 +1,4 @@
+from gftools.dividend import GuruDividendHistory
+from gftools.financials import (GuruFinancials, GuruStockAnnualTenK)
+from gftools.price import GuruPriceHistory
+
---
+++ packages/h2w-gftools/h2w_gftools-1.0.0.tar.gz/h2w_gftools-1.0.0/test/test.py
@@ -0,0 +1,15 @@
+from dotenv import load_dotenv
+from gftools import (GuruFinancials, GuruStockAnnualTenK, GuruDividendHistory, GuruPriceHistory)
+import os
+
+load_dotenv()
+gt = os.getenv('guru_token')
+ticker = 'txn'
+
+
+div_data = GuruDividendHistory(ticker=ticker, token=gt)
+price_data = GuruPriceHistory(ticker=ticker, token=gt)
+fin_data = GuruFinancials(ticker=ticker, token=gt)
+annual_data = GuruStockAnnualTenK(ticker=ticker, token=gt)
+
+print(annual_data.api_data_df_nrm)

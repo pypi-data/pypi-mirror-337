d97973be035ae606dc65d59c927be23c14deb19f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-31 03:19:45 +0000

    Add package rsml67/0.1/rsml67-0.1.tar.gz

---
+++ packages/rsml67/rsml67-0.1.tar.gz/rsml67-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="rsml67",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[],  # No dependencies needed
+    description="A custom library with E1 to E9 functions",
+    author="Your Name",
+    author_email="gowthaamgowthaam541@gmail.com",
+)

836cd3a94b771c57ebe2fb5eb5ca1b679219d63d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-31 14:05:24 +0000

    Add package mtcli/1.0.0/mtcli-1.0.0.tar.gz

---
+++ packages/mtcli/mtcli-1.0.0.tar.gz/mtcli-1.0.0/mtcli/rm.py
@@ -0,0 +1,38 @@
+"""
+Calcula o tamanho médio das barras
+"""
+
+import click
+from mtcli import csv_data
+from mtcli.pa import bar as pa_bar
+from mtcli import conf
+
+
+@click.command()
+@click.argument("symbol")
+@click.option(
+    "--period",
+    "-p",
+    type=click.Choice(conf.timeframes, case_sensitive=False),
+    default="D1",
+    help="Tempo grafico, default D1.",
+)
+@click.option("--count", "-c", default=10, help="Quantidade de barras, default 10.")
+def rm(symbol, period, count):
+    """Calcula o tamanho medio das barras."""
+    fcsv = conf.csv_path + symbol + period + ".csv"
+    ranges = []
+    rates = csv_data.get_data(fcsv)
+    for rate in rates:
+        bar = pa_bar.Bar(rate)
+        # Elimina doji de 4 preços
+        if bar.open == bar.high and bar.high == bar.low and bar.low == bar.close:
+            continue
+        ranges.append(bar.high - bar.low)
+    ranges = ranges[-count:]
+    rm = round(sum(ranges) / len(ranges), conf.digitos)
+    click.echo(rm)
+
+
+if __name__ == "__main__":
+    rm()

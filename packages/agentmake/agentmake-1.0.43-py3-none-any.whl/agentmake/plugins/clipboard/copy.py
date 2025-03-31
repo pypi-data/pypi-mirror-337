87549a649e026aa83dfa61df23f855606a6f6332
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-31 06:05:26 +0000

    Add package agentmake/1.0.43/agentmake-1.0.43-py3-none-any.whl

---
+++ packages/agentmake/agentmake-1.0.43-py3-none-any.whl/agentmake/plugins/clipboard/copy.py
@@ -0,0 +1,6 @@
+def copy_text(content, **kwargs):
+    import pyperclip, shutil, pydoc
+    pydoc.pipepager(content, cmd="termux-clipboard-set") if shutil.which("termux-clipboard-set") else pyperclip.copy(content)
+    return content
+
+CONTENT_PLUGIN = copy_text

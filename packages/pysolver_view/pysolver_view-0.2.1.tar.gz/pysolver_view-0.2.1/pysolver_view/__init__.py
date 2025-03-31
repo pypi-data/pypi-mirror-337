b04e85fc67439cb86ca7e0329fe13983648de776
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-31 16:05:06 +0000

    Add package pysolver_view/0.2.1/pysolver_view-0.2.1.tar.gz

---
+++ packages/pysolver_view/pysolver_view-0.2.1.tar.gz/pysolver_view-0.2.1/pysolver_view/__init__.py
@@ -0,0 +1,24 @@
+# Highlight exception messages
+# https://stackoverflow.com/questions/25109105/how-to-colorize-the-output-of-python-errors-in-the-gnome-terminal/52797444#52797444
+try:
+    import IPython.core.ultratb
+except ImportError:
+    # No IPython. Use default exception printing.
+    pass
+else:
+    import sys
+    sys.excepthook = IPython.core.ultratb.FormattedTB(theme_name='linux', call_pdb=False)
+
+
+from .pysolver_utilities import *
+from .numerical_differentiation import *
+from .optimization import *
+from .optimization_problems import *
+from .optimization_wrappers import *
+from .nonlinear_system import *
+from .nonlinear_system_problems import *
+
+
+__version__ = "0.2.1"
+URL_GITHUB = "https://github.com/turbo-sim/pysolver_view"
+URL_DOCS = "https://turbo-sim.github.io/pysolver_view/"

0125e1b3060b339a8c7a4b56e51277d383c072e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-31 06:05:26 +0000

    Add package agentmake/1.0.43/agentmake-1.0.43.tar.gz

---
+++ packages/agentmake/agentmake-1.0.43.tar.gz/agentmake-1.0.43/agentmake/plugins/uba/refs.py
@@ -0,0 +1,16 @@
+def extract_bible_references(
+    content,
+    **kwargs,
+):
+    from agentmake.plugins.uba.lib.BibleParser import BibleVerseParser
+    refs = BibleVerseParser(False).extractAllReferencesReadable(content)
+    refs if refs else "[not found]"
+    text_output = f"```references\n{refs}\n```"
+
+    print_on_terminal = kwargs.get("print_on_terminal")
+    if print_on_terminal:
+        print(text_output)
+
+    return text_output
+
+CONTENT_PLUGIN = extract_bible_references

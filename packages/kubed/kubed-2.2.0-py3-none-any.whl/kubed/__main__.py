9725bded3ddc6decee6c1b9fba6bb8eb6f4aa1c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-31 14:05:30 +0000

    Add package kubed/2.2.0/kubed-2.2.0-py3-none-any.whl

---
+++ packages/kubed/kubed-2.2.0-py3-none-any.whl/kubed/__main__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+"""
+Main entry point for the kubed package when run as a module.
+"""
+
+from kubed.main import cli
+
+if __name__ == "__main__":
+    cli() 
---
+++ packages/kubed/kubed-2.2.0-py3-none-any.whl/kubed/aliases/aliases.sh
@@ -0,0 +1,36 @@
+# Kubed aliases
+# Kubernetes
+alias k='kubectl'
+alias kgp='kubectl get pods'
+alias kgd='kubectl get deployments'
+alias kgs='kubectl get services'
+alias kgn='kubectl get nodes'
+alias kd='kubectl describe'
+alias kl='kubectl logs'
+alias kx='kubectl exec -it'
+alias kaf='kubectl apply -f'
+alias kdel='kubectl delete'
+
+# Docker
+alias d='docker'
+alias dc='docker-compose'
+alias di='docker images'
+alias dps='docker ps'
+alias drm='docker rm'
+alias drmi='docker rmi'
+
+# Terraform
+alias tf='terraform'
+alias tfa='terraform apply'
+alias tfp='terraform plan'
+alias tfd='terraform destroy'
+alias tfi='terraform init'
+alias tfw='terraform workspace'
+
+# Helm
+alias h='helm'
+alias hl='helm list'
+alias hi='helm install'
+alias hu='helm upgrade'
+alias hun='helm uninstall'
+alias hr='helm repo'

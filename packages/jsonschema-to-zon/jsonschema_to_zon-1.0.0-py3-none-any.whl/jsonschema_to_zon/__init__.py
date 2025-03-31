ffa2ec4d229c416724a2f2de821e5b9b297d5b27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-31 14:04:59 +0000

    Add package jsonschema-to-zon/1.0.0/jsonschema_to_zon-1.0.0-py3-none-any.whl

---
+++ packages/jsonschema-to-zon/jsonschema_to_zon-1.0.0-py3-none-any.whl/jsonschema_to_zon/__init__.py
@@ -0,0 +1,7 @@
+from .schema import SchemaReader, Schema, InvalidSchemaDefinition
+
+__version__ = "1.0.0"
+__author__ = "Nuno Pereira"
+__email__ = "nunoafonso2002@gmail.com"
+__license__ = "MIT"
+__copyright__ = "Copyright 2025, Nuno Pereira"

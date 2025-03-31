90711fea61deb59a2e3205bb98347e9a3b7bdefd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-31 14:05:21 +0000

    Add package ProtPeptigram/1.0.1.dev0/protpeptigram-1.0.1.dev0.tar.gz

---
+++ packages/ProtPeptigram/protpeptigram-1.0.1.dev0.tar.gz/protpeptigram-1.0.1.dev0/ProtPeptigram/__init__.py
@@ -0,0 +1,21 @@
+#bin/python
+# -*- coding: utf-8 -*-
+"""
+Created on 2023-10-02 16:00:00
+"""
+from warnings import filterwarnings
+from rich.traceback import install
+from ProtPeptigram.DataProcessor import PeptideDataProcessor
+from ProtPeptigram.viz import ImmunoViz
+install(show_locals=True)  # type: ignore
+
+filterwarnings("ignore")
+"""
+Peptigram: peptides distribution across proteins
+
+A Python package for mapping peptides to source protein and identifying high desnsity window to core prptides across diffrent source protein
+"""
+
+__version__ = "1.0.1-dev"
+__author__ = "Sanjay Krishna,Prithvi Munday,Chen Li"
+__email__ = "sanjay.sondekoppagopalakrishna@monash.edu"

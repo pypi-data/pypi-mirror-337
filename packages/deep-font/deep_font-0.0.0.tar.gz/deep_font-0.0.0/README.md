09e6949882201e49502c23044d23526a48403d98
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-31 03:19:49 +0000

    Add package deep-font/0.0.0/deep_font-0.0.0.tar.gz

---
+++ packages/deep-font/deep_font-0.0.0.tar.gz/deep_font-0.0.0/README.md
@@ -0,0 +1,10 @@
+# deep-font
+Python Library for Visual Font Recognition (VFR)
+
+> package-name reservation
+
+## Installation
+
+```shell
+pip install deep-font
+```
---
+++ packages/deep-font/deep_font-0.0.0.tar.gz/deep_font-0.0.0/src/deepfont/__init__.py
@@ -0,0 +1,15 @@
+# -*- coding=utf-8 -*-
+r"""
+
+"""
+
+__author__ = "roriwa"
+__copyright__ = "Copyright 2025, novelizer"
+__credits__ = ["roriwa"]
+__license__ = "GPLv3"
+__maintainer__ = "roriwa"
+__email__ = None
+__status__ = "Prototype"  # Prototype, Development, Production
+__description__ = "Python Library for Visual Font Recognition (VFR)"
+__version_info__ = (0, 0, 0)
+__version__ = '.'.join(str(_) for _ in __version_info__)

9858cbda7ff9ca175958193010edb1b89557a665
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-31 14:05:24 +0000

    Add package pyrovelocity/0.4.1/pyrovelocity-0.4.1.tar.gz

---
+++ packages/pyrovelocity/pyrovelocity-0.4.1.tar.gz/pyrovelocity-0.4.1/src/pyrovelocity/tasks/__init__.py
@@ -0,0 +1,14 @@
+import pyrovelocity.tasks.data
+import pyrovelocity.tasks.postprocess
+import pyrovelocity.tasks.preprocess
+import pyrovelocity.tasks.summarize
+import pyrovelocity.tasks.train
+
+
+__all__ = [
+    "data",
+    "postprocess",
+    "preprocess",
+    "summarize",
+    "train",
+]

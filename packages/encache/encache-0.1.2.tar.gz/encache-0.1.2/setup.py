21df4cd88feb2c837a2551bdf45221e2e711a553
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-31 06:05:11 +0000

    Add package encache/0.1.2/encache-0.1.2.tar.gz

---
+++ packages/encache/encache-0.1.2.tar.gz/encache-0.1.2/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="encache", 
+    version="0.1.2",
+    packages=find_packages(),  
+    install_requires=[], 
+    description="A small and easy caching system for every use",
+    author="Emanuel Odén Hesselroth",
+    author_email="emanuelodenhesselroth@gmail.com",
+    url="https://github.com/EmanuelNorsk/encache", 
+)

f05386f16157a179f13a50974154e9f31192adc1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-31 04:05:16 +0000

    Add package elrahapi/1.0.8/elrahapi-1.0.8-py3-none-any.whl

---
+++ packages/elrahapi/elrahapi-1.0.8-py3-none-any.whl/elrahapi/middleware/loggerapp/log_model.py
@@ -0,0 +1,6 @@
+from myproject.settings.database import Base
+from elrahapi.middleware.models import LoggerMiddlewareModel
+
+class Logger(Base, LoggerMiddlewareModel):
+    __tablename__ = "loggers"
+metadata = Base.metadata

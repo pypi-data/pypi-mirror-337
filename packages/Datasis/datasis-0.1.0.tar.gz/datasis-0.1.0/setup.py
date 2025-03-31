0e396fbed984755b61f878e9fec6ee0b09f5c3c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-31 14:05:29 +0000

    Add package Datasis/0.1.0/datasis-0.1.0.tar.gz

---
+++ packages/Datasis/datasis-0.1.0.tar.gz/datasis-0.1.0/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Datasis",  # تأكد من أن الاسم صحيح
+    version="0.1.0",  # تأكد من أن الإصدار صحيح
+    author="Ahmed Eldesoky",
+    author_email="ahmedeldeosky284@yahoo.com",
+    description="A short description of your package",
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",  # Optional: to specify markdown format
+    url="https://github.com/ahmed-eldesoky284/Datasis",
+    packages=find_packages(),  # Automatically discovers packages
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",  # Modify as per your license
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        # List any dependencies you need here
+    ],
+    python_requires='>=3.6',
+)

6ad0bdb1b5fbde1a3e106ea87a2f81609736e16e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-31 04:04:51 +0000

    Add package FlexFL/0.1.3/flexfl-0.1.3.tar.gz

---
+++ packages/FlexFL/flexfl-0.1.3.tar.gz/flexfl-0.1.3/scripts/vm.sh
@@ -0,0 +1,11 @@
+
+VM_LIST="scripts/hostfile.txt"
+
+while read -r IP; do
+    ssh-keyscan -H $IP >> ~/.ssh/known_hosts
+done < "$VM_LIST"
+
+sudo apt update -y
+sudo dpkg --configure -a
+sudo apt upgrade -y
+sudo apt install -y python3.12-venv python3-dev libopenmpi-dev

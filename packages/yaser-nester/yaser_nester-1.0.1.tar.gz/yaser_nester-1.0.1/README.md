ee9d0cb90b663a02e3a6c64cef02d0515b0dc208
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-31 06:05:10 +0000

    Add package yaser-nester/1.0.1/yaser_nester-1.0.1.tar.gz

---
+++ packages/yaser-nester/yaser_nester-1.0.1.tar.gz/yaser_nester-1.0.1/README.md
@@ -0,0 +1,9 @@
+# Yaser Nester
+
+A simple Python module for printing nested lists.
+
+## Installation
+
+Install using pip:
+```bash
+pip install yaser_nester
---
+++ packages/yaser-nester/yaser_nester-1.0.1.tar.gz/yaser_nester-1.0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name="yaser_nester",
+    version="1.0.1",
+    py_modules=["nester"],
+    author="Yaser Developer",
+    author_email="yasernet72@gmail.com",
+    url="https://github.com/Yasser-Developer",
+    description="A simple printer of nested lists",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+)

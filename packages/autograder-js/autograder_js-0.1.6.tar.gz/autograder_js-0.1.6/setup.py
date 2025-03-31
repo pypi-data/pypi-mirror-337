80a0f7e4d518d3d68ddee34181054bfd847ddedc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-31 14:05:25 +0000

    Add package autograder-js/0.1.6/autograder_js-0.1.6.tar.gz

---
+++ packages/autograder-js/autograder_js-0.1.6.tar.gz/autograder_js-0.1.6/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='autograder_js',
+    version='0.1.6',
+    description='Autograder para código JavaScript com feedback interativo usando a API do OpenAI.',
+    author='Lucca Hiratsuca',
+    author_email='luccahiratsuca@gmail.com',
+    packages=find_packages(),
+    install_requires=[
+        'openai>=1.0.0',
+        'python-dotenv',
+        'ipywidgets'
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)

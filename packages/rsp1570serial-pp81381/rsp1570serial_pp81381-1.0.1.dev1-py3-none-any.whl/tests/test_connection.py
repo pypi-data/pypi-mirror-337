2c33f5b937bcc97019c90068c0bd223765c9d084
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-31 06:05:28 +0000

    Add package rsp1570serial-pp81381/1.0.1.dev1/rsp1570serial_pp81381-1.0.1.dev1-py3-none-any.whl

---
+++ packages/rsp1570serial-pp81381/rsp1570serial_pp81381-1.0.1.dev1-py3-none-any.whl/tests/test_connection.py
@@ -0,0 +1,34 @@
+import asyncio
+from unittest import IsolatedAsyncioTestCase
+
+from rsp1570serial.emulator import INITIAL_SOURCE, INITIAL_VOLUME
+from rsp1570serial.rotel_model_meta import RSP1570_META
+from tests.emulator_test_helper import EmulatorTestHelper
+
+
+class AsyncTestConnection(IsolatedAsyncioTestCase):
+    async def asyncSetUp(self):
+        self.helper = EmulatorTestHelper(RSP1570_META, is_on=True)
+        await self.helper.asyncSetUp()
+
+    async def asyncTearDown(self):
+        await self.helper.asyncTearDown()
+
+    async def test_send_command1(self):
+        assert self.helper.device is not None
+        self.assertEqual(self.helper.device._is_muted, False)
+        async with self.helper.create_conn() as conn:
+            await conn.send_command("MUTE_TOGGLE")
+        self.assertEqual(self.helper.device._is_muted, True)
+
+    async def test_send_command2(self):
+        self.assertEqual(self.helper.device._source, INITIAL_SOURCE)
+        async with self.helper.create_conn() as conn:
+            await conn.send_command("SOURCE_TUNER")
+        self.assertEqual(self.helper.device._source, "TUNER")
+
+    async def test_send_volume_direct_command1(self):
+        self.assertEqual(self.helper.device._volume, INITIAL_VOLUME)
+        async with self.helper.create_conn() as conn:
+            await conn.send_volume_direct_command(1, 55)
+        self.assertEqual(self.helper.device._volume, 55)

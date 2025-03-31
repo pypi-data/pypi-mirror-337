21f2625272d93554c01c858e2d6588dccdc0267e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-31 03:19:34 +0000

    Add package sony-custom-layers-dev/0.4.0.dev3/sony_custom_layers_dev-0.4.0.dev3-py3-none-any.whl

---
+++ packages/sony-custom-layers-dev/sony_custom_layers_dev-0.4.0.dev3-py3-none-any.whl/sony_custom_layers/pytorch/nms/__init__.py
@@ -0,0 +1,27 @@
+# -----------------------------------------------------------------------------
+# Copyright 2024 Sony Semiconductor Israel, Inc. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# -----------------------------------------------------------------------------
+from .nms import multiclass_nms, NMSResults
+from .nms_with_indices import multiclass_nms_with_indices, NMSWithIndicesResults
+
+# trigger onnx op registration
+from . import nms_onnx
+
+__all__ = [
+    'multiclass_nms',
+    'multiclass_nms_with_indices',
+    'NMSResults',
+    'NMSWithIndicesResults',
+]

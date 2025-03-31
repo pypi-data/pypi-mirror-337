6a32bb24388e641dbed4df66c1e6c964cbaddc61
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-31 03:19:29 +0000

    Add package simple-colors-treinalinux/1.2.0/simple_colors_treinalinux-1.2.0.tar.gz

---
+++ packages/simple-colors-treinalinux/simple_colors_treinalinux-1.2.0.tar.gz/simple_colors_treinalinux-1.2.0/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='simple_colors_treinalinux',
+    version='1.2.0',
+    packages=find_packages(),
+    author='Alan da Silva Alves',
+    author_email='',
+    description='Simple colors to terminal',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/treinalinux/simple-colors',
+    install_requires=[],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+)

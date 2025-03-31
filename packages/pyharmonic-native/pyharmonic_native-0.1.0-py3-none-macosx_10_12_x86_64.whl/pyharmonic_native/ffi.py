0b13436cd1645b02187c14e9e455d56e6c77b228
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-31 03:19:42 +0000

    Add package pyharmonic-native/0.1.0/pyharmonic_native-0.1.0-py3-none-macosx_10_12_x86_64.whl

---
+++ packages/pyharmonic-native/pyharmonic_native-0.1.0-py3-none-macosx_10_12_x86_64.whl/pyharmonic_native/ffi.py
@@ -0,0 +1,8 @@
+# auto-generated file
+import _cffi_backend
+
+ffi = _cffi_backend.FFI('ffi',
+    _version = 0x2601,
+    _types = b'\x00\x00\x02\x0D\x00\x00\x00\x0F\x00\x00\x03\x03\x00\x00\x12\x01',
+    _globals = (b'\x00\x00\x00\x23py_harmonic_version',0,),
+)

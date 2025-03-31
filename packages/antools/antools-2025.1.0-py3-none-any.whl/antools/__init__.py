4d8af476643054c3d2fdaddf8731f4127113f70b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-31 05:05:46 +0000

    Add package antools/2025.1.0/antools-2025.1.0-py3-none-any.whl

---
+++ packages/antools/antools-2025.1.0-py3-none-any.whl/antools/__init__.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+"""
+ANTOOLS - ANTON'S TOOLS
+
+@ author: Antonín Drozda
+@ author email: adrozda@dev.gmail.com
+@ contact:  Email    (adrozda.dev@gmail.com)
+            GitHub   (https://github.com/antonin-drozda)
+            StackOverFlow (https://stackoverflow.com/users/14750652/anton%c3%adn-drozda?tab=profile)           
+            LinkedIn (https://linkedin.com/in/anton%C3%ADn-drozda-a372b116a/) 
+@ url: https://github.com/antonin-drozda
+@ organization: Freelancer
+
+@ project: Anton's Tools
+@ version: 2025.1.0
+@ copyright: Copyright (C) 2025 Antonín Drozda
+@ license: MIT
+@ date: 30.3.2025
+
+Private library which is also free to public use. Its purpose is to be useful code keeper.
+
+"""
+
+__title__ = "ANTON'S TOOLS"
+__author__ = "Antonin Drozda"
+__author_email__ = "adrozda.dev@gmail.com"
+__contact__ = "adrozda.dev@gmail.com"
+__url__ = "https://github.com/antonin-drozda"
+__organization__ = "Freelancer"
+
+__project__ = "Anton's Tools"
+__version__ = "2025.1.0"
+__copyright__ = "Copyright (C) 2022 Antonín Drozda"
+__license__= "MIT"
+__date__ = "30.3.20225"
+__description__ = "Private library which is also free to public use. Its purpose is to be useful code keeper."
+
+
+# %% ADDITIONAL PYPI METADATA
+PACKAGE_DEPENDENCY = []
+
+KEYWORDS = ['antools']

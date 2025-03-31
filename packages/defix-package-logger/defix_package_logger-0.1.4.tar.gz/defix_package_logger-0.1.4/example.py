76604d845056fcea57f40710ab786edb50ad6eec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-31 05:05:57 +0000

    Add package defix-package-logger/0.1.4/defix_package_logger-0.1.4.tar.gz

---
+++ packages/defix-package-logger/defix_package_logger-0.1.4.tar.gz/defix_package_logger-0.1.4/example.py
@@ -0,0 +1,15 @@
+import defix_package_logger
+
+
+def main():
+    defix_package_logger.initialize({
+        'name': 'test-service'
+    })
+
+    defix_package_logger.logger.info('Logging', extra={
+        'msg_code': 'CRITICAL_ERROR'
+    } | {'xid': '-'})
+
+
+if __name__ == "__main__":
+    main()

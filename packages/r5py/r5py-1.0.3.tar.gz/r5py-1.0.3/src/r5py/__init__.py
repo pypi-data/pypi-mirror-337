8ff23be247e16e327d7d7c81091a61b58be4ec81
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-31 15:06:32 +0000

    Add package r5py/1.0.3/r5py-1.0.3.tar.gz

---
+++ packages/r5py/r5py-1.0.3.tar.gz/r5py-1.0.3/src/r5py/__init__.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+"""Python wrapper for the R5 routing analysis engine."""
+
+__version__ = "1.0.3"
+
+
+from .r5 import (
+    DetailedItineraries,
+    DetailedItinerariesComputer,
+    Isochrones,
+    RegionalTask,
+    TransportMode,
+    TransportNetwork,
+    TravelTimeMatrix,
+    TravelTimeMatrixComputer,
+)
+
+__all__ = [
+    "DetailedItineraries",
+    "DetailedItinerariesComputer",
+    "Isochrones",
+    "RegionalTask",
+    "TransportMode",
+    "TransportNetwork",
+    "TravelTimeMatrix",
+    "TravelTimeMatrixComputer",
+    "__version__",
+]

f101222d7d825f0dbc9f6afa777b0999df3876ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-31 03:19:38 +0000

    Add package at-common-models/0.1.40/at_common_models-0.1.40.tar.gz

---
+++ packages/at-common-models/at_common_models-0.1.40.tar.gz/at_common_models-0.1.40/src/at_common_models/stock/company_profile.py
@@ -0,0 +1,34 @@
+# app/models/user.py
+from sqlalchemy import Column, String, DateTime
+from at_common_models.base import BaseModel
+
+class StockCompanyProfile(BaseModel):
+    __tablename__ = "stock_company_profiles"
+
+    symbol = Column(String(5), nullable=False, primary_key=True)
+    name = Column(String(255), nullable=False)
+    currency = Column(String(3), nullable=True)
+    cik = Column(String(10), nullable=True)
+    isin = Column(String(12), nullable=True)
+    cusip = Column(String(9), nullable=True)
+    exchange_full_name = Column(String(255), nullable=False)
+    exchange = Column(String(16), nullable=False)
+    sector = Column(String(255), nullable=True)
+    industry = Column(String(255), nullable=True)
+    website = Column(String(2083), nullable=True)
+    description = Column(String(8196), nullable=True)
+    ceo = Column(String(255), nullable=True)
+    phone = Column(String(255), nullable=True)
+    address = Column(String(255), nullable=True)
+    country = Column(String(255), nullable=True)
+    city = Column(String(255), nullable=True)
+    state = Column(String(255), nullable=True)
+    zip = Column(String(255), nullable=True)
+    image = Column(String(2083), nullable=True)
+    ipo_date = Column(DateTime, nullable=True)
+
+    def __str__(self):
+        return f"<StockCompanyProfile(exchange={self.exchange}, symbol={self.symbol})>"
+
+    def __repr__(self):
+        return f"<StockCompanyProfile(exchange={self.exchange}, symbol={self.symbol})>"

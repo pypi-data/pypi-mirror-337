dcfb8d8097a1254f3953c1969614563bc14d54d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-31 15:06:30 +0000

    Add package hivebox/2.6.7/hivebox-2.6.7-py3-none-any.whl

---
+++ packages/hivebox/hivebox-2.6.7-py3-none-any.whl/hivebox/common/time.py
@@ -0,0 +1,18 @@
+TIME_DURATION_UNITS = (
+    ('week', 60*60*24*7),
+    ('day', 60*60*24),
+    ('hour', 60*60),
+    ('min', 60),
+    ('sec', 1)
+)
+
+
+def human_time_duration(seconds, full=False):
+    if seconds == 0:
+        return 'inf'
+    parts = []
+    for unit, div in TIME_DURATION_UNITS:
+        amount, seconds = divmod(int(seconds), div)
+        if amount > 0:
+            parts.append('{} {}{}'.format(amount, unit, "" if amount == 1 else "s"))
+    return ', '.join(parts) if full else parts[0]

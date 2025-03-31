6ad443a3e0889d4c651fc3a5cade8d06976ebf1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-31 04:05:12 +0000

    Add package jikime-cookai/0.1.4/jikime_cookai-0.1.4.tar.gz

---
+++ packages/jikime-cookai/jikime_cookai-0.1.4.tar.gz/jikime_cookai-0.1.4/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="jikime_cookai",
+    version="0.1.4",
+    author="Anthony.Kim",
+    author_email="jikime@gmail.com",
+    description="Jikime Cook AI",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/jikime/jikime-cookai",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.10",
+    install_requires=[
+        # 필요한 의존성 패키지들
+    ],
+) 

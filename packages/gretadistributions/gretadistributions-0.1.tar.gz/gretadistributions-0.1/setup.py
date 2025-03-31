59e7f267daef4cfdf599264315d19579d25c01e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-31 04:05:21 +0000

    Add package gretadistributions/0.1/gretadistributions-0.1.tar.gz

---
+++ packages/gretadistributions/gretadistributions-0.1.tar.gz/gretadistributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name="gretadistributions",
+    version="0.1",
+    description="Gaussian distributions",
+    packages=["gretadistributions"],
+    zip_safe=False,
+)

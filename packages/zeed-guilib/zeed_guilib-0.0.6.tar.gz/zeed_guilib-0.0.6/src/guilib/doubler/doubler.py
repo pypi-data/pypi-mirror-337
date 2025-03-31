2bfec988164228b16084914bc94c0c3166bca7d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-31 04:04:51 +0000

    Add package zeed-guilib/0.0.6/zeed_guilib-0.0.6.tar.gz

---
+++ packages/zeed-guilib/zeed_guilib-0.0.6.tar.gz/zeed_guilib-0.0.6/src/guilib/doubler/doubler.py
@@ -0,0 +1,22 @@
+from typing import Any
+from typing import Generic
+from typing import TypeVar
+
+T = TypeVar('T')
+
+
+class Doubler(Generic[T]):
+    def __init__(self, left: T, right: T) -> None:
+        self._left = left
+        self._right = right
+
+    def __getattr__(self, name: str) -> 'Doubler[Any]':
+        return Doubler(getattr(self._left, name), getattr(self._right, name))
+
+    def __call__(self, *args: Any, **kwargs: Any) -> 'Doubler[Any]':
+        if not callable(self._left) or not callable(self._right):
+            raise TypeError
+
+        return Doubler(
+            self._left(*args, **kwargs), self._right(*args, **kwargs)
+        )

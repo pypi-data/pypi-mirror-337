ac07c9cc64ed6ea64283dcaf7060763d5c2c94e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-31 16:05:15 +0000

    Add package deepcore/1.0/deepcore-1.0.tar.gz

---
+++ packages/deepcore/deepcore-1.0.tar.gz/deepcore-1.0/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+
+with open("README.md","r") as f:
+    description = f.read()
+
+setup(
+    name="deepcore",
+    version="1.0",
+    packages=find_packages(),
+    install_requires=[
+        # List your dependencies here
+    ],
+    author="vishal singh",
+    author_email="vishalsinghomr@gmail.com",
+    description="A short description of your library",
+    keywords="measurement of central tendency, library, python , mean , ,median , mode, multimode",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows"
+    ],
+    python_requires=">=3.6",
+)

eb9a0ef096f037aea77d921ad8b498de9e255a70
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-31 04:05:06 +0000

    Add package vuepy-core/0.1.10/vuepy_core-0.1.10-py3-none-any.whl

---
+++ packages/vuepy-core/vuepy_core-0.1.10-py3-none-any.whl/vuepy/utils/common.py
@@ -0,0 +1,36 @@
+# ---------------------------------------------------------
+# Copyright (c) vuepy.org. All rights reserved.
+# ---------------------------------------------------------
+from __future__ import annotations
+
+import dataclasses
+
+from vuepy.log import getLogger
+
+logger = getLogger()
+
+
+def has_changed(value, old) -> bool:
+    try:
+        return bool(value != old)
+    except Exception as e:
+        logger.warn(f"Run has_changed failed, {e}")
+        return True
+
+
+def gen_hash_key(target):
+    try:
+        hash(target)
+    except Exception:
+        return f"id(target)={id(target)})"
+    return target
+
+
+class Nil:
+    pass
+
+
+@dataclasses.dataclass
+class Record:
+    def to_ns(self):
+        return self.__dict__

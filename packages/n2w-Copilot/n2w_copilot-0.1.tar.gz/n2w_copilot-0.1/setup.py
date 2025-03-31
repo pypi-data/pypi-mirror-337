9758ce004502b64b43f8c6808071d05c2cab3bba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-31 15:06:03 +0000

    Add package n2w-Copilot/0.1/n2w_copilot-0.1.tar.gz

---
+++ packages/n2w-Copilot/n2w_copilot-0.1.tar.gz/n2w_copilot-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='n2w_Copilot',version='0.1',py_modules=['n2w_Copilot_3','n2w_Copilot_Tester'],package_data = {'':['n2w_Test_Numeri_Input.txt', 'n2w_test_result.txt']}, install_requires=[
+    #Aggiungi qui le dipendenze necessarie
+    ],entry_points={'console_scripts':['n2w_Copilot = n2w_Copilot_3:main',
+#Assumendo che ci sia una funzione main nel file principale
+],},)

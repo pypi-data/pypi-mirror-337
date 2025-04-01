642dccdba972e825f300b64c987e88a79b5a5585
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-01 06:05:32 +0000

    Add package mkdocstrings-sh/0.0.1/mkdocstrings_sh-0.0.1.tar.gz

---
+++ packages/mkdocstrings-sh/mkdocstrings_sh-0.0.1.tar.gz/mkdocstrings_sh-0.0.1/docs/examples/all.sh
@@ -0,0 +1,18 @@
+
+# @description This is description.
+# This is description secont line.
+# @warning This is warning
+# This is warning second line.
+# @note This is note.
+# This is note second line.
+# @example This is a first example.
+# This is first example second line.
+# @example
+# This is a second example.
+# This is first example second line.
+# @author First author <first@author.com>
+# @author Second author <second@author.com>
+# @maintainer First maintainer <first@maintainer.com>
+# @maintainer Second maintainer <Second@maintainer.com>
+function example() {}
+

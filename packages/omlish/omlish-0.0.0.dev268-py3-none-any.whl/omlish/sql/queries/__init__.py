e6aae90f8e710a0d7e4de4b6dd04762f007c0859
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-01 15:07:12 +0000

    Add package omlish/0.0.0.dev268/omlish-0.0.0.dev268-py3-none-any.whl

---
+++ packages/omlish/omlish-0.0.0.dev268-py3-none-any.whl/omlish/sql/queries/__init__.py
@@ -0,0 +1,105 @@
+from .base import (  # noqa
+    Builder,
+    Node,
+    Value,
+)
+
+from .binary import (  # noqa
+    Binary,
+    BinaryBuilder,
+    BinaryOp,
+    BinaryOps,
+)
+
+from .exprs import (  # noqa
+    CanExpr,
+    CanLiteral,
+    Expr,
+    ExprBuilder,
+    Literal,
+    NameExpr,
+)
+
+from .idents import (  # noqa
+    CanIdent,
+    Ident,
+    IdentBuilder,
+    IdentLike,
+)
+
+from .multi import (  # noqa
+    Multi,
+    MultiBuilder,
+    MultiKind,
+)
+
+from .names import (  # noqa
+    CanName,
+    Name,
+    NameBuilder,
+    NameLike,
+)
+
+from .ops import (  # noqa
+    OpKind,
+)
+
+from .params import (  # noqa
+    CanParam,
+    Param,
+    ParamBuilder,
+    as_param,
+)
+
+from .relations import (  # noqa
+    CanRelation,
+    CanTable,
+    Relation,
+    RelationBuilder,
+    Table,
+)
+
+from .rendering import (  # noqa
+    Renderer,
+    StdRenderer,
+    render,
+)
+
+from .selects import (  # noqa
+    CanRelation,
+    Select,
+    SelectBuilder,
+    SelectItem,
+)
+
+from .std import (  # noqa
+    StdBuilder,
+)
+
+from .stmts import (  # noqa
+    CanExpr,
+    ExprStmt,
+    Stmt,
+    StmtBuilder,
+)
+
+from .unary import (  # noqa
+    Unary,
+    UnaryBuilder,
+    UnaryOp,
+    UnaryOps,
+)
+
+
+##
+
+
+Q = StdBuilder()
+
+
+##
+
+
+from ...lang.imports import _register_conditional_import  # noqa
+
+_register_conditional_import('...marshal', '.marshal', __package__)

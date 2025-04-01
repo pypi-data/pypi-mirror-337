7ad784964006eb38acea57125af0bf4ceccf0019
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-01 03:26:19 +0000

    Add package midii/0.1.5/midii-0.1.5.tar.gz

---
+++ packages/midii/midii-0.1.5.tar.gz/midii-0.1.5/test.py
@@ -0,0 +1,45 @@
+import midii
+import mido
+
+
+def test_sample():
+    print(midii.sample.real)
+    print(midii.sample.dataset)
+
+
+def test_mido_print_tracks():
+    ma = mido.MidiFile(midii.sample.dataset[1])
+    ma.print_tracks()
+
+
+def test_midii_print_tracks():
+    ma = midii.MidiFile(
+        midii.sample.dataset[1], convert_1_to_0=True, lyric_encoding="cp949"
+    )
+    # ma.quantization(unit="32")
+    ma.print_tracks(
+        track_bound=None,
+        track_list=None,
+        blind_note_info=True,
+        blind_lyric=True,
+    )
+
+
+def test_midii_quantization():
+    ma = midii.MidiFile(
+        midii.sample.dataset[1], convert_1_to_0=True, lyric_encoding="cp949"
+    )
+    ma.quantization(unit="32")
+    ma.print_tracks(
+        track_bound=None,
+        track_list=None,
+        blind_note_info=True,
+        blind_lyric=True,
+    )
+
+
+if __name__ == "__main__":
+    # test_sample()
+    # test_mido_print_tracks()
+    test_midii_print_tracks()
+    # test_midii_quantization()

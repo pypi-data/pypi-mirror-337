b5f5b648fbce1d2aaa1ddd091211fba9d290b89f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-01 05:06:20 +0000

    Add package pori-python/1.0.0/pori_python-1.0.0.tar.gz

---
+++ packages/pori-python/pori_python-1.0.0.tar.gz/pori_python-1.0.0/pori_python/ipr/constants.py
@@ -0,0 +1,12 @@
+DEFAULT_URL = "https://iprstaging-api.bcgsc.ca/api"
+GERMLINE_BASE_TERMS = ("pharmacogenomic", "cancer predisposition")  # based on graphkb.constants
+VARIANT_CLASSES = {"Variant", "CategoryVariant", "PositionalVariant", "CatalogueVariant"}
+
+# all possible values for review status are: ['pending', 'not required', 'passed', 'failed', 'initial']
+FAILED_REVIEW_STATUS = "failed"
+
+# Signatures
+COSMIC_SIGNATURE_VARIANT_TYPE = "high signature"
+HLA_SIGNATURE_VARIANT_TYPE = "signature present"
+TMB_HIGH = 10.0  # genomic mutations per mb - https://www.bcgsc.ca/jira/browse/GERO-296
+TMB_HIGH_CATEGORY = "high mutation burden"

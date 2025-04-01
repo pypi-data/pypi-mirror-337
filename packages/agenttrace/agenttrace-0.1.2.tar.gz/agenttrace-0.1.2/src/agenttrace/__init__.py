38e83cf0ed487895f573eca70a7dc4078683f0e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-01 16:05:46 +0000

    Add package agenttrace/0.1.2/agenttrace-0.1.2.tar.gz

---
+++ packages/agenttrace/agenttrace-0.1.2.tar.gz/agenttrace-0.1.2/src/agenttrace/__init__.py
@@ -0,0 +1,10 @@
+"""
+AgentTrace - A tool for evaluating and tracing agent operations.
+"""
+
+# Import main classes to make them available at the top level
+from .agenttrace import TraceManager
+from .agenttrace import TracerEval
+
+# Version information
+__version__ = "0.1.2" 

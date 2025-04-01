52bf72ddbc27ee7d667e41abdd5bf88786dbfa07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-01 07:05:42 +0000

    Add package itvalleyorion/0.1.0/itvalleyorion-0.1.0-py3-none-any.whl

---
+++ packages/itvalleyorion/itvalleyorion-0.1.0-py3-none-any.whl/itvalleyorion/exemplos/texttospeech.py
@@ -0,0 +1,9 @@
+from itvalleyorion.orionsdk import Orion
+import requests
+
+
+client = Orion(api_key="sua_chave_api_aqui")  # Substitua pela sua chave API
+
+response = client.tts.text_to_speech("Olá, mundo!", "output.mp3")
+print("=== CONVERSÃO DE TEXTO EM ÁUDIO ===")
+print("Áudio salvo como output.mp3")

bd2293fcc59e60a461503f94a10733859a23c512
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-01 14:05:50 +0000

    Add package ua-generator/2.0.4/ua_generator-2.0.4-py3-none-any.whl

---
+++ packages/ua-generator/ua_generator-2.0.4-py3-none-any.whl/ua_generator/exceptions.py
@@ -0,0 +1,13 @@
+"""
+Random User-Agent
+Copyright: 2022-2024 Ekin Karadeniz (github.com/iamdual)
+License: Apache License 2.0
+"""
+
+
+class CannotGenerateError(Exception):
+    pass
+
+
+class InvalidArgumentError(Exception):
+    pass

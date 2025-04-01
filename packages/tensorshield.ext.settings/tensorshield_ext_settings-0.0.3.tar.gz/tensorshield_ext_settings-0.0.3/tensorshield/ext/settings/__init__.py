1e398ec2e0835d2d8d177324b67697e4a8caf0fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-01 04:14:15 +0000

    Add package tensorshield.ext.settings/0.0.3/tensorshield_ext_settings-0.0.3.tar.gz

---
+++ packages/tensorshield.ext.settings/tensorshield_ext_settings-0.0.3.tar.gz/tensorshield_ext_settings-0.0.3/tensorshield/ext/settings/__init__.py
@@ -0,0 +1,10 @@
+from ._const import *
+from ._environment import Environment
+
+
+__all__: list[str] = [
+    'get_chain_endpoint',
+    'get_network',
+    'Environment',
+    'DEFAULT_NETWORK'
+]

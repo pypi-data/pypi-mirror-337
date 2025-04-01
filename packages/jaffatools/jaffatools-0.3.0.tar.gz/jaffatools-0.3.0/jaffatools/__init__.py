1049ce2f805307051c9c8b7268cc4f6a6d32aa64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-01 15:07:03 +0000

    Add package jaffatools/0.3.0/jaffatools-0.3.0.tar.gz

---
+++ packages/jaffatools/jaffatools-0.3.0.tar.gz/jaffatools-0.3.0/jaffatools/__init__.py
@@ -0,0 +1,21 @@
+'''
+JaffaTools - A Python package for working with JAFFA fusion gene detection
+'''
+
+__version__ = '0.3.0'
+__author__ = 'Waidong Huang'
+__email__ = 'wdhuang927@gmail.com'
+
+# Import main functions for easy access
+from .fastq_filter import extract_read_names, filter_fastq_parallel
+from .bam_annotator import extract_nb_tags, annotate_jaffa_results
+from .jaffa_runner import run_jaffa
+
+# Define what's available when using `from jaffatools import *`
+__all__ = [
+    'extract_read_names',
+    'filter_fastq_parallel',
+    'extract_nb_tags',
+    'annotate_jaffa_results',
+    'run_jaffa'
+]

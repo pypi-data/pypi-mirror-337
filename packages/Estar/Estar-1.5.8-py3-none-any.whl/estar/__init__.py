f8d531f785b64fe888e62ed57a25181c20157721
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-01 03:26:33 +0000

    Add package Estar/1.5.8/Estar-1.5.8-py3-none-any.whl

---
+++ packages/Estar/Estar-1.5.8-py3-none-any.whl/estar/__init__.py
@@ -0,0 +1,19 @@
+from .binarisation_methods import*
+from .Declustering import*
+from .spatial_methods import*
+from .utils import*
+from .validation_tools import*
+
+print("init file executed")
+print("###########################################")
+print(" Hi! thank you for downloaded this package!")
+print("If there is a problem in the code or a suggestion, please email me at maxime.hoareau@ens-lyon.fr")
+print("For further information see the realted article:")
+print("type help(func), for a given func to see all possibilities")
+print("###########################################")
+print("USEFUL functions for users:")
+print(">>>CurrRange (main)")
+print(">>>runoverfile (main over file)")
+print(">>>BoyceIndexTresh (binarisation)")
+print(">>>PseudoRange (reference range reconstruction)")
+print("############################################")

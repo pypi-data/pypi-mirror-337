8d4842b5c91e61667f410f2688a18cdb45037ec5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-01 06:05:53 +0000

    Add package bookio-fetchfox/5.1.5/bookio_fetchfox-5.1.5-py3-none-any.whl

---
+++ packages/bookio-fetchfox/bookio_fetchfox-5.1.5-py3-none-any.whl/fetchfox/constants/blockchains.py
@@ -0,0 +1,12 @@
+from fetchfox.constants.algorand.basics import ALGORAND
+from fetchfox.constants.cardano.basics import CARDANO
+from fetchfox.constants.ethereum.basics import ETHEREUM
+from fetchfox.constants.polygon.basics import POLYGON
+
+
+BLOCKCHAINS = [
+    ALGORAND,
+    CARDANO,
+    ETHEREUM,
+    POLYGON,
+]

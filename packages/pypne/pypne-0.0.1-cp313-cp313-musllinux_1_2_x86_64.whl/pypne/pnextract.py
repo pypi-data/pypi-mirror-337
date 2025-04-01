574cf4e9b7d3f04ca3037cab0e5122fbde95420e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-01 07:05:55 +0000

    Add package pypne/0.0.1/pypne-0.0.1-cp313-cp313-musllinux_1_2_x86_64.whl

---
+++ packages/pypne/pypne-0.0.1-cp313-cp313-musllinux_1_2_x86_64.whl/pypne/pnextract.py
@@ -0,0 +1,11 @@
+import numpy as np
+import pypne_cpp
+
+
+def pnextract(image,resolution=1.):
+    image = image.astype(np.uint8)
+    nz,ny,nx = image.shape
+    res = pypne_cpp.pnextract(nx,ny,nz,resolution,image.reshape(-1))
+    image_VElems = res['VElems'].reshape(nz+2,ny+2,nx+2)
+    pn = res['pn']
+    return image_VElems,pn

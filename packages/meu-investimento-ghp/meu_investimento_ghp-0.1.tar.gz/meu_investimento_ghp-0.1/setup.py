261ced5b3e50cfe54a95a2aae26e72147624f231
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-01 05:09:23 +0000

    Add package meu-investimento-ghp/0.1/meu_investimento_ghp-0.1.tar.gz

---
+++ packages/meu-investimento-ghp/meu_investimento_ghp-0.1.tar.gz/meu_investimento_ghp-0.1/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+   name='meu_investimento_ghp',
+   version='0.1',
+   packages=find_packages(),
+   install_requires=[],
+   author='Gabriel Henrique Pascon',
+   author_email='gh.pascon@gmail.com',
+   description='Uma biblioteca para cálculos de investimentos.',
+   url='https://github.com/ghpascon/meu_investimento_ghp',
+   classifiers=[
+       'Programming Language :: Python :: 3',
+       'License :: OSI Approved :: MIT License',
+       'Operating System :: OS Independent',
+   ],
+   python_requires='>=3.6',
+)

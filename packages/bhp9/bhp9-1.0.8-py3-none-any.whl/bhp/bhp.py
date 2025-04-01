d619d1117cbc7dc19c532c3128f9f6cae73b56e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-01 05:06:15 +0000

    Add package bhp9/1.0.8/bhp9-1.0.8-py3-none-any.whl

---
+++ packages/bhp9/bhp9-1.0.8-py3-none-any.whl/bhp/bhp.py
@@ -0,0 +1,5 @@
+# Import the main function from the compiled .so file (bhp)
+from .bhp import main2  # This assumes main is exposed by the compiled .so file
+
+if __name__ == "__main2__":
+    main2()  # Call the main function

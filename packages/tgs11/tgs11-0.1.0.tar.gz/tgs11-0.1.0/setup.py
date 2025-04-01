eb6b1ea6a40e246a30891243ab4900bc305d9248
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-01 14:05:34 +0000

    Add package tgs11/0.1.0/tgs11-0.1.0.tar.gz

---
+++ packages/tgs11/tgs11-0.1.0.tar.gz/tgs11-0.1.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='tgs11',
+    version='0.1.0',
+    description='First tea package',
+    url='https://github.com/ganisacik/tgs11',
+    author='ganisacik',
+    author_email='senin-email@example.com',
+    packages=['tgs11'],
+    install_requires=['requests'],  # Örnek bağımlılık
+    project_urls={
+        'Source': 'https://github.com/ganisacik/tgs11',
+    },
+)

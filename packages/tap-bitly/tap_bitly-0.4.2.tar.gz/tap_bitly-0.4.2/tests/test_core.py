8f2fd99d472f5adf290f313de84faa99ebafb914
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-01 05:05:53 +0000

    Add package tap-bitly/0.4.2/tap_bitly-0.4.2.tar.gz

---
+++ packages/tap-bitly/tap_bitly-0.4.2.tar.gz/tap_bitly-0.4.2/tests/test_core.py
@@ -0,0 +1,19 @@
+"""Tests standard tap features using the built-in SDK tests library."""
+
+from __future__ import annotations
+
+from singer_sdk.testing import SuiteConfig, get_tap_test_class
+
+from tap_bitly.tap import TapBitly
+
+TestTapBitly = get_tap_test_class(
+    TapBitly,
+    config={},
+    suite_config=SuiteConfig(
+        ignore_no_records_for_streams=[
+            "bsds",
+            "campaigns",
+            "channels",
+        ],
+    ),
+)

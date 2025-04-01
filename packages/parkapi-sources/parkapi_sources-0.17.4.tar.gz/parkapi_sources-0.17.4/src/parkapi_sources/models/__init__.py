25b7d5f979eabaae42d01aa6447fdb8f9528a399
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-01 16:05:28 +0000

    Add package parkapi-sources/0.17.4/parkapi_sources-0.17.4.tar.gz

---
+++ packages/parkapi-sources/parkapi_sources-0.17.4.tar.gz/parkapi_sources-0.17.4/src/parkapi_sources/models/__init__.py
@@ -0,0 +1,15 @@
+"""
+Copyright 2024 binary butterfly GmbH
+Use of this source code is governed by an MIT-style license that can be found in the LICENSE.txt.
+"""
+
+from .geojson_inputs import (
+    GeojsonBaseFeatureInput,
+    GeojsonBaseFeaturePropertiesInput,
+    GeojsonFeatureGeometryInput,
+    GeojsonFeatureInput,
+    GeojsonInput,
+)
+from .parking_site_inputs import RealtimeParkingSiteInput, StaticParkingSiteInput
+from .source_info import SourceInfo
+from .xlsx_inputs import ExcelOpeningTimeInput, ExcelStaticParkingSiteInput

824b67a05c5d6968d48d6805bd348bc65c464a57
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-01 16:05:34 +0000

    Add package ngio/0.2.1/ngio-0.2.1.tar.gz

---
+++ packages/ngio/ngio-0.2.1.tar.gz/ngio-0.2.1/src/ngio/ome_zarr_meta/__init__.py
@@ -0,0 +1,50 @@
+"""Utilities for reading and writing OME-Zarr metadata."""
+
+from ngio.ome_zarr_meta._meta_handlers import (
+    ImageMetaHandler,
+    LabelMetaHandler,
+    find_image_meta_handler,
+    find_label_meta_handler,
+    find_plate_meta_handler,
+    find_well_meta_handler,
+    get_image_meta_handler,
+    get_label_meta_handler,
+    get_plate_meta_handler,
+    get_well_meta_handler,
+)
+from ngio.ome_zarr_meta.ngio_specs import (
+    AxesMapper,
+    Dataset,
+    ImageInWellPath,
+    NgffVersion,
+    NgioImageMeta,
+    NgioLabelMeta,
+    NgioPlateMeta,
+    NgioWellMeta,
+    PixelSize,
+)
+
+__all__ = [
+    "AxesMapper",
+    "Dataset",
+    "ImageInWellPath",
+    "ImageMetaHandler",
+    "ImageMetaHandler",
+    "LabelMetaHandler",
+    "LabelMetaHandler",
+    "NgffVersion",
+    "NgffVersion",
+    "NgioImageMeta",
+    "NgioLabelMeta",
+    "NgioPlateMeta",
+    "NgioWellMeta",
+    "PixelSize",
+    "find_image_meta_handler",
+    "find_label_meta_handler",
+    "find_plate_meta_handler",
+    "find_well_meta_handler",
+    "get_image_meta_handler",
+    "get_label_meta_handler",
+    "get_plate_meta_handler",
+    "get_well_meta_handler",
+]

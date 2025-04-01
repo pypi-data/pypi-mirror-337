d1917a3b28c201feefb729a06a020f2c228418b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-01 04:12:29 +0000

    Add package meu-invest-andregon/0.0.1/meu_invest_andregon-0.0.1.tar.gz

---
+++ packages/meu-invest-andregon/meu_invest_andregon-0.0.1.tar.gz/meu_invest_andregon-0.0.1/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+   name='meu_invest_andregon',
+   version='0.0.1',
+   packages=find_packages(),
+   install_requires=[],
+   author='Andre G Almeida',
+   author_email='tadriano.dev@gmail.com',
+   description='Uma biblioteca para cálculos de investimentos.',
+   classifiers=[
+       'Programming Language :: Python :: 3',
+       'License :: OSI Approved :: MIT License',
+       'Operating System :: OS Independent',
+   ],
+   python_requires='>=3.6',
+)

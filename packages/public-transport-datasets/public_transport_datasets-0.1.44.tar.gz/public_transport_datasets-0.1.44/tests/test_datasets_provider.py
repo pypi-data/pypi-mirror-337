7b837944215554e8385bb9274ced5ecc1848abe9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-01 05:06:49 +0000

    Add package public-transport-datasets/0.1.44/public_transport_datasets-0.1.44.tar.gz

---
+++ packages/public-transport-datasets/public_transport_datasets-0.1.44.tar.gz/public_transport_datasets-0.1.44/tests/test_datasets_provider.py
@@ -0,0 +1,13 @@
+import sys
+import os
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
+import json
+import pytest
+from public_transport_datasets.datasets_provider import DatasetsProvider
+
+
+def test_load_dataset():
+    countries = DatasetsProvider.get_available_countries()
+    assert len(countries) > 0

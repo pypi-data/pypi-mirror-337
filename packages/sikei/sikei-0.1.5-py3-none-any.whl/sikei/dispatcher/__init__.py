e21b7408b99771e064cac8bebabbf26bbb0b0fb8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-01 06:06:13 +0000

    Add package sikei/0.1.5/sikei-0.1.5-py3-none-any.whl

---
+++ packages/sikei/sikei-0.1.5-py3-none-any.whl/sikei/dispatcher/__init__.py
@@ -0,0 +1,9 @@
+from sikei.dispatcher.default import DefaultDispatcher
+from sikei.dispatcher.dispatch_result import DispatchResult
+from sikei.dispatcher.protocol import Dispatcher
+
+__all__ = (
+    "DispatchResult",
+    "DefaultDispatcher",
+    "Dispatcher",
+)

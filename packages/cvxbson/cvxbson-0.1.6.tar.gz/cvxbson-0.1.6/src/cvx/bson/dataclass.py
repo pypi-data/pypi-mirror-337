ec0589eb7b524e62b449ff3ec7ff6b70a8da3667
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-01 04:08:25 +0000

    Add package cvxbson/0.1.6/cvxbson-0.1.6.tar.gz

---
+++ packages/cvxbson/cvxbson-0.1.6.tar.gz/cvxbson-0.1.6/src/cvx/bson/dataclass.py
@@ -0,0 +1,35 @@
+#    Copyright 2023 Stanford University Convex Optimization Group
+#
+#    Licensed under the Apache License, Version 2.0 (the "License");
+#    you may not use this file except in compliance with the License.
+#    You may obtain a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS,
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    See the License for the specific language governing permissions and
+#    limitations under the License.
+from dataclasses import dataclass
+from typing import Any
+
+from .file import FILE, read_bson, write_bson
+
+
+@dataclass(frozen=True)
+class Data:
+    def to_bson(self, file: FILE) -> int:
+        return write_bson(file, self.__dict__)
+
+    @classmethod
+    def from_bson(cls, file: FILE) -> Any:
+        x = read_bson(file)
+        return cls(**x)
+
+    @classmethod
+    def keys(cls):
+        yield from cls.__dict__["__annotations__"].keys()
+
+    def items(self):
+        yield from self.__dict__.items()
---
+++ packages/cvxbson/cvxbson-0.1.6.tar.gz/cvxbson-0.1.6/src/cvx/json/file.py
@@ -0,0 +1,53 @@
+#    Copyright 2023 Stanford University Convex Optimization Group
+#
+#    Licensed under the Apache License, Version 2.0 (the "License");
+#    you may not use this file except in compliance with the License.
+#    You may obtain a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS,
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    See the License for the specific language governing permissions and
+#    limitations under the License.
+"""
+Tools to support working with json files
+"""
+
+import json
+from collections.abc import Iterable
+from os import PathLike
+from typing import Any, Dict, Union
+
+import numpy as np
+import numpy.typing as npt
+
+# see https://github.com/microsoft/pylance-release/issues/2019
+from typing_extensions import TypeAlias
+
+from .numpyencoder import NumpyEncoder
+
+FILE = Union[str, bytes, PathLike]
+MATRIX: TypeAlias = npt.NDArray[Any]
+DATA = Dict[str, Any]
+
+
+def read_json(json_file: FILE) -> DATA:
+    """Read a json file and return a genaerator of key-value pairs"""
+    with open(json_file) as f:
+        json_data = json.load(f)
+        d = {}
+        for name, data in json_data.items():
+            if isinstance(data, Iterable):
+                d[name] = np.asarray(data)
+            else:
+                d[name] = data
+
+        return d
+
+
+def write_json(json_file: FILE, data: DATA) -> None:
+    """Write a json file with the data"""
+    with open(json_file, "w") as f:
+        json.dump(data, f, cls=NumpyEncoder)

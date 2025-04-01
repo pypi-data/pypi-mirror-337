393255744df45454cbef7eb4606944e2b2cbffa4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-01 03:26:37 +0000

    Add package mkdocs-nodegraph/0.1.2/mkdocs_nodegraph-0.1.2.tar.gz

---
+++ packages/mkdocs-nodegraph/mkdocs_nodegraph-0.1.2.tar.gz/mkdocs_nodegraph-0.1.2/setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup, find_packages
+
+with open('README.md', 'r') as fin:
+    long_description = fin.read()
+
+setup(
+    name="mkdocs-nodegraph",
+    version="0.1.2",
+    author="JeongYong Hwang",
+    author_email="yonge123@gmail.com",
+    keywords=["mkdocs", "plugin", "nodegraph"],
+    url="",
+    packages=find_packages(),
+    license="MIT",
+    license_files=["LICENSE"],
+    install_requires=[
+        "mkdocs>=1.4.0",
+        "mkdocs-material>=9.5.31",
+        "pyembed-markdown>=1.1.0",
+        "mkdocs-glightbox>=0.4.0",
+        "pyvis>=0.3.0",
+        "PyYAML>=6.0.2",
+    ],
+    entry_points={
+        'mkdocs.plugins': [
+            'nodegraph = mkdocs_nodegraph.plugin:GraphViewPlugin',
+        ]
+    },
+    package_data={
+        "mkdocs_nodegraph": [
+            "nodegraph/graph_opts.json",
+            "nodegraph/pyvis_opts.js",
+            "nodegraph/templates/__init__.py",
+            "nodegraph/templates/template.html",
+        ],
+    },
+    description="Node Graph Plugin For Mkdocs Material",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+)   

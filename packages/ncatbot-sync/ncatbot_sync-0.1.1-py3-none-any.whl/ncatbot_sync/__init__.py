cf6548308258938c26ff5cde87dce9283450fabd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-01 15:07:13 +0000

    Add package ncatbot-sync/0.1.1/ncatbot_sync-0.1.1-py3-none-any.whl

---
+++ packages/ncatbot-sync/ncatbot_sync-0.1.1-py3-none-any.whl/ncatbot_sync/__init__.py
@@ -0,0 +1,8 @@
+__version__ = "0.1.1"
+
+from .client import BotClient
+from .flags import Intents
+from .status import StatusType
+from .message import GroupMessage, PrivateMessage, NoticeMessage, RequestMessage
+
+__all__ = ["StatusType", "BotClient", "Intents", "GroupMessage", "PrivateMessage", "NoticeMessage", "RequestMessage"]

6fb43c3b4147eddf1bcb60cf1e0e790f1f18b112
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-01 05:06:14 +0000

    Add package coslab-core/0.9.6/coslab_core-0.9.6.tar.gz

---
+++ packages/coslab-core/coslab_core-0.9.6.tar.gz/coslab_core-0.9.6/README.md
@@ -0,0 +1,88 @@
+# Cross-service Laber Agreement Score
+
+Image recognition services provide different labels for the same images, 
+with differences broadly either being different word choices for the same 
+concept or not seeing same objects on both images.
+Cross-service Laber Agreement Score (COSLAB) is a method to evaluate the 
+semantic similarity on image labels across several image recognition 
+systems.
+It first produces each image a set of labels, one for each service.
+Then it uses word embedding models to find the closest match across label 
+sets to see how much cross-service agreement there are for each label.
+
+## Conceptual how to use
+
+Our work further highlights that image recognition services differ on what 
+they see on images.
+Due to this, we suggest approaching them as **interprentations** instead 
+of objective truths.
+In response, we propose two different strategies which you can employ to 
+use them in your research:
+
+1. Inclusive strategy: to ensure that your work takes different 
+interprentations into account, use more than one image recognition service 
+to label the images.
+Then merge all labels together and acknowldge that results have inheritent 
+differences emerging from different services.
+1. Rigorous strategy: to ensure that your work takes different 
+interprentations into account, use more than one image recognition service 
+to label the images.
+Then use COSLAB to choose only labels above a pre-defined threshold level 
+for further analysis.
+
+## Use through Python code
+
+```
+from coslab import aws
+from coslab import googlecloud
+from coslab import azure_vision
+from coslab import taggerresults
+from coslab import tag_comparator
+
+## establishing a container for all results
+results = taggerresults.TaggerResults()
+
+## establish classifiers
+amazon = aws.AWS(api_id="", api_key="", api_region="")
+google = googlecloud.GoogleCloud(service_account_info="")
+azure = azure_vision.Azure(subscription_key="", endpoint="")
+
+amazon.process_local( results, "image.png")
+google.process_local( results, "image.png")
+azure.process_local( results, "image.png")
+
+results.export_pickle("image.pickle")
+
+tag_comparator.compare_data( results )
+```
+
+## Use through command line
+
+You can access the full fun coslab-cli into the system, allowing 
+
+## Installation
+
+COSLAB is available [via Pypi](https://pypi.org/project/coslab-core/), so you can
+
+```
+python3 -m pip install coslab-core
+```
+
+# References
+
+* [Berg, A., & Nelimarkka, M. (2023). Do you see what I see? Measuring the 
+semantic differences in image‐recognition services' outputs. _Journal of 
+the Association for Information Science and 
+Technology._](https://asistdl.onlinelibrary.wiley.com/doi/full/10.1002/asi.24827)
+* [Nelimarkka, M., & Berg, A. (2023). Is the World Different Depending on 
+Whose AI Is Looking at It? Comparing Image Recognition Services for Social 
+Science Research. _Information Matters_, 
+3(8).](https://informationmatters.org/2023/08/is-the-world-different-depending-on-whose-ai-is-looking-at-it-comparing-image-recognition-services-for-social-science-research/) 
+
+# Applications
+
+* None yet. Please let us know if you use it.
+
+# Acknowledgments
+
+We generiously thank C. V. Åkerlund Foundation for supporting this work.
---
+++ packages/coslab-core/coslab_core-0.9.6.tar.gz/coslab_core-0.9.6/src/coslab/coslabcli.py
@@ -0,0 +1,147 @@
+import os
+import argparse
+import json
+import csv
+
+import filetype
+import yaml
+from progress.bar import Bar
+
+from coslab import aws
+from coslab import googlecloud
+from coslab import azure_vision
+from coslab import taggerresults
+from coslab import tag_comparator
+## import imagetaggers
+
+def image_files(folder):
+    out = []
+    for root, folders, files in os.walk(folder):
+        files = map(lambda f: root + '/' + f, files)
+        files = filter(lambda f: filetype.guess(f).mime.startswith('image/'), files)
+        out += files
+    return list(out)
+
+def load_config(file):
+    if file.endswith('.yaml'):
+        return yaml.safe_load(open(file))
+    if file.endswith('.json'):
+        return json.load(open(file))
+    return {}
+
+def main():
+
+    parser = argparse.ArgumentParser(
+        description="Automatically tag pictures using exernal APIs."
+    )
+
+    parser.add_argument(
+        "--config",
+        type=str,
+        help="Path to the configuration file",
+        required=True
+    )
+
+    ## URL interface not yet implemented
+    group = parser.add_mutually_exclusive_group(required=True)
+    ## group.add_argument("--file", type=str, help="Path to file containing URLs")
+    group.add_argument("--folder", type=str,
+                       help="Path to folder containing images")
+    
+    services = {
+        'aws' : aws.AWS,
+        'google': googlecloud.GoogleCloud,
+        'azure': azure_vision.Azure
+    }
+
+    parser.add_argument(
+        '--api',
+        choices=services.keys(),
+        nargs='+',
+        required=True
+    )
+
+    parser.add_argument(
+        "--sql",
+        type=str,
+        help="Where the SQL results are stored.",
+    )
+
+    parser.add_argument(
+        "--pickle",
+        type=str,
+        help="Where the pickle results are stored.",
+    )
+
+    parser.add_argument(
+        "--csv",
+        type=str,
+        help="Where the CVS results are stored.",
+    )
+
+    comparators = {
+        'glove' : tag_comparator.glove_comparator,
+        'w2v': tag_comparator.w2v_comparator
+    }
+
+    parser.add_argument(
+        '--compare',
+        choices=comparators.keys(),
+        nargs='+',
+    )
+
+    args = parser.parse_args()
+
+    config = load_config(args.config)
+
+    out = taggerresults.TaggerResults()
+    use_services = []
+
+    for service in args.api:
+        tagger = services[ service ].from_config( config )
+        use_services.append( tagger )
+
+    if args.folder:
+        directory = args.folder
+        images = image_files(directory)
+
+        bar = Bar('Images labelled', max=len(images)*len(use_services))
+
+        for image in images:
+            for service in use_services:
+                if 'minimal_confidence' in config:
+                    service.process_local(out, image, min_confidence = config['minimal_confidence'])
+                else:
+                    service.process_local(out, image)
+                bar.next()
+
+        bar.finish()
+
+    comparisons = {}
+
+    for comparator in args.compare:
+        comparisons[ comparator ] = tag_comparator.compare_data( out, comparator=comparators[ comparator ] )
+
+    if args.sql:
+        out.export_sql( args.sql )
+
+    if args.pickle:
+        out.export_pickle( args.pickle )
+
+    if args.csv:
+        out.export_csv( args.csv )
+        
+        for comparator in args.compare:
+            fname = f"{args.csv.replace('.csv', '')}-{comparator}.csv"
+
+            with open( fname, 'w') as f:
+                csvf = csv.writer( f )
+                for services, results in comparisons[ comparator ].items():
+                    for label, scores in results.items():
+                        for score in scores:
+                            csvf.writerow( [ services[0], services[1], label, score ] )
+                f.close()
+
+
+
+

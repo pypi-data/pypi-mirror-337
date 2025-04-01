2f5217ffd11cd5a5773d5503605192aee9fc94dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-01 14:06:03 +0000

    Add package intelliw/2.2.2/intelliw-2.2.2-py3-none-any.whl

---
+++ packages/intelliw/intelliw-2.2.2-py3-none-any.whl/intelliw/interface/__init__.py
@@ -0,0 +1,13 @@
+'''
+Author: hexu
+Date: 2021-10-28 19:43:56
+LastEditTime: 2021-10-28 19:43:56
+LastEditors: your name
+Description: 
+FilePath: /iw-algo-fx/intelliw/interface/__init__.py
+'''
+from intelliw.utils import yms_downloader
+yms_downloader.run()
+
+from intelliw.utils.logger import Logger
+Logger()

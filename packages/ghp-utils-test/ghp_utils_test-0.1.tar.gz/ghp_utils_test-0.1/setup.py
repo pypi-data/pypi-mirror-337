d1e5a2f79bf53ff99309423064284621f665fc5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-01 14:05:20 +0000

    Add package ghp-utils-test/0.1/ghp_utils_test-0.1.tar.gz

---
+++ packages/ghp-utils-test/ghp_utils_test-0.1.tar.gz/ghp_utils_test-0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="ghp_utils_test",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[],  # Adicione aqui as depend�ncias, se houver
+    author="ghpascon",
+    description="Biblioteca de utilitários para saudações e operações básicas",
+    url="https://github.com/ghpascon/ghp_utils_test",  # Substitua com o link real
+    license="MIT",
+    )
+    

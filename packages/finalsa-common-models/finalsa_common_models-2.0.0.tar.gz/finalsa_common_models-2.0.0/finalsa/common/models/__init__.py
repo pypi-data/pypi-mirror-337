94ab48cbb42e999c2ed0c6838344129696d74fa9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-01 14:05:58 +0000

    Add package finalsa-common-models/2.0.0/finalsa_common_models-2.0.0.tar.gz

---
+++ packages/finalsa-common-models/finalsa_common_models-2.0.0.tar.gz/finalsa_common_models-2.0.0/finalsa/common/models/__init__.py
@@ -0,0 +1,14 @@
+from finalsa.common.models.models import (
+    Meta,
+    SqsReponse,
+    parse_message_attributes,
+    to_message_attributes
+)
+
+
+__all__ = [
+    "Meta",
+    "SqsReponse",
+    "parse_message_attributes",
+    "to_message_attributes",
+]

f92245458ed6276c465b3b90d50ba9279f2d0531
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-01 14:05:27 +0000

    Add package tgs12/0.1.0/tgs12-0.1.0.tar.gz

---
+++ packages/tgs12/tgs12-0.1.0.tar.gz/tgs12-0.1.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='tgs12',
+    version='0.1.0',
+    description='Second tea package',
+    url='https://github.com/ganisacik/tgs12',
+    author='ganisacik',
+    author_email='senin-email@example.com',
+    packages=['tgs12'],
+    install_requires=['numpy'],  # Örnek bağımlılık
+    project_urls={
+        'Source': 'https://github.com/ganisacik/tgs12',
+    },
+)

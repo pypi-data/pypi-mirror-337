8e9674e0f96dabb2223981f71cf5fab2f2b81823
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-01 05:08:16 +0000

    Add package databloom/2.0.0/databloom-2.0.0-py3-none-any.whl

---
+++ packages/databloom/databloom-2.0.0-py3-none-any.whl/databloom/__init__.py
@@ -0,0 +1,4 @@
+from .connector import connector, GoogleSheetsConnector, BaseConnector
+from .datasets import Dataset
+
+__all__ = ['connector', 'GoogleSheetsConnector', 'BaseConnector', 'Dataset'] 

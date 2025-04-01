00b3db99254dcef39852b0afb46187e7572de553
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-01 03:26:22 +0000

    Add package langchain-oxylabs/0.1.0/langchain_oxylabs-0.1.0-py3-none-any.whl

---
+++ packages/langchain-oxylabs/langchain_oxylabs-0.1.0-py3-none-any.whl/langchain_oxylabs/__init__.py
@@ -0,0 +1,10 @@
+from langchain_oxylabs.tools import OxylabsSearchRun, OxylabsSearchResults
+from langchain_oxylabs.utilities import OxylabsSearchAPIWrapper
+from langchain_oxylabs._version import __version__
+
+__all__ = [
+    "OxylabsSearchRun",
+    "OxylabsSearchResults",
+    "OxylabsSearchAPIWrapper",
+    "__version__",
+]

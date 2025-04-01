52e760c9167fec75da027556cc134b444ed633d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-01 03:26:14 +0000

    Add package polars-cloud/0.0.6/polars_cloud-0.0.6-cp39-abi3-win_arm64.whl

---
+++ packages/polars-cloud/polars_cloud-0.0.6-cp39-abi3-win_arm64.whl/polars_cloud/exceptions.py
@@ -0,0 +1,19 @@
+"""Module for custom Polars Cloud exceptions."""
+
+from __future__ import annotations
+
+
+class AuthenticationError(Exception):
+    """Exception raised when a valid authentication token could not be found."""
+
+
+class VerificationTimeoutError(Exception):
+    """Exception raised when verification has timed out."""
+
+
+class WorkspaceResolveError(Exception):
+    """Exception raised when workspace could not be resolved."""
+
+
+class ComputeClusterMisspecified(Exception):
+    """Exception raised when compute cluster is not correctly specified."""

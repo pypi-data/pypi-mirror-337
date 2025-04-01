ed042a77200ef6eb5f0c4a58cae8791698279e09
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-01 15:06:36 +0000

    Add package joow-image-processing/0.0.1/joow_image_processing-0.0.1.tar.gz

---
+++ packages/joow-image-processing/joow_image_processing-0.0.1.tar.gz/joow_image_processing-0.0.1/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as f:
+    page_description = f.read()
+
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name="joow_image_processing",
+    version="0.0.1",
+    author="Jonatas",
+    description="Image processing using skimage 'Fork da Karina'",
+    long_description=page_description,
+    long_description_content_type="text/markdown",
+    url="",
+    packages=find_packages(),
+    install_requires=requirements,
+    python_requires='>=3.5',
+)

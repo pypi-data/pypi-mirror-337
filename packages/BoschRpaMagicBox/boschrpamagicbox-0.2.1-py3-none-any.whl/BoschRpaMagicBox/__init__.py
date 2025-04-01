f1a8dd1011d64ed93640f4594079d64ab0970967
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-01 15:06:42 +0000

    Add package BoschRpaMagicBox/0.2.1/boschrpamagicbox-0.2.1-py3-none-any.whl

---
+++ packages/BoschRpaMagicBox/boschrpamagicbox-0.2.1-py3-none-any.whl/BoschRpaMagicBox/__init__.py
@@ -0,0 +1,7 @@
+from common_config import *
+from common_functions import *
+from data_process_functions import *
+from data_process_functions import *
+from helper_functions import *
+from remote_excel_functions import *
+from smb_functions import *

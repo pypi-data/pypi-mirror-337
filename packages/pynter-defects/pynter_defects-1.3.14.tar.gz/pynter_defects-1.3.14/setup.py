1988a013e0b1509ba0ef53255bf09bbc44082e1a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-01 06:05:33 +0000

    Add package pynter-defects/1.3.14/pynter_defects-1.3.14.tar.gz

---
+++ packages/pynter-defects/pynter_defects-1.3.14.tar.gz/pynter_defects-1.3.14/setup.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 25 14:39:37 2023
+
+@author: villa
+"""
+from setuptools import setup, find_namespace_packages
+
+with open("README.md") as file:
+    long_description = file.read()
+
+setup(
+    name='pynter-defects',
+    version='1.3.14',
+    author='Lorenzo Villa',
+    description='Tools for atomistic calculations, provides features for point-defect calculations',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=find_namespace_packages(exclude=["pynter.*.tests", "pynter.*.*.tests"]),
+    install_requires=[
+        'ase',
+        'pymatgen>=2023.3.23',
+        'pymatgen-analysis-defects>=2023.3.25',
+        'pymatgen-db',
+        'PyYAML',
+        'schedule'        
+    ],
+    package_data={
+        "pynter":["*.yml"],
+        "pynter.vasp":["*.yml"],
+        "pynter.slurm":["*.txt"]
+        },
+    extra_requires={'test':'pytest'},
+    entry_points={
+    "console_scripts": [
+        "pynter = pynter.cli.main:main"]}
+)
+
+

89c3ae5572f5d357836d24e63a982de8702d21dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-01 15:07:11 +0000

    Add package dev-laiser/0.2.18/dev_laiser-0.2.18.tar.gz

---
+++ packages/dev-laiser/dev_laiser-0.2.18.tar.gz/dev_laiser-0.2.18/setup.py
@@ -0,0 +1,42 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='dev-laiser',
+    version='0.2.18', 
+    author='Satya Phanindra Kumar Kalaga, Bharat Khandelwal, Prudhvi Chekuri', 
+    author_email='phanindra.connect@gmail.com',  
+    description='LAiSER (Leveraging Artificial Intelligence for Skill Extraction & Research) is a tool designed to help learners, educators, and employers extract and share trusted information about skills. It uses a fine-tuned language model to extract raw skill keywords from text, then aligns them with a predefined taxonomy. You can find more technical details in the project’s paper.md and an overview in the README.md.', 
+    long_description=open('README.md').read(),  
+    long_description_content_type='text/markdown',
+    url='https://github.com/LAiSER-Software/extract-module',  
+    packages=find_packages(),  
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: BSD License', 
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+        'numpy',
+        'pandas',
+        'scipy',
+        'psutil',
+        'skillNer',
+        'scikit-learn',
+        'spacy',
+        'tokenizers',
+        'accelerate',
+        'bitsandbytes',
+        'datasets',
+        'huggingface_hub',
+        'peft',
+        'torch',
+        'trl',
+        'ipython',
+        'python-dotenv',
+        'vllm',
+        'tqdm',
+        'triton'
+    ],
+
+)

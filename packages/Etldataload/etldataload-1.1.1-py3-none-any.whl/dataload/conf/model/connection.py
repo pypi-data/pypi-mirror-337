c66a0fe6fd79f5e55e683522facc3f1aa5d2138c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-01 05:06:21 +0000

    Add package Etldataload/1.1.1/etldataload-1.1.1-py3-none-any.whl

---
+++ packages/Etldataload/etldataload-1.1.1-py3-none-any.whl/dataload/conf/model/connection.py
@@ -0,0 +1,45 @@
+from dataclasses import dataclass
+
+@dataclass
+class Params:
+    select: str
+    where: str
+
+@dataclass
+class Csv:
+    path: str
+
+@dataclass
+class RestAPI:
+    base_url: str
+    authorization: str
+    accept: str
+    key: str
+    param: Params
+
+@dataclass
+class Postgresql:
+    host: str
+    user: str
+    password: str
+    timeout: int
+    max_retries: int
+    port: int
+    database: str
+
+@dataclass
+class Mysql:
+    host: str
+    user: str
+    password: str
+    port: int
+    database: str
+
+@dataclass
+class Connection:
+    alias: str
+    type: str
+    csv : Csv = None
+    restapi: RestAPI = None
+    postgresql: Postgresql = None
+    mysql: Mysql = None

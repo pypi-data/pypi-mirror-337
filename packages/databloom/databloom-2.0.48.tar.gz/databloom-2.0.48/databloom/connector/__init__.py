0cb3e2ff425519f27a4b42059a4fde6900ae871f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-01 14:06:04 +0000

    Add package databloom/2.0.48/databloom-2.0.48.tar.gz

---
+++ packages/databloom/databloom-2.0.48.tar.gz/databloom-2.0.48/databloom/connector/__init__.py
@@ -0,0 +1,16 @@
+"""
+Connector module for databloom.
+This module provides connectors for various data sources.
+"""
+
+from .base import BaseConnector
+from .postgresql import PostgreSQLConnector
+from .mysql import MySQLConnector
+from .ggsheet import GoogleSheetConnector
+
+__all__ = [
+    'BaseConnector',
+    'PostgreSQLConnector',
+    'MySQLConnector',
+    'GoogleSheetConnector',
+] 

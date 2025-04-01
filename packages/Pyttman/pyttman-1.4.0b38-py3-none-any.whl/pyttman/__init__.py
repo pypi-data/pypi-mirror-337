63d8d4113144964e10145b46c56da6c7a10a080e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-01 07:05:50 +0000

    Add package Pyttman/1.4.0b38/pyttman-1.4.0b38-py3-none-any.whl

---
+++ packages/Pyttman/pyttman-1.4.0b38-py3-none-any.whl/pyttman/__init__.py
@@ -0,0 +1,55 @@
+from pyttman.core.internals import PyttmanApp
+
+from pyttman import version
+from pyttman.tools.logger.logger import PyttmanLogger
+
+__version__ = version.__version__
+__author__ = "Simon Olofsson"
+__copyright__ = "(c) Pyttman development Team 2020-2022"
+__licence__ = "MIT"
+
+
+class _SettingsNotConfigured:
+    def __getattr__(self, item):
+        raise NotImplementedError("pyttman.settings cannot be "
+                                  "accessed in this scope. "
+                                  "To use pyttman.settings  "
+                                  "you must use the Pyttman CLI."
+                                  "If you're looking for an "
+                                  "interactive shell for debugging, "
+                                  "use 'pyttman shell <app name>'. ")
+
+
+class _AppNotConfigured:
+    def __getattr__(self, item):
+        raise NotImplementedError("pyttman.app was accessed before its "
+                                  "creation. This error can occur if you're "
+                                  "trying to decorate functions as lifecycle "
+                                  "hooks, in an ability -or intent module, "
+                                  "which is included in the 'ABILITIES' list "
+                                  "in settings.py. To mitigate this error, "
+                                  "move lifecycle hooks to a separate module.")
+
+
+app: PyttmanApp | None = None
+settings = _SettingsNotConfigured
+is_configured = False
+
+logger = PyttmanLogger()
+"""
+Logs function return value and/or exceptions to the application 
+log file. 
+"""
+
+
+"""
+I love you
+
+- That was written by my girlfriend without me knowing about it,
+on that very line. I left my computer unlocked some time during
+a weekend. It's so sweet that I can't remove it, so it's here,
+a part of the framework forever, that's the way it is. Consider
+yourself the founder of an easter egg.
+
+// Simon Olofsson, lead developer and founder of Pyttman
+"""
---
+++ packages/Pyttman/pyttman-1.4.0b38-py3-none-any.whl/pyttman/core/entity_parsing/entity.py
@@ -0,0 +1,37 @@
+from dataclasses import dataclass
+from typing import Any
+
+
+@dataclass
+class Entity:
+    """
+    The Entity class represents a string value, identified
+    in a message object from a user. Entities are strings
+    with value of interest. It could be anything from a
+    phone number, to the name of a restaurant, or which
+    city a weather question relates to.
+
+    The Entity class keeps track of its own occurrence in
+    the message object from which it stems, and its string value.
+    """
+    value: Any
+    index_in_message: int = 0
+    is_fallback_default: bool = False
+    as_list: bool = False
+
+    def __eq__(self, other):
+        if isinstance(other, str):
+            return other == self.value
+
+        try:
+            return other.value == self.value \
+               and other.index_in_message == self.index_in_message
+        except AttributeError:
+            return False
+
+    def is_boolean(self) -> bool:
+        """
+        Returns whether the value held in `self.value` is a bool,
+        in which case it cannot be evaluated with string methods.
+        """
+        return isinstance(self.value, bool)

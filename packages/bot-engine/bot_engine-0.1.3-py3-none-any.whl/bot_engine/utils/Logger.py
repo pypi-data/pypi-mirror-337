19caee23dd271567f91c5a507a9653b66d02cc4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-01 03:26:16 +0000

    Add package bot-engine/0.1.3/bot_engine-0.1.3-py3-none-any.whl

---
+++ packages/bot-engine/bot_engine-0.1.3-py3-none-any.whl/bot_engine/utils/Logger.py
@@ -0,0 +1,42 @@
+import logging
+from utils.Dotenv import Dotenv
+
+# print(123)
+
+class Logger:
+    def __init__(self):
+        #! Dotenv
+        self.environment = Dotenv().get("ENVIRONMENT")
+        self.set_basic_config()
+
+
+    @staticmethod
+    def set_basic_config():
+        logging.basicConfig(
+            level=logging.INFO,
+            format='%(asctime)s  %(message)s',
+            datefmt='%H:%M:%S'
+        )
+        
+    def print_separators(self) -> None:
+        logger = logging.getLogger(__name__)
+        logger.info(f"\n {'='*10}")
+
+
+    def info(self, message: str):
+        if self.environment == "development":
+            logger = logging.getLogger(__name__)
+            logger.info(message)
+            self.print_separators()
+
+
+    @staticmethod
+    def warning(message: str):
+        logger = logging.getLogger(__name__)
+        logger.warning(message)
+
+
+    @staticmethod
+    def error(message: str):
+        logger = logging.getLogger(__name__)
+        logger.error(message)

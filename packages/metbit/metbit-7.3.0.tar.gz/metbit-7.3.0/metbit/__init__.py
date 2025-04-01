e42d25f76f524dab450ec66f4fc12a7a865e3cb8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-01 16:05:29 +0000

    Add package metbit/7.3.0/metbit-7.3.0.tar.gz

---
+++ packages/metbit/metbit-7.3.0.tar.gz/metbit-7.3.0/metbit/__init__.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+
+from .metbit import opls_da, pca
+from .utility import *
+from .lazy_opls_da import lazy_opls_da
+from .spec_norm import *
+from .peak_processe import peak_chops
+from .STOCSY import STOCSY
+from .ui_stocsy import STOCSY_app
+from .ui_picky_peak import pickie_peak
+from .take_intensity import *
+from .nmr_preprocess import *
+
+
+# -*- coding: utf-8 -*-
+
+__auther__ ='aeiwz'
+author_email='theerayut_aeiw_123@hotmail.com'
+__copyright__="Copyright 2024, Theerayut"
+
+__license__ = "MIT"
+__version__ = "7.0.0"
+__maintainer__ = "aeiwz"
+__email__ = "theerayut_aeiw_123@hotmail.com"
+__status__ = "Develop"
+
+
+
+

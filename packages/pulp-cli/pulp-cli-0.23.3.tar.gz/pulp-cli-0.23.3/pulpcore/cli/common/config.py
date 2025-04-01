04cbfe9ae7aec0f39f2e8b36a127d277ca18ba75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-01 04:10:08 +0000

    Add package pulp-cli/0.23.3/pulp-cli-0.23.3.tar.gz

---
+++ packages/pulp-cli/pulp-cli-0.23.3.tar.gz/pulp-cli-0.23.3/pulpcore/cli/common/config.py
@@ -0,0 +1,251 @@
+import re
+from pathlib import Path
+from typing import Any, Callable, Dict, List, MutableMapping, Optional, TypeVar
+
+import click
+import toml
+from pulp_glue.common.i18n import get_translation
+
+from pulpcore.cli.common.generic import pulp_group
+
+translation = get_translation(__package__)
+_ = translation.gettext
+
+_T = TypeVar("_T")
+
+CONFIG_LOCATIONS = [
+    "/etc/pulp/cli.toml",
+    str(Path(click.utils.get_app_dir("pulp"), "settings.toml")),
+    str(Path(click.utils.get_app_dir("pulp"), "cli.toml")),
+]
+FORMAT_CHOICES = ["json", "yaml", "none"]
+SETTINGS = [
+    "base_url",
+    "api_root",
+    "username",
+    "password",
+    "cert",
+    "key",
+    "verify_ssl",
+    "format",
+    "dry_run",
+    "timeout",
+    "verbose",
+    "domain",
+]
+
+CONFIG_OPTIONS = [
+    click.option("--base-url", default="https://localhost", help=_("API base url")),
+    click.option(
+        "--api-root",
+        default="/pulp/",
+        help=_("Absolute API base path on server (not including 'api/v3/')"),
+    ),
+    click.option("--domain", default="default", help=_("Domain to work in if feature is enabled")),
+    click.option("--username", default="", help=_("Username on pulp server")),
+    click.option("--password", default="", help=_("Password on pulp server")),
+    click.option("--cert", default="", help=_("Path to client certificate")),
+    click.option(
+        "--key",
+        default="",
+        help=_("Path to client private key. Not required if client cert contains this."),
+    ),
+    click.option("--verify-ssl/--no-verify-ssl", default=True, help=_("Verify SSL connection")),
+    click.option(
+        "--format",
+        type=click.Choice(FORMAT_CHOICES, case_sensitive=False),
+        default="json",
+        help=_("Format of the response"),
+    ),
+    click.option(
+        "--dry-run/--force",
+        default=False,
+        help=_("Trace commands without performing any unsafe HTTP calls"),
+    ),
+    click.option(
+        "--timeout",
+        "-T",
+        type=int,
+        default=0,
+        help=_("Time to wait for background tasks, set to 0 to wait infinitely"),
+    ),
+    click.option(
+        "-v",
+        "--verbose",
+        type=int,
+        count=True,
+        help=_("Increase verbosity; explain api calls as they are made"),
+    ),
+]
+
+
+def not_none(value: Optional[_T], exc: Optional[Exception] = None) -> _T:
+    if value is None:
+        raise exc or RuntimeError(_("Value cannot be None."))
+    return value
+
+
+def config_options(command: Callable[..., Any]) -> Callable[..., Any]:
+    for option in reversed(CONFIG_OPTIONS):
+        command = option(command)
+    return command
+
+
+def validate_config(config: Dict[str, Any], strict: bool = False) -> bool:
+    """Validate, that the config provides the proper data types"""
+    errors: List[str] = []
+    if "api_root" in config and (config["api_root"][0] != "/" or config["api_root"][-1] != "/"):
+        errors.append(_("'api_root' must begin and end with '/'"))
+    if "format" in config and config["format"].lower() not in FORMAT_CHOICES:
+        errors.append(_("'format' is not one of {choices}").format(choices=FORMAT_CHOICES))
+    if "dry_run" in config and not isinstance(config["dry_run"], bool):
+        errors.append(_("'dry_run' is not a bool"))
+    if "timeout" in config and not isinstance(config["timeout"], int):
+        errors.append(_("'timeout' is not an integer"))
+    if "verbose" in config and not isinstance(config["verbose"], int):
+        errors.append(_("'verbose' is not an integer"))
+    if "domain" in config and not re.match(r"^[-a-zA-Z0-9_]+\Z", config["domain"]):
+        errors.append(_("'domain' must be a slug string"))
+    unknown_settings = set(config.keys()) - set(SETTINGS)
+    if unknown_settings:
+        errors.append(_("Unknown settings: '{}'.").format("','".join(unknown_settings)))
+    if strict:
+        missing_settings = set(SETTINGS) - set(config.keys())
+        if missing_settings:
+            errors.append(_("Missing settings: '{}'.").format("','".join(missing_settings)))
+    if errors:
+        raise ValueError("\n".join(errors))
+    return True
+
+
+def validate_settings(settings: MutableMapping[str, Dict[str, Any]], strict: bool = False) -> bool:
+    errors: List[str] = []
+    if "cli" not in settings:
+        errors.append(_("Could not locate default profile 'cli' setting"))
+
+    for key, profile in settings.items():
+        if key != "cli" and not key.startswith("cli-"):
+            if strict:
+                errors.append(_("Invalid profile '{key}'").format(key=key))
+            continue
+        try:
+            validate_config(profile, strict=strict)
+        except ValueError as e:
+            errors.append(_("Profile {key}:").format(key=key))
+            errors.append(str(e))
+    if errors:
+        raise ValueError("\n".join(errors))
+    return True
+
+
+@pulp_group(name="config", help=_("Manage pulp-cli config file"))
+def config() -> None:
+    pass
+
+
+# This is a mypy bug getting confused with positional args
+# https://github.com/python/mypy/issues/15037
+@config.command(help=_("Create a pulp-cli config settings file"))  # type: ignore [arg-type]
+@config_options
+@click.option("--interactive", "-i", is_flag=True)
+@click.option("--editor", "-e", is_flag=True, help=_("Edit the config file in an editor"))
+@click.option("--overwrite", "-o", is_flag=True, help=_("Overwrite any existing config file"))
+@click.option("--location", default=CONFIG_LOCATIONS[-1], type=click.Path(resolve_path=True))
+@click.pass_context
+def create(
+    ctx: click.Context,
+    interactive: bool,
+    editor: bool,
+    overwrite: bool,
+    location: str,
+    **kwargs: Any,
+) -> None:
+    def _check_location(location: str) -> None:
+        if not overwrite and Path(location).exists():
+            raise click.ClickException(
+                _(
+                    "File '{location}' already exists. Use --overwrite if you want to overwrite it."
+                ).format(location=location)
+            )
+
+    def prompt_config_option(name: str) -> Any:
+        """Find the option from the root command and prompt."""
+        option = next(p for p in ctx.command.params if p.name == name)
+        if isinstance(option, click.Option) and option.help:
+            help = option.help
+        else:
+            help = name
+        return click.prompt(help, default=(option.default), type=option.type)
+
+    settings: MutableMapping[str, Any] = kwargs
+
+    if interactive:
+        location = click.prompt(_("Config file location"), default=location)
+        _check_location(location)
+        for setting in SETTINGS:
+            settings[setting] = prompt_config_option(setting)
+    else:
+        _check_location(location)
+
+    output: str = toml.dumps({"cli": settings})
+
+    if editor:
+        output = not_none(
+            click.edit(output), click.ClickException(_("No output from editor. Aborting."))
+        )
+    try:
+        settings = toml.loads(output)
+        validate_settings(settings)
+    except (ValueError, toml.TomlDecodeError) as e:
+        raise click.ClickException(str(e))
+
+    Path(location).parent.mkdir(parents=True, exist_ok=True)
+    with Path(location).open("w") as sfile:
+        sfile.write(output)
+
+    click.echo(_("Created config file at '{location}'.").format(location=location))
+
+
+@config.command(help=_("Open the settings config file in an editor"))
+@click.option("--location", default=CONFIG_LOCATIONS[-1], type=click.Path(resolve_path=True))
+def edit(location: str) -> None:
+    if not Path(location).exists():
+        raise click.ClickException(
+            _(
+                "File '{location}' does not exists. If you wish to create the file, use the pulp "
+                "create command."
+            ).format(location=location)
+        )
+
+    with Path(location).open("r") as sfile:
+        output: str = sfile.read()
+    while True:
+        output = not_none(
+            click.edit(output), click.ClickException(_("No output from editor. Aborting."))
+        )
+        try:
+            settings = toml.loads(output)
+            validate_settings(settings)
+            break
+        except (ValueError, toml.TomlDecodeError) as e:
+            click.echo(str(e), err=True)
+            click.confirm(_("Retry"), abort=True)
+    with Path(location).open("w") as sfile:
+        sfile.write(output)
+
+
+@config.command(help=_("Validate a pulp-cli config file"))
+@click.option("--location", default=CONFIG_LOCATIONS[-1], type=click.Path(resolve_path=True))
+@click.option("--strict", is_flag=True, help=_("Validate that all settings are present"))
+def validate(location: str, strict: bool) -> None:
+    try:
+        settings = toml.load(location)
+    except toml.TomlDecodeError:
+        raise click.ClickException(_("Invalid toml file '{location}'.").format(location=location))
+
+    try:
+        validate_settings(settings, strict)
+    except ValueError as e:
+        raise click.ClickException(str(e))
+
+    click.echo(_("File '{location}' is a valid pulp-cli config.").format(location=location))
---
+++ packages/pulp-cli/pulp-cli-0.23.3.tar.gz/pulp-cli-0.23.3/pytest_pulp_cli/__init__.py
@@ -0,0 +1,157 @@
+# type: ignore
+
+import os
+import pathlib
+import subprocess
+
+import gnupg
+import pytest
+import requests
+import toml
+
+
+def pytest_collect_file(file_path, parent):
+    if file_path.suffix == ".sh" and file_path.name.startswith("test_"):
+        return ScriptFile.from_parent(parent, path=file_path)
+
+
+class ScriptFile(pytest.File):
+    # To make pytest.Function happy
+    obj = None
+
+    def collect(self):
+        # Extract the name between "test_" and ".sh".
+        name = self.path.name[5:][:-3]
+        yield ScriptItem.from_parent(self, name=name, path=self.path)
+
+
+# HACK: Inherit from `pytest.Function` to be able to use the fixtures
+class ScriptItem(pytest.Function):
+    def __init__(self, path, **kwargs):
+        super().__init__(callobj=self._runscript, **kwargs)
+        self.path = path
+        self.add_marker("script")
+        if path.parts[-3] == "scripts":
+            self.add_marker(path.parts[-2])
+
+    def _runscript(self, pulp_cli_env, tmp_path, pulp_container_log):
+        run = subprocess.run([self.path], cwd=tmp_path)
+        if run.returncode == 23:
+            pytest.skip("Skipped as requested by the script.")
+        if run.returncode != 0:
+            raise ScriptError(f"Script returned with exit code {run.returncode}.")
+
+    def reportinfo(self):
+        return self.path, 0, f"test script: {self.name}"
+
+    def repr_failure(self, excinfo):
+        if isinstance(excinfo.value, ScriptError):
+            return str(excinfo.value)
+        return super().repr_failure(excinfo)
+
+
+class ScriptError(Exception):
+    """Custom exception to mark script execution failure."""
+
+
+@pytest.fixture
+def pulp_cli_vars():
+    """
+    This fixture will return a dictionary that is used by `pulp_cli_env` to setup the environment.
+    To inject more environment variables, it can overwritten.
+    It will be initialized with "PULP_FIXTURE_URL".
+    """
+    PULP_FIXTURES_URL = os.environ.get("PULP_FIXTURES_URL", "https://fixtures.pulpproject.org")
+
+    return {"PULP_FIXTURES_URL": PULP_FIXTURES_URL}
+
+
+@pytest.fixture(scope="session")
+def pulp_cli_settings(tmp_path_factory):
+    """
+    This fixture will setup the config file once per session only.
+    It is most likely not useful to be included standalone.
+    The `pulp_cli_env` fixture, however depends on it and sets $XDG_CONFIG_HOME up accordingly.
+    """
+    settings = toml.load("tests/cli.toml")
+    if os.environ.get("PULP_API_ROOT"):
+        for key in settings:
+            settings[key]["api_root"] = os.environ["PULP_API_ROOT"]
+    settings_path = tmp_path_factory.mktemp("config", numbered=False)
+    (settings_path / "pulp").mkdir(parents=True)
+    with open(settings_path / "pulp" / "cli.toml", "w") as settings_file:
+        toml.dump(settings, settings_file)
+    yield settings_path, settings
+
+
+@pytest.fixture(scope="session")
+def pulp_cli_gnupghome(tmp_path_factory):
+    """
+    This fixture will setup a GPG home directory once per session only.
+    """
+    gnupghome = tmp_path_factory.mktemp("gnupghome")
+    gpg = gnupg.GPG(gnupghome=str(gnupghome))
+
+    key_file = pathlib.Path(__file__).parent / "GPG-PRIVATE-KEY-fixture-signing"
+    if key_file.exists():
+        private_key_data = key_file.read_text()
+    else:
+        private_key_url = "https://github.com/pulp/pulp-fixtures/raw/master/common/GPG-PRIVATE-KEY-fixture-signing"  # noqa: E501
+        private_key_data = requests.get(private_key_url).text
+        key_file.write_text(private_key_data)
+
+    import_result = gpg.import_keys(private_key_data)
+    gpg.trust_keys(import_result.fingerprints[0], "TRUST_ULTIMATE")
+    return gnupghome
+
+
+@pytest.fixture
+def pulp_cli_env(pulp_cli_settings, pulp_cli_vars, pulp_cli_gnupghome, monkeypatch):
+    """
+    This fixture will set up the environment for cli commands by:
+
+    * creating a tmp_dir
+    * placing the config there
+    * pointing XDG_CONFIG_HOME accordingly
+    * supplying other useful environment vars
+    """
+    settings_path, settings = pulp_cli_settings
+    monkeypatch.setenv("XDG_CONFIG_HOME", str(settings_path))
+    monkeypatch.setenv("PULP_BASE_URL", settings["cli"]["base_url"])
+    monkeypatch.setenv("VERIFY_SSL", str(settings["cli"].get("verify_ssl", True)).lower())
+    monkeypatch.setenv("GNUPGHOME", str(pulp_cli_gnupghome))
+
+    for key, value in pulp_cli_vars.items():
+        monkeypatch.setenv(key, value)
+
+    yield settings
+
+
+if "PULP_LOGGING" in os.environ:
+
+    @pytest.fixture(scope="session")
+    def pulp_container_log_stream():
+        with subprocess.Popen(
+            [os.environ["PULP_LOGGING"], "logs", "-f", "--tail", "0", "pulp-ephemeral"],
+            stdin=subprocess.DEVNULL,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.STDOUT,
+        ) as proc:
+            os.set_blocking(proc.stdout.fileno(), False)
+            yield proc.stdout
+            proc.kill()
+
+    @pytest.fixture
+    def pulp_container_log(pulp_container_log_stream):
+        # Flush logs before starting the test
+        pulp_container_log_stream.read()
+        yield
+        logs = pulp_container_log_stream.read()
+        if logs is not None:
+            print(logs.decode())
+
+else:
+
+    @pytest.fixture
+    def pulp_container_log():
+        yield

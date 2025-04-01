b8b52f3c72fa7f9e5b8e5230a5deb32c679cc33b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-01 04:09:08 +0000

    Add package bpp-iplweb/202503.1170/bpp_iplweb-202503.1170-py3-none-any.whl

---
+++ packages/bpp-iplweb/bpp_iplweb-202503.1170-py3-none-any.whl/ewaluacja2021/const.py
@@ -0,0 +1,20 @@
+LATA_2019_2021 = 0
+LATA_2017_2018 = 1
+
+DEC2INT = 10000
+
+# Zakres lat dla wyszukiwania prac do raportów "ewaluacja 2021"
+ROK_MIN = 2017
+ROK_MAX = 2021
+
+
+IMPORT_MAKSYMALNYCH_SLOTOW_COLUMNS = [
+    "Stopień / Tytuł",
+    "Nazwisko",
+    "Imię",
+    "ORCID",
+    "Dyscyplina",
+    "Uwzględniony w ewaluacji",
+    "maksymalna suma udziałów jednostkowych - wszystkie dyscypliny",
+    "maksymalna suma udziałów jednostkowych - monografie",
+]

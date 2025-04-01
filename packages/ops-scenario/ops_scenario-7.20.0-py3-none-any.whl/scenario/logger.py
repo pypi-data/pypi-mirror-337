98a39b7a64db9bc70249a22d67f0aed960452b6a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-01 06:06:05 +0000

    Add package ops-scenario/7.20.0/ops_scenario-7.20.0-py3-none-any.whl

---
+++ packages/ops-scenario/ops_scenario-7.20.0-py3-none-any.whl/scenario/logger.py
@@ -0,0 +1,10 @@
+# Copyright 2023 Canonical Ltd.
+# See LICENSE file for licensing details.
+
+"""Test framework logger"""
+
+import logging
+import os
+
+logger = logging.getLogger("ops-scenario")
+logger.setLevel(os.getenv("OPS_SCENARIO_LOGGING", "WARNING"))

e57be833657848df9855da7d4a5996a776682b3f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-01 04:14:19 +0000

    Add package django-tasks-scheduler/4.0.0b2/django_tasks_scheduler-4.0.0b2-py3-none-any.whl

---
+++ packages/django-tasks-scheduler/django_tasks_scheduler-4.0.0b2-py3-none-any.whl/scheduler/tests/test_mgmt_commands/test_delete_failed_executions.py
@@ -0,0 +1,19 @@
+from django.core.management import call_command
+
+from scheduler.helpers.queues import get_queue
+from scheduler.tests import test_settings  # noqa
+from scheduler.tests.jobs import failing_job
+from scheduler.helpers.tools import create_worker
+from scheduler.tests.test_views.base import BaseTestCase
+
+
+class DeleteFailedExecutionsTest(BaseTestCase):
+    def test_delete_failed_executions__delete_jobs(self):
+        queue = get_queue("default")
+        call_command("delete_failed_executions", queue="default")
+        queue.create_and_enqueue_job(failing_job)
+        worker = create_worker("default", burst=True)
+        worker.work()
+        self.assertEqual(1, len(queue.failed_job_registry))
+        call_command("delete_failed_executions", queue="default")
+        self.assertEqual(0, len(queue.failed_job_registry))

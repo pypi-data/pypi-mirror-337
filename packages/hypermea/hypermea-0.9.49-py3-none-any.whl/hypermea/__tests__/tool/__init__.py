24bc0734263482a414eb9d04e6c0e2732a8b97ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-01 04:15:36 +0000

    Add package hypermea/0.9.49/hypermea-0.9.49-py3-none-any.whl

---
+++ packages/hypermea/hypermea-0.9.49-py3-none-any.whl/hypermea/__tests__/tool/__init__.py
@@ -0,0 +1,1 @@
+FEATURE = 'tool/developer_info.feature'
---
+++ packages/hypermea/hypermea-0.9.49-py3-none-any.whl/hypermea/tool/skel/api/configuration/__init__.py
@@ -0,0 +1,7 @@
+from hypermea.core.settings_manager import SettingsManager
+import configuration.hypermea_settings
+import configuration.api_settings
+
+
+SETTINGS = SettingsManager.instance()
+VERSION = '0.1.0'

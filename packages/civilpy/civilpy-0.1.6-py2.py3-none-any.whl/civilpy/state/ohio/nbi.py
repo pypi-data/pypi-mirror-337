5268388e73f27f1fa5d4a392732b3d7475474a7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-01 04:08:22 +0000

    Add package civilpy/0.1.6/civilpy-0.1.6-py2.py3-none-any.whl

---
+++ packages/civilpy/civilpy-0.1.6-py2.py3-none-any.whl/civilpy/state/ohio/nbi.py
@@ -0,0 +1,13 @@
+item_107 = {
+   "1":  "Concrete Cast-in-Place",
+   "2":  "Concrete Precast Panels",
+   "3":  "Open Grating",
+   "4":  "Closed Grating",
+   "5":  "Steel plate (includes orthotropic)",
+   "6":  "Corrugated Steel",
+   "7":  "Aluminum",
+   "8":  "Wood or Timber",
+   "9":  "Other",
+   "N":  "Not applicable",
+}
+

c49c59b68c113ba32f95c74606f45aeaf1e90c46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-01 06:05:54 +0000

    Add package sculblog/0.1.4/sculblog-0.1.4-py3-none-any.whl

---
+++ packages/sculblog/sculblog-0.1.4-py3-none-any.whl/sculblog/db.py
@@ -0,0 +1,84 @@
+import os
+import sqlite3 
+from contextlib import contextmanager
+
+
+@contextmanager
+def db_connection(db_path: str):
+    conn = sqlite3.connect(db_path)
+    conn.row_factory = sqlite3.Row
+    try:
+        yield conn
+    finally:
+        conn.close()
+
+def send_query(conn, query, params=None):
+    try:
+        with conn:
+            results = execute_query(conn, query, params)
+            if results:
+                for row in results:
+                    for column, value in row.items():
+                        print(f"{column}: \n\t{value} \n")
+                    print("\n-----\n")
+            else:
+                print("No results returned.")
+    except sqlite3.Error as e:
+        print(f"Database error: {e}")
+        return []
+    
+def execute_single_query(conn, query, params=None):
+    try:
+        cursor = conn.cursor()
+        if params:
+            cursor.execute(query, params)
+        else:
+            cursor.execute(query)
+        result = cursor.fetchone()
+        return dict(result) if result else None
+    except sqlite3.Error as e:
+        print(f"Database error: {e}")
+        return None
+
+def insert_row(conn, table, data):
+    columns = ', '.join(data.keys())
+    placeholders = ', '.join(['?'] * len(data))
+    query = f"INSERT INTO {table} ({columns}) VALUES ({placeholders})"
+    try:
+        with conn:
+            conn.execute(query, tuple(data.values()))
+        return True
+    except sqlite3.Error as e:
+        print(f"Insert error: {e}")
+        return False
+
+value_in_column = lambda conn, table, column, target: bool(execute_single_query(conn, f"SELECT 1 FROM {table} WHERE {column} = ?", (target,)))
+
+def update_row(conn, table, where_column, where_value, update_columns):
+    if not update_columns:
+        return
+    
+    query = f"UPDATE {table} SET {', '.join(f'{col} = ?' for col in update_columns)} WHERE {where_column} = ?"
+    
+    try:
+        conn.cursor().execute(query, (*update_columns.values(), where_value))
+        conn.commit()
+        #return cursor.rowcount
+    except sqlite3.Error as e:
+        print(f"Update error: {e}")
+
+def execute_query(conn, query, params=None):
+    try:
+        if conn.row_factory != sqlite3.Row:
+            conn.row_factory = sqlite3.Row
+        with conn:
+            cursor = conn.cursor()
+            if params:
+                cursor.execute(query, params)
+            else:
+                cursor.execute(query)
+            return [dict(row) for row in cursor.fetchall()]
+    except sqlite3.Error as e:
+        print(f"Database error: {e}")
+        return []
+
---
+++ packages/sculblog/sculblog-0.1.4-py3-none-any.whl/sculblog/main.py
@@ -0,0 +1,138 @@
+import sqlite3
+import os
+from bs4 import BeautifulSoup
+from datetime import datetime
+import markdown
+import re
+import html
+from contextlib import contextmanager
+import sys
+import traceback
+
+from sculblog.diego_flavored_markdown import DiegoFlavoredMarkdown, count_markdown_words
+from sculblog.db import db_connection, send_query, execute_single_query, insert_row, update_row, execute_query, value_in_column 
+
+#===============================
+# Configuration and Utilities
+#===============================
+
+db_path = os.path.abspath(os.path.join('/', 'srv', 'http', 'database', 'db.db'))
+
+def sanitize_filename(filename: str) -> str:
+    return re.sub(r'[^\w\-_\. ]', '_', filename)
+
+def remove_extension(filename):
+    return os.path.splitext(filename)[0]
+
+
+#===============================
+# HTML and Content Management
+#===============================
+
+
+def write_page_html(markdown_content: str):
+    try:
+        soup = BeautifulSoup("", 'html.parser')
+        md = markdown.Markdown(extensions=[DiegoFlavoredMarkdown(), 'footnotes'])
+        # If markdown_content is a file object, read it first
+        if hasattr(markdown_content, 'read'):
+            markdown_content = markdown_content.read()
+        html_content = md.convert(markdown_content)
+        soup.append(BeautifulSoup(html_content, 'html.parser'))
+        return str(soup)
+    except Exception as e:
+        print(f"Error in write_page_html: {str(e)}")
+        raise
+
+def write_preview_html(html_content: str):
+    try:
+        soup = BeautifulSoup(html_content, 'html.parser')
+        
+        for a_tag in soup.find_all('a'):
+            span_tag = soup.new_tag('span', **{'class': 'false-external-link'})
+            span_tag.string = a_tag.get_text()
+            a_tag.replace_with(span_tag)
+
+        for h_tag in soup.find_all(['h1', 'h2', 'h3', 'h4', 'h5', 'h6']):
+            span_tag = soup.new_tag('span', **{'class': 'preview-header'})
+            span_tag.string = h_tag.get_text()
+            h_tag.replace_with(span_tag)
+        
+        for tag in soup.find_all():
+            if tag.name not in ['span', 'sup', 'sub']:
+                tag.unwrap()
+        
+        clean_html = ''.join(str(tag) for tag in soup.contents)
+        return clean_html
+    
+    except Exception as e:
+        print(f"Error in write_preview_html: {str(e)}")
+        print("Traceback:")
+        traceback.print_exc()
+        raise
+
+#===============================
+# Main Functions
+#===============================
+
+def process(file_path):
+
+    try:
+        #timestamp = input("What timestamp? Hit enter to use iso-8601 format ") or datetime.now().isoformat()
+        #print(f"\t{timestamp}")
+
+        with db_connection(db_path) as conn:
+
+            with open(os.path.abspath(file_path), "r") as file:
+                markdown_content = file.read()
+
+            page_html = write_page_html(markdown_content)
+
+            post_data = {
+                "text": page_html,
+                "preview_html": write_preview_html(page_html)[:1500],
+                "word_count": count_markdown_words(markdown_content), 
+            }
+
+            file_name = os.path.splitext(file_path)[0]
+
+            if not value_in_column(conn, "blog", "file_name", file_name):
+
+                post_data["header"] = input("what is the header of the page? ")
+                post_data["file_name"] = file_name
+                
+                if insert_row(conn, "blog", post_data):
+                    print("Data inserted successfully.")
+                else:
+                    print("Failed to insert data.")
+            
+            else:
+                
+                update_row(conn, "blog", "file_name", file_name, post_data)
+
+    except Exception as e:
+        print(f"An error of Exception type {type(e).__name__} occurred: {e}")
+
+def hide(file_path):
+    file_name = remove_extension(file_path)
+    with db_connection(db_path) as conn:
+        update_row(conn, "blog", "file_name", file_name, {"hide": "0"})
+
+
+def main():
+    if len(sys.argv) < 3:
+        print("Usage: sculblog <command> <postname> (command: process | hide)")
+        sys.exit(1)
+
+    command, file_path = sys.argv[1], sys.argv[2]
+
+    if command == "process": 
+        process(file_path)
+    elif command == "hide": 
+        hide(file_path)
+    else: 
+        print('invalid command. command must be either "process" or "hide"')
+        sys.exit(1)
+
+if __name__ == "__main__":
+    main()

15e22b8a689c014324e48cf37b00dc54d975f488
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-01 15:06:25 +0000

    Add package types-whatthepatch/1.0.2.20250401/types_whatthepatch-1.0.2.20250401.tar.gz

---
+++ packages/types-whatthepatch/types_whatthepatch-1.0.2.20250401.tar.gz/types_whatthepatch-1.0.2.20250401/setup.py
@@ -0,0 +1,4 @@
+
+from setuptools import setup
+
+setup(package_data={'whatthepatch-stubs': ['__init__.pyi', 'apply.pyi', 'exceptions.pyi', 'patch.pyi', 'snippets.pyi', 'METADATA.toml', 'py.typed']})

3908938c50c6d92031f3c0335b71c2deb8d7b7c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-01 03:26:18 +0000

    Add package liblaf-grapes/0.1.15/liblaf_grapes-0.1.15-py3-none-any.whl

---
+++ packages/liblaf-grapes/liblaf_grapes-0.1.15-py3-none-any.whl/liblaf/grapes/timing/_function.py
@@ -0,0 +1,24 @@
+import functools
+from collections.abc import Callable
+
+import attrs
+
+from liblaf import grapes
+
+from ._base import TimerRecords
+
+
+@attrs.define
+class TimedFunction[**P, T](TimerRecords):
+    _func: Callable[P, T] = attrs.field(alias="func", on_setattr=attrs.setters.frozen)
+
+    def __attrs_post_init__(self) -> None:
+        self.label = self.label or grapes.pretty.func(self._func).plain or "Function"
+        functools.update_wrapper(self, self._func)
+
+    def __call__(self, *args: P.args, **kwargs: P.kwargs) -> T:
+        self._start()
+        result: T = self._func(*args, **kwargs)
+        self._end()
+        self.log_record(depth=2)
+        return result

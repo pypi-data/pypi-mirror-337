93d6620ade905bf4bd6c5c1d54f9ce480f976a10
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-01 07:05:39 +0000

    Add package brainyflow/0.1.1/brainyflow-0.1.1.tar.gz

---
+++ packages/brainyflow/brainyflow-0.1.1.tar.gz/brainyflow-0.1.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='brainyflow',
+    version='0.1.1',
+    py_modules=['brainyflow'],
+    author="Victor Duarte",
+    description="Minimalist AI framework in 65 Lines. Enable LLMs to Program Themselves.",
+    url="https://github.com/zvictor/BrainyFlow",
+)

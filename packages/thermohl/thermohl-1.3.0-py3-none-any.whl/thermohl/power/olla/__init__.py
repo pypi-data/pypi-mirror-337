02d2958fb4f027b9934b9f084c780443b99e0bef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-01 04:08:40 +0000

    Add package thermohl/1.3.0/thermohl-1.3.0-py3-none-any.whl

---
+++ packages/thermohl/thermohl-1.3.0-py3-none-any.whl/thermohl/power/olla/__init__.py
@@ -0,0 +1,17 @@
+# SPDX-FileCopyrightText: 2025 RTE (https://www.rte-france.com)
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+# SPDX-License-Identifier: MPL-2.0
+
+"""Power terms implementation using RTE's olla project choices.
+
+Based on NT-RD-CNER-DL-SLA-20-00215 by RTE.
+"""
+
+from .air import Air
+from .solar_heating import SolarHeating
+from .convective_cooling import ConvectiveCooling
+from .joule_heating import JouleHeating
+from .radiative_cooling import RadiativeCooling

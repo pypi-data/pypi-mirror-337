69b7fe0d4374a8feda0010952381cccaaa25d725
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-01 03:26:17 +0000

    Add package fabric-python-helper/0.3.0/fabric_python_helper-0.3.0-py3-none-any.whl

---
+++ packages/fabric-python-helper/fabric_python_helper-0.3.0-py3-none-any.whl/fabric_python_helper/__init__.py
@@ -0,0 +1,10 @@
+from .graph_api import Emails
+from .pbi_admin import AccessTokens
+from .pbi_admin import Dataflows
+from .pbi_admin import SemanticModels
+
+__version__="0.2.8"
+__author__="Ben Dobbs"
+__author_email__="bdobbs@archwaytrust.co.uk"
+__description__="Collection of useful python code for automation in Microsoft Fabric."
+__url__="https://github.com/ArchwayTrust/Fabric-PythonHelper"

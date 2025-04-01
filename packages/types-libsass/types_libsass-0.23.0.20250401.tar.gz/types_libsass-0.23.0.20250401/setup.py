995f8f498e2ca2c94d9cbe3a652f9619c338a75a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-01 15:06:32 +0000

    Add package types-libsass/0.23.0.20250401/types_libsass-0.23.0.20250401.tar.gz

---
+++ packages/types-libsass/types_libsass-0.23.0.20250401.tar.gz/types_libsass-0.23.0.20250401/setup.py
@@ -0,0 +1,4 @@
+
+from setuptools import setup
+
+setup(package_data={'sass-stubs': ['__init__.pyi', 'METADATA.toml', 'py.typed'], 'sassutils-stubs': ['__init__.pyi', 'builder.pyi', 'distutils.pyi', 'wsgi.pyi', 'METADATA.toml', 'py.typed']})
